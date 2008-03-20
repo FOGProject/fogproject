41cc256fd0af74eae06aa1a884b1f70ec6911414
microleaks <microleaks@71f96598-fa45-0410-b640-bcd6f8691b32>
2008-03-20 15:09:34 +0000

    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@11 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ utils/Boot Image Editor/FOGMountBootImage.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo ;
+echo "This script is only tested on Fedora";
+echo ;
+
+mkdir /tmp/tmpMnt >/dev/null 2>&1;
+
+echo -n "Coping boot image...";
+cp /tftpboot/fog/images/init.gz /tmp/init.gz >/dev/null 2>&1;
+echo  "Done";
+
+echo -n "Unzipping image...";
+cd /tmp
+gunzip init.gz >/dev/null 2>&1;
+echo "Done";
+
+echo -n "Mounting boot image...";
+mount -o loop /tmp/init /tmp/tmpMnt; 
+echo "Done";
+
+echo "Launching nautilus...";
+
+nautilus --no-desktop /tmp/tmpMnt &
+
+sleep 3;
+
+echo "Nautilus should be up soon...";
+echo ;
+echo "Press enter when you are done modifing the boot image to replace it with the original file from the tftp directory.";
+echo ;
+echo "Press Enter when you are ready.";
+echo ;
+read whatever;
+echo -n "Unmounting image...";
+umount /tmp/tmpMnt;
+echo "Done";
+
+echo -n "GZipping image...";
+gzip -9 init;
+echo "Done";
+
+echo -n "Coping file...";
+cp -f init.gz /tftpboot/fog/images/init.gz;
+echo "Done";
+
---
+++ utils/Vista Image Upload/VistaUpload.bat
@@ -0,0 +1,3 @@
+bcdedit /set {bootmgr} device boot
+bcdedit /set {default} device boot
+bcdedit /set {default} osdevice boot 
