07aab44018c01120e8bb30f909dc05f29cfc3bae
Tom Elliott <tommygunsster@gmail.com>
2014-04-21 01:26:49 +0000

    Boot will send tasking to any mac address found so long as it's registered to the host.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1471 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/ipxe/boot.php
+++ packages/web/service/ipxe/boot.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: text/plain");
 require_once('../../commons/base.inc.php');
-$MACAddress = new MACAddress($_REQUEST['mac']);
-$Host = $FOGCore->getClass('HostManager')->getHostByMacAddress($MACAddress);
+$MACs = HostManager::parseMacList($_REQUEST['mac']);
+$Host = $FOGCore->getClass('HostManager')->getHostByMacAddresses($MACs);
 new BootMenu($Host);
