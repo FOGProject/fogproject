8915888d7de71fce4926e06942c8a9bd80c95499
Tom Elliott <tommygunsster@gmail.com>
2014-04-21 10:06:13 +0000

    Fix the hardware display (link from dashboard) so the information display's properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1472 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/hw.php
+++ packages/web/status/hw.php
@@ -1,17 +1,16 @@
 <?php
-echo "@@start\n";
 echo "@@general\n";
-echo  trim(shell_exec("uname -r") . substr("\n", 0, -2)) . "\n";
-echo  trim(shell_exec("hostname")) . "\n";
+echo trim(shell_exec("uname -r") . substr("\n", 0, -2)) . "\n";
+echo trim(shell_exec("hostname")) . "\n";
 echo trim(shell_exec("uptime") ) . "\n";
 echo trim(shell_exec("cat /proc/cpuinfo | head -n2 | tail -n1 | cut -f2 -d: | sed 's| ||'")) . "\n";
-echo trim(shell_exec("grep '^processor' /proc/cpuinfo | tail -n 1 | awk '{print\$3+1}'") ) . "\n";
+echo trim(shell_exec("grep '^processor' /proc/cpuinfo | tail -n 1 | awk '{print \$3+1}'") ) . "\n";
 echo trim( shell_exec("cat /proc/cpuinfo | head -n5 | tail -n1 | cut -f2 -d: | sed 's| ||'") ) . "\n";
-echo trim( shell_exec("cat /proc/cpuinfo | head -n7 | tail -n1 | cut -f2 -d: | sed 's| ||'") ) . "\n";
 echo trim( shell_exec("cat /proc/cpuinfo | head -n8 | tail -n1 | cut -f2 -d: | sed 's| ||'") ) . "\n";
-echo trim( shell_exec("free -m | head -n2 | tail -n1 | awk '{ print $2 }'") ) . "\n"; // total mem
-echo trim( shell_exec("free -m | head -n3 | tail -n1 | awk '{ print $3 }'") ) . "\n";	// mem used
-echo trim( shell_exec("free -m | head -n3 | tail -n1 | awk '{ print $4 }'") ) . "\n";	// mem free
+echo trim( shell_exec("cat /proc/cpuinfo | head -n9 | tail -n1 | cut -f2 -d: | sed 's| ||'") ) . "\n";
+echo trim( shell_exec("free -m | head -n2 | tail -n1 | awk '{ print \$2 }'") ) . "\n"; // total mem
+echo trim( shell_exec("free -m | head -n3 | tail -n1 | awk '{ print \$3 }'") ) . "\n";	// mem used
+echo trim( shell_exec("free -m | head -n3 | tail -n1 | awk '{ print \$4 }'") ) . "\n";	// mem free
 echo "@@fs\n";
 $t = shell_exec("df | grep -vE \"^Filesystem|shm\"");
 $l = explode("\n", $t);
