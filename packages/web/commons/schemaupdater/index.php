67053a684da45b6ae4ac0097f725512756470526
Tom Elliott <tommygunsster@gmail.com>
2014-04-23 11:22:39 +0000

    Add FOG Client/Prep link to the footer page so it's always accessible.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1478 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/schemaupdater/index.php
+++ packages/web/commons/schemaupdater/index.php
@@ -1347,6 +1347,6 @@ print "\n\t\t\t</div>";
 print "\n\t\t</div>";
 print "\n\t</div>";
 print "\n\t<!-- Footer: Be nice, give us some credit -->";
-print "\n\t".'<div id="footer">FOG Project: Chuck Syperski, Jian Zhang, Peter Gilchrist &amp; Tom Elliott</div>';
+print "\n\t".'<div id="footer">FOG Project: Chuck Syperski, Jian Zhang, Peter Gilchrist &amp; Tom Elliott FOG Client/Prep link: <a href="'.$_SERVER['PHP_SELF'].rtrim($GLOBALS['FOGCore']->getSetting('FOG_WEB_ROOT'),'/').'/client">FOG Client/FOG Prep</a></div>';
 print "\n</body>";
 print "\n</html>";
