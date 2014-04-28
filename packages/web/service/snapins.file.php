3ded337f159f4ecbb8315ade10b6cfe38ad787db
Tom Elliott <tommygunsster@gmail.com>
2014-04-28 20:44:15 +0000

    Add stateID to SnapinJobs for filtering reasons.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1526 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/snapins.file.php
+++ packages/web/service/snapins.file.php
@@ -13,44 +13,41 @@ try
 	// Try and get the task.
 	$Task = current($Host->get('task'));
 	// Get the Job if possible
-	$SnapinJobs = $FOGCore->getClass('SnapinJobManager')->find(array('hostID' => $Host->get('id')));
-	foreach($SnapinJobs AS $SnapinJob)
+	$SnapinJob = current($FOGCore->getClass('SnapinJobManager')->find(array('hostID' => $Host->get('id'),'stateID' => 1)));
+	// Get the current task, if possible.
+	$SnapinTask = current($FOGCore->getClass('SnapinTaskManager')->find(array('state' => array(0,1),'jobID' => $SnapinJob->get('id'))));
+	// If there's no more tasks, or job, don't continue on.  Just report there's no jobs.
+	if (!$SnapinJob || !$SnapinTask)
+		throw new Exception('#!ns');
+	// if there's a task and it's not a deploy task (12 or 13) don't send the file (report back)
+	if ($Task && $Task->isValid())
 	{
-		// Get the current task, if possible.
-		$SnapinTask = current($FOGCore->getClass('SnapinTaskManager')->find(array('state' => array(0,1),'jobID' => $SnapinJob->get('id'))));
-		// If there's no more tasks, or job, don't continue on.  Just report there's no jobs.
-		if (!$SnapinJob || !$SnapinTask)
-			throw new Exception('#!ns');
-		// if there's a task and it's not a deploy task (12 or 13) don't send the file (report back)
+		if ($Task->get('typeID') != 12 && $Task->get('typeID') != 13)
+			throw new Exception('#!it');
+	}
+	//Get the snapin to work off of.
+	$Snapin = new Snapin($SnapinTask->get('snapinID'));
+	// Assign the file for sending.
+	$SnapinFile = rtrim($FOGCore->getSetting('FOG_SNAPINDIR'),'/').'/'.$Snapin->get('file');
+	// If it exists and is readable send it!
+	if (file_exists($SnapinFile) && is_readable($SnapinFile))
+	{
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Content-Description: File Transfer");
+		header("Content-Type: application/octet-stream");
+		header("Content-Length: ".filesize($SnapinFile));
+		header("Content-Disposition: attachment; filename=".basename($Snapin->get('file')));
+		@readfile($SnapinFile);
+		// if the Task is deployed then update the task.
 		if ($Task && $Task->isValid())
-		{
-			if ($Task->get('typeID') != 12 && $Task->get('typeID') != 13)
-				throw new Exception('#!it');
-		}
-		//Get the snapin to work off of.
-		$Snapin = new Snapin($SnapinTask->get('snapinID'));
-		// Assign the file for sending.
-		$SnapinFile = rtrim($FOGCore->getSetting('FOG_SNAPINDIR'),'/').'/'.$Snapin->get('file');
-		// If it exists and is readable send it!
-		if (file_exists($SnapinFile) && is_readable($SnapinFile))
-		{
-			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-			header("Content-Description: File Transfer");
-			header("Content-Type: application/octet-stream");
-			header("Content-Length: ".filesize($SnapinFile));
-			header("Content-Disposition: attachment; filename=".basename($Snapin->get('file')));
-			@readfile($SnapinFile);
-			// if the Task is deployed then update the task.
-			if ($Task && $Task->isValid())
-				$Task->set('stateID',3)->save();
-			// Update the snapin task information.
-			$SnapinTask->set('state',1)
-					   ->set('return',-1)
-					   ->set('details','Pending...');
-			// Save and return!
-			if ($SnapinTask->save())
-				print "#!ok";
-		}
+			$Task->set('stateID',3)->save();
+		// Update the snapin task information.
+		$SnapinTask->set('state',1)
+				   ->set('return',-1)
+				   ->set('details','Pending...');
+		// Save and return!
+		if ($SnapinTask->save())
+			print "#!ok";
 	}
 }
 catch (Exception $e)
