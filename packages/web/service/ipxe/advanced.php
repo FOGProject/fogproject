12770449bad6c0727b54f5dd481deb5feafeb626
Tom Elliott <tommygunsster@gmail.com>
2014-04-29 22:56:30 +0000

    Add some more default information for the Advanced.php file.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1535 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/ipxe/advanced.php
+++ packages/web/service/ipxe/advanced.php
@@ -2,5 +2,10 @@
 header("Content-type: text/plain");
 require_once('../../commons/base.inc.php');
 print "#!ipxe\n";
+print "console\n";
+print "cpair --foreground 7 --background 2 2\n";
+print "console --picture http://10.0.7.1/fog/service/ipxe/bg.png --left 100 --right 80\n";
+print "set fog-ip ".$FOGCore->getSetting('FOG_WEB_HOST')."\n";
+print "set fog-webroot ".basename($FOGCore->getSetting('FOG_WEB_ROOT'))."\n";
 print $FOGCore->getSetting('FOG_PXE_ADVANCED');
 ?>
