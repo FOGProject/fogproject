614dba1de5554b05ee5e73ad2396dc8611f3eb82
microleaks <microleaks@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-05-08 19:38:12 +0000

    * checkIfExist only checks images that have an osID of 5 or 6 (win7 or win8) because windows xp images (that are single partition) are not stored in a directory so ->chdir always fails and prohibits the ability to image XP and Vista
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1637 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/Host.class.php
+++ packages/web/lib/fog/Host.class.php
@@ -591,7 +591,7 @@ class Host extends FOGController
 			$StorageGroup = $Image->getStorageGroup();
 			$StorageNode = ($isUpload ? $StorageGroup->getOptimalStorageNode() : $this->getOptimalStorageNode());
 		}
-		if (in_array($TaskType->get('id'),array('1','8','15','17')))
+		if (in_array($TaskType->get('id'),array('1','8','15','17')) && in_array($Image->get('osID'), array('5', '6') ) )
 		{
 			// FTP
 			$ftp = $this->FOGFTP;
