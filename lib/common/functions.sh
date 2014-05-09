9d2ae9ed81fdbc4ce7acdb3b13c6c46be9764673
microleaks <microleaks@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-05-09 20:16:55 +0000

    * Another stab at fixing the installer for storage nodes -> setting the db user information.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1648 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -274,9 +274,7 @@ configureUsers()
 		# save everyone wrist injuries
 		storageftpuser=${username};
 		storageftppass=${password};
-		
-        dbuser=${username};
-        dbpass=${password};
+
 	fi
 	
 	if [ $password != "" ]
