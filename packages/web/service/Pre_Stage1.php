820e67fa18e3d619717b1355638cea13c978cfb1
microleaks <microleaks@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-05-09 18:51:13 +0000

    * Updated Pre_Stage1 to use beter method of determining the best storage node taking into account max client not just number of active clients.  This should also fix the bug encountered when force is used.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1645 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/Pre_Stage1.php
+++ packages/web/service/Pre_Stage1.php
@@ -32,9 +32,6 @@ try
 	{
 		if (!$Task->set('stateID', '3' )->save())
 			throw new Exception(_('Forced Task: Failed to update Task'));
-		// Forced - Success!
-		else
-			print '##@GO';
 	}
 	// Queue checks
 	$totalSlots = $StorageGroup->getTotalSupportedClients();
@@ -42,30 +39,33 @@ try
 	$inFrontOfMe = $Task->getInFrontOfHostCount();
 	$groupOpenSlots = $totalSlots - $usedSlots;
 	// Fail if all Slots are used
-	if ($usedSlots >= $totalSlots)
-		throw new Exception(sprintf('%s, %s %s', _('Waiting for a slot'), $inFrontOfMe, _('PCs are in front of me.')));
-	// At this point we know there are open slots, but are we next in line for that slot (or has the next is line timed out?)
-	if ($groupOpenSlots <= $inFrontOfMe)
-		throw new Exception(sprintf('%s %s %s', _('There are open slots, but I am waiting for'), $inFrontOfMe, _('PCs in front of me.')));
+	if (! $Task->get('isForced')) {
+	    if ($usedSlots >= $totalSlots)
+		    throw new Exception(sprintf('%s, %s %s', _('Waiting for a slot'), $inFrontOfMe, _('PCs are in front of me.')));
+	    // At this point we know there are open slots, but are we next in line for that slot (or has the next is line timed out?)
+	    if ($groupOpenSlots <= $inFrontOfMe)
+		    throw new Exception(sprintf('%s %s %s', _('There are open slots, but I am waiting for'), $inFrontOfMe, _('PCs in front of me.')));
+    }
 	// Determine the best Storage Node to use - based off amount of clients connected
+	
 	$messageArray = array();
-	$clientsOnBestStorageNode = 9999;
-	foreach ($StorageNodes as $StorageNode)
-	{
-		$nodeUsedSlots = $StorageNode->getUsedSlotCount();
-		if ($nodeUsedSlots < $StorageNode->get('maxClients') && $nodeUsedSlots < $clientsOnBestStorageNode)
-		{	
-			if ($StorageNode->getNodeFailure($Host) === null)
-			{
-				$bestStorageNode = $StorageNode;
-				$clientsOnBestStorageNode = $nodeUsedSlots;
-			}
-			else
-				$messageArray[] = sprintf("%s '%s' (%s) %s", _('Storage Node'), $StorageNode->get('name'), $StorageNode->get('ip'), _('is open, but has recently failed for this Host'));
-		}
+	$winner = null;
+	foreach( $StorageNodes AS $StorageNode ) {
+	    if ( $StorageNode->get('maxClients') > 0 ) {
+	        if ( $winner == null ) {
+	            $winner = $StorageNode;
+	        } else if ( $StorageNode->getClientLoad() < $winner->getClientLoad() ) {
+	            if ($StorageNode->getNodeFailure($Host) === null)
+			    {
+                    $winner = $StorageNode;
+			    }
+			    else
+				    $messageArray[] = sprintf("%s '%s' (%s) %s", _('Storage Node'), $StorageNode->get('name'), $StorageNode->get('ip'), _('is open, but has recently failed for this Host'));
+	        }
+	    }
 	}
 	// Failed to find a Storage Node - this should only occur if all Storage Nodes in this Storage Group have failed
-	if (!isset($bestStorageNode) || !$bestStorageNode->isValid())
+	if (!isset($winner) || !$winner->isValid())
 	{
 		// Print failed node messages if we are unable to find a valid node
 		if (count($messageArray))
@@ -74,7 +74,7 @@ try
 	}
 	// All tests passed! Almost there!
 	$Task->set('stateID', '3')
-		 ->set('NFSMemberID', $bestStorageNode->get('id'));
+		 ->set('NFSMemberID', $winner->get('id'));
 	// Update Task State ID -> Update Storage Node ID -> Save
 	if (!$Task->save())
 		throw new Exception(_('Failed to update Task'));
