af29f39d887e53fd43f1f44317402396a49556ea
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-05-18 17:08:49 +0000

    Move init into class object.  privatize the autoloading functions.  should make for less spaghetti.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1705 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/base.inc.php
+++ packages/web/commons/base.inc.php
@@ -2,7 +2,7 @@
 	/* This file just stores the heading information for including *
 	 * In the FOG System.  This should minimize code lines.        *
 	 *                                                             */
-	require_once('system.php');
-	require_once(BASEPATH . '/commons/config.php');
-	require_once(BASEPATH . '/commons/init.php');
-	require_once(BASEPATH . '/commons/text.php');
+	require('system.php');
+	require(BASEPATH.'/commons/config.php');
+	require(BASEPATH.'/commons/init.php');
+	require(BASEPATH.'/commons/text.php');
