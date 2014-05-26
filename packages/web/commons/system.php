d86f9052545b1387ba5687ce61cd193d85e2aa5c
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-05-26 12:55:51 +0000

    Make config.php into class file.  Make system.php into class file.  Both are now called from init.php rather than inherited from base.inc.php.  Add's more Documentation.  Edits config.php from redhat and ubuntu locations so things still operate properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1740 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -1,52 +1,75 @@
 <?php
-/****************************************************
- * FOG Dashboard JS
- *	Author:		$Author$
- *	Created:	5:44 PM 4/12/2011
- *	Revision:	$Revision$
- *	Last Update:	$LastChangedDate$
- ***/
-// These variables are for the FOG system and do NOT need to be modified by the end user
-if (!ini_get('date.timezone'))
-	date_default_timezone_set(date_default_timezone_get());
-define('IS_INCLUDED', true);
-define('FOG_VERSION', '1.1.0');
-define('FOG_SCHEMA', 102);
-define('FOG_SVN_REVISION', '$Revision$');
-define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
-define('PHP_VERSION_REQUIRED', '5.3.0');
-define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
-define('BASEPATH', DetermineBasePath());
-define('SPACE_DEFAULT_STORAGE', '/images');
-// PHP: Version check
-if (PHP_COMPATIBLE === false)
+/**
+* \class System
+* Setups of the system.
+*/
+class System
 {
-	die(sprintf(_('Your systems PHP version is not sufficient. You have version %s, version %s is required.'), PHP_VERSION, PHP_VERSION_REQUIRED));
-	exit;
-}
-function DetermineBasePath()
-{
-	// Find the name of the first directory in the files path
-	if($_SERVER['DOCUMENT_ROOT'] == null)
+	/**
+	* __construct()
+	* method called default_values()
+	* @return void
+	*/
+	public function __construct()
 	{
-		if(file_exists('/var/www/html/fog'))
-			$_SERVER['DOCUMENT_ROOT'] = '/var/www/html/fog';
-		if(file_exists('/var/www/fog'))
-			$_SERVER['DOCUMENT_ROOT'] = '/var/www/fog';
-		define('WEB_ROOT','/'.basename($_SERVER['DOCUMENT_ROOT']).'/');
-		return $_SERVER['DOCUMENT_ROOT'];
+		self::default_values();
 	}
-	if($_SERVER['DOCUMENT_ROOT'] != null)
+	/**
+	* default_values()
+	* initializes php and version information.
+	* exits if php is not compatible.
+	* @return void
+	*/
+	private static function default_values()
 	{
-		if(preg_match('#/fog/#i',$_SERVER['PHP_SELF']))
+		if (!ini_get('date.timezone'))
+			date_default_timezone_set(date_default_timezone_get());
+		define('IS_INCLUDED', true);
+		define('FOG_VERSION', '1.1.0');
+		define('FOG_SCHEMA', 102);
+		define('FOG_SVN_REVISION', '$Revision$');
+		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
+		define('PHP_VERSION_REQUIRED', '5.3.0');
+		define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
+		define('BASEPATH', self::DetermineBasePath());
+		define('SPACE_DEFAULT_STORAGE', '/images');
+		// PHP: Version check
+		if (PHP_COMPATIBLE === false)
 		{
-			define('WEB_ROOT', '/fog/');
-			return $_SERVER['DOCUMENT_ROOT'].WEB_ROOT;
+			die(sprintf(_('Your systems PHP version is not sufficient. You have version %s, version %s is required.'), PHP_VERSION, PHP_VERSION_REQUIRED));
+			exit;
 		}
-		else
+	}
+	/**
+	* DetermineBasePath()
+	* Determines the base path,
+	* sets the WEB_ROOT variable.
+	* @return void
+	*/
+	private static function DetermineBasePath()
+	{
+		// Find the name of the first directory in the files path
+		if($_SERVER['DOCUMENT_ROOT'] == null)
 		{
-			define('WEB_ROOT','/');
+			if(file_exists('/var/www/html/fog'))
+				$_SERVER['DOCUMENT_ROOT'] = '/var/www/html/fog';
+			if(file_exists('/var/www/fog'))
+				$_SERVER['DOCUMENT_ROOT'] = '/var/www/fog';
+			define('WEB_ROOT','/'.basename($_SERVER['DOCUMENT_ROOT']).'/');
 			return $_SERVER['DOCUMENT_ROOT'];
+		}
+		if($_SERVER['DOCUMENT_ROOT'] != null)
+		{
+			if(preg_match('#/fog/#i',$_SERVER['PHP_SELF']))
+			{
+				define('WEB_ROOT', '/fog/');
+				return $_SERVER['DOCUMENT_ROOT'].WEB_ROOT;
+			}
+			else
+			{
+				define('WEB_ROOT','/');
+				return $_SERVER['DOCUMENT_ROOT'];
+			}
 		}
 	}
 }
