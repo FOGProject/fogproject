c3190c1efaf2373b5d05ec045cb6a70cee988af8
Tom Elliott <tommygunsster@gmail.com>
2014-05-27 17:46:38 +0000

    Hopefully fix capone to compare the key sent to the key stored to send the image data.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1747 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/capone.php
+++ packages/web/service/capone.php
@@ -10,31 +10,39 @@ try
 	{
 		$key = trim(base64_decode(trim($_REQUEST['key'])));
 		// Find the key association
-		$Capones = $FOGCore->getClass('CaponeManager')->find(array('key' => $key));
+		$Capones = $FOGCore->getClass('CaponeManager')->find();
 		if (count($Capones) > 0)
 		{
 			foreach($Capones AS $Capone)
 			{
-				$Image = new Image($Capone->get('imageID'));
-				$OS = new OS($Capone->get('osID'));
-				$StorageGroup = new StorageGroup($Image->get('storageGroupID'));
-				$StorageNode = $StorageGroup->getMasterStorageNode();
-				switch($Image->get('imageTypeID'))
+				if (strstr($key,$Capone->get('key')))
 				{
-					case 1:
-						$imgType = 'n';
-						break;
-					case 2:
-						$imgType = 'mps';
-						break;
-					case 3:
-						$imgType = 'mpa';
-						break;
-					case 4:
-						$imgType = 'dd';
-						break;
+					$Image = new Image($Capone->get('imageID'));
+					$OS = new OS($Capone->get('osID'));
+					$StorageGroup = new StorageGroup($Image->get('storageGroupID'));
+					$StorageNode = $StorageGroup->getMasterStorageNode();
+					switch($Image->get('imageTypeID'))
+					{
+						case 1:
+							$imgType = 'n';
+							break;
+						case 2:
+							$imgType = 'mps';
+							break;
+						case 3:
+							$imgType = 'mpa';
+							break;
+						case 4:
+							$imgType = 'dd';
+							break;
+					}
+					print base64_encode($Image->get('path').'|'.$OS->get('id').'|'.$imgType)."\n";
 				}
-				print base64_encode($Image->get('path').'|'.$OS->get('id').'|'.$imgType)."\n";
+				else
+				{
+					throw new Exception(base64_encode('null'));
+					break;
+				}
 			}
 		}
 		else
