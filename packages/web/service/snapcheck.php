48183258a2c19e4a3332bdb2e1371b27b63340cf
Tom Elliott <tommygunsster@gmail.com>
2014-06-08 23:52:42 +0000

    add snapin names sent from snapcheck call.  Update init's and schema for Windows 8.1 as it's relatively different than Windows 8.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1797 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/snapcheck.php
+++ packages/web/service/snapcheck.php
@@ -14,10 +14,27 @@ try
 	// Get the Jobs if possible
 	$SnapinJobs = $FOGCore->getClass('SnapinJobManager')->find(array('hostID' => $Host->get('id')));
 	// Cycle through all the host jobs that have awaiting snapin tasks.
-	foreach($SnapinJobs AS $SnapinJob)
-		// Get the tasks of the job so long as they're active.
-		$SnapinTasks += $FOGCore->getClass('SnapinTaskManager')->count(array('stateID' => array(-1,0,1), 'jobID' => $SnapinJob->get('id')));
-	print ($SnapinTasks ? 1 : 0);
+	if ($_REQUEST['getSnapnames'])
+	{
+		foreach($SnapinJobs AS $SnapinJob)
+		{
+			$SnapinTasks = $FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1),'jobID' => $SnapinJob->get('id')));
+			foreach($SnapinTasks AS $SnapinTask)
+			{
+				$Snapin = new Snapin($SnapinTask->get('snapinID'));
+				$SnapinNames[] = $Snapin->get('name');
+			}
+		}
+		$Snapins = implode(' ',(array)$SnapinNames);
+	}
+	else
+	{
+		foreach($SnapinJobs AS $SnapinJob)
+			// Get the tasks of the job so long as they're active.
+			$SnapinTasks += $FOGCore->getClass('SnapinTaskManager')->count(array('stateID' => array(-1,0,1), 'jobID' => $SnapinJob->get('id')));
+		$Snapins = ($SnapinTasks ? 1 : 0);
+	}
+	print $Snapins;
 }
 catch (Exception $e)
 {
