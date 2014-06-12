033277374b6125127701680840218beb7b2ffa27
Tom Elliott <tommygunsster@gmail.com>
2014-06-12 02:38:43 +0000

    Major reworking so group multicast can operate properly. Update Changelog slightly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1808 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/Post_Stage3.php
+++ packages/web/service/Post_Stage3.php
@@ -13,43 +13,77 @@ try
 	// Task for Host
 	$Task = current($Host->get('task'));
 	if (!$Task->isValid())
-		print '##';
+		throw new Exception(sprintf('%s: %s (%s)',_('No Active Task found for Host'), $Host->get('name'),$MACAddress));
 	// If it's a multicast job, destroy the association and remove the client.
 	if ($Task->get('typeID') == 8)
 	{
-		// Get this systems Multicast Association.
-		$MSA = current($FOGCore->getClass('MulticastSessionsAssociationManager')->find(array('taskID' => $Task->get('id'))));
 		// Get the session itself.
-		$MS = new MulticastSessions($MSA->get('msID'));
-		// Remove the assoc
-		$MSA->destroy();
+		$MS = $FOGCore->getClass('MulticastSessionsManager')->find(array('stateID' => array(0,1,2,3)));
+		// Find the associated tasks based on JOB and TASK ID's
+		foreach ($MS AS $MultiSession)
+		{
+			$MSA = current($FOGCore->getClass('MulticastSessionsAssociationManager')->find(array('msID' => $MultiSession->get('id'),'taskID' => $Task->get('id'))));
+			if ($MSA && $MSA->isValid())
+				break;
+		}
 		// Decrement the client.
-		$MS->set('clients',$MS->get('clients')-1);
+		$MultiSession->set('clients',$MultiSession->get('clients')-1)->save();
 		// If it's zero (or less)
-		if($MS->get('clients') <= 0)
-			// Remove the session so ports can be reused.
-			$MS->destroy();
+		if($MultiSession->get('clients') <= 0)
+		{
+			// Delete all associations.
+			$FOGCore->getClass('MulticastSessionsAssociationManager')->destroy(array('msID' => $MultiSession->get('id')));
+			$ImagingLogs = $FOGCore->getClass('ImagingLogManager')->find(array('hostID' => $Host->get('id')));
+			// Log it
+			foreach($ImagingLogs AS $ImagingLog)
+				$id[] = $ImagingLog->get('id');
+			// Update Last deploy
+			$Host->set('deployed',date('Y-m-d H:i:s'))->save();
+			$il = new ImagingLog(max($id));
+			$il->set('finish',date('Y-m-d H:i:s'))->save();
+			// Task Logging.
+			$TaskLog = new TaskLog($Task);
+			$TaskLog->set('taskID',$Task->get('id'))
+					->set('taskStateID',$Task->get('stateID'))
+					->set('createdTime',$Task->get('createdTime'))
+					->set('createdBy',$Task->get('createdBy'))
+					->save();
+			if (!$Task->save())
+				throw new Exception('Failed to update task.');
+			foreach($MS AS $MulticastTask)
+			{
+				$MSAs = $FOGCore->getClass('MulticastSessionsAssociationManager')->find(array('msID' => $MulticastTask->get('id')));
+				foreach($MSAs AS $MSA)
+					$FOGCore->getClass('Task',$MSA->get('taskID'))->set('stateID',4)->save();
+			}
+			$MultiSession->set('stateID',4)->save();
+		}
+		print '##';
 	}
-	// Set the task as complete.
-	if ($Task->get('stateID') < 4)
-		$Task->set('stateID',4);
-	$ImagingLogs = $FOGCore->getClass('ImagingLogManager')->find(array('hostID' => $Host->get('id')));
-	// Log it
-	foreach($ImagingLogs AS $ImagingLog)
-		$id[] = $ImagingLog->get('id');
-	// Update Last deploy
-	$Host->set('deployed',date('Y-m-d H:i:s'))->save();
-	$il = new ImagingLog(max($id));
-	$il->set('finish',date('Y-m-d H:i:s'))->save();
-	// Task Logging.
-	$TaskLog = new TaskLog($Task);
-	$TaskLog->set('taskID',$Task->get('id'))
-			->set('taskStateID',$Task->get('stateID'))
-			->set('createdTime',$Task->get('createdTime'))
-			->set('createdBy',$Task->get('createdBy'))
-			->save();
-	if (!$Task->save())
-		throw new Exception('Failed to update task.');
+	else
+	{
+		// Set the task as complete.
+		if ($Task->get('stateID') < 4)
+			$Task->set('stateID',4);
+		$ImagingLogs = $FOGCore->getClass('ImagingLogManager')->find(array('hostID' => $Host->get('id')));
+		// Log it
+		foreach($ImagingLogs AS $ImagingLog)
+			$id[] = $ImagingLog->get('id');
+		// Update Last deploy
+		$Host->set('deployed',date('Y-m-d H:i:s'))->save();
+		$il = new ImagingLog(max($id));
+		$il->set('finish',date('Y-m-d H:i:s'))->save();
+		// Task Logging.
+		$TaskLog = new TaskLog($Task);
+		$TaskLog->set('taskID',$Task->get('id'))
+				->set('taskStateID',$Task->get('stateID'))
+				->set('createdTime',$Task->get('createdTime'))
+				->set('createdBy',$Task->get('createdBy'))
+				->save();
+		if (!$Task->save())
+			throw new Exception('Failed to update task.');
+		print '##';
+	}
 }
 catch (Exception $e)
 {
