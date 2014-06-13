a0edda0b4bc8d2060468b223197a2ae34d0021a5
Tom Elliott <tommygunsster@gmail.com>
2014-06-13 14:13:43 +0000

    Relabel as 1.1.1 versioning.  Update init's and buildroot function for this.  Installer has this now as well.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1819 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/config.sh
+++ lib/common/config.sh
@@ -56,7 +56,7 @@ fogutilsdir="${fogprogramdir}/utils";
 fogutilsdirsrc="../packages/utils";
 
 # what version are we working with?
-version="1.1.0";
+version="1.1.1";
 
 # what is the schema version
 schemaversion="23";
