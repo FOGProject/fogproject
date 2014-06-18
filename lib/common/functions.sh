b96d015dee7e1acc36cc384ddcf51aacf45f8f18
Tom Elliott <tommygunsster@gmail.com>
2014-06-18 10:51:20 +0000

    Fix ImageReplicator to only send to enabled nodes.  Hopefully fix resetting of fog user password during upgrade.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1865 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -265,40 +265,44 @@ sendInstallationNotice()
 
 configureUsers()
 {
-	echo -n "  * Setting up fog user";
-	password=`date | md5sum | cut -d" " -f1`;
-	password=${password:0:6}
-	if [ "$installtype" = "S" ]
-	then
-		# save everyone wrist injuries
-		storageftpuser=${username};
-		storageftppass=${password};
-	fi
-	
-	if [ $password != "" ]
-	then
-		useradd -s "/bin/bash" -d "/home/${username}" ${username} >/dev/null 2>&1;
-		if [ "$?" = "0" ] 
+	ret=false
+	getent passwd ${username} >/dev/null 2>&1 && ret=true;
+	if [ "$doupdate" != "1" ] || [ !$ret ]; then
+		echo -n "  * Setting up fog user";
+		password=`date | md5sum | cut -d" " -f1`;
+		password=${password:0:6}
+		if [ "$installtype" = "S" ]
 		then
-			passwd ${username} >/dev/null 2>&1 << EOF
+			# save everyone wrist injuries
+			storageftpuser=${username};
+			storageftppass=${password};
+		fi
+		
+		if [ $password != "" ]
+		then
+			useradd -s "/bin/bash" -d "/home/${username}" ${username} >/dev/null 2>&1;
+			if [ "$?" = "0" ] 
+			then
+				passwd ${username} >/dev/null 2>&1 << EOF
 ${password}
 ${password}
 EOF
-			mkdir "/home/${username}" >/dev/null 2>&1;
-			chown -R ${username} "/home/${username}" >/dev/null 2>&1;
-			echo "...OK";
-		else
-			if [ -f "${webdirdest}/commons/config.php" ]
-			then
-				password=`cat ${webdirdest}/commons/config.php | grep TFTP_FTP_PASSWORD | cut -d"," -f2 | cut -d"\"" -f2`;
+				mkdir "/home/${username}" >/dev/null 2>&1;
+				chown -R ${username} "/home/${username}" >/dev/null 2>&1;
+				echo "...OK";
+			else
+				if [ -f "${webdirdest}/commons/config.php" ]
+				then
+					password=`cat ${webdirdest}/commons/config.php | grep TFTP_FTP_PASSWORD | cut -d"," -f2 | cut -d"\"" -f2`;
+				fi
+				echo "...Exists";
+				bluseralreadyexists="1";
 			fi
-			echo "...Exists";
-			bluseralreadyexists="1";
+			
+		else
+			echo "...Failed!";
+			exit 1;
 		fi
-		
-	else
-		echo "...Failed!";
-		exit 1;
 	fi
 }
 
