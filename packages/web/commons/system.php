bb81717915a4d3c07ae45452b49094913b5d8251
Tom Elliott <tommygunsster@gmail.com>
2014-06-19 23:35:51 +0000

    Understand why read test was in fog.donatefull now. Re-added and rebuilt
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1888 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -27,7 +27,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '1887');
+		define('FOG_VERSION', '1888');
 		define('FOG_SCHEMA', 105);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
