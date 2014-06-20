18bb928cb39a5d051919b3cbbbd2e2f5b3e54cf1
Tom Elliott <tommygunsster@gmail.com>
2014-06-20 18:00:12 +0000

    Fix, again, the imagingTypes settings.  It's a boolean, not an array.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1893 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -27,7 +27,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '1892');
+		define('FOG_VERSION', '1893');
 		define('FOG_SCHEMA', 105);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
