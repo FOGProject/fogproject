7bd9085f9ea8bb32d20d7cbb67e48b946aa3d89d
Tom Elliott <tommygunsster@gmail.com>
2014-06-23 21:48:34 +0000

    Fix, some more, image task pre_stage1 stuff.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1909 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -27,7 +27,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '1908');
+		define('FOG_VERSION', '1909');
 		define('FOG_SCHEMA', 106);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
