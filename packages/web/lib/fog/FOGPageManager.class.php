5befd4c8ee30420d9593c434708fac84bc52457a
Tom Elliott <tommygunsster@gmail.com>
2014-06-24 22:19:26 +0000

    Fix FOGPageManager properly.  Add css/images directory for jquery-ui.css to work properly.  Update imgLegacy to imgFormat in init's.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1915 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGPageManager.class.php
+++ packages/web/lib/fog/FOGPageManager.class.php
@@ -62,41 +62,45 @@ class FOGPageManager extends FOGBase
 	// Call FOGPage->method based on $this->classValue and $this->methodValue
 	public function render()
 	{
-		$this->loadPageClasses();
-		try
+		global $currentUser;
+		if ($currentUser && $currentUser->isValid() && $currentUser->isLoggedIn())
 		{
-			// Variables
-			$class = $this->getFOGPageClass();	// Class that will be used
-			$method = $this->methodValue;		// Method that will be called in the above class. This value changes while $this->methodValue remains constant.
-			// Error checking
-			if (!array_key_exists($this->classValue, $this->nodes))
-				throw new Exception('No FOGPage Class found for this node.');
-			// Figure out which method to call - default to index() if method is not found
-			if (empty($method) || !method_exists($class, $method))
+			$this->loadPageClasses();
+			try
 			{
-				if (!empty($method) && $method != 'list')
-					$this->debug('Class: %s, Method: %s, Error: Method not found in class, defaulting to index()', array(get_class($class), $method));
-				$method = 'index';
+				// Variables
+				$class = $this->getFOGPageClass();	// Class that will be used
+				$method = $this->methodValue;		// Method that will be called in the above class. This value changes while $this->methodValue remains constant.
+				// Error checking
+				if (!array_key_exists($this->classValue, $this->nodes))
+					throw new Exception('No FOGPage Class found for this node.');
+				// Figure out which method to call - default to index() if method is not found
+				if (empty($method) || !method_exists($class, $method))
+				{
+					if (!empty($method) && $method != 'list')
+						$this->debug('Class: %s, Method: %s, Error: Method not found in class, defaulting to index()', array(get_class($class), $method));
+					$method = 'index';
+				}
+				// FOG - Default view override
+				if ($this->methodValue != 'list' && $method == 'index' && $this->FOGCore->getSetting('FOG_VIEW_DEFAULT_SCREEN') != 'list' && method_exists($class, 'search'))
+					$method = 'search';
+				// POST - Append '_post' to method name if request method is POST and the method exists
+				if ($this->FOGCore->isPOSTRequest() && method_exists($class, $method . '_post'))
+					$method = $method . '_post';
+				// AJAX - Append '_ajax' to method name if request is ajax and the method exists
+				if ($this->FOGCore->isAJAXRequest() && method_exists($class, $method . '_ajax'))
+					$method = $method . '_ajax';
+				// Arguments
+				$this->arguments = (!empty($GLOBALS[$class->id]) ? array('id' => $GLOBALS[$class->id]) : array());
+				// Render result to variable - we do this so we can send HTTP Headers in a class method
+				ob_start();
+				call_user_func(array($class, $method));
+				return ob_get_clean();
 			}
-			// FOG - Default view override
-			if ($this->methodValue != 'list' && $method == 'index' && $this->FOGCore->getSetting('FOG_VIEW_DEFAULT_SCREEN') != 'list' && method_exists($class, 'search'))
-				$method = 'search';
-			// POST - Append '_post' to method name if request method is POST and the method exists
-			if ($this->FOGUser && $this->FOGUser->isValid() && $this->FOGCore->isPOSTRequest() && method_exists($class, $method . '_post'))
-				$method = $method . '_post';
-			// AJAX - Append '_ajax' to method name if request is ajax and the method exists
-			if ($this->FOGCore->isAJAXRequest() && method_exists($class, $method . '_ajax'))
-				$method = $method . '_ajax';
-			// Arguments
-			$this->arguments = (!empty($GLOBALS[$class->id]) ? array('id' => $GLOBALS[$class->id]) : array());
-			// Render result to variable - we do this so we can send HTTP Headers in a class method
-			ob_start();
-			call_user_func(array($class, $method));
-			return ob_get_clean();
-		}
-		catch (Exception $e)
-		{
-			$this->debug('Failed to Render Page: Node: %s, Error: %s', array($this->classValue, $e->getMessage()));
+			catch (Exception $e)
+			{
+				$this->debug('Failed to Render Page: Node: %s, Error: %s', array($this->classValue, $e->getMessage()));
+			}
 		}
 		return false;
 	}
