b100b2f008811e576aa1998c0832a77d63f0bb23
Tom Elliott <tommygunsster@gmail.com>
2014-06-25 17:32:40 +0000

    Fix snapin finding when in queued state.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1931 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/snapins.checkin.php
+++ packages/web/service/snapins.checkin.php
@@ -23,57 +23,56 @@ try
 	if (!$SnapinJob)
 		throw new Exception('#!ns');
 	// Work on the current Snapin Task.
-	$SnapinTask = current($FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1),'jobID' => $SnapinJob->get('id'))));
-	if ($SnapinTask)
+	$SnapinTask = current($FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1,2),'jobID' => $SnapinJob->get('id'))));
+	if (!$SnapinTask)
+		throw new Exception('#!ns');
+	// Get the information (the Snapin itself)
+	$Snapin = new Snapin($SnapinTask->get('snapinID'));
+	// Check for task status.  If it's got a numeric exitcode
+	if (strlen($_REQUEST['exitcode']) > 0 && is_numeric($_REQUEST['exitcode']))
 	{
-		// Get the information (the Snapin itself)
-		$Snapin = new Snapin($SnapinTask->get('snapinID'));
-		// Check for task status.  If it's got a numeric exitcode
-		if (strlen($_REQUEST['exitcode']) > 0 && is_numeric($_REQUEST['exitcode']))
+		// Place the task for records, but outside of recognizable as Complete or Done!
+		$SnapinTask->set('stateID','2')
+				   ->set('return',$_REQUEST['exitcode'])
+				   ->set('details',$_REQUEST['exitdesc'])
+				   ->set('complete',date('Y-m-d H:i:s'));
+		if ($SnapinTask->save())
+			print "#!ok";
+		// Get the current count of snapin tasks.
+		$cnt = count($FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1),'jobID' => $SnapinJob->get('id'))));
+		// If that was the last task, delete the job.
+		if ($cnt == 0)
 		{
-			// Place the task for records, but outside of recognizable as Complete or Done!
-			$SnapinTask->set('stateID','2')
-					   ->set('return',$_REQUEST['exitcode'])
-					   ->set('details',$_REQUEST['exitdesc'])
-					   ->set('complete',date('Y-m-d H:i:s'));
-			if ($SnapinTask->save())
-				print "#!ok";
-			// Get the current count of snapin tasks.
-			$cnt = count($FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1),'jobID' => $SnapinJob->get('id'))));
-			// If that was the last task, delete the job.
-			if ($cnt == 0)
-			{
-				// If it's part of a task deployment update the task information.
-				$SnapinJob->set('stateID',2)->save();
-				if ($Task && $Task->isValid())
-					$Task->set('stateID',4)->save();
-			}
-		}
-		else
-		{
-			$SnapinJob->set('stateID',1)->save();
 			// If it's part of a task deployment update the task information.
+			$SnapinJob->set('stateID',2)->save();
 			if ($Task && $Task->isValid())
-			{
-				$Task->set('stateID',3)
-					 ->set('checkInTime',date('Y-m-d H:i:s'))->save();
-			}
-			//If not from above, update the Task information.
-			$SnapinTask->set('stateID',0)
-					   ->set('checkin',date('Y-m-d H:i:s'));
-			// As long as things update, send the information.
-			if ($SnapinTask->save())
-			{
-				print "#!ok\n";
-				print "JOBTASKID=".$SnapinJob->get('id')."\n";
-				print "JOBCREATION=".$SnapinJob->get('createTime')."\n";
-				print "SNAPINNAME=".$Snapin->get('name')."\n";
-				print "SNAPINARGS=".$Snapin->get('args')."\n";
-				print "SNAPINBOUNCE=".$Snapin->get('reboot')."\n";
-				print "SNAPINFILENAME=".basename($Snapin->get('file'))."\n";
-				print "SNAPINRUNWITH=".$Snapin->get('runWith')."\n";
-				print "SNAPINRUNWITHARGS=".$Snapin->get('runWithArgs');
-			}
+				$Task->set('stateID',4)->save();
+		}
+	}
+	else
+	{
+		$SnapinJob->set('stateID',1)->save();
+		// If it's part of a task deployment update the task information.
+		if ($Task && $Task->isValid())
+		{
+			$Task->set('stateID',3)
+				 ->set('checkInTime',date('Y-m-d H:i:s'))->save();
+		}
+		//If not from above, update the Task information.
+		$SnapinTask->set('stateID',0)
+				   ->set('checkin',date('Y-m-d H:i:s'));
+		// As long as things update, send the information.
+		if ($SnapinTask->save())
+		{
+			print "#!ok\n";
+			print "JOBTASKID=".$SnapinJob->get('id')."\n";
+			print "JOBCREATION=".$SnapinJob->get('createTime')."\n";
+			print "SNAPINNAME=".$Snapin->get('name')."\n";
+			print "SNAPINARGS=".$Snapin->get('args')."\n";
+			print "SNAPINBOUNCE=".$Snapin->get('reboot')."\n";
+			print "SNAPINFILENAME=".basename($Snapin->get('file'))."\n";
+			print "SNAPINRUNWITH=".$Snapin->get('runWith')."\n";
+			print "SNAPINRUNWITHARGS=".$Snapin->get('runWithArgs');
 		}
 	}
 }
