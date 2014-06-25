e5d2cf37010ef671e5dbc011bf5e459928e9fbee
Tom Elliott <tommygunsster@gmail.com>
2014-06-25 19:02:21 +0000

    Reapply back to original Snapin task setting as it was stuck in infinite loop.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1934 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/snapins.checkin.php
+++ packages/web/service/snapins.checkin.php
@@ -23,13 +23,8 @@ try
 	if (!$SnapinJob)
 		throw new Exception('#!ns');
 	// Work on the current Snapin Task.
-	$SnapinTasks = $FOGCore->getClass('SnapinTaskManager')->find(array('jobID' => $SnapinJob->get('id')));
-	foreach($SnapinTasks AS $SnapinTask)
-	{
-		if ($SnapinTask->get('stateID') < 2)
-			break;
-	}
-	if (!$SnapinTask->get('stateID') < 2)
+	$SnapinTask = current($FOGCore->getClass('SnapinTaskManager')->find(array('stateID' => array(-1,0,1),'jobID' => $SnapinJob->get('id'))));
+	if (!$SnapinTask)
 		throw new Exception('#!ns');
 	// Get the information (the Snapin itself)
 	$Snapin = new Snapin($SnapinTask->get('snapinID'));
