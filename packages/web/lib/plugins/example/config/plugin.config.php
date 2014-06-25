46f8ee52bce907c87228b185013710ec3a991899
Tom Elliott <tommygunsster@gmail.com>
2014-06-25 17:44:51 +0000

    Fix spelling/grammar issue on example plugin description.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1932 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/example/config/plugin.config.php
+++ packages/web/lib/plugins/example/config/plugin.config.php
@@ -1,7 +1,7 @@
 <?php
 $fog_plugin = array();
 $fog_plugin["name"] = "example";
-$fog_plugin["description"] = "Just an example plugin for those who want to create there own plugins.";
+$fog_plugin["description"] = "Just an example plugin for those who want to create their own plugins.";
 $fog_plugin["menuicon"] = "html/images/{$fog_plugin['name']}.png";
 $fog_plugin["menuicon_hover"] = null;
 $fog_plugin["entrypoint"] = "html/run.php";
