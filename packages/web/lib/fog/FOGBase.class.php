4d6e4f869e77b5652cce39f83b59f5f5533382ec
Tom Elliott <tommygunsster@gmail.com>
2014-06-25 04:04:48 +0000

    Use hook in to create location link.  Added two new functions to FOGBase.class.php.  array_insert_before and after to make inserting links at a specific point that much easier.  Based on information from: http://eosrei.net/articles/2011/11/php-arrayinsertafter-arrayinsertbefore.  Also fixes seemingly random inability to use plugins on plugin page.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1916 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGBase.class.php
+++ packages/web/lib/fog/FOGBase.class.php
@@ -170,4 +170,68 @@ abstract class FOGBase
 	{
 		return (substr($str,strlen($str)-strlen($sub)) === $sub);
 	}
+	/*
+	* Inserts a new key/value before the key in the array.
+	*
+	* @param $key
+	*   The key to insert before.
+	* @param $array
+	*   An array to insert in to.
+	* @param $new_key
+	*   The key to insert.
+	* @param $new_value
+	*   An value to insert.
+	*
+	* @return
+	*   The new array if the key exists, FALSE otherwise.
+	*
+	* @see array_insert_after()
+	*/
+	public function array_insert_before($key, array &$array, $new_key, $new_value)
+	{
+		if (array_key_exists($key, $array)) 
+		{
+			$new = array();
+			foreach ($array as $k => $value)
+			{
+				if ($k === $key)
+					$new[$new_key] = $new_value;
+				$new[$k] = $value;
+			}
+			return $new;
+		}
+		return false;
+	}
+	/*
+	* Inserts a new key/value after the key in the array.
+	*
+	* @param $key
+	*   The key to insert after.
+	* @param $array
+	*   An array to insert in to.
+	* @param $new_key
+	*   The key to insert.
+	* @param $new_value
+	*   An value to insert.
+	*
+	* @return
+	*   The new array if the key exists, FALSE otherwise.
+	*
+	* @see array_insert_before()
+	*/
+	public function array_insert_after($key, array &$array, $new_key, $new_value)
+	{
+		if (array_key_exists($key, $array)) 
+		{
+			$new = array();
+			foreach ($array as $k => $value)
+			{
+				$new[$k] = $value;
+				if ($k === $key)
+					$new[$new_key] = $new_value;
+			}
+			return $new;
+		}
+		return false;
+	}
 }
