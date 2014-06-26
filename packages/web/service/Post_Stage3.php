f3e8f75c51aeff70958c371b411b9700573002dd
Tom Elliott <tommygunsster@gmail.com>
2014-06-26 02:00:32 +0000

    Maybe fix plugin session reset stuff so session is regenerated appropriately on plugin updates and closes.  Maybe fix multicast close out, but still seems to be timing to db connections.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1937 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/Post_Stage3.php
+++ packages/web/service/Post_Stage3.php
@@ -4,51 +4,40 @@ try
 {
 	// Get the MAC
 	$MACAddress = new MACAddress($_REQUEST['mac']);
-	if (!$MACAddress->isValid())
-		throw new Exception(_('Invalid MAC Address'));
+	if (!$MACAddress->isValid()) throw new Exception(_('Invalid MAC Address'));
 	// Host for MAC Address
 	$Host = $MACAddress->getHost();
-	if (!$Host->isValid())
-		throw new Exception(_('Invalid host'));
+	if (!$Host->isValid()) throw new Exception(_('Invalid host'));
 	// Task for Host
 	$Task = current($Host->get('task'));
-	if (!$Task->isValid())
-		throw new Exception(sprintf('%s: %s (%s)',_('No Active Task found for Host'), $Host->get('name'),$MACAddress));
+	if (!$Task->isValid()) throw new Exception(sprintf('%s: %s (%s)',_('No Active Task found for Host'), $Host->get('name'),$MACAddress));
 	$TaskType = new TaskType($Task->get('typeID'));
 	// Set the task to state 4
 	$Task->set('stateID',4);
-	// If it's a multicast job, destroy the association and remove the client.
-	if ($TaskType->isMulticast())
-	{
-		$MSA = current($FOGCore->getClass('MulticastSessionsAssociationManager')->find(array('taskID' => $Task->get('id'))));
-		if ($MSA && $MSA->isValid())
-			$MulticastSession = new MulticastSessions($MSA->get('msID'));
-		if ($MulticastSession && $MulticastSession->isValid())
-		{
-			$MulticastSession->set('clients',(int)$MulticastSession->get('clients')-1)->save();
-			$MulticastSession = new MulticastSessions($MSA->get('msID'));
-			if ($MulticastSession->get('clients') <= 0)
-				$MulticastSession->set('stateID',4)->set('completetime',date('Y-m-d H:i:s'))->save();
-		}
-	}
 	// Log it
 	$ImagingLogs = $FOGCore->getClass('ImagingLogManager')->find(array('hostID' => $Host->get('id')));
-	foreach($ImagingLogs AS $ImagingLog)
-		$id[] = $ImagingLog->get('id');
+	foreach($ImagingLogs AS $ImagingLog) $id[] = $ImagingLog->get('id');
 	// Update Last deploy
 	$Host->set('deployed',date('Y-m-d H:i:s'))->save();
 	$il = new ImagingLog(max($id));
 	$il->set('finish',date('Y-m-d H:i:s'))->save();
 	// Task Logging.
 	$TaskLog = new TaskLog($Task);
-	$TaskLog->set('taskID',$Task->get('id'))
-			->set('taskStateID',$Task->get('stateID'))
-			->set('createdTime',$Task->get('createdTime'))
-			->set('createdBy',$Task->get('createdBy'))
-			->save();
-	if (!$Task->save())
-		throw new Exception('Failed to update task.');
+	$TaskLog->set('taskID',$Task->get('id'))->set('taskStateID',$Task->get('stateID'))->set('createdTime',$Task->get('createdTime'))->set('createdBy',$Task->get('createdBy'))->save();
+	if (!$Task->save()) throw new Exception('Failed to update task.');
 	print '##';
+	// If it's a multicast job, destroy the association and remove the client.
+	if ($TaskType->isMulticast())
+	{
+		$MyMulticastTask = current($FOGCore->getClass('MulticastSessionsAssociationManager')->find(array('taskID' => $Task->get('id'))));
+		if ($MyMulticastTask && $MyMulticastTask->isValid())
+		{
+			$MulticastSession = new MulticastSessions($MyMulticastTask->get('msID'));
+			$MulticastSession->set('clients',($MulticastSession->get('clients') - 1))->save();
+			if ($MulticastSession->get('clients') < 1)
+				$MulticastSession->set('stateID',4)->set('completetime',date('Y-m-d H:i:s'))->save();
+		}
+	}
 }
 catch (Exception $e)
 {
