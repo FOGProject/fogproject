69a5528f53fff9e6de00af2e9813ed0c22c66b39
Tom Elliott <tommygunsster@gmail.com>
2014-07-01 03:20:32 +0000

    Plugin Hooks, built with the system, have been modified to check if their respective plugin is actually loaded or not to perform actions.  This is needed, for now, because if  user installs the plugins, for the first time, and then logs out.  When they login again and try removing the plugin, it will remove successfully, but the hooks will remain active even after they've been removed/reinstalled until they logout and back in again.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1979 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/accesscontrol/hooks/AddMenuNotesItems.hook.php
+++ packages/web/lib/plugins/accesscontrol/hooks/AddMenuNotesItems.hook.php
@@ -8,13 +8,19 @@ class AddMenuNotesItems extends Hook
 	var $node = 'accesscontrol';
 	public function AddMenuData($arguments)
 	{
-		$arguments['main'] = $this->array_insert_after('users',$arguments['main'],$this->node,_('Access Control'));
+		$plugin = current($this->FOGCore->getClass('PluginManager')->find(array('name' => $this->node,'installed' => 1,'state' => 1)));
+		if ($plugin && $plugin->isValid())
+			$arguments['main'] = $this->array_insert_after('users',$arguments['main'],$this->node,_('Access Control'));
 	}
 	public function AddSubMenuData($arguments)
 	{
-		$arguments['submenu'][$this->node]['search'] = $this->foglang['NewSearch'];
-		$arguments['submenu'][$this->node]['list'] = sprintf($this->foglang['ListAll'],_('Controls'));
-		$arguments['submenu'][$this->node]['add'] = sprintf($this->foglang['CreateNew'],_('Control'));
+		$plugin = current($this->FOGCore->getClass('PluginManager')->find(array('name' => $this->node,'installed' => 1,'state' => 1)));
+		if ($plugin && $plugin->isValid())
+		{
+			$arguments['submenu'][$this->node]['search'] = $this->foglang['NewSearch'];
+			$arguments['submenu'][$this->node]['list'] = sprintf($this->foglang['ListAll'],_('Controls'));
+			$arguments['submenu'][$this->node]['add'] = sprintf($this->foglang['CreateNew'],_('Control'));
+		}
 	}
 }
 $AddMenuNotesItems = new AddMenuNotesItems();
