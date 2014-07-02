10de0a22945d48e4ace62f8d6253b940f452365e
Tom Elliott <tommygunsster@gmail.com>
2014-07-02 01:30:53 +0000

    More fixing of node to image page so it doesn't just error out.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1990 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -27,7 +27,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '1989');
+		define('FOG_VERSION', '1990');
 		define('FOG_SCHEMA', 107);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
