4d265f97153b5ae5e7b4a4a9783b194e329cca4b
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2014-07-03 10:12:11 +0000

    If no username set, set username as literally nobody.  Scheduled Tasks now have a taskType identifier.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1996 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '1995');
+		define('FOG_VERSION', '1996');
 		define('FOG_SCHEMA', 107);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
