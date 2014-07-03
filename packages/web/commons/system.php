fc57da5257297cf4c2ff40a2a02dc73071329ca4
Tom Elliott <tommygunsster@gmail.com>
2014-07-03 20:58:05 +0000

    Fix typo on BootMenu.class.php
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2001 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2000');
+		define('FOG_VERSION', '2001');
 		define('FOG_SCHEMA', 107);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
