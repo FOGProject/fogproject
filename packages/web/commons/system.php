af5667cb652e0137f89074ba17a0537f14d87726
Tom Elliott <tommygunsster@gmail.com>
2014-07-09 00:21:25 +0000

    Forgot to push the stuff I needed push.  Here it is.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2030 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2028');
+		define('FOG_VERSION', '2030');
 		define('FOG_SCHEMA', 109);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
