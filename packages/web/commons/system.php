3d910ea521db7b3a174797d2e8254614f61b5276
Tom Elliott <tommygunsster@gmail.com>
2014-07-10 17:21:37 +0000

    DB update to add a type parameter to the Download without snapins task type.  Cleanup some elements in TaskType class file.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2035 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,8 +26,8 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2034');
-		define('FOG_SCHEMA', 109);
+		define('FOG_VERSION', '2035');
+		define('FOG_SCHEMA', 110);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
