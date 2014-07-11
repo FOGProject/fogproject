55868a71cc46e91ad0dbba6d753a5557efc85bf4
Tom Elliott <tommygunsster@gmail.com>
2014-07-11 20:53:03 +0000

    Fix partimage to use hdd rather than partition as that's how it seems to work best.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2042 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2041');
+		define('FOG_VERSION', '2042');
 		define('FOG_SCHEMA', 110);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
