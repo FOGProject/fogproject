eb27bfdcd139fc388a5596db237605ccf3ad1a0e
Tom Elliott <tommygunsster@gmail.com>
2014-07-11 20:53:03 +0000

    Fix partimage to use hdd rather than partition as that's how it seems to work best.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2042 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/buildroot/package/fog/scripts/usr/share/fog/lib/funcs.sh
+++ src/buildroot/package/fog/scripts/usr/share/fog/lib/funcs.sh
@@ -301,14 +301,14 @@ writeImage()
 {
 	if [ "$imgFormat" = "1" ] || [ "$imgLegacy" = "1" ]; then
 		#partimage
-		partimage restore $2 $1 -f3 -b 2>/tmp/status.fog;
+		partimage restore $hd $1 -f3 -b #2>/tmp/status.fog;
 	else 
 		# partclone
 		mkfifo /tmp/pigz1;
 		cat $1 > /tmp/pigz1 &
 		gunzip -d -c < /tmp/pigz1 | partclone.restore --ignore_crc -O $2 -N -f 1 2>/tmp/status.fog;
+		rm /tmp/pigz1;
 	fi
-	rm /tmp/pigz1;
 }
 
 # $1 = Target
@@ -316,7 +316,7 @@ writeImageMultiCast()
 {
 	if [ "$imgFormat" = "1" ] || [ "$imgLegacy" = "1" ]; then
 		#partimage
-		udp-receiver --nokbd --portbase $port --mcast-rdv-address $storageip 2>/dev/null | gunzip -d -c | partimage -f3 -b restore $1 stdin 2>/tmp/status.fog;
+		udp-receiver --nokbd --portbase $port --mcast-rdv-address $storageip 2>/dev/null | gunzip -d -c | partimage -f3 -b restore $hd stdin 2>/tmp/status.fog;
 	else 
 		# partclone
 		udp-receiver --nokbd --portbase $port --mcast-rdv-address $storageip 2>/dev/null | gunzip -d -c | partclone.restore --ignore_crc -O $1 -N -f 1 2>/tmp/status.fog;
