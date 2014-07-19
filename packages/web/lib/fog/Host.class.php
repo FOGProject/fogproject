840ce0ca6633a63060c4266d09106a66a8a6b19e
Tom Elliott <tommygunsster@gmail.com>
2014-07-19 04:47:21 +0000

    Fix snapin deploy's for imaging tasks.  I had a typo, it should've been imagingTypes but I put imageTypes.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2090 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/Host.class.php
+++ packages/web/lib/fog/Host.class.php
@@ -920,7 +920,7 @@ class Host extends FOGController
 				}
 			}
 			// Snapin deploy/cancel after deploy
-			if (!$isUpload && $deploySnapins && $imageTypes && $taskTypeID != '17')
+			if (!$isUpload && $deploySnapins && $imagingTypes && $taskTypeID != '17')
 			{
 				// Remove any exists snapin tasks
 				$SnapinJobs = $this->FOGCore->getClass('SnapinJobManager')->find(array('hostID' => $this->get('id')));
