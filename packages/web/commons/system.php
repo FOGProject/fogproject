18615f8b0a3eb96856c2184e3a5934e2fe71a19b
Tom Elliott <tommygunsster@gmail.com>
2014-07-25 15:16:04 -0400

    Fix conflict.

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -28,8 +28,8 @@ class System
 		define('IS_INCLUDED', true);
 		define('FOG_VERSION', '2107');
 		define('FOG_SCHEMA', 113);
-		define('FOG_SVN_REVISION', '$Revision$');
-		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
+		define('FOG_SVN_REVISION', '$Revision: 2107 $');
+		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2014-07-25 13:00:09 -0400 (Fri, 25 Jul 2014) $');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
 		define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
 		define('BASEPATH', self::DetermineBasePath());
