32130fc75c771e4a7aa2305a577c79182c811d8c
Tom Elliott <tommygunsster@gmail.com>
2014-07-28 23:26:36 +0000

    Update tftp files to latest pull from today.  Update kernels to 3.15.7
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2110 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,8 +26,8 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2109');
-		define('FOG_SCHEMA', 113);
+		define('FOG_VERSION', '2110');
+		define('FOG_SCHEMA', 114);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
