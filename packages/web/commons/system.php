c5100a06a25e9c610599e7855f9a1d7494a08641
Tom Elliott <tommygunsster@gmail.com>
2014-07-29 21:55:12 +0000

    Fix isMulticast error thrown on completetion of tasks.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2113 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2112');
+		define('FOG_VERSION', '2113');
 		define('FOG_SCHEMA', 114);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
