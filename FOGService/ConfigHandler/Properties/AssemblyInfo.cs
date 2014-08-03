7c89133755470624141a50b06b374f5e999b2f5c
Jbob <jbob182@github>
2014-08-03 14:54:01 -0500

    Add CommunicationHandler

--- FOGService/ConfigHandler/Properties/AssemblyInfo.cs
+++ FOGService/ConfigHandler/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConfigHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handle the config file")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConfigHandler")]
+[assembly: AssemblyCompany("FOG")]
+[assembly: AssemblyProduct("FOG:ConfigHandler")]
 [assembly: AssemblyCopyright("Copyright 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
