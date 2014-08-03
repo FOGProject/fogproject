25a46037abeb23f8a4bc48a97806f74fa8bec0f1
Jbob <jbob182@github>
2014-08-03 15:20:20 -0500

    Create NotificationHandler

---
+++ FOGService/NotificationHandler/Notification.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace FOG
+{
+	/// <summary>
+	/// Store neccesary notification information
+	/// </summary>
+	public class Notification {
+		private String title;
+		private String message;
+		private int duration;
+		
+		public Notification(String title, String message, int duration) {
+			this.title = title;
+			this.message = message;
+			this.duration = duration;
+		}
+		
+		public String getTitle() {
+			return this.title;
+		}
+		
+		public String getMessage() {
+			return this.message;
+		}
+		
+		public int getDuration() {
+			return duration;
+		}
+	}
+}
