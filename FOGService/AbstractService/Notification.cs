926ac6fe1bd7154cf734e36faa1a6d9e67a41766
Jbob <jbob182@github>
2014-08-03 11:08:05 -0500

    Clean AbstactService and FOGService, further rewrite needed

---
+++ FOGService/AbstractService/Notification.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace FOG
+{
+	/// <summary>
+	/// Basic notification structure
+	/// </summary>
+	public class Notification
+	{
+		private String title;
+		private String msg;
+		
+		public Notification(String title, String msg)
+		{
+			this.title = title;
+			this.msg = msg;
+		}
+		
+		public String getMessage() {
+			return this.msg;
+		}
+			
+		public String getTitle() {
+			return this.title;
+		}
+	}
+}
