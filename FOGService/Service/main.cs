2b1eca6106208e8e14a48a18890feb2cff8e0042
Jbob <jbob182@github>
2014-08-02 22:15:32 -0500

    Add AbstractService and Service to new Solution

---
+++ FOGService/Service/main.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.ServiceProcess;
+
+namespace FOGService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] { new Service() };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
