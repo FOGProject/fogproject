a2d147e58b5c3bc7bda2d8b99aeca5f4dcf21489
Jbob <jbob182@github>
2014-08-03 15:20:20 -0500

    Create NotificationHandler

---
+++ FOGService/NotificationHandler/NotificationHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace FOG
+{
+	/// <summary>
+	/// Handle all notifications
+	/// </summary>
+	public class NotificationHandler
+	{
+		private List<Notification> notifications;
+		
+		public NotificationHandler() {
+			this.notifications = new List<Notification>();
+		}
+		
+		public void createNotification(Notification notification) {
+			this.notifications.Add(notification);
+		}
+		
+		public List<Notification> getNotifications() {
+			return this.notifications;
+		}
+		
+		public void clearNotifications() {
+			this.notifications.Clear();
+		}
+		
+		public void removeNotification(Notification notification) {
+			this.notifications.Remove(notification);
+		}
+		
+		public void removeNotification(int index) {
+			this.notifications.RemoveAt(index);
+		}
+		
+		
+	}
+}
