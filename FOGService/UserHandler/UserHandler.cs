aa3d8f8c8f9c9a3abb89dcbf1941ce5c682f4c0e
Jbob <jbob182@github>
2014-08-03 14:13:24 -0500

    Add UserHandler

---
+++ FOGService/UserHandler/UserHandler.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace FOG
+{
+	/// <summary>
+	/// Detect the current user
+	/// </summary>
+	public class UserHandler
+	{
+		public Boolean isUserLoggedIn(LogHandler logHandler) {
+			return getUsersLoggedIn(logHandler).Count > 0;
+		}
+		
+		public List<String> getUsersLoggedIn(LogHandler logHandler) {
+			List<String> users = new List<String>();
+			try {
+				ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", 
+				                                                                 "SELECT * FROM Win32_ComputerSystem");
+
+				foreach (ManagementObject queryObj in searcher.Get()) {
+					users.Add( queryObj["UserName"].ToString() );
+				}
+				
+			} catch (Exception ex) {
+				logHandler.log("User Handler", "Error geetting all users: " + ex.Message);
+			}
+			
+			return users;
+		}
+	}
+}
