e7ccfa0412772f6641b6e9f031b7ee33c2eae1da
Jbob <jbob182@github>
2014-08-03 14:54:01 -0500

    Add CommunicationHandler

--- FOGService/ShutdownHandler/Properties/AssemblyInfo.cs
+++ FOGService/ShutdownHandler/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShutdownHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handle shutdown commands")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ShutdownHandler")]
+[assembly: AssemblyCompany("FOG")]
+[assembly: AssemblyProduct("FOG:ShutdownHandler")]
 [assembly: AssemblyCopyright("Copyright 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
