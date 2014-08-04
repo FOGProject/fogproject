9883240e9b5894c66679af3a783ae4fa14ae5d58
Jbob <jbob182@github>
2014-08-04 13:02:37 -0500

    Fix multiple bugs, hard code settings to Tom's vm, shorten modules grealty

---
+++ FOGService/CommunicationHandler/Response.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace FOG {
+	/// <summary>
+	/// Contains the information that the FOG Server responds with
+	/// </summary>
+	public class Response {
+		
+		private Boolean error;
+		private Dictionary<String, String> data;
+		
+		public Response(Boolean error, Dictionary<String, String> data)
+		{
+			this.error = error;
+			this.data = data;
+		}
+		
+		public Response() {
+			this.error = true;
+			this.data = new Dictionary<String, String>();
+		}
+		
+		public void setError(Boolean error) {
+			this.error = error;
+		}
+		
+		public void setData(Dictionary<String, String> data) {
+			this.data = data;
+		}
+		
+		public Boolean wasError() {
+			return this.error;
+		}
+		
+		public String getField(String id) {
+			
+			if(data.ContainsKey(id)) {
+				return data[id];
+			}
+			
+			return "";
+		}
+		
+		public Dictionary<String, String> getData() {
+			return this.data;
+		}
+	}
+}
---
+++ FOGService/TaskReboot/TaskReboot.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace FOG
+{
+	/// <summary>
+	/// Reboot the computer if a task needs to
+	/// </summary>
+	public class TaskReboot : AbstractModule
+	{
+		public TaskReboot(CommunicationHandler communicationHandler,
+		                      LogHandler logHandler,
+		                      NotificationHandler notificationHandler,
+		                      ShutdownHandler shutdownHander,
+		                      UserHandler userHandler) : base(communicationHandler, logHandler,
+			     												notificationHandler, shutdownHander,
+			    												userHandler){
+			this.setName("TaskReboot");
+			this.setDescription("Reboot if a task is scheduled");
+			
+		}
+		
+		protected override void doWork() {
+			if(isEnabled()) {
+			
+				//Get task info
+				Response taskResponse = communicationHandler.getResponse("/fog/service/jobs.php?mac=" +
+				                                                                       communicationHandler.getMacAddresses() +
+				                                                                       "&moduleid=" + getName().ToLower());
+				
+				//Shutdown if a task is avaible and the user is logged out or it is forced
+				if(!taskResponse.wasError() && (!userHandler.isUserLoggedIn(logHandler) || taskResponse.getField("#force").Equals("1") )) {
+					shutdownHander.restart(getName(), 25);
+				}
+			}
+			
+		}
+		
+	}
+}
