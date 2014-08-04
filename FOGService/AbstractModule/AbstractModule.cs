c3bbc93e113e7293eea256e18a3a7acc3ec33482
Jbob <jbob182@github>
2014-08-04 13:17:58 -0500

    Shorten getters and setters to one line

--- FOGService/AbstractModule/AbstractModule.cs
+++ FOGService/AbstractModule/AbstractModule.cs
@@ -9,19 +9,22 @@ namespace FOG
 	/// </summary>
 	public abstract class AbstractModule {
 		
+		//Basic variables every module needs
 		private String moduleName;
 		private String moduleDescription;
 		private Status status;
-		private String checkIfEnabledURL;
+		private String isActiveURL;
 		private int defaultSleepDuration;
 		private int sleepDuration;
 		
+		//Define the handlers
 		protected CommunicationHandler communicationHandler;
 		protected LogHandler logHandler;
 		protected NotificationHandler notificationHandler;
 		protected ShutdownHandler shutdownHander;
 		protected UserHandler userHandler;
 		
+		//Module status -- used for stopping/starting
 		public enum Status {
 			Running = 1,
 			Stopped = 0
@@ -33,12 +36,13 @@ namespace FOG
 		                      ShutdownHandler shutdownHander,
 		                      UserHandler userHandler) {
 			
-			this.moduleName = "";
-			this.moduleDescription = "";
-			this.status = Status.Stopped;
-			this.defaultSleepDuration = 320;
-			this.sleepDuration = this.defaultSleepDuration;
-			this.checkIfEnabledURL = "/fog/service/servicemodule-active.php";
+			//Define variables
+			setName("Generic Module");
+			setDescription("Generic Description");
+			setStatus(Status.Stopped);
+			setDefaultSleepDuration(320);
+			setSleepDuration(getDefaultSleepDuration());
+			setIsActiveURL("/fog/service/servicemodule-active.php");
 
 			this.communicationHandler = communicationHandler;
 			this.logHandler = logHandler;
@@ -49,6 +53,8 @@ namespace FOG
 		
 		protected abstract void doWork();
 		
+		
+		//Default start method
 		public virtual void start() {
 			logHandler.log(getName(), "Starting...");
 			setStatus(Status.Running);
@@ -61,51 +67,36 @@ namespace FOG
 			}
 		}
 		
+		//Default stop method
 		public virtual void stop() {
 			logHandler.log(getName(), "Stopping...");
 			setStatus(Status.Stopped);
 		}
 
 		
-		protected void setName(String name) {
-			this.moduleName = name;
-		}
-
-		protected void setDescription(String description) {
-			this.moduleDescription = description;
-		}
+		//Getters and setters
+		public String getName() { return this.moduleName; }
+		protected void setName(String name) { this.moduleName = name; }
 		
-		protected void setStatus(Status status) {
-			this.status = status;
-		}
+		public String getDescription() { return this.moduleDescription; }
+		protected void setDescription(String description) { this.moduleDescription = description; }
 		
-		public String getName() {
-			return this.moduleName;
-		}
+		public Status getStatus() {	return this.status; }
+		protected void setStatus(Status status) { this.status = status; }
 		
-		public String getDescription() {
-			return this.moduleDescription;
-		}
-		
-		public Status getStatus() {
-			return this.status;
-		}
-		
-		public int getDefaultSleepDuration() {
-			return this.defaultSleepDuration;
-		}
+		public int getDefaultSleepDuration() { return this.defaultSleepDuration; }
+		protected void setDefaultSleepDuration(int defaultSleepDuration) { this.defaultSleepDuration = defaultSleepDuration; }	
 	
-		public int getSleepDuration() {
-			return this.sleepDuration;
-		}
+		public int getSleepDuration() { return this.sleepDuration; }
+		protected void setSleepDuration(int sleepDuration) { this.sleepDuration = sleepDuration; }
+
+		public String getIsActiveURL() { return this.isActiveURL; }
+		protected void setIsActiveURL(String isActiveURL) { this.isActiveURL = isActiveURL; }
 		
-		protected void setSleepDuration(int sleepDuration) {
-			this.sleepDuration = sleepDuration;
-		}	
-		
+		//Check if the module is enabled, also set the sleep duration
 		public Boolean isEnabled() {
 			
-			Response moduleActiveResponse = communicationHandler.getResponse(checkIfEnabledURL + "?mac=" + communicationHandler.getMacAddresses() +
+			Response moduleActiveResponse = communicationHandler.getResponse(getIsActiveURL() + "?mac=" + communicationHandler.getMacAddresses() +
 			                                 								"&moduleid=" + getName().ToLower());
 			
 			//Update the sleep duration between cycles
