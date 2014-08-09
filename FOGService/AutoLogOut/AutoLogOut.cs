a24221919907195e6ee9b1fc796eed56d1fe0a36
Jbob <jbob182@gmail.com>
2014-08-08 21:49:39 -0500

    Add layout of AutoLogOut

---
+++ FOGService/AutoLogOut/AutoLogOut.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace FOG {
+	/// <summary>
+	/// Automatically log out the user after X seconds of inactivity
+	/// </summary>
+	public class AutoLogOut : AbstractModule {
+		
+		private Boolean notifieduser;
+		
+		public AutoLogOut():base() {
+			setName("AutoLogOut");
+			setDescription("Automatically log out the user if they are inactive");
+			this.notifiedUser = false;
+		}
+		
+		protected override void doWork() {
+			if(UserHandler.isUserLoggedIn()) {
+				//Get task info
+				Response taskResponse = CommunicationHandler.getResponse("/fog/service/autologout.php?mac=" + CommunicationHandler.getMacAddresses());
+					
+
+				if(!taskResponse.wasError()) {
+
+				}
+			} else {
+				LogHandler.log(getName(), "No user logged in");
+			}
+			
+		}
+		
+		
+	}
+}
