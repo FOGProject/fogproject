e5ba976c238a7d2dcecbb9911880e0c8473e3b12
Jbob <jbob182@gmail.com>
2014-08-10 13:27:22 -0500

    Add auto log out logic

--- FOGService/AutoLogOut/AutoLogOut.cs
+++ FOGService/AutoLogOut/AutoLogOut.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace FOG {
@@ -8,26 +9,56 @@ namespace FOG {
 	/// </summary>
 	public class AutoLogOut : AbstractModule {
 		
-		private Boolean notifieduser;
-		
+		private int defaultTime;
+
 		public AutoLogOut():base() {
 			setName("AutoLogOut");
 			setDescription("Automatically log out the user if they are inactive");
-			this.notifiedUser = false;
+			this.defaultTime = 1800;
 		}
 		
 		protected override void doWork() {
 			if(UserHandler.isUserLoggedIn()) {
 				//Get task info
 				Response taskResponse = CommunicationHandler.getResponse("/fog/service/autologout.php?mac=" + CommunicationHandler.getMacAddresses());
-					
 
 				if(!taskResponse.wasError()) {
-
+					int timeOut = getTimeOut(taskResponse);
+					LogHandler.log(getName(), "Time set to " + timeOut.ToString());
+					
+					if(UserHandler.getUserInactivityTime() >= timeOut) {
+						NotificationHandler.createNotification(new Notification("You are about to be logged off",
+						                                                        "Due to inactivity you will be logged off if you remain inactive", 20));
+						//Wait 20 seconds and check if the user is no longer inactive
+						Thread.Sleep(20);
+						if(UserHandler.getUserInactivityTime() >= timeOut)
+							ShutdownHandler.logOffUser();
+					}
+					
 				}
 			} else {
 				LogHandler.log(getName(), "No user logged in");
 			}
+			
+		}
+		
+		//Get how long a user must be inactive before logging them out
+		private int getTimeOut(Response taskResponse) {
+			try {
+				int timeOut = int.Parse(taskResponse.getField("#time"));
+				if(timeOut > 60) {
+					return timeOut;
+				} else {
+					LogHandler.log(getName(), "Time set is less than 1 minute");
+				}
+				
+			} catch (Exception ex) {
+				LogHandler.log(getName(), "Unable to parsing time set");
+				LogHandler.log(getName(), "ERROR: " + ex.Message);
+			}
+			LogHandler.log(getName(), "Using default time");
+			
+			return this.defaultTime;
 			
 		}
 		
