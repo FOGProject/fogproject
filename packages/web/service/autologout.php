7fdcd9e82da2632ef96dc93e472f638f33461673
Tom Elliott <tommygunsster@gmail.com>
2014-08-10 18:48:55 +0000

    Send #!it for invalid time for autologout.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2193 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/autologout.php
+++ packages/web/service/autologout.php
@@ -15,7 +15,7 @@ try
 	// Set the time.  If host is set, use it, if not use global.
 	$HaloMan ? $time = $HaloMan->get('time') : $time = $FOGCore->getSetting('FOG_SERVICE_AUTOLOGOFF_MIN');
 	// Send it.
-	throw new Exception(($_REQUEST['newService'] ? "#!ok\n#time=".$time : base64_encode($time)));
+	throw new Exception(($_REQUEST['newService'] && $time >= 5 ? "#!ok\n#time=".$time : ($_REQUEST['newService'] && $time < 5 ? "#!it\n" : base64_encode($time))));
 }
 catch(Exception $e)
 {
