3eb58bd9d4e6e9b03051c653ddd22de0e3178574
Jbob <jbob182@gmail.com>
2014-08-23 12:53:15 -0500

    Change ClientUpdater to retrieve the content from #updatefile

---
+++ FOGService/DisplayManager/DisplayManager.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+namespace FOG {
+	/// <summary>
+	/// Change the resolution of the display
+	/// </summary>
+	public class DisplayManager : AbstractModule {
+		public DisplayManager() : base() {
+			setName("DisplayManager");
+			setDescription("hange the resolution of the display");			
+		}
+		
+		protected override void doWork() {
+			//Get task info
+			Response taskResponse = CommunicationHandler.getResponse("/service/displaymanager.php?mac=" + CommunicationHandler.getMacAddresses());
+			
+			if(!taskResponse.wasError()) {
+				if(!taskResponse.getField("#x").Equals("") && !taskResponse.getField("#y").Equals("") && !taskResponse.getField("#r").Equals("")) {
+					
+				} else {
+					LogHandler.log(getName(), "ERROR");
+					LogHandler.log(getName(), "Not all values set: " + "x=" + taskResponse.getField("#x") + " y=" + taskResponse.getField("#y") + " r=" + taskResponse.getField("#r"));
+				}
+			}
+		}
+		
+	}
+}
