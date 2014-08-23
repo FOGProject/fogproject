56cfd492db69f93b677f312311fd040cc1639f70
Jbob <jbob182@gmail.com>
2014-08-23 12:53:15 -0500

    Change ClientUpdater to retrieve the content from #updatefile

--- FOGService/ClientUpdater/ClientUpdater.cs
+++ FOGService/ClientUpdater/ClientUpdater.cs
@@ -30,12 +30,12 @@ namespace FOG
 				//Loop through each update file and compare its hash to the local copy
 				foreach(String updateFile in updates) {
 					LogHandler.log(getName(), "Possible update for " + updateFile + " found");
-					Response hashResponse = CommunicationHandler.getResponse("/service/updates.php?action=ask&file=" + 
+					Response updateFileResponse = CommunicationHandler.getResponse("/service/updates.php?action=ask&file=" + 
 					                                                         EncryptionHandler.encodeBase64(updateFile));
 					
 					//Check if the response is correct
-					if(!hashResponse.wasError() && !hashResponse.getField("md5").Equals("")) {
-						String updateFileHash = hashResponse.getField("md5");;
+					if(!updateFileResponse.wasError() && !updateFileResponse.getField("#md5").Equals("")) {
+						String updateFileHash = updateFileResponse.getField("#md5");;
 						
 						//Check if the MD5 hashes are note equal
 						if(!EncryptionHandler.generateMD5Hash(AppDomain.CurrentDomain.BaseDirectory 
@@ -44,26 +44,36 @@ namespace FOG
 							LogHandler.log(getName(), "Remote file is newer, attempting to update");
 							
 							//Download the new file
-						   	Boolean downloaded = CommunicationHandler.downloadFile("/service/updates.php?action=get&file=" + 
-						   	                                  EncryptionHandler.encodeBase64(updateFile), AppDomain.CurrentDomain.BaseDirectory +
-						   	                                 @"tmp\" + updateFile);
-						   	if(downloaded) {
-								//Try and move the file, if it fails try again for a few times
-								for(int i=0; i < 5; i++) {
-									try {
-										File.Move(AppDomain.CurrentDomain.BaseDirectory + @"tmp\" + updateFile, 
-										          AppDomain.CurrentDomain.BaseDirectory + @"\" + updateFile);
-										this.updatePending = true;		
-										LogHandler.log(getName(), "Successfully updated " + updateFile);
-										break;
-									} catch (Exception ex) {
-										LogHandler.log(getName(), "Unable to replace " + updateFile);
-										LogHandler.log(getName(), "ERROR: " + ex.Message);
+							if(!updateFileResponse.getField("#updatefile").Equals("")) {
+								
+								try {
+									
+									if(File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"tmp\" + updateFile))
+										File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"tmp\" + updateFile);
+									
+									File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"tmp\" + updateFile, EncryptionHandler.decodeBase64(updateFileResponse.getField("#updatefile")));
+									
+									
+									//Try and move the file, if it fails try again for a few times
+									for(int i=0; i < 5; i++) {
+										try {
+											File.Move(AppDomain.CurrentDomain.BaseDirectory + @"tmp\" + updateFile, 
+											          AppDomain.CurrentDomain.BaseDirectory + @"\" + updateFile);
+											this.updatePending = true;		
+											LogHandler.log(getName(), "Successfully updated " + updateFile);
+											break;
+										} catch (Exception ex) {
+											LogHandler.log(getName(), "Unable to replace " + updateFile);
+											LogHandler.log(getName(), "ERROR: " + ex.Message);
+										}
+										if(i < 4) {
+											LogHandler.log(getName(), "Will attempt to update again in 2 seconds");
+											Thread.Sleep(2000);
+										}
 									}
-									if(i < 4) {
-										LogHandler.log(getName(), "Will attempt to update again in 2 seconds");
-										Thread.Sleep(2000);
-									}
+								} catch (Exception ex) {
+									LogHandler.log(getName(), "Unable to create update file");
+									LogHandler.log(getName(), "ERROR: " + ex.Message);
 								}
 
 						   	}
