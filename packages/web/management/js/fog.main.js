52ea6dc187e49410590e17a6b7f35def6479de15
Tom Elliott <tommygunsster@gmail.com>
2014-09-12 03:13:28 +0000

    Made a rookie mistake a while ago, and nobody noticed until I tried adding some newer/better functionality of the Debug selector.  All forms that were contained in organictabs were not being sent outside of their respective form and rather being sent directly, which was happening with the fix I attempted way earlier back (probably around 1500's).  Update the ipxe files to latest pull.  Move the deploy stuff into its own function and call it rather than repeating ourselves.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2275 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog.main.js
+++ packages/web/management/js/fog.main.js
@@ -9,32 +9,6 @@
 // JQuery autoloader
 $(function()
 {
-	$('#scheduleSingle').click(function() {
-		if ($(this).attr('checked')) {
-			$('#scheduleSingleTime').show();
-		}
-		else {
-			$('#scheduleSingleTime').val('');
-		}
-	});
-	$('#isDebugTask').click(function() {
-		if ($(this).attr('checked')) {
-			$('#scheduleInstant').attr('checked',true);
-			$('#scheduleSingleTime').val('');
-			$('#scheduleCronMin').val('');
-			$('#scheduleCronHour').val('');
-			$('#scheduleCronDOM').val('');
-			$('#scheduleCronMonth').val('');
-			$('#scheduleCronDOW').val('');
-			$('#singleOptions').hide();
-			$('#cronOptions').hide();
-			$('.hideFromDebug').hide();
-		}
-		else
-		{
-			$('.hideFromDebug').show();
-		}
-	});
 	// The below elements just performs the randomization techniques.
 	$('#FOG_AES_PASS_ENCRYPT_KEY_button').click(function() {
 		$.ajax({
@@ -461,4 +435,123 @@ function clearIf( ele, value )
 		if ( ele.value == value )
 			ele.value = '';
 	}
+}
+
+function DeployStuff() {
+	// Bind radio buttons for 'Single' and 'Cron' scheduled task
+	$('input[name="scheduleType"]').click(function()
+	{
+		var $this = $(this);
+		var $content = $this.parents('p').parent().find('p').eq($this.parent().index());
+		
+		if ($this.is(':checked'))
+		{
+			$content.slideDown('fast').siblings('.hidden').slideUp('fast');
+		}
+		else
+		{
+			$content.slideDown('fast');
+			$('.calendar').remove();
+			$('.error').removeClass('error');
+		}
+	});
+	// Basic validation on deployment page
+	$('form#deploy-container').submit(function()
+	{
+		var result = true;
+		var scheduleType = $('input[name="scheduleType"]:checked', $(this)).val();
+		var inputsToValidate = $('#' + scheduleType + 'Options > input').removeClass('error');
+	
+		if (scheduleType == 'cron')
+		{
+			inputsToValidate.each(function()
+			{
+				var $min = $('#scheduleCronMin');
+				var $hour = $('#scheduleCronHour');
+				var $dom = $('#scheduleCronDOM');
+				var $month = $('#scheduleCronMonth');
+				var $dow = $('#scheduleCronDOW');
+				
+				// Basic checks
+				if ($min.val() != '*' && ($min.val() == '' || parseInt($min.val(), 10) != $min.val() || $min.val() > 59 || $min.val() < 0))
+				{
+					result = false;
+					$min.addClass('error');
+				}
+				if ($hour.val() != '*' && ($hour.val() == '' || parseInt($hour.val(), 10) != $hour.val() || $hour.val() > 23 || $hour.val() < 0))
+				{
+					result = false;
+					$hour.addClass('error');
+				}
+				if ($dom.val() != '*' && ($dom.val() == '' || parseInt($dom.val(), 10) != $dom.val() || $dom.val() > 31 || $dom.val() < 1))
+				{
+					result = false;
+					$dom.addClass('error');
+				}
+				if ($month.val() != '*' && ($month.val() == '' || parseInt($month.val(), 10) != $month.val() || $month.val() > 12 || $month.val() < 1))
+				{
+					result = false;
+					$month.addClass('error');
+				}
+				if ($dow.val() != '*' && ($dow.val() == '' || parseInt($dow.val(), 10) != $dow.val() || $dow.val() > 6 || $dow.val() < 0))
+				{
+					result = false;
+					$dow.addClass('error');
+				}
+			});
+		}
+		else if (scheduleType == 'single')
+		{
+			// Format check
+			if (!inputsToValidate.val().match(/\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}/))
+			{
+				result = false;
+					
+				inputsToValidate.addClass('error').click();
+			}
+		}
+		
+		return result;
+	});
+	
+	// Fiddle with calendar to make it auto open and close
+	// TODO: Find a better, modern calendar
+	$('#scheduleSingle').click(function()
+	{
+		if ($(this).is(':checked'))
+		{
+			$('#scheduleSingleTime').parent().slideDown('fast', function()
+			{
+				var dayClickRemoveCalendar = function()
+				{
+					$('.daysrow .day').click(function()
+					{
+						$('.calendar').remove();
+					});
+				}
+				
+				$(this)	.children(0)
+					.focus(function()
+					{
+						$(this).blur();
+					})
+					.click(function()
+					{
+						dayClickRemoveCalendar();
+					}).click();
+				
+				dayClickRemoveCalendar();
+			});
+		}
+	});
+	$('#isDebugTask').click(function() {
+		if ($(this).attr('checked')) {
+			$('#scheduleInstant').attr('checked',true);
+			$('.hideFromDebug').hide();
+		}
+		else
+		{
+			$('.hideFromDebug').show();
+		}
+	});
 }
