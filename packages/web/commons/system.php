cf7f1a88d64ec80f52081169db163229ece84739
Tom Elliott <tommygunsster@gmail.com>
2014-09-12 03:13:28 +0000

    Made a rookie mistake a while ago, and nobody noticed until I tried adding some newer/better functionality of the Debug selector.  All forms that were contained in organictabs were not being sent outside of their respective form and rather being sent directly, which was happening with the fix I attempted way earlier back (probably around 1500's).  Update the ipxe files to latest pull.  Move the deploy stuff into its own function and call it rather than repeating ourselves.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2275 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2274');
+		define('FOG_VERSION', '2275');
 		define('FOG_SCHEMA', 127);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
