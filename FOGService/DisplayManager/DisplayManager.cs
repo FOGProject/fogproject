e28fe0de152684b0618cf70c348c22cfcb684b4b
Jbob <jbob182@gmail.com>
2014-09-13 11:43:07 -0500

    Change Display Manager to run in user-scope

--- FOGService/DisplayManager/DisplayManager.cs
+++ FOGService/DisplayManager/DisplayManager.cs
@@ -9,45 +9,46 @@ namespace FOG {
 	/// Change the resolution of the display
 	/// </summary>
 	public class DisplayManager : AbstractModule {
-		private DisplayChanger display;
+		private Display display;
 		
 			
 		public DisplayManager() : base() {
 			setName("DisplayManager");
 			setDescription("hange the resolution of the display");	
-			this.display = new DisplayChanger();
+			this.display = new Display();
 		}
 		
 		protected override void doWork() {
-			//Get task info
-			Response taskResponse = CommunicationHandler.getResponse("/service/displaymanager.php?mac=" + CommunicationHandler.getMacAddresses());
-
-			if(!taskResponse.wasError()) {
-
-				try {
-					int x = int.Parse(taskResponse.getField("#x"));
-					int y = int.Parse(taskResponse.getField("#y"));
-					int r = int.Parse(taskResponse.getField("#r"));
-					changeResolution(x, y, r);
-				} catch (Exception ex) {
-					LogHandler.log(getName(), "ERROR");
-					LogHandler.log(getName(), ex.Message);
+			display.updateSettings();
+			if(display.settingsLoaded()) {
+				//Get task info
+				Response taskResponse = CommunicationHandler.getResponse("/service/displaymanager.php?mac=" + CommunicationHandler.getMacAddresses());
+	
+				if(!taskResponse.wasError()) {
+	
+					try {
+						int x = int.Parse(taskResponse.getField("#x"));
+						int y = int.Parse(taskResponse.getField("#y"));
+						int r = int.Parse(taskResponse.getField("#r"));
+						changeResolution(x, y, r);
+					} catch (Exception ex) {
+						LogHandler.log(getName(), "ERROR");
+						LogHandler.log(getName(), ex.Message);
+					}
 				}
+			} else {
+				LogHandler.log(getName(), "Settings are not populated; will not attempt to change resolution");
 			}
 		}
 		
 		//Change the resolution of the screen
 		private void changeResolution(int width, int height, int refresh) {
-			if(!(width.Equals(Screen.PrimaryScreen.Bounds.Width) && height.Equals(Screen.PrimaryScreen.Bounds.Height))) {
-				LogHandler.log(getName(), "Current Resolution: " + Screen.PrimaryScreen.Bounds.Width.ToString() + " x " + Screen.PrimaryScreen.Bounds.Height.ToString());
-				LogHandler.log(getName(), "Attempting to change resoltution");
+			if(!(width.Equals(display.getSettings().dmPelsWidth) && height.Equals(display.getSettings().dmPelsHeight) && refresh.Equals(display.getSettings().dmDisplayFrequency))) {
+				LogHandler.log(getName(), "Current Resolution: " + display.getSettings().dmPelsWidth.ToString() + " x " + 
+				               display.getSettings().dmPelsHeight.ToString() + " " + display.getSettings().dmDisplayFrequency + "hz");
+				LogHandler.log(getName(), "Attempting to change resoltution to " + width.ToString() + " x " + height.ToString() + " " + refresh.ToString() + "hz");
 				
-				if(display.changeDisplaySettings(width, height, refresh, 0)) {
-					LogHandler.log(getName(), "Success");
-				} else {
-					LogHandler.log(getName(), "Unable to change resolution");
-					LogHandler.log(getName(), "ERROR: " + "unkown");
-				}
+				display.changeResolution(width, height, refresh);
 				
 			} else {
 				LogHandler.log(getName(), "Current resolution is already set correctly");
