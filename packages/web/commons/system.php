ceea59f9a4e24dfb550c5560911f0ae7b6d766e2
Tom Elliott <tommygunsster@gmail.com>
2014-09-13 18:22:40 +0000

    More fixes for the new service stuff.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2280 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2279');
+		define('FOG_VERSION', '2280');
 		define('FOG_SCHEMA', 127);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
