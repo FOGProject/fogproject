61752572b90722b5ba92d9eaa87fe0ae91dd4797
Jbob <jbob182@gmail.com>
2014-09-13 12:37:29 -0500

    Add DirCleaner

--- FOGService/DirCleaner/Properties/AssemblyInfo.cs
+++ FOGService/DirCleaner/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HostRegister")]
+[assembly: AssemblyTitle("DirCleaner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FOG")]
-[assembly: AssemblyProduct("FOG:HostRegister")]
+[assembly: AssemblyProduct("FOG:DirCleaner")]
 [assembly: AssemblyCopyright("Copyright 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
