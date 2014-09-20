8bd1715ecfffc811b3377ceea6b1c0cba32c7913
Tom Elliott <tommygunsster@gmail.com>
2014-09-20 02:30:21 +0000

    Cleanup now that things seem to work.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2318 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -26,7 +26,7 @@ class System
 		else
 			date_default_timezone_set('UTC');
 		define('IS_INCLUDED', true);
-		define('FOG_VERSION', '2317');
+		define('FOG_VERSION', '2318');
 		define('FOG_SCHEMA', 129);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
