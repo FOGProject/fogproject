e4d940b2aedfb0fa155faa2ee0ea5aea047c952b
Tom Elliott <tommygunsster@gmail.com>
2014-09-23 01:05:03 +0000

    Make printers do the same things as images, snapins, hosts, groups, etc...
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2326 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/Printer.class.php
+++ packages/web/lib/fog/Printer.class.php
@@ -23,7 +23,7 @@ class Printer extends FOGController
 	
 	// Allow setting / getting of these additional fields
 	public $additionalFields = array(
-		'default',
+		'hosts',
 	);
 	
 	// Required database fields
@@ -31,4 +31,97 @@ class Printer extends FOGController
 		'id',
 		'name',
 	);
+
+	// Overrides
+	private function loadHosts()
+	{
+		if (!$this->isLoaded('hosts'))
+		{
+			if ($this->get('id'))
+			{
+				$PrinterAssocs = $this->FOGCore->getClass('PrinterAssociationManager')->find(array('printerID' => $this->get('id')));
+				foreach($PrinterAssocs AS $PrinterAssoc)
+					$this->add('hosts', new Host($PrinterAssoc->get('hostID')));
+			}
+		}
+		return $this;
+	}
+
+	public function get($key = '')
+	{
+		if ($this->key($key) == 'hosts')
+			$this->loadHosts();
+		return parent::get($key);
+	}
+
+	public function set($key, $value)
+	{
+		if ($this->key($key) == 'hosts')
+		{
+			foreach((array)$value AS $Host)
+				$newValue[] = ($Host instanceof Host ? $Host : new Host($Host));
+		}
+		// Set
+		return parent::set($key, $value);
+	}
+
+	public function add($key, $value)
+	{
+		if ($this->key($key) == 'hosts' && !($value instanceof Host))
+		{
+			$this->loadHosts();
+			$value = new Host($value);
+		}
+		// Add
+		return parent::add($key, $value);
+	}
+
+	public function remove($key, $object)
+	{
+		if ($this->key($key) == 'hosts')
+			$this->loadHosts();
+		// Remove
+		return parent::remove($key, $object);
+	}
+
+	public function save()
+	{
+		parent::save();
+		if ($this->isLoaded('hosts'))
+		{
+			// Remove all old entries.
+			$this->FOGCore->getClass('PrinterAssociationManager')->destroy(array('printerID' => $this->get('id')));
+			// Create new Assocs
+			foreach((array)$this->get('hosts') AS $Host)
+			{
+				if (($Host instanceof Host) && $Host->isValid())
+				{
+					$NewPrinter = new PrinterAssociation(array(
+						'printerID' => $this->get('id'),
+						'hostID' => $Host->get('id'),
+					));
+					$NewPrinter->save();
+				}
+			}
+		}
+		return $this;
+	}
+
+	public function addHost($addArray)
+	{
+		// Add
+		foreach((array)$addArray AS $item)
+			$this->add('hosts', $item);
+		// Return
+		return $this;
+	}
+
+	public function removeHost($removeArray)
+	{
+		// Iterate array (or other as array)
+		foreach((array)$removeArray AS $remove)
+			$this->remove('hosts', ($remove instanceof Host ? $remove : new Host((int)$remove)));
+		// Return
+		return $this;
+	}
 }
