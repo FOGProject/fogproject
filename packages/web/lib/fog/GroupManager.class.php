b2a2a272118d1282e59a00045111abe53efacccb
Tom Elliott <tommygunsster@gmail.com>
2014-09-23 23:59:59 +0000

    Make Groups search in the same manner host page now searchs.  Make HostMobile search feature the same as HostManagementPage.  Cleanup Report manager finding hosts without the need for the search function, but rather find based on any mac additional or primary.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2332 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/GroupManager.class.php
+++ packages/web/lib/fog/GroupManager.class.php
@@ -4,20 +4,4 @@
 */
 class GroupManager extends FOGManagerController
 {
-	// Search query
-	public $searchQuery = 'SELECT * FROM groups 
-						LEFT OUTER JOIN
-							(SELECT * FROM hosts INNER JOIN groupMembers ON (gmHostID = hostID) WHERE hostName LIKE "%${keyword}%")
-							groupMembers ON (gmGroupID=groupID)
-						WHERE 
-							groupID LIKE "%${keyword}%" OR
-							groupName LIKE "%${keyword}%" OR
-							groupDesc LIKE "%${keyword}%" OR
-							hostID LIKE "%${keyword}%" OR
-							hostName LIKE "%${keyword}" OR
-							hostDesc LIKE "%${keyword}%"
-						GROUP BY
-							groupName
-						ORDER BY
-							groupName';
 }
