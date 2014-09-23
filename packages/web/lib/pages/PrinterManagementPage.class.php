d88f72966f0a3cdf8eb39654ad482aa99bad8740
Tom Elliott <tommygunsster@gmail.com>
2014-09-23 01:05:03 +0000

    Make printers do the same things as images, snapins, hosts, groups, etc...
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2326 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/pages/PrinterManagementPage.class.php
+++ packages/web/lib/pages/PrinterManagementPage.class.php
@@ -303,6 +303,7 @@ class PrinterManagementPage extends FOGPage
 		$Printer = new Printer($this->request['id']);
 		// Title
 		$this->title = sprintf('%s: %s', 'Edit', $Printer->get('name'));
+		print "\n\t\t\t".'<div id="tab-container">';
 		// Header Data
 		unset($this->headerData);
 		// Attributes
@@ -315,11 +316,13 @@ class PrinterManagementPage extends FOGPage
 			'${field}',
 			'${input}',
 		);
-		if(!isset($_REQUEST['printertype']))
+		// Output
+		print "\n\t\t\t<!-- General -->";
+		print "\n\t\t\t".'<div id="printer-gen">';
+		if (!$_REQUEST['printertype'])
 			$_REQUEST['printertype'] = $Printer->get('config');
-		if(!isset($_REQUEST['printertype']))
-			$_REQUEST['printertype'] = "Local";
-		print "\n\t\t\t".'<form id="printerform" action="?node='.$_REQUEST['node'].'&sub='.$_REQUEST['sub'].'&id='.$_REQUEST['id'].'" method="post" >';
+		if (!$_REQUEST['printertype'])
+			$_REQUEST['printertype'] = 'Local';
 		$printerTypes = array(
 			'Local' => _('Local Printer'),
 			'iPrint' => _('iPrint Printer'),
@@ -327,13 +330,12 @@ class PrinterManagementPage extends FOGPage
 		);
 		foreach ((array)$printerTypes AS $short => $long)
 			$optionPrinter .= "\n\t\t\t\t".'<option value="'.$short.'" '.($_REQUEST['printertype'] == $short ? 'selected="selected"' : '').'>'.$long.'</option>';
-		print "\n\t\t\t".'<select name="printertype" onchange="this.form.submit()">'.$optionPrinter."\n\t\t\t</select>";
-		print "\n\t\t\t</form>";
 		if ($_REQUEST['printertype'] == 'Network')
 		{
 			$fields = array(
 				_('Printer Alias').'*' => '<input type="text" name="alias" value="${printer_name}" />',
 				'e.g. '.addslashes('\\\\printerserver\\printername') => '&nbsp;',
+				'<input type="hidden" name="update" value="1" />' => '&nbsp;',
 			);
 		}
 		if ($_REQUEST['printertype'] == 'iPrint')
@@ -353,7 +355,7 @@ class PrinterManagementPage extends FOGPage
 				_('Printer IP (optional)') => '<input type="text" name="ip" value="${printer_ip}" />',
 			);
 		}
-		$fields['<input type="hidden" name="printertype" value="'.$_REQUEST['printertype'].'" />'] = '<input type="hidden" name="update" value="1" /><input type="submit" value="'._('Update Printer').'" />';
+		$fields['<input type="hidden" name="printertype" value="'.$_REQUEST['printertype'].'" />'] = '<input type="submit" value="'._('Update Printer').'" />';
 		foreach((array)$fields AS $field => $input)
 		{
 			$this->data[] = array(
@@ -366,12 +368,171 @@ class PrinterManagementPage extends FOGPage
 				'printer_ip' => $Printer->get('ip'),
 			);
 		}
-		print "\n\t\t\t".'<form method="post" action="'.$this->formAction.'">';
 		// Hook
 		$this->HookManager->processEvent('PRINTER_EDIT', array('headerData' => &$this->headerData, 'data' => &$this->data, 'templates' => &$this->templates, 'attributes' => &$this->attributes));
-		// Output
+		print "\n\t\t\t".'<form method="post" action="'.$this->formAction.'&tab=printer-type">';
+		print "\n\t\t\t".'<select name="printertype" onchange="this.form.submit()">'.$optionPrinter."\n\t\t\t</select>";
+		print "\n\t\t\t</form>";
+		print "\n\t\t\t".'<form method="post" action="'.$this->formAction.'&tab=printer-gen">';
 		$this->render();
-		print "\n\t\t\t".'</form>';
+		print '</form>';
+		print "\n\t\t\t</div>";
+		unset($this->data);
+		print "\n\t\t\t".'<!-- Hosts with this printer -->';
+		$HostMan = new HostManager();
+		// Get Hosts with this printer assigned
+		foreach((array)$Printer->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+				$HostsWithMe[] = $Host->get('id');
+		}
+		// Get all Host IDs with a printer assigned
+		foreach($this->FOGCore->getClass('PrinterAssociationManager')->find() AS $PrinterAssoc)
+		{
+			if ($PrinterAssoc && $PrinterAssoc->isValid())
+				$HostsWithPrinter[] = $Host->get('id');
+		}
+		// Set the values
+		foreach($HostMan->find() AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				if (!in_array($Host->get('id'),(array)$HostsWithPrinter))
+					$HostNotWithPrinter[] = $Host;
+				if (!in_array($Host->get('id'),(array)$HostsWithMe))
+					$HostNotWithMe[] = $Host;
+			}
+		}
+		print "\n\t\t\t".'<div id="printer-host">';
+		// Create the header data:
+		$this->headerData = array(
+			'',
+			'<input type="checkbox" name="toggle-checkboxprinter1" class="toggle-checkbox1" />',
+			($_SESSION['FOGPingActive'] ? '' : null),
+			_('Host Name'),
+			_('Last Deployed'),
+			_('Registered'),
+		);
+		// Create the template data:
+		$this->templates = array(
+			'<span class="icon icon-help hand" title="${host_desc}"></span>',
+			'<input type="checkbox" name="host[]" value="${host_id}" class="toggle-host${check_num}" />',
+			($_SESSION['FOGPingActive'] ? '<span class="icon ping"></span>' : ''),
+			'<a href="?node=host&sub=edit&id=${host_id}" title="Edit: ${host_name} Was last deployed: ${deployed}">${host_name}</a><br /><small>${host_mac}</small>',
+			'${deployed}',
+			'${host_reg}',
+		);
+		// All hosts not with this set as the image
+		$this->attributes = array(
+			array('width' => 22, 'id' => 'host-${host_name}'),
+			array('class' => 'c', 'width' => 16),
+			($_SESSION['FOGPingActive'] ? array('width' => 20) : ''),
+			array(),
+			array(),
+			array(),
+		);
+		// All hosts not with this printer
+		foreach((array)$HostNotWithMe AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				$this->data[] = array(
+					'host_id' => $Host->get('id'),
+					'deployed' => $this->validDate($Host->get('deployed')) ? $this->formatTime($Host->get('deployed')) : 'No Data',
+					'host_name' => $Host->get('name'),
+					'host_mac' => $Host->get('mac')->__toString(),
+					'host_desc' => $Host->get('description'),
+					'check_num' => '1',
+					'host_reg' => $Host->get('pending') ? _('Pending Approval') : _('Approved'),
+				);
+			}
+		}
+		$PrinterDataExists = false;
+		if (count($this->data) > 0)
+		{
+			$PrinterDataExists = true;
+			$this->HookManager->processEvent('PRINTER_HOST_ASSOC',array('headerData' => &$this->headerData,'data' => &$this->data,'templates' => &$this->templates,'attributes' => &$this->attributes));
+			print "\n\t\t\t<center>"._('Check here to see hosts not assigned with this printer').'&nbsp;&nbsp;<input type="checkbox" name="hostMeShow" id="hostMeShow" />';
+			print "\n\t\t\t".'<form method="post" action="'.$this->formAction.'&tab=printer-host">';
+			print "\n\t\t\t".'<div id="hostNotInMe">';
+			print "\n\t\t\t".'<h2>'._('Modify printer association for').' '.$Printer->get('name').'</h2>';
+			print "\n\t\t\t".'<p>'._('Add hosts to printer').' '.$Printer->get('name').'</p>';
+			$this->render();
+			print "</div>";
+		}
+		// Reset the data for the next value
+		unset($this->data);
+		// Create the header data:
+		$this->headerData = array(
+			'',
+			'<input type="checkbox" name="toggle-checkboxprinter2" class="toggle-checkbox2" />',
+			($_SESSION['FOGPingActive'] ? '' : null),
+			_('Host Name'),
+			_('Last Deployed'),
+			_('Registered'),
+		);
+		// All hosts not with any printer
+		foreach((array)$HostNotWithMe AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				$this->data[] = array(
+					'host_id' => $Host->get('id'),
+					'deployed' => $this->validDate($Host->get('deployed')) ? $this->formatTime($Host->get('deployed')) : 'No Data',
+					'host_name' => $Host->get('name'),
+					'host_mac' => $Host->get('mac')->__toString(),
+					'host_desc' => $Host->get('description'),
+					'check_num' => '2',
+					'host_reg' => $Host->get('pending') ? _('Pending Approval') : _('Approved'),
+				);
+			}
+		}
+		if (count($this->data) > 0)
+		{
+			$PrinterDataExists = true;
+			$this->HookManager->processEvent('PRINTER_HOST_NOT_WITH_ANY',array('headerData' => &$this->headerData,'data' => &$this->data,'templates' => &$this->templates,'attributes' => &$this->attributes));
+			print "\n\t\t\t<center>"._('Check here to see hosts with no printers').'&nbsp;&nbsp;<input type="checkbox" name="hostNoShow" id="hostNoShow" />';
+			print "\n\t\t\t".'<form method="post" action="'.$this->formAction.'&tab=printer-host">';
+			print "\n\t\t\t".'<div id="hostNoPrinter">';
+			print "\n\t\t\t".'<p>'._('Hosts below have no printer associations').'</h2>';
+			print "\n\t\t\t".'<p>'._('Add hosts with printer').' '.$Printer->get('name').'</p>';
+			$this->render();
+			print "</div>";
+		}
+		if ($PrinterDataExists)
+		{
+			print '</br><input type="submit" value="'._('Add Printer to Host(s)').'" />';
+			print "\n\t\t\t</form></center>";
+		}
+		unset($this->data);
+		array_push($this->headerData,_('Remove Printer'));
+		array_push($this->templates,'<input type="checkbox" class="delid" onclick="this.form.submit()" name="hostdel" id="hostdelmem${host_id}" value="${host_id}" /><label for="hostdelmem${host_id}">'.$this->foglang['Delete']);
+		array_push($this->attributes,array());
+		array_splice($this->headerData,1,1);
+		array_splice($this->templates,1,1);
+		array_splice($this->attributes,1,1);
+		foreach((array)$Printer->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				$this->data[] = array(
+					'host_id' => $Host->get('id'),
+					'deployed' => $this->validDate($Host->get('deployed')) ? $this->FOGCore->formatTime($Host->get('deployed')) : 'No Data',
+					'host_name' => $Host->get('name'),
+					'host_mac' => $Host->get('mac')->__toString(),
+					'host_desc' => $Host->get('description'),
+					'host_reg' => $Host->get('pending') ? _('Pending Approval') : _('Approved'),
+				);
+			}
+		}
+		// Hook
+		$this->HookManager->processEvent('PRINTER_EDIT_HOST', array('headerData' => &$this->headerData,'data' => &$this->data,'templates' => &$this->templates,'attributes' => &$this->attributes));
+		// Output
+		print "\n\t\t\t\t".'<form method="post" action="'.$this->formAction.'&tab=printer-host">';
+		$this->render();
+		print '</form>';
+		print "\n\t\t\t\t</div>";
+		print "\n\t\t\t</div>";
 	}
 	public function edit_post()
 	{
@@ -380,90 +541,92 @@ class PrinterManagementPage extends FOGPage
 		// Hook
 		$this->HookManager->processEvent('PRINTER_EDIT_POST', array('Printer' => &$Printer));
 		// POST
-		if ($_REQUEST['update'] != 1)
+		try
 		{
-			$this->FOGCore->setMessage('Printer type changed to: '.$_REQUEST['printertype'].'.');
-			$this->FOGCore->redirect('?node=printer&sub=edit&id='.$Printer->get('id').'&printertype='.$_REQUEST['printertype']);
-		}
-		if ($_REQUEST['update'] == 1)
-		{
-			//Remove beginning and trailing spaces
-			$_REQUEST['alias'] = trim($_REQUEST['alias']);
-			$_REQUEST['port'] = trim($_REQUEST['port']);
-			$_REQUEST['inf'] = trim($_REQUEST['inf']);
-			$_REQUEST['model'] = trim($_REQUEST['model']);
-			$_REQUEST['ip'] = trim($_REQUEST['ip']);
-			try
+			switch ($_REQUEST['tab'])
 			{
-				// PrinterManager
-				$PrinterManager = $this->FOGCore->getClass('PrinterManager');
-				//Error Checking
-				if($_REQUEST['printertype'] == "Local")
-				{
-					if(empty($_REQUEST['alias'])||empty($_REQUEST['port'])||empty($_REQUEST['inf'])||empty($_REQUEST['model']))
-						throw new Exception('You must specify the alias, port, model, and inf. Unable to update!');
-					else
-					{
-						//Update Object
-						$Printer ->set('name',		$_REQUEST['alias'])
-								 ->set('config',	$_REQUEST['printertype'])
-								 ->set('model',		$_REQUEST['model'])
-								 ->set('port',		$_REQUEST['port'])
-								 ->set('file',		$_REQUEST['inf'])
-								 ->set('ip',		$_REQUEST['ip']);
-					}
-				}
-				if($_REQUEST['printertype'] == "iPrint")
-				{
-					if(empty($_REQUEST['alias'])||empty($_REQUEST['port']))
-						throw new Exception('You must specify the alias and port. Unable to update!');
-					else
-					{
-						//Update Object
-						$Printer ->set('name',		$_REQUEST['alias'])
-								 ->set('config',	$_REQUEST['printertype'])
-								 ->set('port',		$_REQUEST['port']);
-					}
-				}
-				if($_REQUEST['printertype'] == "Network")
-				{
-					if(empty($_REQUEST['alias']))
-						throw new Exception('You must specify the alias. Unable to update!');
-					else
-					{
-						//Update Object
-						$Printer ->set('name',		$_REQUEST['alias'])
-								 ->set('config',	$_REQUEST['printertype']);
-					}
-				}
-				if ($PrinterManager->exists($_REQUEST['alias'], $Printer->get('id')))
-					throw new Exception('Printer name already exists, please choose another');
-				// Save
-				if ($Printer->save())
-				{
-					// Hook
-					$this->HookManager->processEvent('PRINTER_UPDATE_SUCCESS', array('Printer' => &$Printer));
-					// Log History event
-					$this->FOGCore->logHistory(sprintf('%s: ID: %s, Name: %s', _('Printer updated'), $Printer->get('id'), $Printer->get('name')));
-					// Set session message
-					$this->FOGCore->setMessage('Printer updated!');
-					// Redirect for user
+				// Switch the printer type
+				case 'printer-type';
+					$this->FOGCore->setMessage('Printer type changed to: '.$_REQUEST['printertype']);
 					$this->FOGCore->redirect('?node=printer&sub=edit&id='.$Printer->get('id'));
-				}
-					else
-						throw new Exception('Printer update failed!');
+				case 'printer-gen';
+					//Remove beginning and trailing spaces
+					$_REQUEST['alias'] = trim($_REQUEST['alias']);
+					$_REQUEST['port'] = trim($_REQUEST['port']);
+					$_REQUEST['inf'] = trim($_REQUEST['inf']);
+					$_REQUEST['model'] = trim($_REQUEST['model']);
+					$_REQUEST['ip'] = trim($_REQUEST['ip']);
+					// Printer Manager
+					$PrinterManager = new PrinterManager();
+					if ($_REQUEST['printertype'] == 'Local')
+					{
+						if (!$_REQUEST['alias'] || !$_REQUEST['port'] || !$_REQUEST['inf'] || !$_REQUEST['model'])
+							throw new Exception(_('You must specify the alias, port, model, and inf'));
+						else
+						{
+							// Update Object
+							$Printer->set('name',$_REQUEST['alias'])
+									->set('config',$_REQUEST['printertype'])
+									->set('model',$_REQUEST['model'])
+									->set('port',$_REQUEST['port'])
+									->set('file',$_REQUEST['inf'])
+									->set('ip',$_REQUEST['ip']);
+						}
+					}
+					if ($_REQUEST['printertype'] == 'iPrint')
+					{
+						if (!$_REQUEST['alias'] || !$_REQUEST['port'])
+							throw new Exception(_('You must specify the alias and port'));
+						else
+						{
+							$Printer->set('name',$_REQUEST['alias'])
+									->set('config',$_REQUEST['printertype'])
+									->set('port',$_REQUEST['port']);
+						}
+					}
+					if ($_REQUEST['printertype'] == 'Network')
+					{
+						if (!$_REQUEST['alias'])
+							throw new Exception(_('You must specify the alias'));
+						else
+							$Printer->set('name',$_REQUEST['alias'])
+									->set('config',$_REQUEST['printertype']);
+					}
+					if ($PrinterManager->exists($_REQUEST['alias']))
+						throw new Exception(_('Printer name already exists, please choose another'));
+				break;
+				case 'printer-host';
+					if ($_REQUEST['host'])
+						$Printer->addHost($_REQUEST['host']);
+					if ($_REQUEST['hostdel'])
+						$Printer->removeHost($_REQUEST['hostdel']);
+				break;
 			}
-			catch (Exception $e)
+			// Save
+			if ($Printer->save())
 			{
 				// Hook
-				$this->HookManager->processEvent('PRINTER_UPDATE_FAIL', array('Printer' => &$Printer));
+				$this->HookManager->processEvent('PRINTER_UPDATE_SUCCESS', array('Printer' => &$Printer));
 				// Log History event
-				$this->FOGCore->logHistory(sprintf('%s update failed: Name: %s, Error: %s', _('Printer'), $_REQUEST['alias'], $e->getMessage()));
+				$this->FOGCore->logHistory(sprintf('%s: ID: %s, Name: %s', _('Printer updated'), $Printer->get('id'), $Printer->get('name')));
 				// Set session message
-				$this->FOGCore->setMessage($e->getMessage());			
-				// Redirect to new entry
-				$this->FOGCore->redirect($this->formAction);
+				$this->FOGCore->setMessage('Printer updated!');
+				// Redirect for user
+				$this->FOGCore->redirect('?node=printer&sub=edit&id='.$Printer->get('id').'#'.$_REQUEST['tab']);
 			}
+			else
+				throw new Exception('Printer update failed!');
+		}
+		catch (Exception $e)
+		{
+			// Hook
+			$this->HookManager->processEvent('PRINTER_UPDATE_FAIL', array('Printer' => &$Printer));
+			// Log History event
+			$this->FOGCore->logHistory(sprintf('%s update failed: Name: %s, Error: %s', _('Printer'), $_REQUEST['alias'], $e->getMessage()));
+			// Set session message
+			$this->FOGCore->setMessage($e->getMessage());			
+			// Redirect to new entry
+			$this->FOGCore->redirect($this->formAction);
 		}
 	}
 	public function delete()
