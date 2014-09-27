fdb240dd93f78f8f2f127c1163b086d8547bbe69
Tom Elliott <tommygunsster@gmail.com>
2014-09-27 21:37:22 +0000

    Fix the methodology to get the date when passing comma strings.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2346 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGCron.class.php
+++ packages/web/lib/fog/FOGCron.class.php
@@ -44,11 +44,9 @@ class FOGCron extends FOGGetSet
 			$step = empty($vvv[1]) ? 1 : $vvv[1];
 			$vvvv = explode('-',$vvv[0]);
 			$_min = count($vvvv) == 2 ? $vvvv[0] : ($vvv[0] == '*' ? $min : $vvv[0]);
-			$_max = count($vvvv) == 2 ? $vvvv[1] : ($vvv[0] == '*' ? $max : $vvv[1]);
+			$_max = count($vvvv) == 2 ? $vvvv[1] : ($vvv[0] == '*' ? $max : $vvv[0]);
 			for ($i = $_min; $i <= $_max; $i += $step)
-			{
 				$result[$i] = intval($i);
-			}
 		}
 		ksort($result);
 		return $result;
