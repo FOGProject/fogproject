96021ca58f8dd6a12866ccbf49cf8f4bff75d5e1
Tom Elliott <tommygunsster@gmail.com>
2014-09-28 15:25:24 +0000

    Fix weird issue with debug task displaying funny on close or reopen.  Remove unneeded try/catch in fogcron.  Add more hideFromDebug clauses.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2348 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGCron.class.php
+++ packages/web/lib/fog/FOGCron.class.php
@@ -3,40 +3,35 @@ class FOGCron extends FOGGetSet
 {
 	public static function parse($Cron,$TimeStamp = null)
 	{
-		try{
-			if (!preg_match('/^((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)$/i',trim($Cron)))
-				throw new Exception("Invalid cron string: ".$Cron);
-			if ($TimeStamp && !is_numeric($TimeStamp))
-				throw new Exception("Invalid timestamp passed: ".$TimeStamp);
-			$Cron = preg_split("/[\s]+/i",trim($Cron));
-			$Start = empty($TimeStamp) ? time() : $TimeStamp;
-			$date = array(
-				'minutes' => self::_parseCronNumbers($Cron[0],0,59),
-				'hours' => self::_parseCronNumbers($Cron[1],0,23),
-				'dom' => self::_parseCronNumbers($Cron[2],1,31),
-				'month' => self::_parseCronNumbers($Cron[3],1,12),
-				'dow' => self::_parseCronNumbers($Cron[4],1,7),
-			);
-			for ($i = 0; $i <= (60*60*24*366); $i += 60)
-			{
-				$dom = in_array(intval(date('j',$Start+$i)),$date['dom']);
-				$month = in_array(intval(date('n',$Start+$i)),$date['month']);
-				$dow = in_array(intval(date('N',$Start+$i)),$date['dow']);
-				$hours = in_array(intval(date('H',$Start+$i)),$date['hours']);
-				$minutes = in_array(intval(date('i',$Start+$i)),$date['minutes']);
-				if ($dom && $month && $dow && $hours && $minutes)
-					return $Start+$i;
-			}
-		}
-		catch (Exception $e)
+		if (!preg_match('/^((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)$/i',trim($Cron)))
+			throw new Exception("Invalid cron string: ".$Cron);
+		if ($TimeStamp && !is_numeric($TimeStamp))
+			throw new Exception("Invalid timestamp passed: ".$TimeStamp);
+		$Cron = preg_split("/[\s]+/i",trim($Cron));
+		$Start = empty($TimeStamp) ? time() : $TimeStamp;
+		$date = array(
+			'minutes' => self::_parseCronNumbers($Cron[0],0,59),
+			'hours' => self::_parseCronNumbers($Cron[1],0,23),
+			'dom' => self::_parseCronNumbers($Cron[2],1,31),
+			'month' => self::_parseCronNumbers($Cron[3],1,12),
+			'dow' => self::_parseCronNumbers($Cron[4],1,7),
+		);
+		for ($i = 0; $i <= (60*60*24*366); $i += 60)
 		{
+			$dom = in_array(intval(date('j',$Start+$i)),$date['dom']);
+			$month = in_array(intval(date('n',$Start+$i)),$date['month']);
+			$dow = in_array(intval(date('N',$Start+$i)),$date['dow']);
+			$hours = in_array(intval(date('H',$Start+$i)),$date['hours']);
+			$minutes = in_array(intval(date('i',$Start+$i)),$date['minutes']);
+			if ($dom && $month && $dow && $hours && $minutes)
+				return $Start+$i;
 		}
 		return false;
 	}
 
 	protected static function _parseCronNumbers($s,$min,$max)
 	{
-		$result = array();
+		$result = null;
 		$v = explode(',',$s);
 		foreach($v AS $vv)
 		{
@@ -46,7 +41,7 @@ class FOGCron extends FOGGetSet
 			$_min = count($vvvv) == 2 ? $vvvv[0] : ($vvv[0] == '*' ? $min : $vvv[0]);
 			$_max = count($vvvv) == 2 ? $vvvv[1] : ($vvv[0] == '*' ? $max : $vvv[0]);
 			for ($i = $_min; $i <= $_max; $i += $step)
-				$result[$i] = intval($i);
+				$result[] = intval($i);
 		}
 		ksort($result);
 		return $result;
