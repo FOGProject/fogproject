3fe5a79b13a54ec54237153f23d10db33b01e926
Tom Elliott <tommygunsster@gmail.com>
2014-10-05 18:59:50 +0000

    Dynamically attempt to receive the events rather than trying to get the events pre-assigned.  Allows for more dynamics I think in that you can simply specify your new hook event rather than having to keep track of which are which.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2383 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/HookManager.class.php
+++ packages/web/lib/fog/HookManager.class.php
@@ -10,208 +10,27 @@ class HookManager extends FOGBase
 {
 	public $logLevel = 0;
 	private $data;
-	public $events = array(
-		// FOG Configuration Hooks
-		'PXE_BOOT_MENU',
-		'CLIENT_UPDATE',
-		'IMPORT',
-		'IMPORT_POST',
-		// Group Hooks
-		'GROUP_DATA',
-		'GROUP_SEARCH',
-		'GROUP_ADD',
-		'GROUP_ADD_POST',
-		'GROUP_ADD_SUCCESS',
-		'GROUP_ADD_FAIL',
-		'GROUP_DATA_GEN',
-		'GROUP_DATA_TASKS',
-		'GROUP_DATA_ADV',
-		'GROUP_HOST_NOT_IN_ME',
-		'GROUP_HOST_NOT_IN_ANY',
-		'GROUP_MEMBERSHIP',
-		'GROUP_IMAGE',
-		'GROUP_SNAP_ADD',
-		'GROUP_SNAP_DEL',
-		'GROUP_MODULES',
-		'GROUP_DISPLAY',
-		'GROUP_ALO',
-		'GROUP_EDIT_AD',
-		'GROUP_ADD_PRINTER',
-		'GROUP_REM_PRINTER',
-		'GROUP_EDIT_POST',
-		'GROUP_EDIT_SUCCESS',
-		'GROUP_EDIT_FAIL',
-		'GROUP_DELETE',
-		'GROUP_DELETE_POST',
-		'GROUP_DELETE_HOST_FORM',
-		'GROUP_DELETE_SUCCESS',
-		'GROUP_DELETE_FAIL',
-		'GROUP_DEPLOY',
-		// Host Hook Events
-		'HOST_DATA',
-		'HOST_HEADER_DATA',
-		'HOST_ADD_GEN',
-		'HOST_ADD_AD',
-		'HOST_ADD_POST',
-		'HOST_ADD_SUCCESS',
-		'HOST_ADD_FAIL',
-		'HOST_EDIT_GEN',
-		'HOST_GROUP_JOIN',
-		'HOST_EDIT_GROUP',
-		'HOST_EDIT_TASKS',
-		'HOST_EDIT_ADV',
-		'HOST_EDIT_AD',
-		'HOST_ADD_PRINTER',
-		'HOST_EDIT_PRINTER',
-		'HOST_SNAPIN_JOIN',
-		'HOST_EDIT_SERVICE',
-		'HOST_EDIT_DISPSERV',
-		'HOST_EDIT_ALO',
-		'HOST_INVENTORY',
-		'HOST_VIRUS',
-		'HOST_IMAGE_HIST',
-		'HOST_SNAPIN_HIST',
-		'HOST_USER_LOGIN',
-		'HOST_EDIT_POST',
-		'HOST_EDIT_SUCCESS',
-		'HOST_EDIT_FAIL',
-		'HOST_DEL',
-		'HOST_DEL_POST',
-		'HOST_DELETE_SUCCESS',
-		'HOST_DELETE_FAIL',
-		'HOST_IMPORT',
-		'HOST_EXPORT',
-		// Host Mobile Hooks
-		'HOST_MOBILE_SEARCH',
-		'HOST_MOBILE_DATA',
-		// Image Hooks
-		'IMAGE_DATA',
-		'IMAGE_ADD',
-		'IMAGE_ADD_POST',
-		'IMAGE_ADD_SUCCESS',
-		'IMAGE_ADD_FAIL',
-		'IMAGE_HOST_ASSOC',
-		'IMAGE_HOST_NOT_WITH_ANY',
-		'IMAGE_EDIT',
-		'IMAGE_EDIT_HOST',
-		'IMAGE_EDIT_POST',
-		'IMAGE_UPDATE_SUCCESS',
-		'IMAGE_UPDATE_FAIL',
-		'IMAGE_DELETE',
-		'IMAGE_DELETE_POST',
-		'IMAGE_DELETE_SUCCESS',
-		'IMAGE_DELETE_FAIL',
-		// Plugin Hooks
-		'PLUGIN_DATA', // TODO: Actually name the hooks something else.
-		// Printer Hooks
-		'PRINTER_DATA',
-		'PRINTER_SEARCH',
-		'PRINTER_ADD',
-		'PRINTER_ADD_POST',
-		'PRINTER_ADD_SUCCESS',
-		'PRINTER_ADD_FAIL',
-		'PRINTER_EDIT',
-		'PRINTER_EDIT_POST',
-		'PRINTER_UPDATE_SUCCESS',
-		'PRINTER_UPDATE_FAIL',
-		'PRINTER_DELETE',
-		'PRINTER_DELETE_POST',
-		'PRINTER_DELETE_SUCCESS',
-		'PRINTER_DELETE_FAIL',
-		// Report Page Hooks TODO
-		// ServerInfo Hooks
-		'SERVER_INFO_DISP',
-		// Service Page Hooks TODO
-		'SERVICE_EDIT_POST',
-		'SERVICE_EDIT_SUCCESS',
-		'SERVICE_EDIT_FAIL',
-		// Snapin Page Hooks
-		'SNAPIN_DATA',
-		'SNAPIN_SEARCH',
-		'SNAPIN_ADD',
-		'SNAPIN_ADD_POST',
-		'SNAPIN_ADD_SUCCESS',
-		'SNAPIN_ADD_FAIL',
-		'SNAPIN_EDIT',
-		'SNAPIN_EDIT_HOST',
-		'SNAPIN_EDIT_POST',
-		'SNAPIN_UPDATE_SUCCESS',
-		'SNAPIN_UPDATE_FAIL',
-		'SNAPIN_DELETE',
-		'SNAPIN_DELETE_POST',
-		'SNAPIN_DELETE_SUCCESS',
-		'SNAPIN_DELETE_FAIL',
-		// Storage Page Hooks
-		'STORAGE_NODE_DATA',
-		'STORAGE_NODE_ADD',
-		'STORAGE_NODE_ADD_POST',
-		'STORAGE_NODE_ADD_SUCCESS',
-		'STORAGE_NODE_ADD_FAIL',
-		'STORAGE_NODE_EDIT',
-		'STORAGE_NODE_EDIT_POST',
-		'STORAGE_NODE_EDIT_SUCCESS',
-		'STORAGE_NODE_EDIT_FAIL',
-		'STORAGE_NODE_DELETE',
-		'STORAGE_NODE_DELETE_POST',
-		'STORAGE_NODE_DELETE_SUCCESS',
-		'STORAGE_NODE_DELETE_FAIL',
-		'STORAGE_GROUP_DATA',
-		'STORAGE_GROUP_ADD',
-		'STORAGE_GROUP_ADD_POST',
-		'STORAGE_GROUP_ADD_POST_SUCCESS',
-		'STORAGE_GROUP_ADD_POST_FAIL',
-		'STORAGE_GROUP_EDIT',
-		'STORAGE_GROUP_EDIT_POST',
-		'STORAGE_GROUP_EDIT_POST_SUCCESS',
-		'STORAGE_GROUP_EDIT_POST_FAIL',
-		'STORAGE_GROUP_DELETE',
-		'STORAGE_GROUP_DELETE_POST',
-		'STORAGE_GROUP_DELETE_POST_SUCCESS',
-		'STORAGE_GROUP_DELETE_POST_FAIL',
-		// Task Page Hooks
-		'TASK_DATA',
-		'TasksListGroupData',
-		'TASK_FORCE',
-		'TASK_CANCEL',
-		'TaskActiveMulticastData',
-		'TaskActiveSnapinsData',
-		'TaskScheduleData',
-		'TaskScheduledRemove',
-		'TaskScheduledRemoveSuccess',
-		'TaskScheduledRemoveFail',
-		// Task Mobile Page Hooks TODO
-		// User Page Hooks
-		'USER_DATA',
-		'USER_SEARCH',
-		'USER_ADD',
-		'USER_ADD_POST',
-		'USER_ADD_SUCCESS',
-		'USER_ADD_FAIL',
-		'USER_EDIT',
-		'USER_EDIT_POST',
-		'USER_UPDATE_SUCCESS',
-		'USER_UPDATE_FAIL',
-		'USER_DELETE',
-		'USER_DELETE_POST',
-		'USER_DELETE_SUCCESS',
-		'USER_DELETE_FAIL',
-		// FOGCore getMessages Hook
-		'MessageBox',
-		// Mainmenu Data Hook
-		'MAIN_MENU_DATA',
-		// Submenu data Hook
-		'SUB_MENULINK_DATA',
-		// ProcessLogin Login Hooks
-		'LoginSuccess',
-		'LoginFail',
-		'Login',
-		// Main Index page Hooks
-		'LOGOUT',
-		'CONTENT_DISPLAY',
-		'CSS',
-		'SubMenuData',
-		'JAVASCRIPT',
-	);
+	public function __construct()
+	{
+		$dir = new RecursiveDirectoryIterator(BASEPATH,FilesystemIterator::SKIP_DOTS);
+		$Iterator = new RecursiveIteratorIterator($dir);
+		$regexp = '#processEvent\([\'\"](.*?)[\'\"]\,#';
+		foreach($Iterator AS $file)
+			preg_match_all($regexp,file_get_contents($file),$matches[]);
+		$matches = $this->array_filter_recursive($matches);
+		foreach($matches AS $match => $value)
+		{
+			if ($matches[$match][1])
+				$matching[] = $matches[$match][1];
+		}
+		foreach($matching AS $ind => $arr)
+		{
+			foreach($arr AS $val)
+				$this->events[] = $val;
+		}
+		$this->events = array_unique($this->events);
+		return parent::__construct();
+	}
 	public function register($event, $function)
 	{
 		try
