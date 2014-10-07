4d956c5048fcc10f9c350b8a0cffc825fedd55af
Tom Elliott <tommygunsster@gmail.com>
2014-10-07 22:18:17 +0000

    Better methods for saving the host data I think.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2404 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/pages/GroupManagementPage.class.php
+++ packages/web/lib/pages/GroupManagementPage.class.php
@@ -828,7 +828,7 @@ class GroupManagementPage extends FOGPage
 		// Group Edit 
 		try
 		{
-			switch($this->REQUEST['tab'])
+			switch($_REQUEST['tab'])
 			{
 				// Group Main Edit
 				case 'group-general';
@@ -894,80 +894,31 @@ class GroupManagementPage extends FOGPage
 				// Image Association
 				case 'group-image';
 					// Error Checking
-					if (empty($_REQUEST['image']))
+					if ($_REQUEST['image'])
 						throw new Exception('Select an Image');
 					else
-					{
-						foreach ((array)$Group->get('hosts') AS $Host)
-						{
-							if ($Host && $Host->isValid())
-							{
-								$Task = current($this->getClass('TaskManager')->find(array('hostID' => $Host->get('id'),'stateID' => array(1,2,3))));
-								if ($Task && $Task->isValid() && !$_REQUEST['image'])
-									throw new Exception('Cannot unset image.<br />Host is currently in a tasking.');
-								else
-									$Host->set('imageID', $this->REQUEST['image']);
-								$Host->save();
-							}
-						}
-					}
+						$Group->addImage($_REQUEST['image']);
 				break;
 				// Snapin Add
 				case 'group-snap-add';
-					foreach((array)$Group->get('hosts') AS $Host)
-					{
-						if ($Host && $Host->isValid())
-							$Host->addSnapin($_REQUEST['snapin'])->save();
-					}
+					$Group->addSnapin($_REQUEST['snapin']);
 				break;
 				// Snapin Del
 				case 'group-snap-del';
-					foreach((array)$Group->get('hosts') AS $Host)
-					{
-						if ($Host && $Host->isValid())
-							$Host->removeSnapin($_REQUEST['snapin'])->save();
-					}
+					$Group->removeSnapin($_REQUEST['snapin']);
 				break;
 				// Active Directory
 				case 'group-active-directory';
-					foreach ((array)$Group->get('hosts') AS $Host)
-					{
-						if ($this->FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['domainpassword'])
-						{
-							$decrypt = $this->FOGCore->aesdecrypt($_REQUEST['domainpassword'],$this->FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
-							if ($decrypt && mb_detect_encoding($decrypt, 'UTF-8', true))
-								$password = $this->FOGCore->aesencrypt($decrypt,$this->FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
-							else
-								$password = $this->FOGCore->aesencrypt($_REQUEST['domainpassword'],$this->FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
-						}
-						else
-							$password = $_REQUEST['domainpassword'];
-						if ($Host && $Host->isValid())
-						{
-							$Host->set('useAD', ($this->REQUEST['domain'] == "on" ? '1' : '0'))
-								 ->set('ADDomain', $this->REQUEST['domainname'])
-								 ->set('ADOU', $this->REQUEST['ou'])
-								 ->set('ADUser', $this->REQUEST['domainuser'])
-								 ->set('ADPass', $password);
-							$Host->save();
-						}
-					}
+					$useAD = ($_REQUEST['domain'] == 'on');
+					$domain = $_REQUEST['domainname'];
+					$ou = $_REQUEST['ou'];
+					$user = $_REQUEST['domainuser'];
+					$pass = $_REQUEST['domainpassword'];
+					$Group->setAD($useAD,$domain,$ou,$user,$pass);
 				break;
 				// Printer Add/Rem
 				case 'group-printers';
-					// Error Checking
-					foreach ((array)$Group->get('hosts') AS $Host)
-					{
-						if ($Host && $Host->isValid())
-						{
-							$Host->set('printerLevel', $_REQUEST['level']);
-							$Host->addPrinter($_REQUEST['prntadd']);
-							$Host->removePrinter($_REQUEST['prntdel']);
-							$Host->save();
-							if (isset($_REQUEST['default']))
-								$Host->updateDefault($_REQUEST['default']);
-						}
-					}
+					$Group->addPrinter($_REQUEST['prntadd'],$_REQUEST['prntdel'],$_REQUEST['default'],$_REQUEST['level']);
 				break;
 				// Update Services
 				case 'group-service';
@@ -991,7 +942,7 @@ class GroupManagementPage extends FOGPage
 							if($_REQUEST['updatestatus'] == '1')
 							{
 								foreach((array)$ServiceSetting AS $id => $onoff)
-									$onoff ? $Host->addModule($id) : $Host->removeModule($id);
+									$onoff ? $Host->addModule($id)->save('modules') : $Host->removeModule($id)->save('modules');
 							}
 							if ($_REQUEST['updatedisplay'] == '1')
 								$Host->setDisp($x,$y,$r);
