0e5c54095492619a7df17e9a1da5af7288e510ca
Tom Elliott <tommygunsster@gmail.com>
2014-10-07 09:52:39 +0000

    Fix renaming of host so host mac is entered properly.  Fix Multicast Tasking so it can be scheduled properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2401 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -22,7 +22,7 @@ class System
 	private static function default_values()
 	{
 		(ini_get('date.timezone') ? date_default_timezone_set(ini_get('date.timezone')) : date_default_timezone_set('UTC'));
-		define('FOG_VERSION', '2400');
+		define('FOG_VERSION', '2401');
 		define('FOG_SCHEMA', 130);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
