bdcca8f87add344c0f2bd2bdcd12bbfdea64877d
Tom Elliott <tommygunsster@gmail.com>
2014-10-08 21:03:11 +0000

    Use the label's of 'check here' items to allow selecting of the checkbox.  Make Group page handle many of the host association stuff through the group class rather than directly on the page.  Printers are handled at the printer class as well.  Made the checkboxes for the host and group of default printers a radio rather than a checkbox as you can only set one default at a time.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2408 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/Group.class.php
+++ packages/web/lib/fog/Group.class.php
@@ -124,6 +124,96 @@ class Group extends FOGController
         return $this;
     }
 
+	public function addImage($imageID)
+	{
+		if (!$imageID)
+			throw new Exception(_('Select an image'));
+		$Image = ($imageID instanceof Image ? $imageID : new Image((int)$imageID));
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				if ($Host->get('task') && $Host->get('task')->isValid())
+					throw new Exception(_('There is a host in a tasking'));
+				if (!$Image || !$Image->isValid())
+					throw new Exception(_('Image is not valid'));
+				else
+					$Host->set('imageID', $Image->get('id'));
+				$Host->save();
+			}
+		}
+		return $this;
+	}
+
+	public function addSnapin($snapArray)
+	{
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+				$Host->addSnapin($snapArray)->save();
+		}
+		return $this;
+	}
+
+	public function removeSnapin($snapArray)
+	{
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+				$Host->removeSnapin($snapArray)->save();
+		}
+		return $this;
+	}
+
+	public function setAD($useAD, $domain, $ou, $user, $pass)
+	{
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+				$Host->setAD($useAD,$domain,$ou,$user,$pass)->save();
+		}
+		return $this;
+	}
+
+	public function addPrinter($printAdd,$printDel,$level = 0)
+	{
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				$Host->set('printerLevel',$level)
+					 ->addPrinter($printAdd)
+					 ->removePrinter($printDel)
+					 ->save();
+				if ($default)
+					$Host->updateDefault($default);
+			}
+		}
+		return $this;
+	}
+
+	public function updateDefault($printerid,$onoff)
+	{
+		foreach($this->get('hosts') AS $Host)
+		{
+			if ($Host && $Host->isValid())
+			{
+				foreach($printerid AS $printer)
+				{
+					$Printer = new Printer($printer);
+					if ($Printer && $Printer->isValid())
+					{
+						if ($Printer->get('id') == $onoff)
+							$Host->updateDefault($Printer->get('id'),1);
+						else
+							$Host->updateDefault($Printer->get('id'),0);
+					}
+				}
+			}
+		}
+		return $this;
+	}
+
 	// Custom Variables
 	public function doMembersHaveUniformImages()
 	{
