33d61fe7f62259aeffab90d142ba6437332deffc
Tom Elliott <tommygunsster@gmail.com>
2014-10-09 13:33:15 +0000

    fix usertracking to work properly as the action is also passed base64_encoded on legacy client.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2411 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/usertracking.report.php
+++ packages/web/service/usertracking.report.php
@@ -10,7 +10,7 @@ try
 	$Host = $HostManager->getHostByMacAddresses($MACs);
 	if (!$Host->isValid())
 		throw new Exception('#!nf');
-	if (!in_array(strtolower($_REQUEST['action']),array('login','start','logout')))
+	if (!in_array(strtolower(($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? $_REQUEST['action'] : base64_decode($_REQUEST['action']))),array('login','start','logout')))
 		throw new Exception('#!er: Postfix requires an action of login,logout, or start to operate');
 	$user = explode(chr(92),strtolower(($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? $_REQUEST['user'] : base64_decode($_REQUEST['user']))));
 	if ($user == null)
@@ -18,21 +18,18 @@ try
 	if (count($user) == 2)
 		$user = $user[1];
 	$date = $FOGCore->nice_date();
-	$tmpDate = ($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? $_REQUEST['date'] : base64_decode($_REQUEST['date']));
-	if ($tmpDate != null && strlen($tmpDate) > 0)
-	{
-		$date = $FOGCore->nice_date($tmpDate);
-		$desc = _('Replay from journal: real insert time').' '.$date->format("M j, Y g:i:s a");
-	}
+	$tmpDate = ($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? $FOGCore->nice_date($_REQUEST['date']) : $FOGCore->nice_date(base64_decode($_REQUEST['date'])));
+	if ($tmpDate < $date)
+		$desc = _('Replay from journal: real insert time').' '.$date->format('M j, Y g:i:s a');
 	$actionText = ($_REQUEST['action'] == 'login' ? 1 : ($_REQUEST['action'] == 'logout' ? 0 : 99));
 	$user = $_REQUEST['action'] == 'start' ? '' : $user;
 	$UserTracking = new UserTracking(array(
 		'hostID'	=> $Host->get('id'),
 		'username'	=> $user,
 		'action'	=> $actionText,
-		'datetime'	=> $FOGCore->formatTime($date->getTimestamp(),'Y-m-d H:i:s'),
+		'datetime'	=> $date->format('Y-m-d H:i:s'),
 		'description' => $desc,
-		'date' => $FOGCore->formatTime($date->getTimestamp(),'Y-m-d'),
+		'date' => $date->format('Y-m-d'),
 	));
 	if ($UserTracking->save())
 		$Datatosend = '#!ok';
