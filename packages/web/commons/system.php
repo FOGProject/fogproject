c3f9d1f17f0dd2b8ea9c3e1a2a8f8952936c7464
Tom Elliott <tommygunsster@gmail.com>
2014-10-24 02:42:15 +0000

    Fix the caller as private setting variable in a function doesn't work so well.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2471 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -22,7 +22,7 @@ class System
 	private static function default_values()
 	{
 		(ini_get('date.timezone') ? date_default_timezone_set(ini_get('date.timezone')) : date_default_timezone_set('UTC'));
-		define('FOG_VERSION', '2470');
+		define('FOG_VERSION', '2471');
 		define('FOG_SCHEMA', 132);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
