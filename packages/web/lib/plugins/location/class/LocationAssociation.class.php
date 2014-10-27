bd2deed0536490075674e55137ef22e001b7e6d6
Tom Elliott <tommygunsster@gmail.com>
2014-10-27 02:05:05 +0000

    Add hook-ins for elements.  Move all the location from boot/snapin standpoint to location.  Create the hook to handle the location needed stuff. Edit the ubuntu and arch FOGSnapinReplicator to have descriptions of snapin rather than image.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2482 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/location/class/LocationAssociation.class.php
+++ packages/web/lib/plugins/location/class/LocationAssociation.class.php
@@ -11,4 +11,33 @@ class LocationAssociation extends FOGController
 		'locationID'		=> 'laLocationID',
 		'hostID' => 'laHostID',
 	);
+
+	// Get the storageNode for this location
+	public function getStorageNode()
+	{
+		$Location = new Location($this->get('locationID'));
+		if ($Location && $Location->isValid())
+		{
+			if ($Location->get('storageNodeID'))
+				$StorageNode = new StorageNode($Location->get('storageNodeID'));
+			else
+				$StorageNode = $this->getStorageGroup()->getOptimalStorageNode();
+		}
+		return $StorageNode;
+	}
+	// Get the storageGroup for this location
+	public function getStorageGroup()
+	{
+		$Location = new Location($this->get('locationID'));
+		if ($Location && $Location->isValid())
+			$StorageGroup = new StorageGroup($Location->get('storageGroupID'));
+		return $StorageGroup;
+	}
+	// Get if the location is tftp or not.
+	public function isTFTP()
+	{
+		$Location = new Location($this->get('locationID'));
+		if ($Location && $Location->isValid())
+			return $Location->get('tftp');
+	}
 }
