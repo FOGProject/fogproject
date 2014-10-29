426d9e406815e607b3d757d3475e6f3f1fe03848
Tom Elliott <tommygunsster@gmail.com>
2014-10-29 06:46:49 -0400

    Maybe fix master.

--- packages/web/service/usercleanup-users.php
+++ packages/web/service/usercleanup-users.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-<?php
-require('../commons/base.inc.php');
-try
-{
-	print "#!start\n";
-	foreach ($FOGCore->getClass('UserCleanupManager')->find() AS $User)
-		print base64_encode($User->get('name'))."\n";
-	print "#!end\n";
-}
-catch (Exception $e)
-{
-	print $e->getMessage();
-}
-=======
 <?php
 require('../commons/base.inc.php');
 if ($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'])
@@ -35,4 +20,3 @@ if ($FOGCore->getSetting('FOG_NEW_CLIENT') && $FOGCore->getSetting('FOG_AES_ENCR
 	print "#!en=".$FOGCore->aesencrypt($Datatosend,$FOGCore->getSetting('FOG_AES_PASS_ENCRYPT_KEY'));
 else
 	print $Datatosend;
->>>>>>> dev-branch
