95e8613fd244ed75c6a934367d3421d7807f260b
Tom Elliott <tommygunsster@gmail.com>
2014-10-29 06:39:45 -0400

    Maybe fix the svn repo

--- packages/web/service/jobs.php
+++ packages/web/service/jobs.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<?php
-require('../commons/base.inc.php');
-try
-{
-	$HostManager = new HostManager();
-	$MACs = HostManager::parseMacList($_REQUEST['mac']);
-	if (!$MACs)
-		throw new Exception('#!im');
-	// Get the Host
-	$Host = $HostManager->getHostByMacAddresses($MACs);
-	if (!$Host->isValid())
-		throw new Exception('#!er:No Host Found');
-	// Find out about tasks in queue.
-	$Task = current($Host->get('task'));
-	// If there is no task, or it's of snapin deploy type, don't reboot.
-	if (!$Task->isValid() || ($Task->get('typeID') == 12 || $Task->get('typeID') == 13))
-		throw new Exception('#nj');
-	else
-		print ('#!ok');
-}
-catch (Exception $e)
-{
-	print $e->getMessage();
-}
-=======
 <?php
 require('../commons/base.inc.php');
 try
@@ -52,4 +26,3 @@ if ($FOGCore->getSetting('FOG_NEW_CLIENT') && $FOGCore->getSetting('FOG_AES_ENCR
 	print "#!en=".$FOGCore->aesencrypt($Datatosend,$FOGCore->getSetting('FOG_AES_PASS_ENCRYPT_KEY'));
 else
 	print $Datatosend;
->>>>>>> dev-branch
