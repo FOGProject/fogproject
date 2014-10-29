089daa1b9f2edb87ac4d109008017131718acaa2
Tom Elliott <tommygunsster@gmail.com>
2014-10-29 06:39:45 -0400

    Maybe fix the svn repo

--- packages/web/management/js/fog.host.deploy.js
+++ packages/web/management/js/fog.host.deploy.js
@@ -2,8 +2,8 @@
  * FOG Host Management - Deploy - JavaScript
  *	Author:		Blackout
  *	Created:	9:18 AM 27/12/2011
- *	Revision:	$Revision$
- *	Last Update:	$LastChangedDate$
+ *	Revision:	$Revision: 2275 $
+ *	Last Update:	$LastChangedDate: 2014-09-11 23:13:28 -0400 (Thu, 11 Sep 2014) $
  ***/
 $(function(){
 	DeployStuff();
