dd7c08701ba6107c6951ed6b9397618a5428f42d
Tom Elliott <tommygunsster@gmail.com>
2014-10-29 06:44:22 -0400

    Maybe fix the dev-branch

--- packages/web/lib/fog/TaskManager.class.php
+++ packages/web/lib/fog/TaskManager.class.php
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-// Blackout - 11:31 AM 2/10/2011
-class TaskManager extends FOGManagerController
-{
-	// Table
-	public $databaseTable = 'tasks';
-	
-	// Search query
-	public $searchQuery = 'SELECT tasks.* FROM tasks
-		LEFT OUTER JOIN hosts ON (taskHostID=hostID)
-		LEFT OUTER JOIN hostMAC ON (taskHostID=hmHostID)
-		LEFT OUTER JOIN taskTypes ON (taskTypeID=ttID)
-		LEFT OUTER JOIN taskStates ON (taskStateID=tsID)
-		LEFT OUTER JOIN images ON (hostImage=imageID)
-		WHERE
-			hostID LIKE "%${keyword}%" OR
-			hostName LIKE "%${keyword}%" OR
-			hostDesc LIKE "%${keyword}%" OR
-			hostMAC LIKE "%${keyword}%" OR
-			hmMAC LIKE "%${keyword}%" OR
-			tsName LIKE "%${keyword}%" OR
-			ttName LIKE "%${keyword}%" OR
-			imageName LIKE "%${keyword}%" OR
-			taskCreateBy LIKE "%${keyword}%"
-		GROUP BY
-			taskID DESC
-		ORDER BY hostName';
-
-	// Custom
-	// Clean up
-	function hasActiveTaskCheckedIn($taskid)
-	{
-		$Task = new Task($taskid);
-		return ((strtotime($Task->get('checkInTime')) - strtotime($Task->get('createdTime'))) > 2);
-	}
-}
-=======
 <?php
 
 // Blackout - 11:31 AM 2/10/2011
@@ -53,4 +14,3 @@ class TaskManager extends FOGManagerController
 		return ((strtotime($Task->get('checkInTime')) - strtotime($Task->get('createdTime'))) > 2);
 	}
 }
->>>>>>> dev-branch
--- packages/web/lib/plugins/accesscontrol/class/Accesscontrol.class.php
+++ packages/web/lib/plugins/accesscontrol/class/Accesscontrol.class.php
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-<?php
-/** Class Name: Example
-	This just serves as an example class file.
-
-	This is only for the Example Plugin created, but
-	could serve to help the user create their own class files.
-*/
-class Accesscontrol extends FOGController
-{
-	// Table
-	public $databaseTable = 'example';
-	// Name -> Database field name
-	public $databaseFields = array(
-		'id'		=> 'eID',
-		'name'		=> 'eName',
-		'other'		=> 'eOther',
-		'hostID'	=> 'eHostID',
-	);
-}
-=======
 <?php
 /** Class Name: Example
 	This just serves as an example class file.
@@ -40,4 +19,3 @@ class Accesscontrol extends FOGController
 		'groupID'	=> 'acGroupID',
 	);
 }
->>>>>>> dev-branch
