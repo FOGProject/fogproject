161de0d09a329cf3c5d5d703d8d42d5287337348
Tom Elliott <tommygunsster@gmail.com>
2014-10-29 06:39:45 -0400

    Maybe fix the svn repo

--- packages/web/lib/hooks/Template.hook.php
+++ packages/web/lib/hooks/Template.hook.php
@@ -3,8 +3,8 @@
  * FOG Hook: Template
  *	Author:		Blackout
  *	Created:	8:57 AM 31/08/2011
- *	Revision:	$Revision$
- *	Last Update:	$LastChangedDate$
+ *	Revision:	$Revision: 1732 $
+ *	Last Update:	$LastChangedDate: 2014-05-24 15:37:43 -0400 (Sat, 24 May 2014) $
  ***/
 // Hook Template
 class Template extends Hook
