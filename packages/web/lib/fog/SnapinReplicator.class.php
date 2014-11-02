80aa9b652104162b38d9a4ab5d8815189740d659
Tom Elliott <tommygunsster@gmail.com>
2014-11-02 19:08:56 +0000

    More refining of snapin replicator.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2538 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/SnapinReplicator.class.php
+++ packages/web/lib/fog/SnapinReplicator.class.php
@@ -76,16 +76,16 @@ class SnapinReplicator extends FOGBase
 						}
 					}
 					$this->outall(sprintf(" * Checking nodes within my group."));
-					foreach($Snapins AS $Snapin)
+					if (count($StorageNodeCount) > 0)
 					{
-						if (count($StorageNodeCount) > 0)
+						$this->outall(sprintf(" * Found: %s other member(s).",count($StorageNodeCount)));
+						$this->outall(sprintf(''));
+						$myRoot = rtrim($StorageNode->get('snapinpath'),'/');
+						$this->outall(sprintf(" * My root: %s",$myRoot));
+						$this->outall(sprintf(" * Starting Sync."));
+						foreach($StorageNodeCount AS $StorageNodeFTP)
 						{
-							$this->outall(sprintf(" * Found: %s other member(s).",count($StorageNodeCount)));
-							$this->outall(sprintf(''));
-							$myRoot = rtrim($StorageNode->get('snapinpath'),'/');
-							$this->outall(sprintf(" * My root: %s",$myRoot));
-							$this->outall(sprintf(" * Starting Sync."));
-							foreach($StorageNodeCount AS $StorageNodeFTP)
+							foreach($Snapins AS $Snapin)
 							{
 								if ($StorageNodeFTP->get('isEnabled'))
 								{
@@ -106,9 +106,9 @@ class SnapinReplicator extends FOGBase
 								}
 							}
 						}
-						else
-							$this->outall(sprintf(" * I am the only member, no need to copy anything!"));
 					}
+					else
+						$this->outall(sprintf(" * I am the only member, no need to copy anything!"));
 				}
 				else
 					$this->outall(sprintf(" * There are no snapins to replicate!"));
