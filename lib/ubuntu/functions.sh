7f60420714140188f62883c57b3fad143aa37ac4
Tom Elliott <tommygunsster@gmail.com>
2014-11-03 20:50:15 +0000

    Maybe now less output?
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2551 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/ubuntu/functions.sh
+++ lib/ubuntu/functions.sh
@@ -568,10 +568,10 @@ installPackages()
 	
 	for x in $packages
 	do
-		dpkg -l $x >/dev/null | grep '^ii' 2>&1;
+		dpkg -l $x >/dev/null | grep '^ii' >/dev/null 2>&1;
 		if [ "$?" != "0" -a "$x" == "php5-json" ]; then
 			x="php5-common";
-			dpkg -l $x >/dev/null | grep '^ii' 2>&1;
+			dpkg -l $x >/dev/null | grep '^ii' >/dev/null 2>&1;
 		elif [ "$?" != "0" ]; then
 			echo  "  * Installing package: $x";
 			if [ "$x" = "mysql-server" ]
@@ -609,18 +609,18 @@ confirmPackageInstallation()
 	for x in $packages
 	do
 		echo -n "  * Checking package: $x...";
-		dpkg -l $x >/dev/null | grep '^ii' 2>&1;
+		dpkg -l $x >/dev/null | grep '^ii' >/dev/null 2>&1;
 		if [ "$?" != "0" -a "$x" == "php5-json" ]
 		then
 			x="php5-common";
-			dpkg -l $x >/dev/null | grep '^ii' 2>&1;
+			dpkg -l $x >/dev/null | grep '^ii' >/dev/null 2>&1;
 		fi
 		if [ "$?" != "0" ]; then
 			echo "Failed!"
 			if [ "$x" = "$dhcpname" ]
 			then			
 				echo -n "  * Checking for legacy package: $olddhcpname";
-				dpkg -l $olddhcpname >/dev/null | grep '^ii' 2>&1;
+				dpkg -l $olddhcpname >/dev/null | grep '^ii' >/dev/null 2>&1;
 				if [ "$?" != "0" ]
 				then
 					echo "Failed!"
