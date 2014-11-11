d06a534c58f827d16cbb3d0f80ad880d21d2ea4f
Tom Elliott <tommygunsster@gmail.com>
2014-11-11 00:45:32 +0000

    One more again?
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2617 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/SnapinReplicator.class.php
+++ packages/web/lib/fog/SnapinReplicator.class.php
@@ -68,7 +68,7 @@ class SnapinReplicator extends FOGBase
 								$this->outall(sprintf(" * Found snapin to transfer to %s group(s)",count($Snapin->get('storageGroups')) -1));
 								$this->outall(sprintf(" | Snapin name: %s",$Snapin->get('name')));
 								$this->outall(sprintf(" * Syncing: %s",$StorageNodeToSend->get('name')));
-								$process[] = popen("lftp -e \"set ftp:list-options -a;set net:max-retries 1;set net:timeout 30;".$limit." mirror ".implode('-i ',$mySnapFile)." -n --ignore-time -R -vvv --delete-first $mySnapin $remSnapin; exit\" -u $username,$password $ip 2>&1","r");
+								$process[] = popen("lftp -e \"set ftp:list-options -a;set net:max-retries 1;set net:timeout 30;".$limit." mirror -i ".implode('-i ',$mySnapFile)." -n --ignore-time -R -vvv --delete-first $mySnapin $remSnapin; exit\" -u $username,$password $ip 2>&1","r");
 							}
 						}
 					}
@@ -109,7 +109,7 @@ class SnapinReplicator extends FOGBase
 								if ($limitsend > 0)
 									$limit .= "set net:limit-rate 0:$limitsend;";
 								$this->outall(sprintf(" * Syncing: %s",$StorageNodeFTP->get('name')));
-								$process[] = popen("lftp -e \"set ftp:list-options -a;set net:max-retries 1;set net:timeout 30;".$limit." mirror ".implode('-i ',$mySnapFile)." -n --ignore-time -R -vvv --delete-first $myRoot $remRoot; exit\" -u $username,$password $ip 2>&1","r");
+								$process[] = popen("lftp -e \"set ftp:list-options -a;set net:max-retries 1;set net:timeout 30;".$limit." mirror -i ".implode('-i ',$mySnapFile)." -n --ignore-time -R -vvv --delete-first $myRoot $remRoot; exit\" -u $username,$password $ip 2>&1","r");
 							}
 						}
 						foreach((array)$process AS $proc)
