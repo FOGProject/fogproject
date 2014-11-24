5ad1ece5e951c5fa354d3f425a4f098345a9878a
Tom Elliott <tommygunsster@gmail.com>
2014-11-24 17:00:46 +0000

    Use dialogs rather than confirm boxes for update/delete mac listings  it just looks better.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2683 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.about.mac-list.js
+++ packages/web/management/js/fog/fog.about.mac-list.js
@@ -1,19 +1,36 @@
-// 12:21 PM 9/05/2011
-
-// TODO: JAVASCRIPT: Move this to seperate file
 function clearMacs() {
-	if (confirm('Are you sure you wish to clear all mac address listings?')) {
-		hideButtons();
-		window.location.href='?node=about&sub=mac-list-post&clear=1'
-	}
+	$('#delete').html('Are you sure you wish to clear all mac address listings?');
+	$('#delete').dialog({
+		resizable: false,
+		modal: true,
+		title: 'Delete MACs',
+		buttons: {
+			'Yes': function() {
+				$('.macButtons').fadeOut('slow');
+				$(this).dialog('close');
+				location.href='?node=about&sub=mac-list-post&clear=1';
+			},
+			'No': function() {
+				$(this).dialog('close');
+			}
+		}
+	});
 }
 function updateMacs() {
-	if (confirm('Are you sure you wish to update the mac address listing?')) {
-		hideButtons();
-		window.location.href='?node=about&sub=mac-list-post&update=1'			
-	}
-}
-function hideButtons() {
-	$("#delete").fadeOut('slow');
-	$("#update").fadeOut('slow');
+	$('#update').html('Are you sure you wish to update the mac address listings?');
+	$('#update').dialog({
+		resizable: false,
+		modal: true,
+		title: 'Update MACs',
+		buttons: {
+			'Yes': function() {
+				$('.macButtons').fadeOut('slow');
+				$(this).dialog('close');
+				location.href='?node=about&sub=mac-list-post&update=1';
+			},
+			'No': function() {
+				$(this).dialog('close');
+			}
+		}
+	});
 }
