f1b3d720a7af44eb04dbf37caae79797247b3514
Tom Elliott <tommygunsster@gmail.com>
2014-11-24 01:23:43 +0000

    Better handling on the mac-list updating (much much faster).
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2678 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.about.maclist.js
+++ packages/web/management/js/fog/fog.about.maclist.js
@@ -1,26 +1,19 @@
 // 12:21 PM 9/05/2011
 
 // TODO: JAVASCRIPT: Move this to seperate file
-function clearMacs()
-{
-	if ( confirm( 'Are you sure you wish to clear all mac address listings?') )
-	{
+function clearMacs() {
+	if (confirm('Are you sure you wish to clear all mac address listings?')) {
 		hideButtons();
 		window.location.href='?node=about&sub=maclist&clear=1'
 	}
 }
-
-function updateMacs()
-{
-	if ( confirm( 'Are you sure you wish to update the mac address listing?'  ) )
-	{
+function updateMacs() {
+	if (confirm('Are you sure you wish to update the mac address listing?')) {
 		hideButtons();
 		window.location.href='?node=about&sub=maclist&update=1'			
-	}	
-}		
-
-function hideButtons()
-{
+	}
+}
+function hideButtons() {
 	$("#delete").fadeOut('slow');
 	$("#update").fadeOut('slow');
 }
