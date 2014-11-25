b57b0b9a19f28d74e2164fbcdf85290ab3f4fa89
Tom Elliott <tommygunsster@gmail.com>
2014-11-25 18:35:26 +0000

    Better handling of encrypting the ad password. It stores the key used to encrypt it with it.  While seemingly insecure, it's only relevant for new clients, in which case the data will be completely encrypted anyway to a point before AES even has an affect.  If anything, it makes it more secure in that it is constantly changing.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2697 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/hostname.php
+++ packages/web/service/hostname.php
@@ -14,9 +14,11 @@ try
 		throw new Exception('#!ih');
 	if ($Host->get('ADPass') && $_REQUEST['newService'] && $FOGCore->getSetting('FOG_NEW_CLIENT'))
 	{
-		$decrypt = $FOGCore->aesdecrypt($Host->get('ADPass'),$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
+		$encdat = substr($Host->get('ADPass'),0,-32);
+		$enckey = substr($Host->get('ADPass'),-32);
+		$decrypt = $FOGCore->aesdecrypt($encdat,$enckey);
 		if ($decrypt && mb_detect_encoding($decrypt,'UTF-8',true))
-			$password = $FOGCore->aesencrypt($decrypt,$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
+			$password = $FOGCore->aesencrypt($decrypt,$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY').$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
 		else
 			$password = $Host->get('ADPass');
 		$Host->set('ADPass',trim($password))->save();
@@ -27,7 +29,7 @@ try
 	$Datatosend .= '#ADDom='.$Host->get('ADDomain')."\n";
 	$Datatosend .= '#ADOU='.$Host->get('ADOU')."\n";
 	$Datatosend .= '#ADUser='.($Host->get('useAD') ? (strpos($Host->get('ADUser'),'\\') || strpos($Host->get('ADUser'),'@') ? $Host->get('ADUser') : $Host->get('ADDomain').'\\'.$Host->get('ADUser')) : '')."\n";
-	$Datatosend .= '#ADPass='.$Host->get('ADPass').$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY');
+	$Datatosend .= '#ADPass='.$Host->get('ADPass');
 	if (trim(base64_decode($Host->get('productKey'))))
 		$Datatosend .= "\n#Key=".base64_decode($Host->get('productKey'));
 	$FOGCore->setSetting('FOG_AES_ADPASS_ENCRYPT_KEY',$FOGCore->randomString(32));
