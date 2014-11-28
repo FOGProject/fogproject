40f4567f2a0efbcbb194b9395c6d59cfb42b1696
Tom Elliott <tommygunsster@gmail.com>
2014-11-28 01:53:51 +0000

    More jquery/javascript cleanup.  Make tasks page only update if on active tasks pages, rather than on all pages dealing with tasks.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2707 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.tasks.js
+++ packages/web/management/js/fog/fog.tasks.js
@@ -9,51 +9,50 @@
 var ActiveTasksContainer;
 var ActiveTasksLastCount;
 // Auto loader
-$(function()
-{
+$(function() {
 	// Show Task Container if we have items
 	ActiveTasksContainer = $('#active-tasks');
 	if (ActiveTasksContainer.find('tbody > tr').size() > 0) ActiveTasksContainer.show();
 	// Hook buttons
 	ActiveTasksButtonHook();
-	// Update on load
-	//ActiveTasksUpdate();
 	// Update timer
 	ActiveTasksUpdateTimerStart();
 });
-
-function ActiveTasksUpdateTimerStart()
-{
-	ActiveTasksUpdateTimer = setTimeout(function()
-	{
-		if (!ActiveTasksRequests.length)
-		{
-			ActiveTasksUpdate();
-		}
-		else
-		{
-			//alert('ajax requests processing, ignoring update');
-		}
-	}, ActiveTasksUpdateInterval);
+function getQueryParams(qs) {
+	qs = qs.split("+").join(" ");
+	var params = {},
+		tokens,
+		re = /[?&]?([^=]+)=([^&]*)/g
+	while (tokens = re.exec(qs)) {
+		params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+	}
+	return params;
 }
-
-function ActiveTasksUpdate()
-{
+function ActiveTasksUpdateTimerStart() {
+	var $_GET = getQueryParams(document.location.search);
+	var Subs = ['search','listhosts','listgroups','active-multicast','active-snapins','scheduled'];
+	if ($.inArray($_GET['sub'],Subs) == -1) {
+		ActiveTasksUpdateTimer = setTimeout(function() {
+			if (!ActiveTasksRequests.length) {
+				ActiveTasksUpdate();
+			}
+		}, ActiveTasksUpdateInterval);
+	}
+}
+function ActiveTasksUpdate() {
 	if (ActiveTasksAJAX) return;
 	ActiveTasksAJAX = $.ajax({
-		'type':		'GET',
-		'url':		'?node=tasks',
-		'cache':	false,
-		'dataType':	'json',
-		'beforeSend':	function()
-		{
+		type: 'GET',
+		url: '?node=tasks',
+		cache: false,
+		dataType: 'json',
+		beforeSend:	function() {
 			if (ActiveTasksLastCount)
 				Loader.fogStatusUpdate(_L['ACTIVE_TASKS_FOUND'].replace(/%1/, ActiveTasksLastCount).replace(/%2/, (ActiveTasksLastCount == 1 ? '' : 's')), { 'Class': 'loading' });
 			else
 				Loader.fogStatusUpdate(_L['ACTIVE_TASKS_LOADING'], { 'Class': 'loading' });
 		},
-		'success':	function(response)
-		{
+		success: function(response)	{
 			// Loader
 			Loader.fogStatusUpdate(_L['ACTIVE_TASKS_FOUND'].replace(/%1/, response['data'].length).replace(/%2/, (response['data'].length == 1 ? '' : 's')));
 			// Variables
@@ -63,21 +62,17 @@ function ActiveTasksUpdate()
 			// Empty search table
 			tbody.empty();
 			// Do we have search results?
-			if (response['data'].length > 0)
-			{
+			if (response['data'].length > 0) {
 				var rows = '';
 				// Iterate data
-				for (var i in response['data'])
-				{
+				for (var i in response['data']) {
 					// Reset
 					var row = '<tr id="task-' + response['data'][i]['id'] + '" class="' + (i % 2 ? 'alt2' : 'alt1')  + (response['data'][i]['percent'] ? ' with-progress' : '') + '">';
 					// Add column templates
-					for (var j in response['templates'])
-					{
+					for (var j in response['templates']) {
 						// Add attributes to columns
 						var attributes = [];
-						for (var k in response['attributes'][j])
-						{
+						for (var k in response['attributes'][j]) {
 							attributes[attributes.length] = k + '="' + response['attributes'][j][k] + '"';
 						}
 						// Add
@@ -85,20 +80,17 @@ function ActiveTasksUpdate()
 					}
 					//response['data'][i]['percentText'].remove();
 					// Replace variable data
-					if (response['data'][i]['percent'] > 0 && response['data'][i]['percent'] < 100)
-					{
+					if (response['data'][i]['percent'] > 0 && response['data'][i]['percent'] < 100) {
 						row += '<td colspan="7">' + '<tr id="progress-${host_id}" class="${class}"><td colspan="7" class="task-progress-td min"><div class="task-progress-fill min" style="width: ${width}px"></div><div class="task-progress min"><ul><li>${elapsed}/${remains}</li><li>${percentText}%</li><li>${copied} of ${total} (${bpm}/min)</li></ul></div></td></tr></td>';
 					}
-					for (var k in response['data'][i])
-					{
+					for (var k in response['data'][i]) {
 						row = row.replace(new RegExp('\\$\\{' + k + '\\}', 'g'), response['data'][i][k]);
 					}
 					row = row.replace(new RegExp('\\$\\{\w+\\}', 'g'), '');
 					// Add to rows
 					rows += row + "</tr>";
 					// Percentage data
-					if (response['data'][i]['percent'])
-					{
+					if (response['data'][i]['percent']) {
 						rows += response['data'][i]['percent'];
 					}
 				}
@@ -121,8 +113,7 @@ function ActiveTasksUpdate()
 			// Schedule another update
 			ActiveTasksUpdateTimerStart();
 		},
-		'error':	function()
-		{
+		error: function() {
 			// Reset
 			ActiveTasksAJAX = null;
 			// Display error in loader
@@ -132,31 +123,24 @@ function ActiveTasksUpdate()
 		}
 	});
 }
-function ActiveTasksButtonHook()
-{
+function ActiveTasksButtonHook() {
 	// Hook: Click: Kill Button - Legacy GET call still works if AJAX fails
-	$('.icon-kill').parent().unbind('click').click(function()
-	{
+	$('.icon-kill').parent().unbind('click').click(function() {
 		var $this = $(this);
 		var ID = $this.parents('tr').attr('id').replace(/^host-/, '');
 		var ProgressBar = $('#progress-' + ID, ActiveTasksContainer);
 		ActiveTasksRequests[ActiveTasksRequests.length] = $.ajax({
-			'type':		'GET',
-			'url':		$this.attr('href'),
-			//'url':		'ajax/sleep.php',
-			'beforeSend':	function()
-			{
+			type: 'GET',
+			url: $this.attr('href'),
+			beforeSend: function() {
 				// Loader
 				$this.find('span').removeClass().addClass('icon icon-loading');
-				
 				// Unhook this button - multiple clicks now do nothing
 				$this.unbind('click').click(function() { return false; });
 			},
-			'success':	function(data)
-			{
+			success: function(data) {
 				// Fade row out
-				$this.parents('tr').fadeOut('fast', function()
-				{
+				$this.parents('tr').fadeOut('fast', function() {
 					// Remove tr element
 					$(this).remove();
 					// Remove progress bar
@@ -169,41 +153,35 @@ function ActiveTasksButtonHook()
 				// Remove this request from our AJAX request tracking
 				ActiveTasksRequests.splice(0, 1);
 			},
-			'error':	function()
-			{
+			error: function() {
 				// Re-hook buttons
 				ActiveTasksButtonHook();
 				// Remove this request from our AJAX request tracking
 				ActiveTasksRequests.splice(0, 1);
 			}
 		});
-		
 		// Stop default event
 		return false;
 	});
 	// Hook: Click: Force Button - Legacy GET call still works if AJAX fails
-	$('.icon-force').parent().unbind('click').click(function()
-	{
+	$('.icon-force').parent().unbind('click').click(function() {
 		var $this = $(this);
 		ActiveTasksRequests[ActiveTasksRequests.length] = $.ajax({
-			'type':		'GET',
-			'url':		$this.attr('href'),
-			'beforeSend':	function()
-			{
+			type: 'GET',
+			url: $this.attr('href'),
+			beforeSend: function() {
 				// Loader
 				$this.find('span').removeClass().addClass('icon icon-loading');
 				// Unhook this button - multiple clicks now do nothing
 				$this.unbind('click').click(function() { return false; });
 			},
-			'success':	function(data)
-			{
+			success: function(data) {
 				// Indicate job has been forced
 				$this.find('span').removeClass().addClass('icon icon-forced');
 				// Remove this request from our AJAX request tracking
 				ActiveTasksRequests.splice(0, 1);
 			},
-			'error':	function()
-			{
+			error: function() {
 				// Remove this request from our AJAX request tracking
 				ActiveTasksRequests.splice(0, 1);
 			}
@@ -212,43 +190,35 @@ function ActiveTasksButtonHook()
 		return false;
 	});
 	// Hook: Hover: Show Progress Bar on Active Task
-	$('.with-progress').hover(function()
-	{
+	$('.with-progress').hover(function() {
 		var id = $(this).attr('id').replace(/^host-/, '');
 		var progress = $('#progress-' + id);
 		progress.show();
 		progress.find('.min').addClass('no-min').removeClass('min').end().find('ul').show();
-	}, function()
-	{
+	}, function() {
 		var id = $(this).attr('id').replace(/^host-/, '');
 		var progress = $('#progress-' + id);
 		progress.find('.no-min').addClass('min').removeClass('no-min').end().find('ul').hide();
 	});
 	// Hook: Hover: Show Progress Bar on Progress Bar
-	$('tr[id^="progress-"]').hover(function()
-	{
+	$('tr[id^="progress-"]').hover(function() {
 		$(this).find('.min').addClass('no-min').removeClass('min').end().find('ul').show();
-	}, function()
-	{
+	}, function() {
 		$(this).find('.no-min').addClass('min').removeClass('no-min').end().find('ul').hide();
 	});
 }
-function ActiveTasksTableCheck()
-{
+function ActiveTasksTableCheck() {
 	// Variables
 	var tbody = $('tbody', ActiveTasksContainer);
 	var tbodyRows = tbody.find('tr');
 	// If we have rows in the table
-	if (tbodyRows.size() > 0)
-	{
+	if (tbodyRows.size() > 0) {
 		// Adjust alt colours
 		var i = 0;
-		tbodyRows.each(function()
-		{
+		tbodyRows.each(function() {
 			$(this).removeClass().addClass('alt' + (i++ % 2 ? '2' : '1'));
 		});
-	}
-	// No rows in the table
-	else
+	} else {
 		tbody.html('<tr><td colspan="7" class="no-active-tasks">' + _L['NO_ACTIVE_TASKS'] + '</td></tr>');
+	}
 }
