75c59416ada0f988722ac231de17c6af9d7fac49
Tom Elliott <tommygunsster@gmail.com>
2014-11-29 14:06:24 +0000

    Remove jpgraph and the lib folder from the management folder as there's now no reason to use it.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2710 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/system.php
+++ packages/web/commons/system.php
@@ -22,8 +22,8 @@ class System
 	private static function default_values()
 	{
 		(ini_get('date.timezone') ? date_default_timezone_set(ini_get('date.timezone')) : date_default_timezone_set('UTC'));
-		define('FOG_VERSION', '2709');
-		define('FOG_SCHEMA', 149);
+		define('FOG_VERSION', '2710');
+		define('FOG_SCHEMA', 150);
 		define('FOG_SVN_REVISION', '$Revision$');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate$');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
