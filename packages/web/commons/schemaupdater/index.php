d556eb4910910dc3b5a0254d1c71d21b34b1a486
Tom Elliott <tommygunsster@gmail.com>
2014-11-29 14:06:24 +0000

    Remove jpgraph and the lib folder from the management folder as there's now no reason to use it.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2710 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/schemaupdater/index.php
+++ packages/web/commons/schemaupdater/index.php
@@ -1755,6 +1755,10 @@ $databaseSchema[] = array(
 $databaseSchema[] = array(
 	"ALTER TABLE `" . DATABASE_NAME ."`.`images` ADD COLUMN `imageCompress` INT(11)",
 );
+// 150
+$databaseSchema[] = array(
+	"DELETE FROM `". DATABASE_NAME ."`.`globalSettings` WHERE `settingKey`='FOG_JPGRAPH_VERSION'",
+);
 print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
 print "\n".'<html xmlns="http://www.w3.org/1999/xhtml">';
 print "\n\t<head>";
