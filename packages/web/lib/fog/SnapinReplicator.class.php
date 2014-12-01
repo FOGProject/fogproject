3c9d14fcd65f21fed3f60839004e550e4eeab110
Tom Elliott <tommygunsster@gmail.com>
2014-12-01 11:16:48 +0000

    Better handling of hosttoIP and IPtohost for the fog services.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2719 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/SnapinReplicator.class.php
+++ packages/web/lib/fog/SnapinReplicator.class.php
@@ -19,7 +19,7 @@ class SnapinReplicator extends FOGBase
 	}
 	private function commonOutput()
 	{
-		$StorageNode = current($this->getClass('StorageNodeManager')->find(array('isMaster' => 1,'isEnabled' => 1,'ip' => array(gethostbyaddr($this->FOGCore->getIPAddress()),$this->FOGCore->getIPAddress()))));
+		$StorageNode = current($this->getClass('StorageNodeManager')->find(array('isMaster' => 1,'isEnabled' => 1,'ip' => $this->FOGCore->getIPAddress())));
 		try
 		{
 			if ($StorageNode)
