41cbfddbd77f8ae784b37e55893faf40462fd0b5
Tom Elliott <tommygunsster@gmail.com>
2014-12-01 16:40:07 +0000

    Clear the buffer breaks report saving.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2725 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/ReportMaker.class.php
+++ packages/web/lib/fog/ReportMaker.class.php
@@ -22,14 +22,12 @@ class ReportMaker extends FOGBase
 			print implode($this->strHTML,"\n");
 		else if ( $intType === self::FOG_REPORT_CSV )
 		{
-			ob_end_clean();
 			header('Content-Type: application/octet-stream');
 			header('Content-Disposition: attachment; filename="'.$this->filename.'.csv"');
 			print implode($this->strLine,"\n");
 		}
 		else if ( $intType === self::FOG_REPORT_PDF )
 		{
-			ob_end_clean();
 			header('Content-Type: application/octet-stream');
 			header('Content-Disposition: attachment; filename="'.$this->filename.'.pdf"');
 			$proc = proc_open("htmldoc --links --header . --linkstyle plain --numbered --size letter --no-localfiles -t pdf14 --quiet --jpeg --webpage --size letter --left 0.25in --right 0.25in --top 0.25in --bottom 0.25in --header ... --footer ... -", array(0 => array("pipe", "r"), 1 => array("pipe", "w")), $pipes);
@@ -40,7 +38,6 @@ class ReportMaker extends FOGBase
 		}
 		else if ($intType === self::FOG_BACKUP_SQL)
 		{
-			ob_end_clean();
 			$filename="fog_backup.sql";
 			$path=BASEPATH.'/management/other/';
 			exec('mysqldump --opt -u'.DATABASE_USERNAME.' -p"'.DATABASE_PASSWORD.'" -h'.preg_replace('#p:#','',DATABASE_HOST).' '.DATABASE_NAME.' > '.$path.$filename);
@@ -51,7 +48,6 @@ class ReportMaker extends FOGBase
 		}
 		else if ($intType === self::FOG_EXPORT_SQL )
 		{
-			ob_end_clean();
 			$filename="host_export.sql";
 			$path=BASEPATH.'/management/other/';
 			$backup[]=exec('mysqldump -u'.DATABASE_USERNAME.' -p'.DATABASE_PASSWORD.' -h'.DATABASE_HOST.' '.DATABASE_NAME.' hosts > '.$path.$filename);
@@ -61,7 +57,6 @@ class ReportMaker extends FOGBase
 		}
 		else if ( $intType === self::FOG_EXPORT_HOST )
 		{
-			ob_end_clean();
 			header('Content-Type: application/octet-stream');
 			header("Content-Disposition: attachment; filename=host_export.csv");	
 			print implode($this->strLine,"\n");
