1a3de385c75ea64cdc318886c19d97f42d59b1ea
Tom Elliott <tommygunsster@gmail.com>
2014-12-06 00:46:22 +0000

    Maybe a few more, enhancements.  One to allow HookDebugger to still, kind of, work.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2744 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/init.php
+++ packages/web/commons/init.php
@@ -197,31 +197,28 @@ class Initiator
 	}
 }
 // Initialize everything
-if (!$Init)
+if (is_null($Init) || !($Init instanceof Initiator))
 	$Init = new Initiator();
 $Init::startInit();
 // Get the configuration
-if (!$Config)
+if (is_null($Config) || !($Config instanceof Config))
 	$Config = new Config();
 // Core
-if (!$FOGFTP)
+if (is_null($FOGFTP) || !($FOGFTP instanceof FOGFTP))
 	$FOGFTP = new FOGFTP();
-if (!$FOGCore)
+if (is_null($FOGCore) || !($FOGCore instanceof FOGCore))
 	$FOGCore = new FOGCore();
 // Generate the Server's Key Pairings
 $FOGCore->createKeyPair();
 // Database Load initiator
-if (!$DatabaseManager)
+if (is_null($DatabaseManager) || !($DatabaseManager instanceof DatabaseManager))
 	$DatabaseManager = new DatabaseManager();
-if (!$DB)
+if (is_null($DB) || !($DB instanceof MySQL))
 	$DB = $FOGCore->DB = $DatabaseManager->connect()->DB;
 // Set the memory limits
 ini_set('memory_limit',is_numeric($FOGCore->getSetting('FOG_MEMORY_LIMIT')) && $FOGCore->getSetting('FOG_MEMORY_LIMIT') >= 128 ? $FOGCore->getSetting('FOG_MEMORY_LIMIT').'M' : ini_get('memory_limit'));
 // HookManager
-if (!$HookManager)
+if (is_null($HookManager) || !($HookManager instanceof HookManager))
 	$HookManager = new HookManager();
 $HookManager->load();
-// FOGPageManager
-if (!$FOGPageManager)
-	$FOGPageManager = new FOGPageManager();
 $Init::endInit();
