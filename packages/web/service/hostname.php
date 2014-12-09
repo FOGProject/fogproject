51f8f20b82e4e3d9f9c8a76d2c017f24dcf43555
Tom Elliott <tommygunsster@gmail.com>
2014-12-09 22:56:59 +0000

    Make hostname changer only send data when Join AD is enabled.  It still sends the fields legacy clients need to operate, but it will send the values as if it's null set.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2768 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/hostname.php
+++ packages/web/service/hostname.php
@@ -18,7 +18,7 @@ try
 		$enckey = substr($Host->get('ADPass'),-32);
 		$decrypt = $FOGCore->aesdecrypt($encdat,$enckey);
 		if ($decrypt && mb_detect_encoding($decrypt,'UTF-8',true))
-			$password = $FOGCore->aesencrypt($decrypt,$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY').$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY'));
+			$password = $FOGCore->aesencrypt($decrypt,$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY')).$FOGCore->getSetting('FOG_AES_ADPASS_ENCRYPT_KEY');
 		else
 			$password = $Host->get('ADPass');
 		$Host->set('ADPass',trim($password))->save();
@@ -26,13 +26,15 @@ try
 	// Send the information.
 	$Datatosend = $FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? "#!ok\nhostname=".$Host->get('name')."\n" : '#!ok='.$Host->get('name')."\n";
 	$Datatosend .= '#AD='.$Host->get('useAD')."\n";
-	$Datatosend .= '#ADDom='.$Host->get('ADDomain')."\n";
-	$Datatosend .= '#ADOU='.$Host->get('ADOU')."\n";
+	$Datatosend .= '#ADDom='.($Host->get('useAD') ? $Host->get('ADDomain') : '')."\n";
+	$Datatosend .= '#ADOU='.($Host->get('useAD') ? $Host->get('ADOU') : '')."\n";
 	$Datatosend .= '#ADUser='.($Host->get('useAD') ? (strpos($Host->get('ADUser'),'\\') || strpos($Host->get('ADUser'),'@') ? $Host->get('ADUser') : $Host->get('ADDomain').'\\'.$Host->get('ADUser')) : '')."\n";
-	$Datatosend .= '#ADPass='.$Host->get('ADPass');
+	$Datatosend .= '#ADPass='.($Host->get('useAD') ? $Host->get('ADPass') : '');
 	if (trim(base64_decode($Host->get('productKey'))))
 		$Datatosend .= "\n#Key=".base64_decode($Host->get('productKey'));
-	$FOGCore->setSetting('FOG_AES_ADPASS_ENCRYPT_KEY',$FOGCore->randomString(32));
+	if ($_REQUEST['newService'] && $FOGCore->getSetting('FOG_NEW_CLIENT'))
+		$FOGCore->setSetting('FOG_AES_ADPASS_ENCRYPT_KEY',$FOGCore->randomString(32));
+
 }
 catch (Exception $e)
 {
