38a0b3ea02af8015f00259ab2328ec409f25ec4b
Tom Elliott <tommygunsster@gmail.com>
2014-12-12 02:20:34 +0000

    Keep redhat installs from installing init's/kernels twice.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2784 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/redhat/functions.sh
+++ lib/redhat/functions.sh
@@ -380,10 +380,6 @@ configureHttpd()
 		fi
 		mkdir "$webdirdest";
 		cp -Rf $webdirsrc/* $webdirdest/
-		wget -O "${webdirdest}/service/ipxe/bzImage" "http://downloads.sourceforge.net/project/freeghost/KernelList/bzImage" >/dev/null 2>&1
-		wget -O "${webdirdest}/service/ipxe/bzImage32" "http://downloads.sourceforge.net/project/freeghost/KernelList/bzImage32" >/dev/null 2>&1
-		wget -O "${webdirdest}/service/ipxe/init.xz" "http://downloads.sourceforge.net/project/freeghost/InitList/init.xz" >/dev/null 2>&1
-		wget -O "${webdirdest}/service/ipxe/init_32.xz" "http://downloads.sourceforge.net/project/freeghost/InitList/init_32.xz" >/dev/null 2>&1
 		echo "<?php
 /**
 * Class Name: Config
