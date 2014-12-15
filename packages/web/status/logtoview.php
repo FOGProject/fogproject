0427de14fc25f0489dafd3395274ba340c40e0db
Tom Elliott <tommygunsster@gmail.com>
2014-12-15 01:59:17 +0000

    Even better handling.  It should report access log without reporting the request to get our information.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2805 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/logtoview.php
+++ packages/web/status/logtoview.php
@@ -1,3 +1,3 @@
 <?php
-$vals = shell_exec("tail -n {$_REQUEST['lines']} {$_REQUEST['file']}");
+$vals = shell_exec("cat {$_REQUEST['file']} | grep -v logtoview.php | tail -n {$_REQUEST['lines']}");
 print json_encode($vals);
