72be2b76041819602c3b91d63ace236d192f289d
Tom Elliott <tommygunsster@gmail.com>
2014-12-15 13:35:36 +0000

    Maybe ensure line numbers are always passed.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2809 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/logtoview.php
+++ packages/web/status/logtoview.php
@@ -1,3 +1,3 @@
 <?php
-$vals = shell_exec("cat {$_REQUEST['file']} | grep -v logtoview.php | tail -n {$_REQUEST['lines']}");
+$vals = shell_exec("cat {$_REQUEST['file']} | grep -v logtoview.php | tail -".$_REQUEST['lines'] ? $_REQUEST['lines'] : '20');
 print json_encode($vals);
