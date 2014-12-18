2f36d37a7f2f94ce0be618cef3eb0a3c96ad1ba3
Tom Elliott <tommygunsster@gmail.com>
2014-12-18 19:04:47 +0000

    Maybe make tasks that checkin with Post_Wipe script not really care if the host is valid or not so donate or wipe tasks can just happen.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2822 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/Post_Wipe.php
+++ packages/web/service/Post_Wipe.php
@@ -9,25 +9,29 @@ try
 	if (!$MACs) throw new Exception($foglang['InvalidMAC']);
 	// Get the Host
 	$Host = $HostManager->getHostByMacAddresses($MACs);
-	if (!$Host->isValid())
-		throw new Exception(_('Invalid Host'));
-	// Task for Host
-	$Task = $Host->get('task');
-	if (!$Task->isValid())
-		throw new Exception(sprintf('%s: %s (%s)',_('No Active Task found for Host'),$Host->get('name'),$MACAddress));
-	if (!in_array($Task->get('typeID'),array(12,13)))
-		$Task->set('stateID',4);
-	if ($Task->save())
+	if ($Host && $Host->isValid())
 	{
-		// Task Logging.
-		$TaskLog = new TaskLog($Task);
-		$TaskLog->set('taskID',$Task->get('id'))
-				->set('taskStateID',$Task->get('stateID'))
-				->set('createdTime',$Task->get('createdTime'))
-				->set('createdBy',$Task->get('createdBy'))
-				->save();
-		print '##';
+		// Task for Host
+		$Task = $Host->get('task');
+		if (!$Task->isValid())
+			throw new Exception(sprintf('%s: %s (%s)',_('No Active Task found for Host'),$Host->get('name'),$MACAddress));
+		if ($Task && $Task->isValid())
+		{
+			if (!in_array($Task->get('typeID'),array(12,13)))
+				$Task->set('stateID',4);
+			if ($Task->save())
+			{
+				// Task Logging.
+				$TaskLog = new TaskLog($Task);
+				$TaskLog->set('taskID',$Task->get('id'))
+						->set('taskStateID',$Task->get('stateID'))
+						->set('createdTime',$Task->get('createdTime'))
+						->set('createdBy',$Task->get('createdBy'))
+						->save();
+			}
+		}
 	}
+	print '##';
 }
 catch (Exception $e)
 {
