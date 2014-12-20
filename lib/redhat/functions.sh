af67b1f63d09aa91d6f71f14990141947dd7ece8
Tom Elliott <tommygunsster@gmail.com>
2014-12-20 05:08:36 +0000

    Fix permissions setting on installer again.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2829 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/redhat/functions.sh
+++ lib/redhat/functions.sh
@@ -495,7 +495,7 @@ class Config
 		echo -n "  * Changing permissions on apache log files...";
 		chmod +rx /var/log/httpd;
 		chmod +rx /var/log/httpd/{access,error}_log;
-		chmod -R ${apacheuser}:${apacheuser} /var/www;
+		chown -R ${apacheuser}:${apacheuser} /var/www;
 		echo "OK";
 		echo -n "  * Downloading kernels and inits...";
 		wget -O "${webdirdest}/service/ipxe/bzImage" "http://downloads.sourceforge.net/project/freeghost/KernelList/bzImage" >/dev/null 2>&1
