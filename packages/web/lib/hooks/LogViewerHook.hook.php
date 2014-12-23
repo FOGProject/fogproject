d104616e56eaf5fe817efe2d9c0712c691fd7c81
Tom Elliott <tommygunsster@gmail.com>
2014-12-23 23:42:06 +0000

    Add a few comments to the log viewer to make it more easy to use.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2840 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/hooks/LogViewerHook.hook.php
+++ packages/web/lib/hooks/LogViewerHook.hook.php
@@ -1,4 +1,13 @@
 <?php
+/** class LogViewerHook
+ * Just allows user to add in any logs they feel they need on the log viewer
+ *
+ * NOTE: The log file will need web access to view.  This is given by the root
+ * of the folder to read the contents and files of with:
+ * chmod -R 755 <foldername>
+ * list translates in ls -l to:
+ * drwxr-xr-x
+ */
 class LogViewerHook extends Hook
 {
 	var $name = 'LogViewerHook';
@@ -10,8 +19,8 @@ class LogViewerHook extends Hook
 		foreach($arguments['files'] AS $name => $filearray)
 		{
 			$ftpstart = $arguments['ftpstart'][$name];
-			$logfile = "/var/log/rsync.log";
-			$shortdesc = "Rsync";
+			$logfile = "/var/log/syslog";
+			$shortdesc = "System Log";
 			if (file_exists($ftpstart.$logfile))
 				$arguments['files'][$name][$shortdesc] = $ftpstart.$logfile;
 		}
