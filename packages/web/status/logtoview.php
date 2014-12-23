4625a9d2b1fbd850178c7fc28c0fdccffb2251cb
Tom Elliott <tommygunsster@gmail.com>
2014-12-23 15:25:02 +0000

    Better handling of the log files tailing.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2838 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/logtoview.php
+++ packages/web/status/logtoview.php
@@ -1,22 +1,16 @@
 <?php
 function read_file($file, $lines = 20, $max_chunk_size = 4096) {
-	$data = '';
-	$fp = fopen($file,"r");
-	$block = 4096;
-	$max = filesize($file);
-	for ($len = 0; $len < $max; $len += $block) {
-		$seekSize = ($max - $len > $block) ? $block : $max - $len;
-		fseek($fp, ($len + $seekSize) * -1, SEEK_END);
-		$data = fread($fp, $seekSize) . $data;
-		if (substr_count($data, "\n") >= $lines + 1) {
-			if (substr($data, strlen($data)-1,1) !== "\n") {
-				$data .= "\n";
-			}
-			preg_match("!(.*?\n){". $lines ."}$!", $data, $match);
-			return $match[0];
-		}
+	$data = file_get_contents($file);
+	$data = explode("\n",$data);
+	$data = array_reverse($data);
+	$cnt = 0;
+	foreach($data AS $line)
+	{
+		$text[] = $line;
+		if ($cnt++ == $lines) break;
 	}
-	return $data;
+	$data = array_reverse($text);
+	return implode($data,"\n");
 }
 $vals = read_file($_REQUEST['file'],$_REQUEST['lines'] ? $_REQUEST['lines'] : 20);
 print json_encode($vals);
