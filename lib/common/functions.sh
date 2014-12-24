b38b4130fe000c7b56f1d4b7bcac691824b87644
Tom Elliott <tommygunsster@gmail.com>
2014-12-24 11:36:23 +0000

    Hopefully fix the linking stuff properly
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2846 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -314,12 +314,12 @@ EOF
 
 linkOptFogDir()
 {
-	if [ ! -L "/var/log/fog" ]; then
+	if [ ! -h "/var/log/fog" ]; then
 		echo -n "  * Linking /opt/fog/log to /var/log/fog...";
 		ln -s "/opt/fog/log" "/var/log/fog";
 		echo "OK";
 	fi
-	if [ ! -L "/etc/fog" ]; then
+	if [ ! -h "/etc/fog" ]; then
 		echo -n "  * Linking /opt/fog/service/etc to /etc/fog...";
 		ln -s "/opt/fog/service/etc" "/etc/fog";
 		echo "OK";
