1a4845babda5b85aed84720fa730dd200c186661
Tom Elliott <tommygunsster@gmail.com>
2014-12-24 10:23:28 +0000

    Add button to log viewer to allow pause/start so people can copy and paste from the log viewer.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2843 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.about.log.js
+++ packages/web/management/js/fog/fog.about.log.js
@@ -1,9 +1,23 @@
 var LogToView;
 var LinesToView;
+var LogTimer
 $(function() {
 	LogToView = $('#logToView').val();
 	LinesToView = $('#linesToView').val();
+	$('#logpause').val('Pause');
 	LogGetData();
+	$('#logpause').click(function(e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).val('Pause');
+			LogGetData();
+		} else {
+			$(this).addClass('active');
+			$(this).val('Continue');
+			clearTimeout(LogTimer);
+		}
+	});
 	$('#logToView, #linesToView').change(function() {
 		LogToView = $('#logToView').val();
 		LinesToView = $('#linesToView').val();
@@ -22,7 +36,9 @@ function LogGetData() {
 		},
 		dataType: 'json',
 		success: displayLog,
-		complete: setTimeout(LogGetData,10000)
+		complete: function() {
+			LogTimer = setTimeout(LogGetData,10000);
+		}
 	});
 }
 function displayLog(data) {
