5d76dfdb18c8541241195fe5ef914a6299dea87d
Tom Elliott <tommygunsster@gmail.com>
2015-02-02 12:24:10 +0000

    Update kernel files for 3.18.5.  Update installers, hopefully, to install php-mysqlnd properly.  This means Centos/Fedora needs the remi repo which I've inserted into installer to install at load out if it isn't already.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2958 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/redhat/functions.sh
+++ lib/redhat/functions.sh
@@ -557,7 +557,13 @@ installPackages()
 	then
 		packages="$packages $langPackages"
 	fi
-	
+	RHVER=`rpm -qa | grep release | xargs rpm -q --queryformat '%{VERSION}' | cut -c -1`;
+	if [ -d "/etc/yum.repos.d/" -a ! -f "/etc/yum.repos.d/remi.repo" ]; then
+		echo -n "  * Copying needed repo...";
+		rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-$RHVER.rpm
+		rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
+		echo "OK";
+	fi
 	echo "  * Packages to be installed: $packages";
 	echo "";
 
