85326802a755053200a4cf176cf8c253e537972d
Tom Elliott <tommygunsster@gmail.com>
2015-02-05 12:27:13 +0000

    Maybe fix the LDAP plugin a little better?
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2978 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/ldap/class/LDAP.class.php
+++ packages/web/lib/plugins/ldap/class/LDAP.class.php
@@ -27,22 +27,28 @@ class LDAP extends FOGController
 		$this->getClass('LDAPManager')->find(array('LDAPID' => $this->get('id')));
 		return parent::destroy($field);
 	}
+	private function LDAPUp($timeout = 3)
+	{
+		if ($this->get('port') != 389 && $this->get('port') != 689)
+			throw new Exception(_('Port is not valid ldap/ldaps ports');
+		$port = $this->get('port');
+		$server = $this->get('port') == 689 ? 'ldaps://'.$this->get('address') : $this->get('address');
+		$sock = fsockopen($server, $this->get('port'), $errno, $errstr, $timeout);
+		if (!$sock) return false;
+		fclose($sock);
+		$this->set('address', $server);
+		return true;
+	}
 	public function authLDAP($user,$pass)
 	{
-		$server = $this->get('address');
-		$port = $this->get('port');
-		if ($fp = fsockopen($server,$port,$errno,$errstr,3))
-			fclose($fp);
-		// $con = @ldap_connect("ldaps://".$server , $port);
-		$con = @ldap_connect($server,$port);
+		if (!$this->LDAPUp()) return false;
+		$ldapconn = @ldap_connect($this->get('address'),$this->get('port'));
 		// set protocol options
-		ldap_set_option($con, LDAP_OPT_PROTOCOL_VERSION, 3);
-		ldap_set_option($con, LDAP_OPT_REFERRALS, 0);
-		$r=ldap_bind($con, "uid=".$user.",".$this->get('DN'), $pass);
-		ldap_close($con);
-		if ($r)
-			return true;
-		else
+		ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+		if (!ldap_bind($ldapconn,"uid=$user,{$this->get(DN)}",$pass))
 			return false;
+		ldap_close($ldapconn);
+		return true;
 	}
 }
