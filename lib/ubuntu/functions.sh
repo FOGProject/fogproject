71160d9673645997d2493fe8ebf28112469031fe
Tom Elliott <tommygunsster@gmail.com>
2015-02-07 01:30:26 +0000

    Found issue in vsftp checker.  This should correct it properly once again.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@2985 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/ubuntu/functions.sh
+++ lib/ubuntu/functions.sh
@@ -139,7 +139,7 @@ tcp_wrappers=YES" > "$ftpconfig";
 	vsftp=`vsftpd -version 0>&1`;
 	vsvermaj=`echo $vsftp | awk -F. '{print $1}' | awk '{print $3}'`;
 	vsverbug=`echo $vsftp | awk -F. '{print $3}'`;
-	if [ "$vsvermaj" -gt 3 ] || [ "$vsvermaj" -e 3 -a "$vsverbug" -ge 2 ]; then
+	if [ "$vsvermaj" -gt 3 ] || [ "$vsvermaj" = "3" -a "$vsverbug" -ge 2 ]; then
 		echo "seccomp_sandbox=NO" >> "$ftpconfig";
 	fi
 	sysv-rc-conf vsftpd on >/dev/null 2>&1;
