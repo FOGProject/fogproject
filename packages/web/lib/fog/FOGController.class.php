5ce7f85fbfa5744434905f0f4052ce57e6228d1f
Tom Elliott <tommygunsster@gmail.com>
2015-02-24 14:56:22 +0000

    Fix issue that I created....again.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3040 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGController.class.php
+++ packages/web/lib/fog/FOGController.class.php
@@ -331,7 +331,7 @@ abstract class FOGController extends FOGBase
 		$orderedData = array_merge($this->databaseFieldsFlipped,$classData);
 		$this->data = array_combine(array_keys($this->databaseFields),$orderedData);
 		foreach((array)$orderedData AS $key => $value)
-			$this->set($this->key($key),$value);
+			$this->set($key,$value);
 		foreach((array)$this->databaseFieldClassRelationships AS $class => $fields)
 			$this->add($fields[2],$this->getClass($class)->setQuery($queryData));
 		return $this;
