16b93e1fc79a2c72af452d1f375b0c19872e2a44
Tom Elliott <tommygunsster@gmail.com>
2015-02-25 18:21:09 +0000

    Officially add the pushbullet plugin from Jbob.  Update the host class so that the host modules can update/remove as needed.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3048 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ packages/web/lib/plugins/pushbullet/hooks/AddPushbulletMenuItem.hook.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *	Author:		Jbob
+**/
+class AddPushbulletMenuItem extends Hook
+{
+	var $name = 'AddPushbulletMenuItem';
+	var $description = 'Add menu item for pushbullet';
+	var $author = 'Joe Schmitt';
+	var $active = true;
+	var $node = 'pushbullet';
+	public function MenuData($arguments)
+	{
+		if ($_SESSION[$this->node])
+			$arguments['main'] = $this->array_insert_after('tasks',$arguments['main'],$this->node,array(_('Pushbullet Management'),'fa fa-bell fa-2x'));
+	}
+}
+$AddPushbulletMenuItem = new AddPushbulletMenuItem();
+// Register hooks
+$HookManager->register('MAIN_MENU_DATA', array($AddPushbulletMenuItem, 'MenuData'));
