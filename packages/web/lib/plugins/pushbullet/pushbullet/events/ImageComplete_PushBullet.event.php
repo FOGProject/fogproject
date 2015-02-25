6f7e5a4f007cb2957e5121a8572c48c41bc02b81
Jbob <contact@jbob.io>
2015-02-24 22:38:52 -0600

    Add pushbullet plugin

---
+++ packages/web/lib/plugins/pushbullet/pushbullet/events/ImageComplete_PushBullet.event.php
@@ -0,0 +1,29 @@
+<?php
+
+/****************************************************
+ *  Called when imaging is completed
+ *	Author:		Jbob
+ ***/
+require_once(BASEPATH.'/lib/plugins/pushbullet/libs/PushbulletHandler.php');
+
+
+class ImageComplete_PushBullet extends Event {
+	// Class variables
+	var $name = 'ImageComplete_PushBullet';
+	var $description = 'Triggers when a host finishes imaging';
+	var $author = 'Jbob';
+	var $active = true;
+	
+	public function onEvent($event, $data) {
+
+		foreach ((array)$this->getClass('PushbulletManager')->find() AS $Token)
+        {
+            $bulletHandler = new PushbulletHandler($Token->get('token'));
+            $bulletHandler->pushNote('', $data['HostName'].' Complete', 'This host has finished imaging');
+
+        }		
+
+	}
+}
+
+$EventManager->register('HOST_IMAGE_COMPLETE', new ImageComplete_PushBullet());
