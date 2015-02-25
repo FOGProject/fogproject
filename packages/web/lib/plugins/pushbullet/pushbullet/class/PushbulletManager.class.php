1075ba2f2f4e68b011f168f80e6099f9169707e0
Jbob <contact@jbob.io>
2015-02-24 22:38:52 -0600

    Add pushbullet plugin

---
+++ packages/web/lib/plugins/pushbullet/pushbullet/class/PushbulletManager.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *	Author:		Jbob
+**/
+class PushbulletManager extends FOGManagerController {
+	/**	install($name)
+		Method that installs the relevant plugin.
+
+		$name just sends the plugin name.  Useful
+		for schema adding.
+	*/
+	public function install($name) {   
+        $sql = "CREATE TABLE pushbullet
+        (pID INTEGER NOT NULL AUTO_INCREMENT,
+        pToken VARCHAR(250) NOT NULL,
+        pName VARCHAR(250) NOT NULL,
+        pEmail VARCHAR(250) NOT NULL,
+        PRIMARY KEY(pID),
+		KEY new_index (pToken))
+        ENGINE = MyISAM";
+        if (!$this->DB->query($sql))
+			return false;
+
+        return true;
+    }
+    
+	public function uninstall() {
+		if (!$this->DB->query("DROP TABLE pushbullet"))
+			return false;
+		return true;
+	}
+}
