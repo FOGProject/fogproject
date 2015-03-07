0cc6fa19bdc49643dbe6eb7eb3f37b03c53eb0bc
Tom Elliott <tommygunsster@gmail.com>
2015-03-07 18:58:05 +0000

    Move the node client/server stuff into it's own plugin.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3084 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ packages/web/lib/plugins/nodeclient/hooks/AddClientMenuItem.hook.php
@@ -0,0 +1,25 @@
+<?php
+class AddClientMenuItem extends Hook
+{
+	var $name = 'AddClientMenuItem';
+	var $description = 'Add menu item for Client';
+	var $author = 'Tom Elliott';
+	var $active = true;
+	var $node = 'nodeclient';
+	public function MenuData($arguments)
+	{
+		if (in_array($this->node,$_SESSION['PluginsInstalled']))
+		{
+			$arguments['main'] = $this->array_insert_after('service',$arguments['main'],$this->node,array(_('Node Client Management'),'fa fa-weixin fa-2x'));
+		}
+	}
+	public function addSearch($arguments)
+	{
+		if (in_array($this->node,$_SESSION['PluginsInstalled']))
+			array_push($arguments['searchPages'],$this->node);
+	}
+}
+$AddClientMenuItem = new AddClientMenuItem();
+// Register hooks
+$HookManager->register('MAIN_MENU_DATA', array($AddClientMenuItem, 'MenuData'));
+$HookManager->register('SEARCH_PAGES', array($AddClientMenuItem, 'addSearch'));
