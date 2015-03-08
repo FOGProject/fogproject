1d59b371f52527d9db9126223855e9eb02e1e14d
Tom Elliott <tommygunsster@gmail.com>
2015-03-08 18:12:08 +0000

    Attempt, once again, with an async mysql request as well as async processing/background processing of mysql files for upload.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3087 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/ubuntu/functions.sh
+++ lib/ubuntu/functions.sh
@@ -379,6 +379,7 @@ configureHttpd()
 		dbuser=$snmysqluser;
 	fi
 	echo -n "  * Setting up and starting Apache Web Server...";
+	php5enmod mysqlnd >/dev/null 2>&1;
 	sysv-rc-conf apache2 on;
 	mv /etc/apache2/mods-available/php5* /etc/apache2/mods-enabled/  >/dev/null 2>&1
 	/etc/init.d/apache2  stop  >/dev/null 2>&1
