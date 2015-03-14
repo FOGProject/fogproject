23438e6bfd744f7c460b7b214e3bb9add2d49041
Tom Elliott <tommygunsster@gmail.com>
2015-03-14 20:18:11 +0000

    Move functions to fogbase.  Hopefully use the new methods.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3115 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/greenfog.php
+++ packages/web/service/greenfog.php
@@ -2,26 +2,13 @@
 require_once('../commons/base.inc.php');
 try
 {
-	$HostManager = new HostManager();
-	$MACs = FOGCore::parseMacList($_REQUEST['mac']);
-	if (!$MACs)
-		throw new Exception('#!im');
-	// Get the Host
-	$Host = $HostManager->getHostByMacAddresses($MACs);
-	if (!$Host || !$Host->isValid() || $Host->get('pending'))
-		throw new Exception('#!ih');
-//	if ($_REQUEST['newService'] && !$Host->get('pub_key'))
-//		throw new Exception('#!ihc');
 	$index = 0;
 	foreach($FOGCore->getClass('GreenFogManager')->find() AS $gf)
 	{
 		$Datatosend .= ($FOGCore->getSetting('FOG_NEW_CLIENT') && $_REQUEST['newService'] ? ($index == 0 ? "#!ok\n" : '')."#task$index=".$gf->get('hour').'@'.$gf->get('min').'@'.$gf->get('action') : base64_encode($gf->get('hour').'@'.$gf->get('min').'@'.$gf->get('action')))."\n";
 		$index++;
 	}
-//	if ($_REQUEST['newService'])
-//		print "#!enkey=".$FOGCore->certEncrypt($Datasend,$Host);
-//	else
-		print $Datatosend;
+	$FOGCore->sendData($Datatosend);
 }
 catch (Exception $e)
 {
