4a57053b908980b5f046bd9041deb1eca4f7cd85
Tom Elliott <tommygunsster@gmail.com>
2015-03-19 16:35:26 +0000

    Add simplistic client version checker for right now.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3125 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -22,10 +22,11 @@ class System
 	private static function default_values()
 	{
 		(ini_get('date.timezone') ? date_default_timezone_set(ini_get('date.timezone')) : date_default_timezone_set('UTC'));
-		define('FOG_VERSION', '3124');
+		define('FOG_VERSION', '3125');
 		define('FOG_SCHEMA', 166);
 		define('FOG_SVN_REVISION', '$Revision: 2868 $');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
+		define('FOG_CLIENT_VERSION', '0.6.1');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
 		define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
 		define('SPACE_DEFAULT_STORAGE', '/images');
