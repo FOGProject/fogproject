44c3b4bd514903f493ecb3bf31a510654b9a38fd
Tom Elliott <tommygunsster@gmail.com>
2015-03-23 22:06:18 +0000

    Fix a dumb typo for line 6 of snapins.check.php.  Should not be $$FOGCore but rather $FOGCore
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3135 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -22,7 +22,7 @@ class System
 	private static function default_values()
 	{
 		(ini_get('date.timezone') ? date_default_timezone_set(ini_get('date.timezone')) : date_default_timezone_set('UTC'));
-		define('FOG_VERSION', '3134');
+		define('FOG_VERSION', '3135');
 		define('FOG_SCHEMA', 167);
 		define('FOG_SVN_REVISION', '$Revision: 2868 $');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
