514f4c95a583cdbff9123d3c8515919428468887
Tom Elliott <tommygunsster@gmail.com>
2015-04-24 17:40:58 +0000

    Fix FOGURLRequests to allow for custom sending maybe through other items.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3297 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGURLRequests.class.php
+++ packages/web/lib/fog/FOGURLRequests.class.php
@@ -2,7 +2,7 @@
 class FOGURLRequests extends FOGBase
 {
 	private $handle,$contextOptions;
-	public function __construct()
+	public function __construct($method = false,$data = null,$sendAsJSON = false,$auth = false)
 	{
 		parent::__construct();
 		$ProxyUsed = false;
@@ -28,6 +28,7 @@ class FOGURLRequests extends FOGBase
 			CURLOPT_ENCODING => '',
 			CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.6.12) Gecko/20110319 Firefox/4.0.1 ( .NET CLR 3.5.30729; .NET4.0E)',
 			CURLOPT_MAXREDIRS => 20,
+			CURLOPT_HEADER => false,
 		);
 		if ($ProxyUsed)
 		{
@@ -38,12 +39,30 @@ class FOGURLRequests extends FOGBase
 				$this->contextOptions[CURLOPT_PROXYUSERPWD] = $username.':'.$password;
 		}
 	}
-	public function process($urls, $callback = false)
+	public function process($urls, $method = false,$data = null,$sendAsJSON = false,$auth = false,$callback = false)
 	{
 		if (!is_array($urls)) $urls = array($urls);
 		foreach ($urls AS $url)
 		{
+			if ($method == 'GET' && $data !== null)
+				$url .= '?'.http_build_query($data);
 			$ch = curl_init($url);
+			$this->contextOptions[CURLOPT_URL] = $url;
+			if ($auth)
+				$this->contextOptions[CURLOPT_USERPWD] = $auth;
+			if ($method == 'POST' && $data !== null)
+			{
+				if ($sendAsJSON)
+				{
+					$data = json_encode($data);
+					$this->contextOptions[CURLOPT_HTTPHEADER] = array(
+						'Content-Type: application/json',
+						'Content-Length: '.strlen($data),
+					);
+				}
+				$this->contextOptions[CURLOPT_POSTFIELDS] = $data;
+			}
+			$this->contextOptions[CURLOPT_CUSTOMREQUEST] = $method;
 			curl_setopt_array($ch,$this->contextOptions);
 			$curl[$url] = $ch;
 			curl_multi_add_handle($this->handle,$ch);
@@ -60,14 +79,24 @@ class FOGURLRequests extends FOGBase
 			do
 			{
 				$mrc = curl_multi_exec($this->handle,$active);
+				$httpCode = curl_multi_info_read($this->handle);
+				if ($mrc > 0)
+					throw new Exception('cURL Error: '.curl_multi_strerror($mrc));
+				if ($httpCode[0] >= 400)
+				{
+					curl_multi_close($this->handle);
+					throw new Exception('cURL HTTP Error Code: '.$httpCode[0]);
+				}
 			} while ($mrc == CURLM_CALL_MULTI_PERFORM);
 		}
 		foreach($curl AS $url => $ch)
 		{
-			$data[] = curl_multi_getcontent($ch);
+			if ($callback)
+				$callback($ch);
+			$response[] = curl_multi_getcontent($ch);
 			curl_multi_remove_handle($this->handle,$ch);
 		}
-		return $data;
+		return $response;
 	}
 	public function __destruct()
 	{
