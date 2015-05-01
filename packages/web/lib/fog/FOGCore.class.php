e82c90844872741722b70816232fd6b4fa27cedd
Tom Elliott <tommygunsster@gmail.com>
2015-05-01 23:52:36 +0000

    Remove the prior file and recreate it as needed.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3340 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/FOGCore.class.php
+++ packages/web/lib/fog/FOGCore.class.php
@@ -336,11 +336,10 @@ class FOGCore extends FOGBase {
 			// Free the private key
 			openssl_free_key($privateKey);
 		}
-		//if (!file_exists($pub_path.'srvpublic.key')) {
-			$pub_key = openssl_pkey_get_private(file_get_contents($priv_path.'.srvprivate.key'));
-			$pub_key = openssl_pkey_get_details($pub_key);
-			file_put_contents($pub_path.'srvpublic.key',$pub_key['key']);
-		//}
+		if (file_exists($pub_path)) exec("rm {$pub_path}srvpublic.key &");
+		$pub_key = openssl_pkey_get_private(file_get_contents($priv_path.'.srvprivate.key'));
+		$pub_key = openssl_pkey_get_details($pub_key);
+		file_put_contents($pub_path.'srvpublic.key',$pub_key['key']);
 	}
 	public function setSessionEnv() {
 		$_SESSION['theme'] = $this->getSetting('FOG_THEME');
