1eadb9ce404de5196ce7dfdb184258e23b0c134c
Tom Elliott <tommygunsster@gmail.com>
2015-05-04 11:14:17 +0000

    Re-add smbios version patch.  Update util-linux to 2.26.2.  Make other files a little cleaner as needed/possible.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3348 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/StorageNode.class.php
+++ packages/web/lib/fog/StorageNode.class.php
@@ -1,10 +1,5 @@
 <?php
-/** Class Name: StorageNode
-	Extends the FOGController class
-	Gets the storage nodes.
-*/
-class StorageNode extends FOGController
-{
+class StorageNode extends FOGController {
 	// Table
 	public $databaseTable = 'nfsGroupMembers';
 	// Name -> Database field name
@@ -32,30 +27,22 @@ class StorageNode extends FOGController
 		'path'
 	);
 	// Overrides
-	public function get($key = '')
-	{
+	public function get($key = '') {
 		// Path: Always remove trailing slash on NFS path
-		if ($this->key($key) == 'path')
-			return rtrim(parent::get($key), '/');
+		if ($this->key($key) == 'path') return rtrim(parent::get($key), '/');
 		// FOGController get()
 		return parent::get($key);
 	}
-	public function getStorageGroup()
-	{
-		return new StorageGroup($this->get('storageGroupID'));
-	}
-	public function getNodeFailure($Host)
-	{
+	public function getStorageGroup() {return new StorageGroup($this->get('storageGroupID'));}
+	public function getNodeFailure($Host) {
 		$DateInterval = $this->nice_date('-5 minutes');
 		$NodeFailures = $this->getClass('NodeFailureManager')->find(array(
 			'storageNodeID'	=> $this->get('id'), 
 			'hostID'	=> $this->DB->sanitize($Host instanceof Host ? $Host->get('id') : $Host),
 		));
-		foreach($NodeFailures AS $NodeFailure)
-		{
+		foreach($NodeFailures AS $NodeFailure) {
 			$DateTime = $this->nice_date($NodeFailure->get('failureTime'));
-			if ($DateTime->format('Y-m-d H:i:s') >= $DateInterval->format('Y-m-d H:i:s'))
-				return $NodeFailure;
+			if ($DateTime->format('Y-m-d H:i:s') >= $DateInterval->format('Y-m-d H:i:s')) return $NodeFailure;
 		}
 	}
 	public function getClientLoad() {
@@ -65,22 +52,16 @@ class StorageNode extends FOGController
 	    }
 	    return 0;
 	}
-	public function getUsedSlotCount()
-	{
+	public function getUsedSlotCount() {
 		$UsedTasks = explode(',',$this->FOGCore->getSetting('FOG_USED_TASKS'));
 		$countTasks = 0;
-		if (in_array(8,(array)$UsedTasks))
-		{
-			foreach($UsedTasks AS $ind => $val)
-			{
-				if ($val = 8)
-					unset($UsedTasks[$ind]);
+		if (in_array(8,(array)$UsedTasks)) {
+			foreach($UsedTasks AS $ind => $val) {
+				if ($val = 8) unset($UsedTasks[$ind]);
 			}
-			foreach ($this->getClass('TaskManager')->find(array('stateID' => 3,'typeID' => 8)) AS $MulticastTask)
-			{
+			foreach ($this->getClass('TaskManager')->find(array('stateID' => 3,'typeID' => 8)) AS $MulticastTask) {
 				$Multicast = current($this->getClass('MulticastSessionsAssociationManager')->find(array('taskID' => $MulticastTask->get('id'))));
-				if ($Multicast && $Multicast->isValid())
-				{
+				if ($Multicast && $Multicast->isValid()) {
 					$MulticastJob = new MulticastSessions($Multicast->get('msID'));
 					if ($MulticastJob && $MulticastJob->isValid())
 						$MulticastJobID[] = $MulticastJob->get('id');
@@ -97,8 +78,7 @@ class StorageNode extends FOGController
 		));
 		return $countTasks;
 	}
-	public function getQueuedSlotCount()
-	{
+	public function getQueuedSlotCount() {
 		return $this->getClass('TaskManager')->count(array(
 			'stateID' => array(1,2),
 			'typeID' => array(1,2,8,15,16,17),
