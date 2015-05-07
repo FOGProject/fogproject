8042d8f348c8223be4ccc4e9911586306e0385c1
Tom Elliott <tommygunsster@gmail.com>
2015-05-07 20:29:59 +0000

    update fog client version
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3372 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,11 +6,11 @@ class System {
 	  * @return void
 	  */
 	public function __construct() {
-		define('FOG_VERSION', '3371');
+		define('FOG_VERSION', '3372');
 		define('FOG_SCHEMA', 177);
 		define('FOG_SVN_REVISION', '$Revision: 2868 $');
 		define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
-		define('FOG_CLIENT_VERSION', '0.7.2');
+		define('FOG_CLIENT_VERSION', '0.7.4');
 		define('PHP_VERSION_REQUIRED', '5.3.0');
 		define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
 		define('SPACE_DEFAULT_STORAGE', '/images');
