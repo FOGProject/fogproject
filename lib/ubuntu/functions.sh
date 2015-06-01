0afd8c52d67c078166b1f2ffd98519cfc0a30156
Tom Elliott <tommygunsster@gmail.com>
2015-06-01 19:12:43 +0000

    ensure request to get client goes through http
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3502 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/ubuntu/functions.sh
+++ lib/ubuntu/functions.sh
@@ -624,10 +624,10 @@ class Config {
 		cd "${webdirdest}/service"
 		count=0;
 		while [ -z "$clientVer" -a "$count" -le 10 ]; do
-			clientVer=`wget $ipaddress/fog/service/getclient.php -q -O -`;
+			clientVer=`wget http://$ipaddress/fog/service/getclient.php -q -O -`;
 			if [ -z "$clientVer" ]; then
-				clientVer=`wget $ipaddress/service/getclient.php -q -O -`;
-			fi
+				clientVer=`wget http://$ipaddress/service/getclient.php -q -O -`;
+			fi5
 			count=`expr $count '+' 1`
 			sleep 2;
 		done
