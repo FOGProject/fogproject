726d1fde14b9ab729317dbc6fc31307932b2d0c8
Tom Elliott <tommygunsster@gmail.com>
2015-06-05 17:32:50 +0000

    Use a more secure/proper means to generate the fog user password.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3522 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
 	  * @return void
 	  */
 	public function __construct() {
-		define('FOG_VERSION', '3521');
+		define('FOG_VERSION', '3522');
 		define('FOG_SCHEMA', 181);
 		define('FOG_BCACHE_VER',1);
 		define('FOG_SVN_REVISION', '$Revision: 2868 $');
