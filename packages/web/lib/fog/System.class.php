ccc2679d98ec0d3535cbeba0d1cc1d1874a2dc49
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-06-05 11:30:54 +0000

    Fix issue with pending macs list.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3517 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
 	  * @return void
 	  */
 	public function __construct() {
-		define('FOG_VERSION', '3516');
+		define('FOG_VERSION', '3517');
 		define('FOG_SCHEMA', 181);
 		define('FOG_BCACHE_VER',1);
 		define('FOG_SVN_REVISION', '$Revision: 2868 $');
