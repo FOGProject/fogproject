4a4a095675dc44548d7fd4de05cecd7425993cf3
cspenceiv <cspenceiv@users.noreply.github.com>
2015-06-11 14:53:26 -0500

    Fixed wget statement in installation script
    Fixed for people like me who need to install on the Internet, but go isolated as soon as FOG is installed.
    
    In any case where a user wants to deploy on a different IP than what is currently assigned, this fix will allow installations to continue.

--- lib/ubuntu/functions.sh
+++ lib/ubuntu/functions.sh
@@ -624,9 +624,9 @@ class Config {
 		cd "${webdirdest}/service"
 		count=0;
 		while [ -z "$clientVer" -a "$count" -le 10 ]; do
-			clientVer=`wget http://$ipaddress/fog/service/getclient.php -q -O -`;
+			clientVer=`wget http://127.0.0.1/fog/service/getclient.php -q -O -`;
 			if [ -z "$clientVer" ]; then
-				clientVer=`wget http://$ipaddress/service/getclient.php -q -O -`;
+				clientVer=`wget http://127.0.0.1/service/getclient.php -q -O -`;
 			fi
 			count=`expr $count '+' 1`
 			sleep 2;
