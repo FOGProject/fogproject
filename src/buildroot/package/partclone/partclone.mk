df5b68cf0c3d4ff13a015634e3d39798b0e072ad
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-06-22 14:23:31 +0000

    Update partclone make file for neater linking of libs and includes.  Only call enable static once rather than twice as was happening during the compilation process.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3589 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/buildroot/package/partclone/partclone.mk
+++ src/buildroot/package/partclone/partclone.mk
@@ -10,11 +10,11 @@ PARTCLONE_SITE = http://sourceforge.net/projects/partclone/files/testing/src
 PARTCLONE_INSTALL_STAGING = YES
 PARTCLONE_AUTORECONF = YES
 PARTCLONE_DEPENDENCIES = attr e2fsprogs libgcrypt lzo xz zlib xfsprogs ncurses host-pkgconf
-PARTCLONE_CONF_OPTS = --enable-static --enable-xfs --enable-btrfs --enable-ntfs --enable-extfs --enable-fat --enable-hfsp --enable-static --enable-ncursesw
+PARTCLONE_CONF_OPTS = --enable-static --enable-xfs --enable-btrfs --enable-ntfs --enable-extfs --enable-fat --enable-hfsp --enable-ncursesw
 
 define PARTCLONE_LINK_LIBRARIES_TOOL
-	ln -f -s $(@D)/../xfsprogs-*/include/xfs $(@D)/../../staging/usr/include/
-    ln -f -s $(@D)/../xfsprogs-*/libxfs/.libs/libxfs.* $(@D)/../../staging/usr/lib/
+    ln -f -s $(BUILD_DIR)/xfsprogs-*/include/xfs $(STAGING_DIR)/usr/include/
+    ln -f -s $(BUILD_DIR)/xfsprogs-*/libxfs/.libs/libxfs.* $(STAGING_DIR)/usr/lib/
 endef
 
 PARTCLONE_POST_PATCH_HOOKS += PARTCLONE_LINK_LIBRARIES_TOOL
