6f2617a2fad3e01eeb1a1b8e673cc0a0cfada082
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-06-25 02:27:33 +0000

    Cleanup installer for CentOS service starting.  They changed the names and although its running mariadb, mariadb is nothing and mysql is whats present. Test if both fail then drop out, otherwise we should be okay. Update iPXE files to latest pull. Tweak the url requests to use a single do/while loop rather than two back to back.  Update a little better mechanisms for ImageReplicator and MulticastManager.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3601 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/MulticastManager.class.php
+++ packages/web/lib/fog/MulticastManager.class.php
@@ -33,7 +33,7 @@ class MulticastManager extends FOGService {
     private function serviceLoop() {
         while(true) {
             try {
-                $StorageNode = current($this->getClass('StorageNodeManager')->find(array('isMaster' => 1,'isEnabled' => 1,'ip' => $this->FOGCore->getIPAddress())));
+                foreach ($this->getClass('StorageNodeManager')->find(array('isMaster' => 1,'isEnabled' => 1,'ip' => $this->FOGCore->getIPAddress())) AS $StorageNode) if ($StorageNode->isValid()) break;
                 if (!$StorageNode || !$StorageNode->isValid()) throw new Exception(sprintf(" | This is not the master node"));
                 $myroot = $StorageNode->get('path');
                 $allTasks = MulticastTask::getAllMulticastTasks($myroot);
@@ -66,28 +66,25 @@ class MulticastManager extends FOGService {
                 foreach((array)$allTasks AS $curTask) {
                     if($this->isMCTaskNew($KnownTasks, $curTask->getID())) {
                         $this->outall(sprintf(" | Task (%s) %s is new!",$curTask->getID(),$curTask->getName()));
-                        if(file_exists($curTask->getImagePath())) {
-                            $this->outall(sprintf(" | Task (%s) %s image file found.",$curTask->getID(),$curTask->getImagePath()));
-                            if($curTask->getClientCount() > 0) {
-                                $this->outall(sprintf(" | Task (%s) %s client(s) found.",$curTask->getID(),$curTask->getClientCount()));
-                                if(is_numeric($curTask->getPortBase()) && $curTask->getPortBase() % 2 == 0) {
-                                    $this->outall(sprintf(" | Task (%s) %s sending on base port: %s",$curTask->getID(),$curTask->getName(),$curTask->getPortBase()));
-                                    $this->outall(sprintf("CMD: %s",$curTask->getCMD()));
-                                    if($curTask->startTask()) {
-                                        $this->outall(sprintf(" | Task (%s) %s has started.",$curTask->getID(),$curTask->getName()));
-                                        $KnownTasks[] = $curTask;
-                                    } else {
-                                        $this->outall(sprintf(" | Task (%s) %s failed to start!",$curTask->getID(),$curTask->getName()));
-                                        $this->outall(sprintf(" | * Don't panic, check all your settings!"));
-                                        $this->outall(sprintf(" |       even if the interface is incorrect the task won't start."));
-                                        $this->outall(sprintf(" |       If all else fails run the following command and see what it says:"));
-                                        $this->outall(sprintf(" |  %s",$curTask->getCMD()));
-                                        if($curTask->killTask()) $this->outall(sprintf(" | Task (%s) %s has been cleaned.",$curTask->getID(),$curTask->getName()));
-                                        else $this->outall(sprintf(" | Task (%s) %s has NOT been cleaned.",$curTask->getID(),$curTask->getName()));
-                                    }
-                                } else $this->outall(sprintf(" | Task (%s) %s failed to execute, port must be even and numeric.",$curTask->getID(),$curTask->getName()));
-                            } else $this->outall(sprintf(" | Task (%s) %s failed to execute, no clients are included!",$curTask->getID(),$curTask->getName()));
-                        } else $this->outall(sprintf(" | Task (%s) %s failed to execute, image file:%s not found!",$curTask->getID(),$curTask->getName(),$curTask->getImagePath()));
+                        if(!file_exists($curTask->getImagePath())) throw new Exception(sprintf(" Task (%s) %s failed to execute, image file:%s not found!",$curTask->getID(),$curTask->getName(),$curTask->getImagePath()));
+                        if (!$curTask->getClientCount()) throw new Exception(sprintf(" Task (%s) %s failed to execute, no clients are included!",$curTask->getID(),$curTask->getName()));
+                        if (!is_numeric($curTask->getPortBase()) || !($curTask->getPortBase() % 2 == 0)) throw new Exception(sprintf(" Task (%s) %s failed to execute, port must be even and numeric.",$curTask->getID(),$curTask->getName()));
+                        if (!$curTask->startTask()) {
+                            $this->outall(sprintf(" | Task (%s) %s failed to start!",$curTask->getID(),$curTask->getName()));
+                            $this->outall(sprintf(" | * Don't panic, check all your settings!"));
+                            $this->outall(sprintf(" |       even if the interface is incorrect the task won't start."));
+                            $this->outall(sprintf(" |       If all else fails run the following command and see what it says:"));
+                            $this->outall(sprintf(" |  %s",$curTask->getCMD()));
+                            $curTask->killTask();
+                            throw new Exception(" Task (%s) %s has been cleaned.");
+                        }
+                        $this->outall(sprintf(" | Task (%s) %s has been cleaned.",$curTask->getID(),$curTask->getName()));
+                        $this->outall(sprintf(" | Task (%s) %s image file found.",$curTask->getID(),$curTask->getImagePath()));
+                        $this->outall(sprintf(" | Task (%s) %s client(s) found.",$curTask->getID(),$curTask->getClientCount()));
+                        $this->outall(sprintf(" | Task (%s) %s sending on base port: %s",$curTask->getID(),$curTask->getName(),$curTask->getPortBase()));
+                        $this->outall(sprintf(" | CMD: %s",$curTask->getCMD()));
+                        $this->outall(sprintf(" | Task (%s) %s has started.",$curTask->getID(),$curTask->getName()));
+                        $KnownTasks[] = $curTask;
                     } else {
                         $runningTask = $this->getMCExistingTask($KnownTasks, $curTask->getID());
                         $curSession = new MulticastSessions($runningTask->getID());
@@ -104,10 +101,9 @@ class MulticastManager extends FOGService {
                         } else {
                             $this->outall(sprintf(" | Task (%s) %s is no longer running.",$runningTask->getID(),$runningTask->getName()));
                             if ($jobcancelled || $curSession->get('stateID') == 5) {
-                                if ($runningTask->killTask()) {
-                                    $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
-                                    $this->outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$runningTask->getID(),$runningTask->getName()));
-                                } else $this->outall(sprintf(" | Failed to kill task (%s) %s!",$runningTask->getID(),$runningTask->getName()));
+                                $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
+                                if (!$runningTask->killTask()) throw new Exception(sprintf(" Failed to kill task (%s) %s PID:%s!",$runningTask->getID(),$runningTask->getName(),$runningTask->getPID()));
+                                $this->outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$runningTask->getID(),$runningTask->getName()));
                             } else {
                                 $curSession->set('clients',0)->set('completetime',$this->nice_date()->format('Y-m-d H:i:s'))->set('name','')->set('stateID',4)->save();
                                 $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
