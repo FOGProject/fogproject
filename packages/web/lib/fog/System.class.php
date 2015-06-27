5b67c68ca72ed2c14f1eeb6d1cecf36c0a398790
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-06-27 02:55:18 +0000

    Attempt making everything more autoaccept centric
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3628 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3627');
+        define('FOG_VERSION', '3628');
         define('FOG_SCHEMA', 182);
         define('FOG_BCACHE_VER',2);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
