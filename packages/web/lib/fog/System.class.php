357adb2b6cf1d98722e418e26bb08d5df2b63fc8
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-06-30 16:50:04 +0000

    More fixing should work properly now.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3651 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3650');
+        define('FOG_VERSION', '3651');
         define('FOG_SCHEMA', 183);
         define('FOG_BCACHE_VER',5);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
