df06e019e70473f20ddc5e1c127f7b69962d5c03
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-07-04 04:56:44 +0000

    Fix up the host membership page elements.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3681 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,9 +6,9 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3680');
+        define('FOG_VERSION', '3681');
         define('FOG_SCHEMA', 184);
-        define('FOG_BCACHE_VER',5);
+        define('FOG_BCACHE_VER',6);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.8.4');
