2fdd49a6281a93b4cd6992326d41624b2798ed55
Tom Elliott <tommygunsster@gmail.com>
2015-07-05 01:21:03 +0000

    Now all should work/look properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3689 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3688');
+        define('FOG_VERSION', '3689');
         define('FOG_SCHEMA', 184);
         define('FOG_BCACHE_VER',6);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
