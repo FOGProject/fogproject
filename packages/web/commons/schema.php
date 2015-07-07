fd962ae6db36d7402f33dc5b19c9d802067f8c76
Tom Elliott <tommygunsster@gmail.com>
2015-07-07 21:55:35 +0000

    Fix the text alignment.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3716 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/schema.php
+++ packages/web/commons/schema.php
@@ -1989,5 +1989,5 @@ $this->schema[] = array(
     "DELETE FROM `".DATABASE_NAME."`.`globalSettings` WHERE `settingKey`='FOG_NEW_CLIENT'",
     "ALTER TABLE `" . DATABASE_NAME ."`.`hosts` ADD COLUMN `hostADPassLegacy` LONGTEXT AFTER `hostADPass`",
 	"UPDATE `".DATABASE_NAME."`.`globalSettings` SET `settingDesc`='This setting defines the default value to populate the hosts Active Directory password value but only uses the old FOGCrypt method of encryption.  This setting must be encrypted before stored.' WHERE `settingKey`='FOG_AD_DEFAULT_PASSWORD_LEGACY'",
-    "UPDATE `".DATABASE_NAME."`.`globalSettings` SET `settingDesc`='This setting defines the default value to populate the host\'s Active Directory password value.  This setting will encrypt and store the plain text value entered in this field automatically.' WHERE `settingKey`='FOG_AD_DEFAULT_PASSWORD'",
+    "UPDATE `".DATABASE_NAME."`.`globalSettings` SET `settingDesc`='This setting defines the default value to populate the host\'s Active Directory password value.  This setting will encrypt and store then encrypted value of the plain text value entered in this field automatically.' WHERE `settingKey`='FOG_AD_DEFAULT_PASSWORD'",
 );
