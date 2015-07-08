6bc34f10d0169791370390f213f72dc7181a7ca7
Tom Elliott <tommygunsster@gmail.com>
2015-07-08 14:34:16 +0000

    Now ImageReplicator should work properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3730 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3729');
+        define('FOG_VERSION', '3730');
         define('FOG_SCHEMA', 186);
         define('FOG_BCACHE_VER',7);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
