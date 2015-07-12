b3c92811d0db3d0aae7129bcf51748708cc154a1
Tom Elliott <tommygunsster@gmail.com>
2015-07-12 01:52:08 +0000

    Fix more things in service.  Move over to hopefully a much better logging and useful mechanism for replication.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3759 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/register.php
+++ packages/web/service/register.php
@@ -1,9 +1,9 @@
 <?php
 require_once('../commons/base.inc.php');
 try {
-    if (!$_REQUEST['newService'] && $_REQUEST['version'] != 2) throw new Exception('#!er:Invalid Version Number, please update this module.');
+    if (!$_REQUEST[newService] && $_REQUEST[version] != 2) throw new Exception('#!er:Invalid Version Number, please update this module.');
     // The total number of pending macs that can be used.
-    $maxPending = $FOGCore->getSetting('FOG_QUICKREG_MAX_PENDING_MACS');
+    $maxPending = $FOGCore->getSetting(FOG_QUICKREG_MAX_PENDING_MACS);
     // Get the actual host (if it is registered)
     $MACs = $FOGCore->getHostItem(true,false,false,true);
     $Host = $FOGCore->getHostItem(true,false,true,false,true);
@@ -16,12 +16,13 @@ try {
             $Host = $FOGCore->getClass(Host)
                 ->set(name, $_REQUEST[hostname])
                 ->set(description,'Pending Registration created by FOG_CLIENT')
-                ->set(pending,1)
-                ->addModule($ModuleIDs)
-                ->addPriMAC($PriMAC)
-                ->addAddMAC($MACs);
+                ->set(pending,1);
             if (!$Host->save()) throw new Exception("#!ih\n");
-			throw new Exception("#!ok\n");
+            $Host->addModule($ModuleIDs)
+                ->addPriMAC($PriMAC)
+                ->addAddMAC($MACs)
+                ->save();
+            throw new Exception("#!ok\n");
         }
     }
     // Check if count is okay.
@@ -29,7 +30,7 @@ try {
     // Cycle the MACs
     foreach($MACs AS $MAC) $AllMacs[] = strtolower($MAC);
     // Cycle the already known macs
-    $KnownMacs = $FOGCore->getClass('Host')->getMyMacs(false);
+    $KnownMacs = $FOGCore->getClass(Host)->getMyMacs(false);
     $MACs = array_unique(array_diff((array)$AllMacs,(array)$KnownMacs));
     if (count($MACs)) {
         $Host->addPendMAC($MACs);
