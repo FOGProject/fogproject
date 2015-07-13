3f8cf5c3589b8a653d2220c3074fe3b2e11395c3
Tom Elliott <tommygunsster@gmail.com>
2015-07-13 21:41:12 +0000

    Add the libcom32.c32 file to the tftpboot directory so pxelinux.0 chainloading should work.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3771 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '3770');
+        define('FOG_VERSION', '3771');
         define('FOG_SCHEMA', 187);
         define('FOG_BCACHE_VER',7);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
