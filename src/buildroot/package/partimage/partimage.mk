b009aea56f63b5e35fcabc32fdea23b8799aa3dd
Tom Elliott <tommygunsster@gmail.com>
2015-07-17 09:14:48 -0400

    More tweaks away from sourceforge downloads

--- src/buildroot/package/partimage/partimage.mk
+++ src/buildroot/package/partimage/partimage.mk
@@ -6,7 +6,7 @@
 
 PARTIMAGE_VERSION = 0.6.9
 PARTIMAGE_SOURCE = partimage-$(PARTIMAGE_VERSION).tar.bz2
-PARTIMAGE_SITE = http://sourceforge.net/projects/partimage/files/stable/$(PARTIMAGE_VERSION)
+PARTIMAGE_SITE = ftp://ftp.edu.ee/gentoo/distfiles
 PARTIMAGE_DEPENDENCIES = e2fsprogs
 PARTIMAGE_INSTALL_STAGING = YES
 PARTIMAGE_CONF_OPTS = ac_cv_func_setpgrp_void=yes
