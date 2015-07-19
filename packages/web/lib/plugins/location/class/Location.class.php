135a6b89e3d67f0c587ccce00fbab8fd109ac864
Tom Elliott <tommygunsster@gmail.com>
2015-07-19 17:37:47 -0400

    More foreach adjustments

--- packages/web/lib/plugins/location/class/Location.class.php
+++ packages/web/lib/plugins/location/class/Location.class.php
@@ -1,35 +1,94 @@
 <?php
-
-class Location extends FOGController
-{
+class Location extends FOGController {
 	// Table
 	public $databaseTable = 'location';
-	
 	// Name -> Database field name
 	public $databaseFields = array(
-		'id'		=> 'lID',
-		'name'		=> 'lName',
+		'id' => 'lID',
+		'name' => 'lName',
 		'description' => 'lDesc',
-		'createdBy'	=> 'lCreatedBy',
+		'createdBy' => 'lCreatedBy',
 		'createdTime' => 'lCreatedTime',
-		'storageGroupID'		=> 'lStorageGroupID',
+		'storageGroupID' => 'lStorageGroupID',
 		'storageNodeID' => 'lStorageNodeID',
 		'tftp' => 'lTftpEnabled',
 	);
-
+    // Allow setting / getting of these additional fields
+    public $additionalFields = array(
+        'hosts',
+        'hostsnotinme',
+    );
 	public $databaseFieldsRequired = array(
 		'name',
 		'storageGroupID',
 	);
-	public function destroy($field = 'id')
-	{
-		$this->getClass('LocationAssociationManager')->find(array('locationID' => $this->get('id')));
+    // Overides
+    private function loadHosts() {
+        if (!$this->isLoaded(hosts) && $this->get(id)) {
+            $HostIDs = $this->getClass(LocationAssociationManager)->find(array(locationID=>$this->get(id)),'','','','','','','hostID');
+            $this->set(hosts,$HostIDs);
+            $this->set(hostsnotinme,$this->getClass(HostManager)->find(array(id=>$HostIDs),'','','','','',true,'id'));
+        }
+        return $this;
+    }
+    public function get($key = '') {
+        if (in_array($this->key($key),array(hosts,hostsnotinme))) $this->loadHosts();
+        return parent::get($key);
+    }
+    public function set($key,$value) {
+        if ($this->key($key) == 'hosts') $this->loadHosts();
+        return parent::set($key,$value);
+    }
+    public function add($key,$value) {
+        if ($this->key($key) == 'hosts') $this->loadHosts();
+        return parent::add($key,$value);
+    }
+    public function remove($key,$value) {
+        if ($this->key($key) == 'hosts') $this->loadHosts();
+        return parent::remove($key,$value);
+    }
+    public function load($field = 'id') {
+        parent::load($field);
+        $methods = get_class_methods($this);
+        foreach($methods AS $i => &$method) {
+            if (strlen($method) > 5 && strpos($method,'load'))
+                $this->$method();
+        }
+        unset($method);
+    }
+    public function save() {
+        parent::save();
+        if ($this->isLoaded(hosts)) {
+            // Remove old rows
+            $this->getClass(LocationAssociationManager)->destroy(array(groupID=>$this->get(id)));
+            // Create assoc
+            foreach ($this->get(hosts) AS $i => &$Host) $this->getClass(LocationAssociation)->set(hostID,$Host)->set(locationID,$this->get(id))->save();
+            unset($Host);
+        }
+        return $this;
+    }
+	public function destroy($field = 'id') {
+		$this->getClass(LocationAssociationManager)->find(array(locationID=>$this->get(id)));
 		return parent::destroy($field);
 	}
+    public function addHost($addArray) {
+        // Add
+        foreach((array)$addArray AS $i => &$item) $this->add(hosts,$item);
+        unset($item);
+        // Return
+        return $this;
+    }
+    public function removeHost($removeArray) {
+        // Iterate array (or other as array)
+        foreach ((array)$removeArray AS $i => &$remove) $this->remove(hosts,$remove);
+        unset($remove);
+        // Return
+        return $this;
+    }
 	public function getStorageGroup() {
-		return $this->getClass('StorageGroup',$this->get('storageGroupID'));
+		return $this->getClass(StorageGroup,$this->get(storageGroupID));
 	}
 	public function getStorageNode() {
-		return $this->getClass('StorageNode',$this->get('storageNodeID'));
+		return $this->getClass(StorageNode,$this->get(storageNodeID));
 	}
 }
--- packages/web/lib/plugins/location/js/fog.location.js
+++ packages/web/lib/plugins/location/js/fog.location.js
@@ -1,11 +1,20 @@
 $(function() {
-		checkboxToggleSearchListPages();
-		$('#action-boxdel').submit(function() {
-			var checked = $('input.toggle-action:checked');
-			var locationIDArray = new Array();
-			for (var i = 0,len = checked.size();i < len;i++) {
-			locationIDArray[locationIDArray.length] = checked.eq(i).attr('value');
-			}
-			$('input[name="locationIDArray"]').val(locationIDArray.join(','));
-			});
-		});
+        checkboxToggleSearchListPages();
+        $('#action-boxdel').submit(function() {
+            var checked = $('input.toggle-action:checked');
+            var locationIDArray = new Array();
+            for (var i = 0,len = checked.size();i < len;i++) {
+            locationIDArray[locationIDArray.length] = checked.eq(i).attr('value');
+            }
+            $('input[name="locationIDArray"]').val(locationIDArray.join(','));
+            });
+        // Show hide based on checked state.
+        $('#hostNotInMe').hide();
+        $('#hostNoGroup').hide();
+        $('#hostMeShow').click(function() {
+            $('#hostNotInMe').toggle();
+            });
+        $('#hostNoShow').click(function() {
+            $('#hostNoGroup').toggle();
+            });
+        });
