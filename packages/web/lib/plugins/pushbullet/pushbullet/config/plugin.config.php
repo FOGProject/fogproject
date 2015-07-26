b8c5f6b26a39193ccc0a4657773eb65611233cf0
Tom Elliott <tommygunsster@gmail.com>
2015-07-26 18:04:03 -0400

    Attempt linking fog to itself just in case. remove the pushbullet plugin from the pushbullet plugin.  This was not necessary and just taking up space

--- packages/web/lib/plugins/pushbullet/pushbullet/config/plugin.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fog_plugin = array();
-$fog_plugin["name"] = "pushbullet";
-$fog_plugin["description"] = "Adds pushbullet integration. Get android/ios/desktop notifications on task completetion.";
-$fog_plugin["menuicon"] = "html/images/{$fog_plugin['name']}.png";
-$fog_plugin["menuicon_hover"] = null;
-$fog_plugin["entrypoint"] = "html/run.php";
