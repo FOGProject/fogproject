fae011d13d2999bf75cb2969b12fde19b12ce971
Tom Elliott <tommygunsster@gmail.com>
2015-07-29 16:49:09 +0000

    Fixup to image page to make sure things operate more properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3814 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4250');
+        define('FOG_VERSION', '4252');
         define('FOG_SCHEMA', 190);
         define('FOG_BCACHE_VER',8);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
