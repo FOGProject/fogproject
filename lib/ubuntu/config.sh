41c1f48e877741063b84cdf634e60927d6587fdf
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-07-30 22:57:51 +0000

    Maybe properly add add-apt-repository command for ubuntu.  Fix GroupManagementPage.class.php on line 621 as reported by Junkhacker.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3822 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/ubuntu/config.sh
+++ lib/ubuntu/config.sh
@@ -84,7 +84,6 @@ ftpconfig="/etc/vsftpd.conf"
 
 # where do snapins go?
 snapindir="/opt/fog/snapins"
-
 packageinstaller="apt-get -yq install -o Dpkg::='--force-confdef' -o Dpkg::Options::='--force-confold'"
 packagelist="apt-cache pkgnames | grep"
 packageupdater="apt-get -yq upgrade -o Dpkg::='--force-confdef' -o Dpkg::Options::='--force-confold'"
