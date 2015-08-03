e3eda743301abe3545b4e4502079a17e8ad16d61
Tom Elliott <tommygunsster@gmail.com>
2015-08-03 22:25:07 +0000

    Update fogversion appropriately
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3835 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4290');
+        define('FOG_VERSION', '4299');
         define('FOG_SCHEMA', 190);
         define('FOG_BCACHE_VER',10);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
