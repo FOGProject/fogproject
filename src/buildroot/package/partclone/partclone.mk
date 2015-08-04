1504d49eefb94c982481145fa333cc6afc8292c3
Tom Elliott <tommygunsster@gmail.com>
2015-08-04 18:00:00 +0000

    Hopefully make the builder create a link to the fail-mbr.bin.orig in the after patch setup.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3843 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/buildroot/package/partclone/partclone.mk
+++ src/buildroot/package/partclone/partclone.mk
@@ -13,8 +13,9 @@ PARTCLONE_DEPENDENCIES = attr e2fsprogs libgcrypt lzo xz zlib xfsprogs ncurses h
 PARTCLONE_CONF_OPTS = --enable-static --enable-xfs --enable-btrfs --enable-ntfs --enable-extfs --enable-fat --enable-hfsp --enable-ncursesw
 
 define PARTCLONE_LINK_LIBRARIES_TOOL
-    ln -f -s $(BUILD_DIR)/xfsprogs-*/include/xfs $(STAGING_DIR)/usr/include/
-    ln -f -s $(BUILD_DIR)/xfsprogs-*/libxfs/.libs/libxfs.* $(STAGING_DIR)/usr/lib/
+	ln -f -s $(BUILD_DIR)/xfsprogs-*/include/xfs $(STAGING_DIR)/usr/include/
+	ln -f -s $(BUILD_DIR)/xfsprogs-*/libxfs/.libs/libxfs.* $(STAGING_DIR)/usr/lib/
+	ln -f -s $(@D)/fail-mbr/fail-mbr.bin $(@D)/fail-mbr/fail-mbr.bin.orig
 endef
 
 PARTCLONE_POST_PATCH_HOOKS += PARTCLONE_LINK_LIBRARIES_TOOL
