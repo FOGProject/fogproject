6f89da7307edcdac5ddd1c069813327f51640a6b
Tom Elliott <tommygunsster@gmail.com>
2015-08-06 10:48:10 +0000

    Now valid ip returns properly
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3859 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -102,7 +102,7 @@ backupReports() {
     errorStat $?
 }
 validip() {
-    local ip=$1
+    local ip="$1"
     local stat=1
     if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
         OIFS=$IFS
@@ -112,7 +112,7 @@ validip() {
         [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
         stat=$?
     fi
-    return $stat
+    echo $stat
 }
 mask2cidr() {
     nbits=0
