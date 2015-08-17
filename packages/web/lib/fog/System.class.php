3d8912490b7830e883e8e1595d578dd80ff9a814
Tom Elliott <tommygunsster@gmail.com>
2015-08-17 13:55:31 +0000

    Update configs for kernel to 4.1.6.  Update buildroot configs to install badblocks package.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3902 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4441');
+        define('FOG_VERSION', '4443');
         define('FOG_SCHEMA', 192);
         define('FOG_BCACHE_VER',19);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
