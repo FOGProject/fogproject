4b0901f3318fdf1184c1ab2398c3b48942676410
Tom Elliott <tommygunsster@gmail.com>
2015-08-17 00:25:01 +0000

    Fix background alignment on default theme.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3899 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4435');
+        define('FOG_VERSION', '4437');
         define('FOG_SCHEMA', 192);
         define('FOG_BCACHE_VER',17);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
