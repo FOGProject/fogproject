d022936861ef869dd65dfa47ae582591a2add86a
Tom Elliott <tommygunsster@gmail.com>
2015-08-19 15:56:17 +0000

    Add iperf and iperf3 to the inits.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3909 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4455');
+        define('FOG_VERSION', '4457');
         define('FOG_SCHEMA', 192);
         define('FOG_BCACHE_VER',20);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
