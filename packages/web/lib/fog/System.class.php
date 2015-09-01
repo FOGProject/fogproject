bd1729fce839ee43f09047e3c95f108b6f43a9d1
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-09-01 21:35:19 +0000

    Ensure the isset for newService is set
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3957 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4551');
+        define('FOG_VERSION', '4553');
         define('FOG_SCHEMA', 192);
         define('FOG_BCACHE_VER',22);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
