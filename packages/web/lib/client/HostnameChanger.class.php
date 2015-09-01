6c341f75ed8e4b7b9d9d67ceb6f7c50415bdfc3e
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-09-01 10:21:51 +0000

    More work to move client service stuff to class based items.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3954 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ packages/web/lib/client/HostnameChanger.class.php
@@ -0,0 +1,23 @@
+<?php
+class HostnameChanger extends FOGClient implements FOGClientSend {
+    public function send() {
+        $this->send = '#!ok';
+        $password = $this->Host->get(ADPassLegacy);
+        if ($this->newService) {
+            $this->send .= "\n#hostname=".$this->Host->get(name)."\n";
+            $password = $this->aesdecrypt($this->Host->get(ADPass));
+            $this->Host->setAD();
+        }
+        else $this->send .= '='.$this->Host->get(name)."\n";
+        $adUser = (strpos($this->Host->get(ADUser),chr(92)) || strpos($this->Host->get(ADUser),chr(64)) ? $this->Host->get(ADUser) : $this->Host->get(ADDomain).chr(92).$this->Host->get(ADUser));
+        $this->send .= '#AD='.$this->Host->get(useAD)."\n";
+        if ($this->Host->get(useAD)) {
+            $this->send .= '#ADDom='.$this->Host->get(ADDomain)."\n";
+            $this->send .= '#ADOU='.$this->Host->get(ADOU)."\n";
+            $this->send .= '#ADUser='.$adUser."\n";
+            $this->send .= '#ADPass='.$password;
+            $productKey = trim(base64_decode($this->Host->get(productKey)));
+            if ($productKey) $this->send .= "\n#Key=".$productKey;
+        }
+    }
+}
