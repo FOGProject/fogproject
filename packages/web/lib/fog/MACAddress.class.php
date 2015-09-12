b5e33e10354a75b736e73150d6d14b71521582ad
Tom Elliott <tommygunsster@gmail.com>
2015-09-12 02:41:05 +0000

    More tweaks to improve mac address validation on a quad dot, dash delimited, and colon delimited MAC Address format, just in case.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@3985 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/MACAddress.class.php
+++ packages/web/lib/fog/MACAddress.class.php
@@ -1,7 +1,15 @@
 <?php
 class MACAddress extends FOGBase {
+    private $patterns = array(
+        '/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/',
+        '/^([a-fA-F0-9]{2}\-){5}[a-fA-F0-9]{2}$/',
+        '/^[a-fA-F0-9]{12}$/',
+        '/^([a-fA-F0-9]{4}\.){2}[a-fA-F0-9]{4}$/',
+    );
     /** $MAC the MAC to test */
-    private $MAC;
+    protected $MAC;
+    /** $tmpMAC the temp mac */
+    protected $tmpMAC;
     /** $Host the Host if used */
     private $Host;
     /** __construct() initiates
@@ -16,43 +24,44 @@ class MACAddress extends FOGBase {
     /** setMAC() sets the mac
      * @return class back
      */
-    public function setMAC() {
+    protected function setMAC() {
         try {
-            if (is_object($this->tmpMAC)) $MAC = trim(($this->tmpMAC instanceof MACAddress) ? strtolower($this->tmpMAC) : (($this->tmpMAC instanceof MACAddressAssociation) ? strtolower($this->tmpMAC->get(mac)) : ''));
-            else if (is_array($this->tmpMAC)) $MAC = trim($MAC[0]);
-            else if (strlen($this->tmpMAC) == 12) {
-                for ($i=0;$i<12;$i=$i+2) $newMAC[] = $this->tmpMAC{$i}.$this->tmpMAC{$i+1};
-                $MAC = implode(':',$newMAC);
-            } else if (strlen($this->tmpMAC) == 17) $MAC = str_replace('-', ':', $this->tmpMAC);
-            else $MAC = $this->tmpMAC;
-            $this->MAC = $MAC;
-			if (!$this->isValid()) throw new Exception("#!im\n");
+            if ($this->tmpMAC instanceof MACAddress) $this->MAC = self::normalizeMAC($this->tmpMAC);
+            else if ($this->tmpMAC instanceof MACAddressAssociation) $this->MAC = self::normalizeMAC($this->tmpMAC->get(mac));
+            else if (is_array($this->tmpMAC)) $this->MAC = self::normalizeMAC($this->tmpMAC[0]);
+            else $this->MAC = self::normalizeMAC($this->tmpMAC);
+            if (!$this->isValid()) throw new Exception("#!im\n");
         } catch (Exception $e) {
-            if ($this->debug) $this->FOGCore->debug($e->getMessage().' MAC: %s',$MAC);
+            if ($this->debug) $this->FOGCore->debug($e->getMessage().' MAC: %s', $this->MAC);
         }
         return $this;
+    }
+    /** normalizeMAC()
+     * @param $MAC the mac to normalize
+     * @return the normalized MAC
+     */
+    protected static function normalizeMAC($MAC) {
+        $hexDigits = preg_replace('/[^[:xdigit:]]/','',$MAC);
+        if (strlen($hexDigits) !== 12) throw new Exception("#!im\n");
+        return strtolower($hexDigits);
     }
     /** getMACPrefix() get the MACs prefix
      * @return the prefix
      */
     public function getMACPrefix() {
-        $tmpMAC = strtolower($this->MAC);
-        $tmpMAC = substr(str_replace(array(':','-'),'',$tmpMAC),0,6);
-        return join('-',str_split($tmpMAC,2));
+        return join('-',str_split(substr($this->MAC,0,6),2));
     }
     /** __toString() Magic method to return the string as defined
      * @return the mac address with colons
      */
     public function __toString() {
-        $tmpMAC = strtolower($this->MAC);
-        $tmpMAC = str_replace(array(':','-'),'',$tmpMAC);
-        return join(':',str_split($tmpMAC,2));
+        return join(':',str_split($this->MAC,2));
     }
     /** isValid() returns if the mac is valid
      * @return true or false
      */
     public function isValid() {
-        return preg_match('/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/', $this->MAC);
+        return preg_match('/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/', $this->MAC) || preg_match('/^([a-fA-F0-9]{2}\-){5}[a-fA-F0-9]{2}$/', $this->MAC) || preg_match('/^[a-fA-F0-9]{12}$/', $this->MAC) || preg_match('/^([a-fA-F0-9]{4}\.){2}[a-fA-F0-9]{4}$/', $this->MAC) ? true : false;
     }
     public function isPending() {
         $MAC = $this->getClass(MACAddressAssociationManager)->find(array(mac=>$this->MAC));
