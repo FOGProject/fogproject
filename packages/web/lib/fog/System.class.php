b6b9642f2e4dde5cd0c49ec2d7e101d09493d822
Tom Elliott <tommygunsster@gmail.com>
2015-09-18 00:16:59 +0000

    Service module checks should not be encrypted either.  They are just simple return of the check anyway.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4029 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4698');
+        define('FOG_VERSION', '4700');
         define('FOG_SCHEMA', 195);
         define('FOG_BCACHE_VER',23);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
