a97ab830a6d64ccdbcdcec34f5beed665ba27661
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-09-19 23:50:47 +0000

    Add a command display element so users can better understand the snapin runs with, snapin runs with args, the snapinfile, and the args the file is to run
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4036 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.snapin.js
+++ packages/web/management/js/fog/fog.snapin.js
@@ -8,4 +8,15 @@ $(function() {
         }
         $('input[name="snapinIDArray"]').val(snapinIDArray.join(','));
     });
+    updateCmdStore();
+    $('.cmdlet1,.cmdlet2,.cmdlet4').keyup(function() {
+        updateCmdStore();
+    });
+    $('.cmdlet3').change(function() {
+        updateCmdStore();
+    });
 });
+function updateCmdStore() {
+    var snapCMD = [$('.cmdlet1').val(),$('.cmdlet2').val(),$('.cmdlet3').val(),$('.cmdlet4').val()];
+    $('.snapincmd').val(snapCMD.join(' ')).css({width: '100%',border: 0,resize: 'none'});
+}
