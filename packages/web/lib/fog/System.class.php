7121866fa25a2464b13725488c7fc10469207bc9
Tom Elliott <tommygunsster@gmail.com>
2015-09-19 18:59:13 +0000

    More fixes, and appropriately update tasks for cancelled or complete.  Snapin tasks cancel if the snapinjob is still open, but no snapin tasks are avaiable.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4033 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4706');
+        define('FOG_VERSION', '4708');
         define('FOG_SCHEMA', 195);
         define('FOG_BCACHE_VER',24);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
