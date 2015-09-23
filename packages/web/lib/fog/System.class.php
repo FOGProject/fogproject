e24c8f5f88191f6cc435911b155ca1efa9b412a6
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-09-23 15:04:57 +0000

    Maybe make hostname changer work even for legacy clients.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4055 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -6,7 +6,7 @@ class System {
      * @return void
      */
     public function __construct() {
-        define('FOG_VERSION', '4750');
+        define('FOG_VERSION', '4752');
         define('FOG_SCHEMA', 196);
         define('FOG_BCACHE_VER',25);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
