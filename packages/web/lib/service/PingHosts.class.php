e0dfdca47c1f06c1417dee505a80bb7814c18392
Tom Elliott <tommygunsster@gmail.com>
2015-09-28 11:33:46 +0000

    Use a slightly more refined method to get items and load them.  This should be a slight bit faster.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4066 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/service/PingHosts.class.php
+++ packages/web/lib/service/PingHosts.class.php
@@ -5,25 +5,32 @@ class PingHosts extends FOGService {
     public $zzz = PINGHOSTSLEEPTIME;
     private function commonOutput() {
         try {
+            $webServerIP = $this->FOGCore->resolveHostName($this->FOGCore->getSetting('FOG_WEB_HOST'));
+            $this->outall(sprintf(' * FOG Web Host IP: %s',$webServerIP));
+            $serverIPs = $this->getIPAddress();
+            foreach ((array)$serverIPs AS $i => &$ip) {
+                if (!$i) $this->outall(" * This server's IP Addresses");
+                $this->outall(" |\t$ip");
+            }
             if (!in_array($this->FOGCore->resolveHostname($this->FOGCore->getSetting(FOG_WEB_HOST)),$this->getIPAddress())) throw new Exception(_('I am not the fog web server'));
             $this->outall(' * Attempting to ping '.$this->getClass(HostManager)->count().' host(s).');
-            $Hosts = $this->getClass(HostManager)->find();
+            $Hosts = $this->getClass(HostManager)->find('','','','','','','','id');
             foreach ($Hosts AS $i => &$Host) {
                 // Ensures the hostIP regardless of how it is entered,
                 // to remove any beginning/ending white space
-                $hostIP = trim($Host->get(ip));
+                $hostIP = trim($this->getClass(Host,$Host)->get(ip));
                 // Test IP Value and if valid, use it as the pinging source
                 if (filter_var($hostIP,FILTER_VALIDATE_IP)) $ip = $hostIP;
                 // Otherwise attempt to get the hostname resolved.
-                else $ip = $this->FOGCore->resolveHostname($Host->load()->get(name));
+                else $ip = $this->FOGCore->resolveHostname($this->getClass(Host,$Host)->get(name));
                 // If the host still isn't found, set value to -1
                 // Allows us to clarify what is up.
                 if (!filter_var($ip,FILTER_VALIDATE_IP)) {
-                    $Host->set(pingstatus,-1)->save();
+                    $this->getClass(Host,$Host)->set(pingstatus,-1)->save();
                     continue;
                 }
                 // If all above makes it here, perform the ping
-                $Host->set(pingstatus,(int)$this->getClass(Ping,$ip)->execute())->save();
+                $this->getClass(Host,$Host)->set(pingstatus,(int)$this->getClass(Ping,$ip)->execute())->save();
                 // Give CPU a little breather between pings
                 usleep(1000);
             }
