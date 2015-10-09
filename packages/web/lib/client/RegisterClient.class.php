e58c06cac965a450ed98d2f1d946d5073a9bcc39
Tom Elliott <tommygunsster@gmail.com>
2015-10-09 11:56:11 +0000

    Should fix pending host display and adds in general.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4129 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/RegisterClient.class.php
+++ packages/web/lib/client/RegisterClient.class.php
@@ -3,26 +3,25 @@ class RegisterClient extends FOGClient implements FOGClientSend {
     public function send() {
         $maxPending = 0;
         $MACs = array();
-        $maxPending = $this->FOGCore->getSetting(FOG_QUICKREG_MAX_PENDING_MACS);
+        $maxPending = $this->FOGCore->getSetting('FOG_QUICKREG_MAX_PENDING_MACS');
         $MACs = $this->getHostItem(true,false,false,true);
-        foreach ($MACs AS $i => &$MAC) $this->getClass(MACAddress,$MAC)->isValid() ? $macs[] = strtolower($MAC) : null;
+        foreach ($MACs AS $i => &$MAC) $this->getClass('MACAddress',$MAC)->isValid() ? $macs[] = strtolower($MAC) : null;
         unset($MAC);
         $MACs = $macs;
         unset($macs);
         if ($this->newService) {
+            $_REQUEST['hostname'] = trim($_REQUEST['hostname']);
             if (!($this->Host instanceof Host && $this->Host->isValid())) {
-                $this->Host = $this->getClass(HostManager)->find(array(name=>$_REQUEST[hostname]));
+                $this->Host = $this->getClass('HostManager')->find(array('name'=>$_REQUEST['hostname']));
                 $this->Host = @array_shift($this->Host);
-                if (!($this->Host instanceof Host && $this->Host->isValid() && !$this->Host->get(pending))) {
-                    $_REQUEST[hostname] = trim($_REQUEST[hostname]);
-                    if ($this->getClass(Host)->isHostnameSafe($_REQUEST[hostname])) throw new Exception('#!ih');
-                    $ModuleIDs = $this->getClass(ModuleManager)->find(array(isDefault=>1),'','','','','','','id');
+                if (!($this->Host instanceof Host && $this->Host->isValid() && !$this->Host->get('pending'))) {
+                    if (!$this->getClass('Host')->isHostnameSafe($_REQUEST['hostname'])) throw new Exception('#!ih');
                     $PriMAC = @array_shift($MACs);
-                    $this->Host = $this->getClass(Host)
-                        ->set(name,$_REQUEST[hostname])
-                        ->set(description,_('Pending Registration created by FOG_CLIENT'))
-                        ->set(pending,1)
-                        ->addModule($ModuleIDs)
+                    $this->Host = $this->getClass('Host')
+                        ->set('name',$_REQUEST['hostname'])
+                        ->set('description',_('Pending Registration created by FOG_CLIENT'))
+                        ->set('pending',1)
+                        ->addModule($this->getSubObjectIDs('Module',array('isDefault'=>1),'id'))
                         ->addPriMAC($PriMAC)
                         ->addAddMAC($MACs);
                     if (!$this->Host->save()) throw new Exception('#!db');
--- packages/web/lib/pages/DashboardPage.class.php
+++ packages/web/lib/pages/DashboardPage.class.php
@@ -17,11 +17,11 @@ class DashboardPage extends FOGPage {
         else if ($_SESSION['Pending-MACs']) $this->FOGCore->setMessage($macPend);
         $SystemUptime = $this->FOGCore->SystemUptime();
         $fields = array(
-            _('Username') => $this->FOGUser ? $this->FOGUser->get(name) : '',
-            _('Web Server') => $this->FOGCore->getSetting(FOG_WEB_HOST),
-            _('TFTP Server') => $this->FOGCore->getSetting(FOG_TFTP_HOST),
-            _('Load Average') => $SystemUptime[load],
-            _('System Uptime') => $SystemUptime[uptime],
+            _('Username') => $_SESSION['FOG_USERNAME'],
+            _('Web Server') => $this->FOGCore->getSetting('FOG_WEB_HOST'),
+            _('TFTP Server') => $this->FOGCore->getSetting('FOG_TFTP_HOST'),
+            _('Load Average') => $SystemUptime['load'],
+            _('System Uptime') => $SystemUptime['uptime'],
         );
         $this->templates = array(
             '${field}',
@@ -39,25 +39,25 @@ class DashboardPage extends FOGPage {
             );
         }
         unset($fielddata);
-        $this->HookManager->processEvent(DashboardData,array(data=>&$this->data,templates=>&$this->templates,attributes=>&$this->attributes));
+        $this->HookManager->processEvent('DashboardData',array('data'=>&$this->data,'templates'=>&$this->templates,'attributes'=>&$this->attributes));
         $this->render();
         echo '</li><li><h4>'._('System Activity').'</h4><div class="graph pie-graph" id="graph-activity"></div></li><li><h4>'._('Disk Information').'</h4><div id="diskusage-selector">';
-        $Nodes = $this->getClass(StorageNodeManager)->find(array(isEnabled=>1,isGraphEnabled=>1));
+        $Nodes = $this->getClass('StorageNodeManager')->find(array('isEnabled'=>1,'isGraphEnabled'=>1));
         foreach ($Nodes AS $i => &$StorageNode) {
-            $curroot = trim(trim($StorageNode->get(webroot),'/'));
+            $curroot = trim(trim($StorageNode->get('webroot'),'/'));
             $webroot = '/'.(strlen($curroot) > 1 ? $curroot.'/' : '');
-            $version = $this->FOGURLRequests->process($StorageNode->get(ip).$webroot.'/service/getversion.php','GET');
-            $options .= '<option value="'.$StorageNode->get(id).'">'.$StorageNode->get(name).($StorageNode->get(isMaster) == 1 ? " * " : ' ')."(${version[0]})".'</option>';
+            $version = $this->FOGURLRequests->process($StorageNode->get('ip').$webroot.'/service/getversion.php','GET');
+            $options .= '<option value="'.$StorageNode->get('id').'">'.$StorageNode->get('name').($StorageNode->get('isMaster') == 1 ? " * " : ' ')."(${version[0]})".'</option>';
         }
         unset($StorageNode);
         echo ($options ? '<select name="storagesel" style="whitespace: no-wrap; width: 100px; position: relative; top: 100px;">'.$options.'</select>' : null);
-        echo '</div><a href="?node=hwinfo"><div class="graph pie-graph" id="graph-diskusage"></div></a></li></ul><h3>'._('Imaging over the last 30 days').'</h3><div id="graph-30day" class="graph"></div><h3 id="graph-bandwidth-title">'.$this->foglang[Bandwidth].'- <span>'.$this->foglang[Transmit].'</span><!-- (<span>2 Minutes</span>)--></h3><div id="graph-bandwidth-filters"><div><a href="#" id="graph-bandwidth-filters-transmit" class="l active">'.$this->foglang[Transmit].'</a><a href="#" id="graph-bandwidth-filters-receive" class="l">'.$this->foglang[Receive].'</a></div><div class="spacer"></div><div><a href="#" rel="3600" class="r">'._('1 hour').'</a><a href="#" rel="1800" class="r">'._('30 Minutes').'</a><a href="#" rel="600" class="r">'._('10 Minutes').'</a><a href="#" rel="120" class="r active">'._('2 Minutes').'</a></div></div><div id="graph-bandwidth" class="graph"></div>';
-        for ($i = 0; $i <= 30; $i++) $Graph30dayData .= '["'.(1000*$this->nice_date()->modify("-$i days")->getTimestamp()).'", '.$this->getClass(ImagingLogManager)->count(array(start=>$this->nice_date()->modify("-$i days")->format('Y-m-d%'),finish=>$this->nice_date()->modify("-$i days")->format('Y-m-d%')),'OR').']'.($i < 30 ? ', ' : '');
+        echo '</div><a href="?node=hwinfo"><div class="graph pie-graph" id="graph-diskusage"></div></a></li></ul><h3>'._('Imaging over the last 30 days').'</h3><div id="graph-30day" class="graph"></div><h3 id="graph-bandwidth-title">'.$this->foglang['Bandwidth'].'- <span>'.$this->foglang['Transmit'].'</span><!-- (<span>2 Minutes</span>)--></h3><div id="graph-bandwidth-filters"><div><a href="#" id="graph-bandwidth-filters-transmit" class="l active">'.$this->foglang['Transmit'].'</a><a href="#" id="graph-bandwidth-filters-receive" class="l">'.$this->foglang['Receive'].'</a></div><div class="spacer"></div><div><a href="#" rel="3600" class="r">'._('1 hour').'</a><a href="#" rel="1800" class="r">'._('30 Minutes').'</a><a href="#" rel="600" class="r">'._('10 Minutes').'</a><a href="#" rel="120" class="r active">'._('2 Minutes').'</a></div></div><div id="graph-bandwidth" class="graph"></div>';
+        for ($i = 0; $i <= 30; $i++) $Graph30dayData .= '["'.(1000*$this->nice_date()->modify("-$i days")->getTimestamp()).'", '.$this->getClass('ImagingLogManager')->count(array('start'=>$this->nice_date()->modify("-$i days")->format('Y-m-d%'),'finish'=>$this->nice_date()->modify("-$i days")->format('Y-m-d%')),'OR').']'.($i < 30 ? ', ' : '');
         $ActivityActive = 0;
         $ActivityQueued = 0;
         $ActivitySlots = 0;
         $ActivityTotalClients = 0;
-        $Nodes = $this->getClass(StorageNodeManager)->find(array(isEnabled=>1));
+        $Nodes = $this->getClass('StorageNodeManager')->find(array('isEnabled'=>1));
         foreach($Nodes AS $i => &$StorageNode) {
             if ($StorageNode->isValid()) {
                 $ActivityActive += $StorageNode->getUsedSlotCount();
@@ -73,13 +73,13 @@ class DashboardPage extends FOGPage {
      * Display's the bandwidth bar on the dashboard page.
      */
     public function bandwidth() {
-        $Nodes = $this->getClass(StorageNodeManager)->find(array(isGraphEnabled=>1));
+        $Nodes = $this->getClass('StorageNodeManager')->find(array('isGraphEnabled'=>1));
         // Loop each storage node -> grab stats
         foreach($Nodes AS $i => &$StorageNode) {
-            $fetchedData = $this->FOGURLRequests->process(sprintf('http://%s/%s?dev=%s',$StorageNode->get(ip),ltrim($this->FOGCore->getSetting(FOG_NFS_BANDWIDTHPATH),'/'), $StorageNode->get('interface')),'GET');
+            $fetchedData = $this->FOGURLRequests->process(sprintf('http://%s/%s?dev=%s',$StorageNode->get('ip'),ltrim($this->FOGCore->getSetting('FOG_NFS_BANDWIDTHPATH'),'/'), $StorageNode->get('interface')),'GET');
             foreach((array)$fetchedData AS $i => &$dataSet) {
                 if (preg_match('/(.*)##(.*)/U', $dataSet,$match)) $data[$StorageNode->get(name)] = array(rx=>$match[1],tx=>$match[2]);
-                else $data[$StorageNode->get(name)] = json_decode($dataSet,true);
+                else $data[$StorageNode->get('name')] = json_decode($dataSet,true);
             }
             unset($dataSet);
         }
@@ -91,21 +91,21 @@ class DashboardPage extends FOGPage {
      */
     public function diskusage() {
         // Get the node ID -> grab the ino:
-        if ($this->obj->get(isGraphEnabled)) {
+        if ($this->obj instanceof StorageNode && $this->obj->get('isGraphEnabled')) {
             try {
-                $curroot = trim(trim($this->obj->get(webroot),'/'));
+                $curroot = trim(trim($this->obj->get('webroot'),'/'));
                 $webroot = '/'.(strlen($curroot) > 1 ? $curroot.'/' : '');
-                $URL = sprintf('http://%s%sstatus/freespace.php?path=%s',$this->obj->get(ip),$webroot,base64_encode($this->obj->get(path)));
+                $URL = sprintf('http://%s%sstatus/freespace.php?path=%s',$this->obj->get('ip'),$webroot,base64_encode($this->obj->get('path')));
                 if ($Response = $this->FOGURLRequests->process($URL,'GET')) {
                     // Legacy client
                     if (preg_match('#(.*)@(.*)#', $Response[0], $match)) $Data = array(free=>$match[1],used=>$match[2]);
                     else {
                         $Response = json_decode($Response[0], true);
-                        $Data = array(free=>$Response[free],used=>$Response[used]);
+                        $Data = array('free'=>$Response['free'],'used'=>$Response['used']);
                     }
-                } else throw new Exception('Failed to connect to '.$this->obj->get(name));
+                } else throw new Exception('Failed to connect to '.$this->obj->get('name'));
             } catch (Exception $e) {
-                $Data[error] = $e->getMessage();
+                $Data['error'] = $e->getMessage();
             }
         }
         echo json_encode((array)$Data);
@@ -116,20 +116,20 @@ class DashboardPage extends FOGPage {
     public function clientcount() {
         $ActivityActive = $ActivityQueued = $ActivityTotalClients = 0;
         if ($this->obj instanceof StorageNode) {
-            $Nodes = $this->getClass(StorageNodeManager)->find(array(isEnabled=>1,storageGroupID=>$this->obj->get(storageGroupID)));
+            $Nodes = $this->getClass('StorageNodeManager')->find(array('isEnabled'=>1,'storageGroupID'=>$this->obj->get('storageGroupID')));
             foreach($Nodes AS $i => &$SN) {
                 if ($SN->isValid()) {
                     $ActivityActive += $SN->getUsedSlotCount();
                     $ActivityQueued += $SN->getQueuedSlotCount();
-                    $ActivityTotalClients += $SN->get(maxClients) - $SN->getUsedSlotCount();
+                    $ActivityTotalClients += $SN->get('maxClients') - $SN->getUsedSlotCount();
                 }
             }
             unset($SN);
         }
         $data = array(
-            ActivityActive=>$ActivityActive,
-            ActivityQueued=>$ActivityQueued,
-            ActivitySlots=>$ActivityTotalClients,
+            'ActivityActive'=>$ActivityActive,
+            'ActivityQueued'=>$ActivityQueued,
+            'ActivitySlots'=>$ActivityTotalClients,
         );
         echo json_encode($data);
     }
