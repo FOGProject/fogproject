8d3ee1c07acbc975f4f0d85984efb06f96179bb9
Tom Elliott <tommygunsster@gmail.com>
2015-10-14 11:49:49 +0000

    Use better methods to test if user is logged in, by only checking once rather than multiple times.  This should assist in random timeouts due to regenerating data all too often.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4155 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/commons/init.php
+++ packages/web/commons/init.php
@@ -66,11 +66,10 @@ class Initiator {
         @error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
         @ini_set('session.save_handler','mm');
         @ini_set('session.cookie_httponly',true);
-        @session_start();
-        @session_cache_limiter('no-cache');
-        @session_set_cookie_params(0,null,null,true,true);
+        session_cache_limiter('no-cache');
+        if (!session_id()) session_start();
         @set_magic_quotes_runtime(0);
-        @ob_start(array(self,'sanitize_output'));
+        ob_start(array(self,'sanitize_output'));
         self::verCheck();
         self::extCheck();
         foreach($_REQUEST as $key => $val) $_REQUEST[$key] = is_array($val) ? filter_var_array($val,FILTER_SANITIZE_FULL_SPECIAL_CHARS,FILTER_FLAG_NO_ENCODE_QUOTES) : filter_var($val,FILTER_SANITIZE_FULL_SPECIAL_CHARS,FILTER_FLAG_NO_ENCODE_QUOTES);
