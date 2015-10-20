3da5187da3c23f17d33e8e6a32dfb5b50f6ea419
Tom Elliott <tommygunsster@gmail.com>
2015-10-20 17:03:38 +0000

    Fix a few other bugs and tweak other classes to commonize even further.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4189 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5018');
+        define('FOG_VERSION', '5020');
         define('FOG_SCHEMA', 197);
         define('FOG_BCACHE_VER',30);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
--- packages/web/lib/plugins/location/class/Location.class.php
+++ packages/web/lib/plugins/location/class/Location.class.php
@@ -1,9 +1,7 @@
 <?php
 class Location extends FOGController {
-	// Table
-	public $databaseTable = 'location';
-	// Name -> Database field name
-	public $databaseFields = array(
+	protected $databaseTable = 'location';
+	protected $databaseFields = array(
 		'id' => 'lID',
 		'name' => 'lName',
 		'description' => 'lDesc',
@@ -13,83 +11,85 @@ class Location extends FOGController {
 		'storageNodeID' => 'lStorageNodeID',
 		'tftp' => 'lTftpEnabled',
 	);
-    // Allow setting / getting of these additional fields
-    public $additionalFields = array(
-        'hosts',
-        'hostsnotinme',
-    );
-	public $databaseFieldsRequired = array(
+	protected $databaseFieldsRequired = array(
 		'name',
 		'storageGroupID',
 	);
-    // Overides
-    private function loadHosts() {
-        if (!$this->isLoaded(hosts) && $this->get(id)) {
-            $HostIDs = $this->getClass(LocationAssociationManager)->find(array(locationID=>$this->get(id)),'','','','','','','hostID');
-            $this->set(hosts,$HostIDs);
-            $this->set(hostsnotinme,$this->getClass(HostManager)->find(array(id=>$HostIDs),'','','','','',true,'id'));
-        }
-        return $this;
-    }
+    protected $additionalFields = array(
+        'hosts',
+        'hostsnotinme',
+    );
     public function get($key = '') {
-        if (in_array($this->key($key),array(hosts,hostsnotinme))) $this->loadHosts();
+        $key = $this->key($key);
+        if (!$this->isLoaded($key)) $this->loadItem($key);
         return parent::get($key);
     }
     public function set($key,$value) {
-        if ($this->key($key) == 'hosts') $this->loadHosts();
+        $key = $this->key($key);
+        if (!$this->isLoaded($key)) $this->loadItem($key);
         return parent::set($key,$value);
     }
     public function add($key,$value) {
-        if ($this->key($key) == 'hosts') $this->loadHosts();
+        $key = $this->key($key);
+        if (!$this->isLoaded($key)) $this->loadItem($key);
         return parent::add($key,$value);
     }
     public function remove($key,$value) {
-        if ($this->key($key) == 'hosts') $this->loadHosts();
+        $key = $this->key($key);
+        if (!$this->isLoaded($key)) $this->loadItem($key);
         return parent::remove($key,$value);
     }
-    public function load($field = 'id') {
-        parent::load($field);
-        $methods = get_class_methods($this);
-        foreach($methods AS $i => &$method) {
-            if (strlen($method) > 5 && strpos($method,'load'))
-                $this->$method();
-        }
-        unset($method);
-        return $this;
+    public function destroy($field = 'id') {
+        $this->getClass('LocationAssociationManager')->destroy(array('locationID'=>$this->get('id')));
+        return parent::destroy($field);
     }
     public function save() {
         parent::save();
-        if ($this->isLoaded(hosts)) {
-            // Remove old rows
-            $this->getClass(LocationAssociationManager)->destroy(array(groupID=>$this->get(id)));
-            // Create assoc
-            foreach ($this->get(hosts) AS $i => &$Host) $this->getClass(LocationAssociation)->set(hostID,$Host)->set(locationID,$this->get(id))->save();
+        if ($this->isLoaded('hosts')) {
+            $DBHostIDs = $this->getSubObjectIDs('LocationAssociation',array('locationID'=>$this->get('id'),'hostID'));
+            $RemoveHostIDs = array_diff((array)$DBHostIDs,(array)$this->get('hosts'));
+            if (count($RemoveHostIDs)) {
+                $this->getClass('LocationAssociationManager')->destroy(array('locationID'=>$this->get('id'),'hostID'=>$RemoveHostIDs));
+                $DBHostIDs = $this->getSubObjectIDs('LocationAssociation',array('locationID'=>$this->get('id'),'hostID'));
+                unset($RemoveHostIDs);
+            }
+            $Hosts = array_diff((array)$this->get('hosts'),(array)$DBHostIDs);
+            foreach ((array)$Hosts AS $i => &$Host) {
+                $this->getClass('LocationAssociation')
+                    ->set('hostID',$Host)
+                    ->set('locationID',$this->get('id'))
+                    ->save();
+            }
             unset($Host);
         }
         return $this;
     }
-	public function destroy($field = 'id') {
-		$this->getClass(LocationAssociationManager)->find(array(locationID=>$this->get(id)));
-		return parent::destroy($field);
-	}
     public function addHost($addArray) {
-        // Add
-        foreach((array)$addArray AS $i => &$item) $this->add(hosts,$item);
-        unset($item);
-        // Return
+        $Hosts = array_unique(array_diff((array)$addArray,(array)$this->get('hosts')));
+        if (count($Hosts)) {
+            $Hosts = array_merge((array)$this->get('hosts'),(array)$Hosts);
+            $this->set('hosts',$Hosts);
+        }
         return $this;
     }
     public function removeHost($removeArray) {
-        // Iterate array (or other as array)
-        foreach ((array)$removeArray AS $i => &$remove) $this->remove(hosts,$remove);
-        unset($remove);
-        // Return
+        $this->set('hosts',array_unique(array_diff((array)$this->get('hosts'),(array)$removeArray)));
         return $this;
     }
 	public function getStorageGroup() {
-		return $this->getClass(StorageGroup,$this->get(storageGroupID));
+		return $this->getClass('StorageGroup',$this->get('storageGroupID'));
 	}
 	public function getStorageNode() {
-		return $this->getClass(StorageNode,$this->get(storageNodeID));
+		return $this->getClass('StorageNode',$this->get('storageNodeID'));
 	}
+    protected function loadHosts() {
+        if ($this->get('id')) $this->set('hosts',$this->getSubObjectIDs('LocationAssociation',array('locationID'=>$this->get('id')),'hostID'));
+    }
+    protected function loadHostsnotinme() {
+        if ($this->get('id')) {
+            $find = array('id'=>$this->get('hosts'));
+            $this->set('hostsnotinme',$this->getSubObjectIDs('Host',$find,'',true));
+            unset($find);
+        }
+    }
 }
