28bb1dbc9ca87abafc04dcb4140877e1dad6dd5e
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-10-21 17:33:29 +0000

    Fix the presentation of hte default printer so it displays appropriately.  Values were stored accurately, but the way it was presented was not correct.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4194 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5028');
+        define('FOG_VERSION', '5030');
         define('FOG_SCHEMA', 197);
         define('FOG_BCACHE_VER',30);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
