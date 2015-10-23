056d81783405c6b1649b78fce0d224036ce449c3
Tom Elliott <tommygunsster@gmail.com>
2015-10-23 12:56:17 +0000

    Attempt more properly fixing the sprintf error for printerclient.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4203 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5048');
+        define('FOG_VERSION', '5050');
         define('FOG_SCHEMA', 197);
         define('FOG_BCACHE_VER',30);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
