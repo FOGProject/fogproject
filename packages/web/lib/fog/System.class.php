e2364aa186901b06194a881cb705445b1c7a8318
Tom Elliott <tommygunsster@gmail.com>
2015-10-26 13:51:03 +0000

    Make host page conform to the id getters for all functions/methods
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4217 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5076');
+        define('FOG_VERSION', '5078');
         define('FOG_SCHEMA', 198);
         define('FOG_BCACHE_VER',31);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
