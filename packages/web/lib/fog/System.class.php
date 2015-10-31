56a55665a9bd8c69439fd110ca46786e8d2f9dc2
Tom Elliott <tommygunsster@gmail.com>
2015-10-31 18:58:57 +0000

    Tweak fogcore to set memory only if the memory limit is lower than our config.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4262 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5169');
+        define('FOG_VERSION', '5171');
         define('FOG_SCHEMA', 199);
         define('FOG_BCACHE_VER',31);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
