a7a188e80b2fad35a08cf89e28f920a60f1c9f74
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-01 21:42:53 +0000

    Fix string sending for legacy and new clients to use a common method that appears to work regardless now.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4269 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/PrinterClient.class.php
+++ packages/web/lib/client/PrinterClient.class.php
@@ -1,5 +1,8 @@
 <?php
 class PrinterClient extends FOGClient implements FOGClientSend {
+    private function getString($stringsend,&$Printer) {
+        return sprintf($stringsend,$Printer->get('port'),$Printer->get('file'),$Printer->get('model'),$Printer->get('name'),$Printer->get('ip'),(int)$this->Host->getDefault($Printer->get('id')));
+    }
     public function send() {
         $level = $this->Host->get('printerLevel');
         $Printers = $this->getClass('PrinterManager')->find(array('id'=>$this->Host->get('printers')));
@@ -7,15 +10,14 @@ class PrinterClient extends FOGClient implements FOGClientSend {
         if (!$this->newService) {
             $level = "#!mg=$level";
             $this->send = '';
-            if ($level) {
-                $strtosend = "%s|%s|%s|%s|%s|%s";
-                foreach ($Printers AS $i => &$Printer) {
-                    if (!$Printer->isValid()) continue;
-                    $this->send .= base64_encode(sprintf($strtosend,$Printer->get('file'),$Printer->get('model'),$Printer->get('name'),$Printer->get('ip'),(int)$this->Host->getDefault($Printer->get('id'))))."\n";
-                    unset($Printer);
-                }
-                unset($Printers);
+            if ($level === 0) throw new Exception(sprintf('%s%s',base64_encode($level),"\n"));
+            $strtosend = "%s|%s|%s|%s|%s|%s";
+            foreach ($Printers AS $i => &$Printer) {
+                if (!$Printer->isValid()) continue;
+                $this->send .= base64_encode($this->getString($strtosend,$Printer))."\n";
+                unset($Printer);
             }
+            unset($Printers);
             $this->send = base64_encode($level)."\n".$this->send;
         } else {
             if (!$this->getClass('PrinterAssociationManager')->count(array('printerID'=>$this->Host->get('printers')))) throw new Exception("#!np\n#mode=$mode\n");
@@ -35,8 +37,8 @@ class PrinterClient extends FOGClient implements FOGClientSend {
             } else {
                 $Printer = $this->getClass('Printer',$_REQUEST['id']);
                 if (!$Printer->isValid()) throw new Exception(_('Printer is invalid'));
-                $strtosend = "#!ok\n#type=%s\n#port=%s\n#file=%s\n#model=%s\n#name=%s\n#ip=%s\n#default=%s";
-                $this->send .= sprintf($strtosend,$Printer->get('config'),$Printer->get('port'),$Printer->get('file'),$Printer->get('model'),$Printer->get('name'),$Printer->get('ip'),(int)$this->Host->getDefault($Printer->get('id')));
+                $strtosend = "#port=%s\n#file=%s\n#model=%s\n#name=%s\n#ip=%s\n#default=%s";
+                $this->send .= sprintf("#!ok\n#type=%s\n%s",$Printer->get('config'),$this->getString($strtosend,$Printer));
             }
         }
     }
