3e98f09bff30d3ea29dd8de19a8bdd21a1a978ae
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-02 11:35:00 +0000

    Fix missing param for preg_replace
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4272 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5189');
+        define('FOG_VERSION', '5191');
         define('FOG_SCHEMA', 199);
         define('FOG_BCACHE_VER',31);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
