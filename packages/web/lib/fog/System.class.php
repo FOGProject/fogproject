6cfc775ff061a91b550f546a9cdf556aec5636f9
Tom Elliott <tommygunsster@gmail.com>
2015-11-03 15:57:55 +0000

    Turn off keep alive requests in an attempt to lessen potential connection spamming.  WHile it does create more sessions, it should not hold them open.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4282 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5209');
+        define('FOG_VERSION', '5211');
         define('FOG_SCHEMA', 199);
         define('FOG_BCACHE_VER',31);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
