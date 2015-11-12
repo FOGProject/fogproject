abbaa9aec57cf65a9c60dec0fcb55c6b9e3428dc
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-12 01:13:44 +0000

    Fix the pushbullet/events system so things work again.  Update client to v0.9.6 and fix a few other issues
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4329 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/pushbullet/events/ImageFail_PushBullet.event.php
+++ packages/web/lib/plugins/pushbullet/events/ImageFail_PushBullet.event.php
@@ -4,14 +4,14 @@
  *	Author:		Jbob
  ***/
 class ImageFail_PushBullet extends Event {
-	// Class variables
-	var $name = 'ImageFail_PushBullet';
-	var $description = 'Triggers when a host fails imaging';
-	var $author = 'Jbob';
-	var $active = true;
-	public function onEvent($event, $data) {
-		foreach ((array)$this->getClass('PushbulletManager')->find() AS $Token)
-			$this->getClass('PushbulletHandler',$Token->get('token'))->pushNote('', $data['HostName'].' Failed', 'This host has failed to image');
-	}
+    // Class variables
+    var $name = 'ImageFail_PushBullet';
+    var $description = 'Triggers when a host fails imaging';
+    var $author = 'Jbob';
+    var $active = true;
+    public function onEvent($event, $data) {
+        foreach ((array)$this->getClass('PushbulletManager')->find() AS $Token)
+            $this->getClass('PushbulletHandler',$Token->get('token'))->pushNote('', $data['HostName'].' Failed', 'This host has failed to image');
+    }
 }
 $EventManager->register('HOST_IMAGE_FAIL', new ImageFail_PushBullet());
