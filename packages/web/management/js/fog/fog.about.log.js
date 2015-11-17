0ad3d0f8f8210c826f56d17790f1143757a65853
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-17 19:17:41 +0000

    Tweak the jquery file for log viewing to ensure data is okay
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4376 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.about.log.js
+++ packages/web/management/js/fog/fog.about.log.js
@@ -1,29 +1,28 @@
 var LogToView;
 var LinesToView;
-var LogTimer
+var LogTimer;
 $(function() {
-    LogToView = $('#logToView').val()
-        LinesToView = $('#linesToView').val()
-        $('#logpause').val('Pause')
-        LogGetData()
-        $('#logpause').click(function(e) {
-            e.preventDefault()
-                if ($(this).hasClass('active')) {
-                    $(this).removeClass('active')
-                        $(this).val('Pause')
-                        LogGetData()
-                } else {
-                    $(this).addClass('active')
-                        $(this).val('Continue')
-                }
-        })
+    LogToView = $('#logToView').val();
+    LinesToView = $('#linesToView').val();
+    $('#logpause').val('Pause');
+    LogGetData();
+    $('#logpause').click(function(e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active').val('Pause');
+            LogGetData;
+        } else {
+            $(this).addClass('active').val('Continue');
+            clearTimeout(LogTimer);
+        }
+    });
     $('#logToView, #linesToView').change(function(e) {
-        e.preventDefault()
-            LogToView = $('#logToView').val()
-            LinesToView = $('#linesToView').val()
-            $('#logpause').val('Pause')
-            if ($('#logpause').hasClass('active')) $('#logpause').removeClass('active')
-                LogGetData()
+        e.preventDefault();
+        LogToView = $('#logToView').val();
+        LinesToView = $('#linesToView').val();
+        $('#logpause').val('Pause');
+        if ($('#logpause').hasClass('active')) $('#logpause').removeClass('active');
+        LogGetData();
     })
 })
 function LogGetData() {
@@ -43,7 +42,7 @@ function LogGetData() {
             dataType: 'json',
             success: displayLog,
             complete: function() {
-                LogTimer = setTimeout(LogGetData,10000)
+                LogTimer = setTimeout(LogGetData,10000);
             }
         })
     }
