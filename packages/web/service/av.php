d0900803ee91344fdc2278f3708948db651095d8
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-17 18:14:45 +0000

    Make log viewer not use ftp, but rather poll the related server.  Added many more checks, and still encrypting the ip information for safety.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4372 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/av.php
+++ packages/web/service/av.php
@@ -1,19 +1,17 @@
 <?php
 require_once('../commons/base.inc.php');
 try {
-    // Get the mode.
-    if (trim($_REQUEST[mode]) != array('q','s')) throw new Exception(_('Invalid operational mode'));
-    // Get the info
+    if (trim($_REQUEST['mode']) != array('q','s')) throw new Exception(_('Invalid operational mode'));
     $string = explode(':',base64_decode($_REQUEST['string']));
     $vInfo = explode(' ',trim($string[1]));
-    $Virus = $FOGCore->getClass(Virus)
-        ->set(name,$vInfo[0])
-        ->set(hostMAC,strtolower($FOGCore->getHostItem(false)->get(mac)))
+    $Virus = $FOGCore->getClass('Virus')
+        ->set('name',$vInfo[0])
+        ->set('hostMAC',strtolower($_REQUEST['mac']))
         ->set('file',$string[0])
         ->set('date',$FOGCore->formatTime('now','Y-m-d H:i:s'))
-        ->set(mode,$_REQUEST[mode]);
-    if ($Virus->save()) throw new Exception(_('Accepted'));
-    else throw new Exception(_('Failed'));
+        ->set('mode',$_REQUEST['mode']);
+    if (!$Virus->save()) throw new Exception(_('Failed'));
+    throw new Exception(_('Accepted'));
 } catch (Exception $e) {
     echo $e->getMessage();
 }
