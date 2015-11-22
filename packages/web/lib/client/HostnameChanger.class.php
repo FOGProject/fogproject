e76439a1a380ea0ab260c39a02bb8e78bf099ae5
Tom Elliott <tommygunsster@gmail.com>
2015-11-22 19:02:25 +0000

    Recode the methodology of the HostnameChanger script in an attempt to allow disjoining domains.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4402 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/HostnameChanger.class.php
+++ packages/web/lib/client/HostnameChanger.class.php
@@ -1,26 +1,28 @@
 <?php
 class HostnameChanger extends FOGClient implements FOGClientSend {
-    public function send() {
-        $this->send = '#!ok';
-        $password = $this->Host->get('ADPassLegacy');
-        if ($this->newService) {
-            $this->send .= "\n#hostname=".$this->Host->get('name')."\n";
-            $password = $this->aesdecrypt($this->Host->get('ADPass'));
-            $productKey = $this->aesdecrypt($this->Host->get('productKey'));
-            $this->Host->setAD();
-        }
-        else $this->send .= '='.$this->Host->get('name')."\n";
-        $username = trim($this->Host->get('ADUser'));
-        if (strpos($username,chr(92)) || strpos($username,chr(64))) $adUser = $username;
-        else if ($username) $adUser = sprintf('%s\%s',$this->Host->get('ADDomain'),$username);
-        else $adUser = '';
-        $this->send .= '#AD='.$this->Host->get('useAD')."\n";
-        if (!$this->newService || $this->Host->get('useAD')) {
-            $this->send .= '#ADDom='.$this->Host->get('ADDomain')."\n";
-            $this->send .= '#ADOU='.$this->Host->get('ADOU')."\n";
-            $this->send .= '#ADUser='.$this->DB->sanitize($adUser)."\n";
-            $this->send .= '#ADPass='.$this->DB->sanitize($password);
-            if ($productKey) $this->send .= "\n#Key=".$productKey;
-        }
+  public function send() {
+    ob_start();
+    echo '#!ok';
+    $productKey = $this->aesdecrypt($this->Host->get('productKey'));
+    if ($this->newService) {
+      $password = $this->aesdecrypt($this->Host->get('ADPass'));
+      printf("\n#hostname=%s\n",$this->Host->get('name'));
+    } else {
+      $password = $this->Host->get('ADPassLegacy');
+      printf("=%s\n",$this->Host->get('name'));
     }
+    $this->Host->setAD();
+    $username = trim($this->Host->get('ADUser'));
+    if (strpos($username,chr(92)) || strpos($username,chr(64))) $adUser = $username;
+    else if ($username) $adUser = sprintf('%s\%s',$this->Host->get('ADDomain'),$username);
+    else $adUser = '';
+    printf("#AD=%s\n#ADDom=%s\n#ADOU=%s\n#ADUser=%s\n#ADPass=%s",
+    $this->Host->get('useAD'),
+    $this->Host->get('ADDomain'),
+    $this->Host->get('ADOU'),
+    $adUser,
+    $password
+  );
+  if ($productKey) printf("\n#Key=%s",$productKey);
+  $this->send = ob_get_clean();
 }
--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -3,7 +3,7 @@ class System {
     public function __construct() {
         define('FOG_VERSION', '5463');
         define('FOG_SCHEMA', 200);
-        define('FOG_BCACHE_VER',35);
+        define('FOG_BCACHE_VER',36);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.9.7');
