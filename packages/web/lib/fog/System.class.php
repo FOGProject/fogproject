2b1be79ee461b0caef885d7b8ad5ffd75dd4f9e4
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-11-26 01:21:49 +0000

    Test the log viewer for the storage group to see if there are any nodes enabled for that group.  If there are no enabled nodes (or no nodes assigned) it will just continue on so things will still run.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4428 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/System.class.php
+++ packages/web/lib/fog/System.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5513');
+        define('FOG_VERSION', '5515');
         define('FOG_SCHEMA', 200);
         define('FOG_BCACHE_VER',36);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
