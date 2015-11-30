e196c2b3459b86c6d3e75e6fdb1afa179d22939e
Tom Elliott <tommygunsster@gmail.com>
2015-11-30 14:42:12 +0000

    Maybe now task deletion button will display
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4455 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5566');
+        define('FOG_VERSION', '5568');
         define('FOG_SCHEMA', 200);
         define('FOG_BCACHE_VER',37);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
