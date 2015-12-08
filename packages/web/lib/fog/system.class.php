4ba590e67d897ee7854dc1ed3593485a761ca8d9
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-12-08 03:47:04 +0000

    Stop display errors implicitely.  Fix security token generation as the setter was incorrect
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4508 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5672');
+        define('FOG_VERSION', '5674');
         define('FOG_SCHEMA', 204);
         define('FOG_BCACHE_VER',39);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
