a3251d5ea8cdbcffeab3aede680f15e76c7de61f
Tom Elliott <tommygunsster@gmail.com>
2015-12-08 19:40:00 +0000

    update to 0.9.9 for client
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4511 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,12 +1,12 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5678');
+        define('FOG_VERSION', '5680');
         define('FOG_SCHEMA', 204);
         define('FOG_BCACHE_VER',39);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
-        define('FOG_CLIENT_VERSION', '0.9.8');
+        define('FOG_CLIENT_VERSION', '0.9.9');
         define('PHP_VERSION_REQUIRED', '5.3.0');
         define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
         define('SPACE_DEFAULT_STORAGE', '/images');
