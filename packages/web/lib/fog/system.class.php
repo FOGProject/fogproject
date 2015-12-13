7405626a808e26c7084272ce0637c8b623544c11
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2015-12-13 18:41:16 +0000

    Make all the functions file use bash pure layout and remove erroneous semicolons.  Add a display center to the logo display. This is untested, but should work
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4527 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5710');
+        define('FOG_VERSION', '5712');
         define('FOG_SCHEMA', 204);
         define('FOG_BCACHE_VER',40);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
