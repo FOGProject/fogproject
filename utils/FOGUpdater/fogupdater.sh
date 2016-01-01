3d7245f0eafae026d73eadf46e1a82710813134f
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-01-01 22:32:30 +0000

    Initial attempt to redirect error's to a log file rather than pushing to a black hole.  Modify the updater script in hopes to better approach the install paths
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4578 71f96598-fa45-0410-b640-bcd6f8691b32

--- utils/FOGUpdater/fogupdater.sh
+++ utils/FOGUpdater/fogupdater.sh
@@ -1,140 +1,130 @@
-#!/bin/sh
-
-COMMONWEBROOTS="/var/www/html /var/www";
-UPDATEMIRRORS="http://internap.dl.sourceforge.net/sourceforge/freeghost/ http://voxel.dl.sourceforge.net/sourceforge/freeghost/ http://kent.dl.sourceforge.net/sourceforge/freeghost/ http://heanet.dl.sourceforge.net/sourceforge/freeghost/";
-DOWNLOADDIR="/opt/";
-CONFIGPATH="fog/lib/fog/Config.class.php";
-
-displayBanner()
-{
-	echo "        ___           ___           ___      ";
-	echo "       /\  \         /\  \         /\  \     ";
-	echo "      /::\  \       /::\  \       /::\  \    ";
-	echo "     /:/\:\  \     /:/\:\  \     /:/\:\  \   ";
-	echo "    /::\-\:\  \   /:/  \:\  \   /:/  \:\  \  ";
-	echo "   /:/\:\ \:\__\ /:/__/ \:\__\ /:/__/_\:\__\ ";
-	echo "   \/__\:\ \/__/ \:\  \ /:/  / \:\  /\ \/__/ ";
-	echo "        \:\__\    \:\  /:/  /   \:\ \:\__\   ";
-	echo "         \/__/     \:\/:/  /     \:\/:/  /   ";
-	echo "                    \::/  /       \::/  /    ";
-	echo "                     \/__/         \/__/     ";
-	echo "";
-	echo "  ###########################################";
-	echo "  #     Free Computer Imaging Solution      #";
-	echo "  #                                         #";
-	echo "  #     Updater Version 1.0                 #";
-	echo "  #                                         #";
-	echo "  #     Created by:                         #";
-	echo "  #         Chuck Syperski                  #";	
-	echo "  #         Jian Zhang                      #";
-	echo "  #                                         #";		
-	echo "  #     GNU GPL Version 3                   #";		
-	echo "  ###########################################";
-	echo "";
-}
-
-clear;
-
-displayBanner;
-
-echo "";
-echo "";
-echo " Notice:  Your FOG server may go offline during this upgrade process!";
-echo "";
-echo "";
-sleep 5;
-echo -ne " Checking running version: \t\t";
-blWebRootFound=0;
-webRoot="";
-ver="";
-for wroot in $COMMONWEBROOTS
-do
-	if [ "$blWebRootFound" = "0" ]
-	then
-		if [ -d "$wroot" ]
-		then
-			if [ -f "$wroot/fog/lib/fog/Config.class.php" ]
-			then
-				fle="$wroot/fog/lib/fog/Config.class.php";
-				ver=`cat "$fle" | grep "FOG_VERSION" | cut -d"," -f2 | cut -d"\"" -f2`
-				if [ "$ver" != "" ]
-				then
-					blWebRootFound=1;
-					webRoot=$wroot;
-					echo $ver;
-				fi
-			fi
-		fi
-	fi
-done 
-
-if [ "$blWebRootFound" = "1" ]
-then
-	latest=`wget -O - http://freeghost.sourceforge.net/version/version.php 2>/dev/null`
-	echo -ne " Checking latest version: \t\t";
-	if [ "$latest" != "" ]
-	then
-		echo $latest;
-		
-		if [ "$latest" != "$ver" ]
-		then
-			echo "";
-			echo " A new version of fog has been released...";
-			echo "";		
-			sleep 2;
-			echo " Attempting to download FOG to $DOWNLOADDIR";
-			echo "";
-			sleep 2;
-			blDownloaded=0;
-			for url in $UPDATEMIRRORS
-			do
-				if [ "$blDownloaded" = "0" ] 
-				then
-					u="${url}fog_${latest}.tar.gz";
-					echo " Trying mirror: ";
-					echo "      $u";
-					echo "";
-					lf="${DOWNLOADDIR}fog_${latest}.tar.gz"
-					wget -O "$lf" $u;	
-					
-					if [ "$?" = "0" ]
-					then
-
-						if [ -f "$lf" ]
-						then
-							echo ""
-							echo " Download complete";
-							echo ""			
-							blDownloaded=1;
-							echo -ne " Extracting package:\t\t";
-							cd ${DOWNLOADDIR};
-							tar xvzf $lf >/dev/null 2>&1;
-							echo "OK";
-							
-							cd "${DOWNLOADDIR}fog_${latest}/bin";
-							./installfog.sh
-										
-						else
-							echo ""
-							echo " Download Failed!";
-							echo ""							
-						fi
-					else
-						echo " Download failed!";
-					fi
-				fi
-			done	
-		else
-			echo "";
-			echo " Your FOG server is up to date";
-			echo "";
-		fi
-	else
-		echo "Unable to determine latest version.";
-	fi
-else
-	echo "Unable to determine current version.";
+#!/bin/bash
+. ../../lib/common/functions.sh
+if [[ ! -f /opt/fog/.fogsettings ]]; then
+    echo "No fog settings found so nothing to update"
+    exit 1
 fi
-echo "";
-echo "";
-
+. /opt/fog/.fogsettings
+if [[ ! -d $docroot ]]; then
+    echo "No web folder found"
+    exit 1
+fi
+if [[ ! -d ${docroot}${webroot} ]]; then
+    echo "No fog web directory found"
+    exit 1
+fi
+if [[ ! -f ${docroot}${webroot}config.class.php && ! -f ${docroot}${webroot}Config.class.php ]]; then
+    echo "No config file found"
+    exit 1
+fi
+if [[ -f ${docroot}${webroot}config.class.php ]]; then
+    configpath=${docroot}${webroot}config.class.php
+else
+    configpath=${docroot}${webroot}Config.class.php
+fi
+OS=$(uname -s)
+case $OS in
+    [Ll][Ii][Nn][Uu][Xx])
+        if [[ -z $downloaddir ]]; then
+            downloaddir="/opt/"
+        fi
+        if [[ -z $updatemirrors ]]; then
+            updatemirrors="http://internap.dl.sourceforge.net/sourceforge/freeghost/ http://voxel.dl.sourceforge.net/sourceforge/freeghost/ http://kent.dl.sourceforge.net/sourceforge/freeghost/ http://heanet.dl.sourceforge.net/sourceforge/freeghost/"
+        fi
+        clear
+        displayBanner
+        echo
+        echo
+        display_center "***************************************************************"
+        display_center "*                         ** Notice **                        *"
+        display_center "***************************************************************"
+        display_center "*                                                             *"
+        display_center "* Your FOG server may go offline during this upgrade process! *"
+        display_center "*                                                             *"
+        display_center "***************************************************************"
+        echo
+        echo
+        sleep 5
+        dots " * Checking running version"
+        version="$(awk -F\' /"define\('FOG_VERSION'[,](.*)"/'{print $4}' $configpath | tr -d '[[:space:]]')"
+        if [[ -z $version ]]; then
+            false
+            errorStat $?
+        fi
+        echo "Done"
+        echo
+        echo
+        display_center "Current FOG Version: $version"
+        echo
+        echo
+        dots " * Checking latest version"
+        if [[ -z $trunk ]]; then
+            latest=$(wget --no-check-certificate -qO - --post-data="stable" https://fogproject.org/version/index.php)
+        else
+            latest=$(wget --no-check-certificate -qO - --post-data="dev" https://fogproject.org/version/index.php)
+        fi
+        if [[ -z $latest ]]; then
+            false
+            errorStat $?
+        fi
+        echo "Done"
+        echo
+        echo
+        display_center "Latest FOG Version: $latest"
+        echo
+        echo
+        if [[ -z $trunk ]]; then
+            if [[ $(trim $version) == $(trim $latest) ]]; then
+                echo "You are already up to date"
+                exit 0
+            fi
+            display_center "You are not running the latest stable version"
+            echo
+            echo
+            sleep 3
+            display_center "Preparing to upgrade"
+            display_center "Attempting to download latest stable to $downloaddir"
+            echo
+            echo
+            sleep 3
+            downloaded=""
+            for url in $updatemirrors; do
+                echo " * Trying mirror $url"
+                dots " * Attempting Download"
+                fileplace=$downloaddir/fog_${latest}.tar.gz
+                filedownload=$url/fog_${latest}.tar.gz
+                wget --no-check-certificate -qO $fileplace $filedownload
+                if [[ ! $? -eq 0 ]]; then
+                    echo "Failed"
+                    continue
+                fi
+                echo "Done"
+                downloaded=1
+                break
+            done
+            if [[ -z $downloaded ]]; then
+                echo "Failed to download current file"
+                exit 1
+            fi
+            echo
+            echo
+            display_center "Extracting package $fileplace"
+            echo
+            echo
+            dots " * Extracting"
+            cwd=$(pwd)
+            cd $download
+            tar -xzf $fileplace >/dev/null 2>&1
+            errorStat $?
+            echo "Done"
+            echo
+            dots
+            cd $downloaddir/fog_${latest}/bin
+            ./installfog.sh -y
+        fi
+        ;;
+    *)
+        echo "We do not currently support installation on non-linux operating systems"
+        exit 1
+        ;;
+esac
