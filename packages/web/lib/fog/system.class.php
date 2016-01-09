f569f961aca290fd7940c263eac43589f6aa08da
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-01-09 20:13:02 +0000

    Try to ensure service scripts of systemctl are not set with executable bits.  More tweaks to init scripts
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4625 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5911');
+        define('FOG_VERSION', '5913');
         define('FOG_SCHEMA', 205);
         define('FOG_BCACHE_VER',42);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
