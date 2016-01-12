c5fca94bc737665fcbb106d000c6bdc70f7eac4a
Tom Elliott <tommygunsster@gmail.com>
2016-01-12 19:26:45 +0000

    Fix an issue with pauseUpdate function not being called properly.  Multiple other fix up's as well.  Fix the fog.man.reg.script to use a function to generate system listings and what not.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4633 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.group.js
+++ packages/web/management/js/fog/fog.group.js
@@ -1,12 +1,4 @@
 $(function() {
     checkboxToggleSearchListPages();
-    $('#action-boxdel').submit(function(e) {
-        var checked = $('input.toggle-action:checked').not(':hidden');
-        var groupIDArray = new Array();
-        for (var i = 0,len = checked.size();i < len;i++) {
-            groupIDArray[groupIDArray.length] = checked.eq(i).not(':hidden').attr('value');
-        }
-        $('input[name="groupIDArray"]').val(groupIDArray.join(','));
-    });
     ProductUpdate();
 });
--- packages/web/management/js/fog/fog.snapin.js
+++ packages/web/management/js/fog/fog.snapin.js
@@ -1,13 +1,5 @@
 $(function() {
     checkboxToggleSearchListPages();
-    $('#action-boxdel').submit(function() {
-        var checked = $('input.toggle-action:checked').not(':hidden');
-        var snapinIDArray = new Array();
-        for (var i = 0,len = checked.size();i < len;i++) {
-            snapinIDArray[snapinIDArray.length] = checked.eq(i).prop('value');
-        }
-        $('input[name="snapinIDArray"]').val(snapinIDArray.join(','));
-    });
     updateCmdStore();
     $('.cmdlet1,.cmdlet2,.cmdlet4').keyup(function() {
         updateCmdStore();
