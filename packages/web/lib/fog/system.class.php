2ccec08e9beaa19e38b60e70ba50ac862966b09c
Tom Elliott <tommygunsster@gmail.com>
2016-01-13 18:24:14 +0000

    Make attempts to more accurately return data as return 0 seems to affect many other things
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4637 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '5935');
+        define('FOG_VERSION', '5937');
         define('FOG_SCHEMA', 205);
         define('FOG_BCACHE_VER',44);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
