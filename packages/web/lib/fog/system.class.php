1572c37614b7f2e2b9c162b0dc88542baca8135f
Tom Elliott <tommygunsster@gmail.com>
2016-01-17 20:40:38 +0000

    More minor edits to the javascript files to use checkboxAssociations function
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4672 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6008');
+        define('FOG_VERSION', '6010');
         define('FOG_SCHEMA', 207);
         define('FOG_BCACHE_VER',45);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
--- packages/web/management/js/fog/fog.printer.edit.js
+++ packages/web/management/js/fog/fog.printer.edit.js
@@ -1,10 +1,12 @@
 $(function() {
     $('#hostNotInMe,#hostNoPrinter').hide();
-    $('#hostMeShow').change(function() {
+    $('#hostMeShow').change(function(e) {
         $('#hostNotInMe').toggle();
+        e.preventDefault();
     });
-    $('#hostNoShow').change(function() {
+    $('#hostNoShow').change(function(e) {
         $('#hostNoPrinter').toggle();
+        e.preventDefault();
     });
     checkboxAssociations('.toggle-checkbox1:checkbox','.toggle-host1:checkbox');
     checkboxAssociations('.toggle-checkbox2:checkbox','.toggle-host2:checkbox');
