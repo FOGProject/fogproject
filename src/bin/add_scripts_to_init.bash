4dfd2110bc6ef14f7fa60f2a598cd00611b86f88
Tom Elliott <tommygunsster@gmail.com>
2016-01-17 01:01:27 +0000

    Just some cleanups to other areas
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4665 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/bin/add_scripts_to_init.bash
+++ src/bin/add_scripts_to_init.bash
@@ -1,97 +1,58 @@
 #!/bin/bash
-
+scriptTransfer() {
+    for s in $scripts; do
+        [[ -r $scriptdir/$s ]] && cp -f $scriptdir/$s $mountpoint/bin/$s || echo "$scriptdir/$s is not there"
+    done
+    for l in $libs; do
+        [[ -r $libdir/$l ]] && cp -f $libdir/$l $mountpoint/usr/share/fog/lib/$l || echo "$libdir/$l is not there"
+    done
+}
+handleError() {
+    echo "$1"
+    echo $2
+}
+[[ $(whoami) != "root" ]] && handleError "Must run as root" 1
 FOGROOT="$1"
-
-if [ `whoami` != "root" ]; then
-    echo "Must run as root."
-    exit 1
-fi
-
-if [ ! -d "$FOGROOT" ]; then
-    echo "usage: $0 fogrootdir"
-    exit 1
-fi
-
+[[ ! -d $FOGROOT ]] && handleError "Usage: $0 fogrootdir" 1
 mountpoint="tmp-loop-mount"
-scriptdir="${FOGROOT}/src/buildroot/package/fog/scripts/bin"
-libdir="${FOGROOT}/src/buildroot/package/fog/scripts/usr/share/fog/lib"
+scriptdir="$FOGROOT/src/buildroot/package/fog/scripts/bin"
+[[ ! -d $scriptdir ]] && handleError "Cannot find script location" 1
+libdir="$FOGROOT/src/buildroot/package/fog/scripts/usr/share/fog/lib"
+[[ ! -d $libdir ]] && handleError "Cannot find lib location" 1
 initdir="/var/www/fog/service/ipxe"
+[[ ! -d $initdir ]] && initdir="/var/www/html/fog/service/ipxe"
+[[ ! -d $initdir ]] && initdir="/srv/http/fog/service/ipxe"
+[[ ! -d $initdir ]] && handleError "Could not locate install directory" 1
 scripts="fog.upload fog.download"
 libs="funcs.sh partition-funcs.sh procsfdisk.awk"
-
-if [ ! -d "$scriptdir" -o ! -d "$libdir" -o ! -d "$initdir" ]; then
-    echo "Misconfigured fogroot: $FOGROOT"
-    exit 1
-fi
-
 pushd $scriptdir >> /dev/null
-scripts=`ls -1`
+scripts=$(ls -1)
 popd >> /dev/null
 pushd $libdir >> /dev/null
-libs=`ls -1`
+libs=$(ls -1)
 popd >> /dev/null
-
-if [ ! -d $mountpoint ]; then
-    mkdir $mountpoint
-fi
-
+[[ ! -d $mountpoint ]] && mkdir -p $mountpoint >/dev/null 2>&1
+[[ ! -d $mountpoint ]] && handleError "Failed to create mount point" 1
 echo "uncompressing 64-bit image"
-xzcat ${initdir}/init.xz > init-64
+xzcat $initdir/init.xz > init-64
 mount -o loop init-64 $mountpoint
-
-for s in $scripts; do
-    if [ -r ${scriptdir}/${s} ]; then
-        cp -f ${scriptdir}/${s} $mountpoint/bin/${s}
-    else
-        echo "${scriptdir}/${s} isn't there."
-    fi
-done
-for l in $libs; do
-    if [ -r ${libdir}/${l} ]; then
-        cp -f ${libdir}/${l} $mountpoint/usr/share/fog/lib/${l}
-    else
-        echo "${libdir}/${l} isn't there."
-    fi
-done
-
-rm -f $mountpoint/bin/*~
-rm -f $mountpoint/usr/share/fog/lib/*~
-umount $mountpoint
+scriptTransfer
+rm -f $mountpoint/bin/*~ >/dev/null 2>&1
+rm -f $mountpoint/usr/share/fog/lib/*~ >/dev/null 2>&1
+umount $mountpoint >/dev/null 2>&1
 echo "compressing 64-bit image"
-xz -C crc32 -z -c init-64 > ${initdir}/init.xz
-rm init-64
-
-
+xz -C crc32 -z -c init-64 > $initdir/init.xz
+rm init-64 >/dev/null 2>&1
 # repeat for 32bit
-
 echo "uncompressing 32-bit image"
-xzcat ${initdir}/init_32.xz > init-32
+xzcat $initdir/init_32.xz > init-32
 mount -o loop init-32 $mountpoint
-
-for s in $scripts; do
-    if [ -r ${scriptdir}/${s} ]; then
-        cp -f ${scriptdir}/${s} $mountpoint/bin/${s}
-    else
-        echo "${scriptdir}/${s} isn't there."
-    fi
-done
-for l in $libs; do
-    if [ -r ${libdir}/${l} ]; then
-        cp -f ${libdir}/${l} $mountpoint/usr/share/fog/lib/${l}
-    else
-        echo "${libdir}/${l} isn't there."
-    fi
-done
-
-rm -f $mountpoint/bin/*~
-rm -f $mountpoint/usr/share/fog/lib/*~
-umount $mountpoint
+scriptTransfer
+rm -f $mountpoint/bin/*~ >/dev/null 2>&1
+rm -f $mountpoint/usr/share/fog/lib/*~ >/dev/null 2>&1
+umount $mountpoint >/dev/null 2>&1
 echo "compressing 32-bit image"
 xz -C crc32 -z -c init-32 > ${initdir}/init_32.xz
 rm init-32
-
-if [ -d $mountpoint ]; then
-    rmdir $mountpoint
-fi
-
+[[ -d $mountpoint ]] && rmdir $mountpoint >/dev/null 2>&1
 exit 0
