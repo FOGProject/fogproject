d824bfee3b30050beeeb7786346e2cc84597589c
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-01-17 01:01:27 +0000

    Just some cleanups to other areas
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4665 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/bin/install_inits.bash
+++ src/bin/install_inits.bash
@@ -1,31 +1,17 @@
 #!/bin/bash
-
+handleError() {
+    echo "$1"
+    echo $2
+}
+[[ $(whoami) != "root" ]] && handleError "Must run as root" 1
 FOGROOT="$1"
-
-if [ `whoami` != "root" ]; then
-    echo "Must run as root."
-    exit 1
-fi
-
-if [ ! -d "$FOGROOT" ]; then
-    echo "usage: $0 fogrootdir"
-    exit 1
-fi
-
-initdir="${FOGROOT}/packages/web/service/ipxe"
+[[ ! -d $FOGROOT ]] && handleError "Usage: $0 fogrootdir" 1
+initdir="$FOGROOT/packages/web/service/ipxe"
+[[ ! -d $initdir ]] && handleError "Cannot find init path" 1
 installdir="/var/www/fog/service/ipxe"
-
-if [ ! -d "$initdir" ]; then
-    echo "Misconfigured fogroot: $FOGROOT"
-    exit 1
-fi
-
-if [ ! -d "$installdir" ]; then
-    echo "Misconfigured installdir: $installdir"
-    exit 1
-fi
-
+[[ ! -d $installdir ]] && installdir="/var/www/html/fog/service/ipxe"
+[[ ! -d $installdir ]] && installdir="/srv/http/fog/service/ipxe"
+[[ ! -d $installdir ]] && handleError "Cannot find install directory" 1
 cp ${initdir}/init.xz ${installdir}/init.xz
 cp ${initdir}/init_32.xz ${installdir}/init_32.xz
-
 exit 0
