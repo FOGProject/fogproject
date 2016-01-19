be3becf3eb811caf8ea99f013cacd31339454ed1
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-01-19 18:46:08 +0000

    More refinement to the image and file path javascript stuff
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4681 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.image.js
+++ packages/web/management/js/fog/fog.image.js
@@ -1,20 +1,23 @@
 $(function() {
     checkboxToggleSearchListPages();
+    var iFileVal = $('#iFile').val();
     $('#iFile').on('change keyup',function(e) {
         var start = this.selectionStart,
             end = this.selectionEnd;
         this.value = this.value.replace(/[^\w+]/g,'');
         this.setSelectionRange(start,end);
+        iFileVal = this.value;
         e.preventDefault();
     });
     $('#iName').on('change keyup',function(e) {
         var start = this.selectionStart,
             end = this.selectionEnd;
-        $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+]/g,''));
         this.setSelectionRange(start,end);
         e.preventDefault();
     }).blur(function(e) {
-        $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        iFileVal = $('#iFile').val();
         e.preventDefault();
     });
 });
