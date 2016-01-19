bd0b4ba620e1650bb8947b76fe85a819342506ed
Tom Elliott <tommygunsster@gmail.com>
2016-01-19 18:07:51 +0000

    Fix image creation/edit that failed to have the duplicate printed out (less special characters) in the file name field
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4679 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.image.js
+++ packages/web/management/js/fog/fog.image.js
@@ -1,3 +1,20 @@
 $(function() {
     checkboxToggleSearchListPages();
+    $('#iFile').on('change keyup',function(e) {
+        var start = this.selectionStart,
+            end = this.selectionEnd;
+        this.value = this.value.replace(/[^\w+]/g,'');
+        this.setSelectionRange(start,end);
+        e.preventDefault();
+    });
+    $('#iName').on('change keyup',function(e) {
+        var start = this.selectionStart,
+            end = this.selectionEnd;
+        $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        this.setSelectionRange(start,end);
+        e.preventDefault();
+    }).blur(function(e) {
+        $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        e.preventDefault();
+    });
 });
