2141147ec662c678c196b22cf5ecd37276d5882c
Tom Elliott <tommygunsster@gmail.com>
2016-01-21 20:31:31 +0000

    Fix the scripts a bit more, man I'm a total idiot
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4692 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6048');
+        define('FOG_VERSION', '6050');
         define('FOG_SCHEMA', 208);
         define('FOG_BCACHE_VER',47);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
