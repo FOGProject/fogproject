2430a2d211932571898f54c0ce80bafc54ce38dd
Tom Elliott <tommygunsster@gmail.com>
2016-01-23 12:41:03 +0000

    Fix dhcp config stuff
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4702 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6068');
+        define('FOG_VERSION', '6070');
         define('FOG_SCHEMA', 208);
         define('FOG_BCACHE_VER',48);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
