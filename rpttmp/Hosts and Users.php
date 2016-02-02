37401edacf4b0f5d50b693620df217bc2a3a65eb
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-02-01 18:54:52 -0600

    Set DHCP to support multiple architectures for FOG by default.

--- rpttmp/Hosts and Users.php
+++ rpttmp/Hosts and Users.php
@@ -1,16 +1,15 @@
 <?php
-class HostUsers extends FOGBase
-{
-	public function __construct()
-	{
-		parent::__construct();
-		$this->makeReport();
+class HostUsers extends ReportManagementPage {
+	public function __construct($name = '') {
+		$this->name = 'Hosts and Users';
+		$this->node = 'report';
+		parent::__construct($this->name);
+		$this->index();
 	}
-	private function makeReport()
-	{
-		print '<h2>'._('FOG Hosts and Users Login').'<a href="export.php?type=csv&filename=HostsandUsers" target="_blank"><img class="noBorder" src="images/csv.png" /></a><a href="export.php?type=pdf&filename=HostsandUsers" target="_blank"><img class="noBorder" src="images/pdf.png" /></a></h2>';
-		$Hosts = $this->getClass('HostManager')->find();
-		$report = new ReportMaker();
+	public function index() {
+		$this->title =_('FOG Hosts and Users Login');
+		echo '<p class="c"><a href="export.php?type=csv" target="_blank"><i class="fa fa-file-excel-o fa-2x"></i></a><a href="export.php?type=pdf" target="_blank"><i class="fa fa-file-pdf-o fa-2x"></i></a></p><br/>';
+		$report = $this->getClass('ReportMaker');
 		$report->appendHTML('<table cellpadding="0" cellspacing="0" border="0" width="100%">');
 		$report->appendHTML('<tr bgcolor="#BDBDBD"><td><b>Hostname</b></td><td><b>MAC</b></td><td><b>Registered</b></td></tr>');
 		$report->addCSVCell('Hostname');
@@ -18,36 +17,35 @@ class HostUsers extends FOGBase
 		$report->addCSVCell('Registered');
 		$report->endCSVLine();
 		$cnt = 0;
-		foreach ($Hosts AS $Host)
-		{
+		foreach ($this->getClass('HostManager')->find('','','','','','name') AS $i => &$Host) {
+            if (!$Host->isValid()) continue;
 			$bg = ($cnt++ % 2 == 0 ? "#E7E7E7" : '');
-			$report->appendHTML('<tr bgcolor="'.$bg.'"><td>'.$Host->get('name').'</td><td>'.$Host->get('mac').'</td><td>'.$Host->get('createdTime').'</td></tr>');
+            $report->appendHTML(sprintf('<tr bgcolor="%s"><td>%s</td><td>%s</td><td>%s</td></tr>',$bg,$Host->get('name'),$Host->get('mac'),$Host->get('createdTime')));
 			$report->addCSVCell($Host->get('name'));
 			$report->addCSVCell($Host->get('mac'));
-			$report->addCSVCell($Host->get('createdTime'));
-			if ($Host->get('users'))
-			{
-				$report->endCSVLine();
-				$report->appendHTML('<tr bgcolor="#BDBDBD"><td><b>Username</b></td><td><b>Action</b></td><td><b>Date</b></td><td><b>Time</b></td></tr>');
-				$report->addCSVCell('Username');
-				$report->addCSVCell('Action');
-				$report->addCSVCell('Time');
-				$report->endCSVLine();
-				$cnt1 = 0;
-				foreach ($Host->get('users') AS $User)
-				{
-					$bg1 = ($cnt1++ % 2 == 0 ? "#E7E7E7" : '');
-					$logintext = ($User->get('action') == 1 ? 'Login' : (!$User->get('action') ? 'Logout' : ($User->get('action') == 99 ? 'Service Start' : 'N/A')));
-					if ($logintext == 'Login' || $logintext == 'Logout')
-					{
-						$report->appendHTML('<tr bgcolor="'.$bg1.'"><td>'.$User->get('username').'</td><td>'.$logintext.'</td><td>'.$User->get('date').'</td><td>'.$User->get('datetime').'</td></tr>');
-						$report->addCSVCell($User->get('username'));
-						$report->addCSVCell($logintext);
-						$report->addCSVCell($User->get('date'));
-						$report->addCSVCell($User->get('datetime'));
-						$report->endCSVLine();
-					}
-				}
+            $report->addCSVCell($Host->get('createdTime'));
+            if (!count($Host->get('users'))) {
+                $report->endCSVLine();
+                continue;
+            }
+            $report->endCSVLine();
+            $report->appendHTML('<tr bgcolor="#BDBDBD"><td><b>Username</b></td><td><b>Action</b></td><td><b>Date</b></td><td><b>Time</b></td></tr>');
+            $report->addCSVCell('Username');
+            $report->addCSVCell('Action');
+            $report->addCSVCell('Time');
+            $report->endCSVLine();
+            $cnt1 = 0;
+            foreach ((array)$this->getClass('UserTrackingManager')->find(array('id'=>$Host->get('users'),'action'=>array(null,0,1)),'','datetime','DESC','','username') AS $i => $User) {
+                if (!$User->isValid()) continue;
+                if ($User->get('username') == 'Array') continue;
+                $bg1 = ($cnt1++ % 2 == 0 ? "#E7E7E7" : '');
+                $logintext = ($User->get('action') == 1 ? 'Login' : 'Logout' );
+                $report->appendHTML(sprintf('<tr bgcolor="%s"><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>',$bg1,$User->get('username'),$logintext,$User->get('date'),$User->get('datetime')));
+                $report->addCSVCell($User->get('username'));
+                $report->addCSVCell($logintext);
+                $report->addCSVCell($User->get('date'));
+                $report->addCSVCell($User->get('datetime'));
+                $report->endCSVLine();
 				$report->appendHTML('<table cellpadding="0" cellspacing="0" border="0" width="100%">');
 				$report->appendHTML('<tr bgcolor="#BDBDBD"><td><b>Hostname</b></td><td><b>MAC</b></td><td><b>Registered</b></td></tr>');
 				$report->addCSVCell('Hostname');
@@ -55,11 +53,9 @@ class HostUsers extends FOGBase
 				$report->addCSVCell('Registered');
 				$report->endCSVLine();
 			}
-			else
-				$report->endCSVLine();
 		}
 		$report->appendHTML('</table>');
-		$report->outputReport(ReportMaker::FOG_REPORT_HTML);
+		$report->outputReport(0);
 		$_SESSION['foglastreport'] = serialize($report);
 	}
 }
