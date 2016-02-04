611ab73d650f71e6f250ccb94ecaeaf590ef3990
Tom Elliott <tommygunsster@gmail.com>
2016-02-04 23:04:36 +0000

    Fix up, hopefully, windows xp/vista imaging using the newer image formats (d1p#.img) and still maintain legacy style xp/vista imaging (imagename as file or imagename in directory and file).  Won't know until told otherwise though.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4763 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6203');
+        define('FOG_VERSION', '6205');
         define('FOG_SCHEMA', 208);
         define('FOG_BCACHE_VER',46);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
