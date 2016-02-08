f819d4a5d016f038dce762258fac12f9a839d396
Tom Elliott <tommygunsster@gmail.com>
2016-02-08 23:13:47 +0000

    If you're sending to a channel rather than directly to a user, it will send as the creator of the link.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4786 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/slack/events/imagefail_slack.event.php
+++ packages/web/lib/plugins/slack/events/imagefail_slack.event.php
@@ -8,8 +8,11 @@ class ImageFail_Slack extends Event {
     public function onEvent($event, $data) {
         foreach ((array)$this->getClass('SlackManager')->find() AS &$Token) {
             if (!$Token->isValid()) continue;
-            $channel_user = preg_match('/^#/',$Token->get('name')) ? $Token->get('name') : "@{$Token->get(name)}";
-            $this->getClass('SlackHandler',$Token->get('token'))->call('chat.postMessage',array('channel'=>$channel_user,'text'=>$data['HostName'].' Failed to complete imaging'));
+            $args = array(
+                'channel' => $Token->get('name'),
+                'text' => "{$data[HostName]} Failed imaging",
+            );
+            $Token->call('chat.postMessage',$args);
             unset($Token);
         }
     }
--- packages/web/lib/plugins/slack/events/loginfailure_slack.event.php
+++ packages/web/lib/plugins/slack/events/loginfailure_slack.event.php
@@ -8,8 +8,11 @@ class LoginFailure_Slack extends Event {
     public function onEvent($event, $data) {
         foreach ((array)$this->getClass('SlackManager')->find() AS &$Token) {
             if (!$Token->isValid()) continue;
-            $channel_user = preg_match('/^#/',$Token->get('name')) ? $Token->get('name') : "@{$Token->get(name)}";
-            $this->getClass('SlackHandler',$Token->get('token'))->call('chat.postMessage',array('channel'=>$channel_user,'text'=>$data['Failure'].' failed to login.'));
+            $args = array(
+                'channel' => $Token->get('name'),
+                'text' => "{$data[Failure]} failed to login.",
+            );
+            $Token->call('chat.postMessage',$args);
             unset($Token);
         }
     }
