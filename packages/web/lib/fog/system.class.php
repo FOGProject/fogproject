8b7c82f31e481b948ca0e4cd405e5f73cbf3f317
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-02-17 23:46:56 +0000

    Fix issue with registration script where if there are multiple pages, it keeps cycling even if you made an entry
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4815 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,9 +1,9 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6307');
+        define('FOG_VERSION', '6309');
         define('FOG_SCHEMA', 210);
-        define('FOG_BCACHE_VER',47);
+        define('FOG_BCACHE_VER',48);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.9.11');
