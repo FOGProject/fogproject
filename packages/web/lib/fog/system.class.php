9c35a20aa543e3661a4adcd57a23a1fd29a20a83
Tom Elliott <tommygunsster@gmail.com>
2016-02-18 00:37:56 +0000

    Now fix the init's properly for registration and setting of id's
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4816 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6309');
+        define('FOG_VERSION', '6311');
         define('FOG_SCHEMA', 210);
         define('FOG_BCACHE_VER',48);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
