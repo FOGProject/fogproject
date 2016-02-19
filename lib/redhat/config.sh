76e68348266840d2c4f2bc28dba6e4a549884c94
Tom Elliott <tommygunsster@gmail.com>
2016-02-19 22:11:23 +0000

    Enable use of repos as a variable to allow for different repos to be enabled/added
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4833 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/redhat/config.sh
+++ lib/redhat/config.sh
@@ -32,14 +32,14 @@ case $linuxReleaseName in
         packages="httpd php php-cli php-common php-gd mysql mysql-server dhcp tftp-server nfs-utils vsftpd net-tools wget xinetd tar gzip make m4 gcc gcc-c++ lftp php-mysqlnd curl php-mcrypt php-mbstring mod_ssl php-fpm php-process"
         command -v dnf >>$workingdir/error_logs/fog_error_${version}.log 2>&1
         if [[ $? -eq 0 ]]; then
-            packageinstaller="dnf -y --enablerepo=remi install"
-            packagelist="dnf list --enablerepo=remi"
-            packageupdater="dnf -y --enablerepo=remi update"
-            packmanUpdate="dnf --enablerepo=remi check-update"
+            packageinstaller="dnf -y --enablerepo=$repos install"
+            packagelist="dnf list --enablerepo=$repos"
+            packageupdater="dnf -y --enablerepo=$repos update"
+            packmanUpdate="dnf --enablerepo=$repos check-update"
         else
-            packageinstaller="yum -y --enablerepo=remi,epel install"
-            packagelist="yum --enablerepo=remi,epel list"
-            packageupdater="yum -y --enablerepo=remi,epel update"
+            packageinstaller="yum -y --enablerepo=$repos install"
+            packagelist="yum --enablerepo=$repos list"
+            packageupdater="yum -y --enablerepo=$repos update"
             packmanUpdate="yum check-update"
             command -v yum-config-manager >/dev/null 2>&1
             if [[ ! $? -eq 0 ]]; then
