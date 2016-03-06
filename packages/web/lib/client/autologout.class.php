5e007c12a035730914c665b69a7deeafe5a8962f
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-06 20:49:01 +0000

    Fix replication issues. Work on implementing a potential single request mechanism for the client
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4954 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/autologout.class.php
+++ packages/web/lib/client/autologout.class.php
@@ -4,9 +4,15 @@ class Autologout extends FOGClient implements FOGClientSend {
     private $time;
     public function send() {
         $this->time = $this->getSetting('FOG_SERVICE_AUTOLOGOFF_MIN');
-        if ($this->Host->getAlo() instanceof HostAutoLogout) $this->time = $this->Host->getAlo()->get('time');
+        if ($this->Host->getAlo() > 4) $this->time = $this->Host->getAlo();
         $this->send = base64_encode($this->time);
         if ($this->newService) {
+            if ($this->json) {
+                $this->send = null;
+                if ($this->time < 5) $this->time = 0;
+                $val['time'] = $this->time * 60;
+                return $val;
+            }
             $time = sprintf("#!ok\n#time=%s",($this->time * 60));
             if ($this->time < 5) $time = '#!time';
             $this->send = $time;
