e76254641b171a9b933b97dfa1c6c8c88b0d4d12
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-07 01:47:45 +0000

    Revert the S40network script back to workable state, but change the break to break out completely if the ip is found on any network
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4959 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '6607');
+        define('FOG_VERSION', '6609');
         define('FOG_SCHEMA', 213);
         define('FOG_BCACHE_VER',50);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
