34f9b24917abf3a05687b39fd1e82510a4034ff4
Tom Elliott <tommygunsster@gmail.com>
2016-03-09 03:21:39 +0000

    Add Sebastians suggestions with a few (maybe?) minor tweaks.  getMACAddresses() function is a good example I think.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@4974 71f96598-fa45-0410-b640-bcd6f8691b32

--- src/buildroot/package/fog/scripts/usr/share/fog/lib/funcs.sh
+++ src/buildroot/package/fog/scripts/usr/share/fog/lib/funcs.sh
@@ -52,8 +52,9 @@ displayBanner() {
 }
 # Gets all system mac addresses except for loopback
 getMACAddresses() {
-    local lomac="00:00:00:00:00:00"
-    cat /sys/class/net/*/address | grep -v $lomac | tr '\n' '|' | sed s/.$//g
+    read ifaces <<< $(/sbin/ip -4 -o addr | awk -F'([ /])+' '/global/ {print $2}' | tr '[:space:]' '|' | sed -e 's/^[|]//g' -e 's/[|]$//g')
+    read mac_addresses <<< $(/sbin/ip -0 -o addr | awk "/$ifaces/ {print \$14}" | tr '[:space:]' '|' | sed -e 's/^[|]//g' -e 's/[|]$//g')
+    echo $mac_addresses
 }
 # Verifies that there is a network interface
 verifyNetworkConnection() {
