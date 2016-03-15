c08159c69ebecdde51bd1de0e5d9cc593265b62f
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-15 18:07:33 +0000

    Fix issue with return data not coming back when using the buildSelectBox method.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5015 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/fogmanagercontroller.class.php
+++ packages/web/lib/fog/fogmanagercontroller.class.php
@@ -171,7 +171,7 @@ abstract class FOGManagerController extends FOGBase {
         $matchID = ($_REQUEST['node'] == 'image' ? ($matchID === 0 ? 1 : $matchID) : $matchID);
         if (empty($elementName)) $elementName = strtolower($this->childClass);
         $this->orderBy($orderBy);
-        $listArray = array_map(function(&$Object) {
+        $listArray = array_map(function(&$Object) use ($matchID,$elementName,$orderBy,$filter,$template) {
             if (!$Object->isValid()) return;
             if (array_key_exists('isEnabled',$this->databaseFields) && !$Object->get('isEnabled')) return;
             $listArray = sprintf('<option value="%s"%s>%s</option>',$Object->get('id'),($matchID == $Object->get('id') ? ' selected' : ($template ? " \${selected_item{$Object->get(id)}" : '')),"{$Object->get(name)} - ({$Object->get(id)})");
