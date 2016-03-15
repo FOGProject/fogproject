393079440c552261cdb27e19bd8f549664f87728
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-15 15:49:46 +0000

    Rewrite the serverinfo page class to fall more in line with newer methods
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5013 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/pages/serverinfo.class.php
+++ packages/web/lib/pages/serverinfo.class.php
@@ -6,13 +6,13 @@ class ServerInfo extends FOGPage {
         parent::__construct($this->name);
         $this->obj = $this->getClass('StorageNode',$_REQUEST['id']);
         $this->menu = array(
-            "?node=storage&sub=edit&id={$_REQUEST['id']}" => _('Edit Node'),
+            "?node=storage&sub=edit&id={$_REQUEST['id']}" => _('Edit Node')
         );
         $this->notes = array(
             "{$this->foglang['Storage']} {$this->foglang['Node']}" => $this->obj->get('name'),
             _('Hostname / IP') => $this->obj->get('ip'),
             $this->foglang['ImagePath'] => $this->obj->get('path'),
-            $this->foglang['FTPPath'] => $this->obj->get('ftppath'),
+            $this->foglang['FTPPath'] => $this->obj->get('ftppath')
         );
     }
     public function index() {
@@ -28,103 +28,110 @@ class ServerInfo extends FOGPage {
             '${field}',
             '${input}',
         );
-        if ($this->obj) {
-            $curroot = trim(trim($this->obj->get(webroot),'/'));
-            $webroot = sprintf('/%s',(strlen($curroot) > 1 ? sprintf('%s/',$curroot) : ''));
-            $URL = sprintf('http://%s%sstatus/hw.php',$this->obj->get('ip'),$webroot);
-            if ($ret = $this->FOGURLRequests->process($URL)) {
+        if (!$this->obj->isValid()) {
+            printf('<p>%s</p>',_('Invalid Server Information!'));
+            return;
+        }
+        $curroot = trim(trim($this->obj->get('webroot'),'/'));
+        $webroot = sprintf('/%s',(strlen($curroot) > 1 ? sprintf('%s/',$curroot) : ''));
+        $URL = sprintf('http://%s%sstatus/hw.php',$this->obj->get('ip'),$webroot);
+        $ret = $this->FOGURLRequests->process($URL);
+        $ret = trim($ret[0]);
+        if (empty($ret) || !$ret) {
+            printf('<p>%s</p>',_('Unable to pull server information!'));
+            return;
+        }
+        $section = 0;
+        $arGeneral = array();
+        $arFS = array();
+        $arNIC = array();
+        array_map(function(&$line) use (&$section,&$arGeneral,&$arFS,&$arNIC) {
+            $line = trim($line);
+            switch ($line) {
+            case '@@start':
+            case '@@end':
+                break;
+            case '@@general':
                 $section = 0;
-                $arGeneral = array();
-                $arFS = array();
-                $arNIC = array();
-                foreach((array)explode("\n",$ret[0]) AS $i => &$line) {
-                    $line = trim( $line );
-                    switch ($line) {
-                    case '@@start':
-                        break;
-                    case '@@general':
-                        $section = 0;
-                        break;
-                    case '@@fs':
-                        $section = 1;
-                        break;
-                    case '@@nic':
-                        $section = 2;
-                        break;
-                    case '@@end':
-                        $section = 3;
-                        break;
-                    default:
-                        switch ($section) {
-                        case 0:
-                            $arGeneral[] = $line;
-                            break;
-                        case 1:
-                            $arFS[] = $line;
-                            break;
-                        case 2:
-                            $arNIC[] = $line;
-                            break;
-                        }
-                        break;
-                    }
+                break;
+            case '@@fs':
+                $section = 1;
+                break;
+            case '@@nic':
+                $section = 2;
+                break;
+            default:
+                switch ($section) {
+                case 0:
+                    $arGeneral[] = $line;
+                    break;
+                case 1:
+                    $arFS[] = $line;
+                    break;
+                case 2:
+                    $arNIC[] = $line;
+                    break;
                 }
-                unset($line);
-                for($i=0;$i<count($arNIC);$i++) {
-                    $arNicParts = explode("$$", $arNIC[$i]);
-                    if (count($arNicParts) == 5) {
-                        $NICTransSized[] = $this->formatByteSize($arNicParts[2]);
-                        $NICRecSized[] = $this->formatByteSize($arNicParts[1]);
-                        $NICErrInfo[] = $arNicParts[3];
-                        $NICDropInfo[] = $arNicParts[4];
-                        $NICTrans[] = sprintf('%s %s',$arNicParts[0],_('TX'));
-                        $NICRec[] = sprintf('%s %s',$arNicParts[0],_('RX'));
-                        $NICErr[] =	sprintf('%s %s',$arNicParts[0],_('Errors'));
-                        $NICDro[] = sprintf('%s %s',$arNicParts[0],_('Dropped'));
-                    }
-                }
-                if(count($arGeneral)>=1) {
-                    $fields = array(
-                        sprintf('<b>%s</b>',_('General Information')) => '&nbsp;',
-                        _('Storage Node') => $this->obj->get(name),
-                        _('IP') => $this->FOGCore->resolveHostname($this->obj->get('ip')),
-                        _('Kernel') => $arGeneral[0],
-                        _('Hostname') => $arGeneral[1],
-                        _('Uptime') => $arGeneral[2],
-                        _('CPU Type') => $arGeneral[3],
-                        _('CPU Count') => $arGeneral[4],
-                        _('CPU Model') => $arGeneral[5],
-                        _('CPU Speed') => $arGeneral[6],
-                        _('CPU Cache') => $arGeneral[7],
-                        _('Total Memory') => $arGeneral[8],
-                        _('Used Memory') => $arGeneral[9],
-                        _('Free Memory') => $arGeneral[10],
-                        sprintf('<b>%s</b>',_('File System Information')) => '&nbsp;',
-                        _('Total Disk Space') => $arFS[0],
-                        _('Used Disk Space') => $arFS[1],
-                        sprintf('<b>%s</b>',_('Network Information')) => '&nbsp;',
-                    );
-                    $i = 0;
-                    foreach((array)$NICTrans AS $i => &$txtran) {
-                        $ethName = explode(' ',$NICTrans[$i]);
-                        $fields[sprintf('<b>%s %s</b>',$ethName[0],_('Information'))] = '&nbsp;';
-                        $fields[$NICTrans[$i]] = $NICTransSized[$i];
-                        $fields[$NICRec[$i]] = $NICRecSized[$i];
-                        $fields[$NICErr[$i]] = $NICErrInfo[$i];
-                        $fields[$NICDro[$i]] = $NICDropInfo[$i];
-                        $i++;
-                    }
-                    unset($txtran);
-                }
-                foreach((array)$fields AS $field => &$input) {
-                    $this->data[] = array(
-                        'field'=>$field,
-                        'input'=>$input,
-                    );
-                }
-                $this->HookManager->processEvent('SERVER_INFO_DISP',array('headerData'=>&$this->headerData,'data'=>&$this->data,'templates'=>&$this->templates,'attributes'=>&$this->attributes));
-                $this->render();
-            } else printf('<p>%s</p>',_('Unable to pull server information!'));
-        } else printf('<p>%s</p>',_('Invalid Server Information!'));
+                break;
+            }
+            unset($line);
+        },(array)explode("\n",$ret));
+        array_map(function(&$nic) use (&$NICTransSized,&$NICRecSized,&$NICErrInfo,&$NICDropInfo,&$NICTrans,&$NICRec,&$NICErr,&$NICDro) {
+            $nicparts = explode("$$", $nic);
+            if (count($nicparts) == 5) {
+                $NICTransSized[] = $this->formatByteSize($nicparts[2]);
+                $NICRecSized[] = $this->formatByteSize($nicparts[1]);
+                $NICErrInfo[] = $nicparts[3];
+                $NICDropInfo[] = $nicparts[4];
+                $NICTrans[] = sprintf('%s %s',$nicparts[0],_('TX'));
+                $NICRec[] = sprintf('%s %s',$nicparts[0],_('RX'));
+                $NICErr[] =	sprintf('%s %s',$nicparts[0],_('Errors'));
+                $NICDro[] = sprintf('%s %s',$nicparts[0],_('Dropped'));
+            }
+            unset($nic);
+        },(array)$arNIC);
+        if (count($arGeneral) < 1) {
+            printf(_('Unable to find basic information'));
+            return;
+        }
+        $fields = array(
+            sprintf('<b>%s</b>',_('General Information')) => '&nbsp;',
+            _('Storage Node') => $this->obj->get('name'),
+            _('IP') => $this->FOGCore->resolveHostname($this->obj->get('ip')),
+            _('Kernel') => $arGeneral[0],
+            _('Hostname') => $arGeneral[1],
+            _('Uptime') => $arGeneral[2],
+            _('CPU Type') => $arGeneral[3],
+            _('CPU Count') => $arGeneral[4],
+            _('CPU Model') => $arGeneral[5],
+            _('CPU Speed') => $arGeneral[6],
+            _('CPU Cache') => $arGeneral[7],
+            _('Total Memory') => $arGeneral[8],
+            _('Used Memory') => $arGeneral[9],
+            _('Free Memory') => $arGeneral[10],
+            sprintf('<b>%s</b>',_('File System Information')) => '&nbsp;',
+            _('Total Disk Space') => $arFS[0],
+            _('Used Disk Space') => $arFS[1],
+            sprintf('<b>%s</b>',_('Network Information')) => '&nbsp;',
+        );
+        array_walk($NICTrans,function(&$txtran,&$index) use (&$NICTransSized,&$NICRecSized,&$NICErrInfo,&$NICDropInfo,&$NICTrans,&$NICRec,&$NICErr,&$NICDro,&$fields) {
+            $ethName = explode(' ',$txtran);
+            $fields[sprintf('<b>%s %s</b>',$ethName[0],_('Information'))] = '&nbsp;';
+            $fields[$NICTrans[$index]] = $NICTransSized[$index];
+            $fields[$NICRec[$index]] = $NICRecSized[$index];
+            $fields[$NICErr[$index]] = $NICErrInfo[$index];
+            $fields[$NICDro[$index]] = $NICDropInfo[$index];
+            unset($txtran,$index);
+        });
+        unset($arGeneral,$arNIC,$arFS,$NICTransSized,$NICRecSized,$NICErrInfo,$NICDropInfo,$NICTrans,$NICRec,$NICErr,$NICDro);
+        $this->data = array();
+        array_walk($fields,function(&$input,&$field) {
+            $this->data[] = array(
+                'field' => $field,
+                'input' => $input,
+            );
+        });
+        $this->HookManager->processEvent('SERVER_INFO_DISP',array('headerData'=>&$this->headerData,'data'=>&$this->data,'templates'=>&$this->templates,'attributes'=>&$this->attributes));
+        $this->render();
     }
 }
