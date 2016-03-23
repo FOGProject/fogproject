b92bc596213117b799126c26e75321613a056b15
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-23 21:31:38 +0000

    Fix listing count as logic was backwards
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5109 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/grouplisting.php
+++ packages/web/service/grouplisting.php
@@ -1,7 +1,7 @@
 <?php
 require('../commons/base.inc.php');
 try {
-    if ($FOGCore::getClass('GroupManager')->count()) throw new Exception(_('There are no groups on this server.'));
+    if (!$FOGCore::getClass('GroupManager')->count()) throw new Exception(_('There are no groups on this server.'));
     foreach ((array)$FOGCore::getClass('GroupManager')->find() AS $i => &$Group) {
         if (!$Group->isValid()) continue;
         printf('\tID# %d\t-\t%s\n',$Group->get('id'),$Group->get('name'));
