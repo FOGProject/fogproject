a91f3ce5aeee2daae3d200eaf3b16f46a550be7d
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-25 11:24:18 +0000

    Fix the task queue to show the appropriate number of clients in front.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5121 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/reg-task/taskqueue.class.php
+++ packages/web/lib/reg-task/taskqueue.class.php
@@ -5,7 +5,7 @@ class TaskQueue extends TaskingElement {
             if ($this->imagingTask) {
                 if ($this->Task->isMulticast()) {
                     $this->Task
-                        ->set('checkinTime',$this->formatTime('','Y-md H:i:s'))
+                        ->set('checkinTime',$this->formatTime('','Y-m-d H:i:s'))
                         ->set('stateID',$this->getCheckedInState());
                     if (!$this->Task->save()) throw new Exception(_('Failed to update task'));
                     $MulticastSession = self::getClass('MulticastSessions',@max($this->getSubObjectIDs('MulticastSessionsAssociation',array('taskID'=>$this->Task->get('id')),'msID')));
@@ -27,34 +27,42 @@ class TaskQueue extends TaskingElement {
                     if ($this->Task->isUpload()) $this->StorageNode = $this->Image->StorageGroup->getMasterStorageNode();
                     self::$HookManager->processEvent('TASK_NODE',array('StorageNode'=>&$this->StorageNode,'Host'=>&$this->Host));
                 } else {
-                    $totalSlots = $this->StorageGroup->getTotalSupportedClients();
-                    $usedSlots = $this->StorageGroup->getUsedSlotCount();
-                    $inFront = $this->Task->getInFrontOfHostCount();
-                    $groupOpenSlots = $totalSlots - $usedSlots;
-                    if ($groupOpenSlots < 1) throw new Exception(sprintf('%s, %s %d %s',_('No open slots'),_('There are'),$inFront,_('before me.')));
-                    if ($groupOpenSlots <= $inFront) throw new Exception(sprintf('%s %d %s',_('There are open slots, but'),$inFront,_('before me')));
-                    $method = ($this->Task->isUpload() ? 'getMasterStorageNode' : 'getOptimalStorageNode');
-                    $this->StorageNode = self::nodeFail($this->Image->getStorageGroup()->$method($this->Task->get('imageID')),$this->Host->get('id'));
-                    foreach ($this->StorageNodes AS $i => &$StorageNode) {
-                        if (!$StorageNode->isValid()) continue;
-                        if ($StorageNode->get('maxClients') < 1) continue;
-                        $nodeAvailableSlots = (int)$StorageNode->get('maxClients') - (int)$StorageNode->getUsedSlotCount();
-                        if ($nodeAvailableSlots < 1) continue;
-                        if (!isset($tmpStorageNode)) {
-                            $tmpStorageNode = self::nodeFail($StorageNode,$this->Host->get('id'));
-                            continue;
+                    if (in_array($this->Task->get('stateID'),array_merge((array)$this->getQueuedState(),(array)$this->getCheckedInState()))) {
+                        $this->Task
+                            ->set('checkInTime',$this->formatTime('now','Y-m-d H:i:s'))
+                            ->set('stateID',$this->getCheckedInState())
+                            ->save();
+                        $totalSlots = $this->StorageGroup->getTotalSupportedClients();
+                        $usedSlots = $this->StorageGroup->getUsedSlotCount();
+                        $inFront = $this->Task->getInFrontOfHostCount();
+                        $groupOpenSlots = $totalSlots - $usedSlots;
+                        if ($groupOpenSlots < 1) throw new Exception(sprintf('%s, %s %d %s',_('No open slots'),_('There are'),$inFront,_('before me.')));
+                        if ($groupOpenSlots <= $inFront) throw new Exception(sprintf('%s %d %s',_('There are open slots, but'),$inFront,_('before me')));
+                        $method = ($this->Task->isUpload() ? 'getMasterStorageNode' : 'getOptimalStorageNode');
+                        $this->StorageNode = self::nodeFail($this->Image->getStorageGroup()->$method($this->Task->get('imageID')),$this->Host->get('id'));
+                        foreach ($this->StorageNodes AS &$StorageNode) {
+                            if (!$StorageNode->isValid()) continue;
+                            if ($StorageNode->get('maxClients') < 1) continue;
+                            $nodeAvailableSlots = (int)$StorageNode->get('maxClients') - (int)$StorageNode->getUsedSlotCount();
+                            if ($nodeAvailableSlots < 1) continue;
+                            if (!isset($tmpStorageNode)) {
+                                $tmpStorageNode = self::nodeFail($StorageNode,$this->Host->get('id'));
+                                continue;
+                            }
+                            if ($StorageNode->getClientLoad() < $tmpStorageNode->getClientLoad()) $tmpStorageNode = self::nodeFail($StorageNode,$this->Host->get('id'));
+                            unset($StorageNode);
                         }
-                        if ($StorageNode->getClientLoad() < $tmpStorageNode->getClientLoad()) $tmpStorageNode = self::nodeFail($StorageNode,$this->Host->get('id'));
-                        unset($StorageNode);
                     }
+                    if ($tmpStorageNode instanceof StorageNode && $tmpStorageNode->get('id') != $this->StorageNode->get('id')) $this->StorageNode = $tmpStorageNode;
+                    if (!$this->StorageNode->isValid()) throw new Exception(_('Unable to find a auitable Storage Node for imaging!'));
                 }
-                if ($tmpStorageNode instanceof StorageNode && $tmpStorageNode->get('id') != $this->StorageNode->get('id')) $this->StorageNode = $tmpStorageNode;
-                if (!$this->StorageNode->isValid()) throw new Exception(_('Unable to find a auitable Storage Node for imaging!'));
                 $this->Task
                     ->set('NFSMemberID',$this->StorageNode->get('id'));
                 if (!$this->ImageLog(true)) throw new Exception(_('Failed to update/create image log'));
             }
-            $this->Task->set('stateID',$this->getProgressState());
+            $this->Task
+                ->set('stateID',$this->getProgressState())
+                ->set('checkInTime',$this->formatTime('now','Y-m-d H:i:s'));
             if (!$this->Task->save()) throw new Exception(_('Failed to update Task'));
             if (!$this->TaskLog()) throw new Exception(_('Failed to update/create task log'));
             self::$EventManager->notify('HOST_CHECKIN',array('Host'=>&$this->Host));
