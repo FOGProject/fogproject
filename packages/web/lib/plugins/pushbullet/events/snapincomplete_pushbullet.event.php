db6abdf3698b77ef43ab97ddef01e30f3d1233e9
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-25 00:44:54 +0000

    Fix snapin pushbullet event so it actually works.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5118 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/pushbullet/events/snapincomplete_pushbullet.event.php
+++ packages/web/lib/plugins/pushbullet/events/snapincomplete_pushbullet.event.php
@@ -11,7 +11,7 @@ class SnapinComplete_PushBullet extends PushbulletExtends {
     public $active = true;
     public function onEvent($event, $data) {
         self::$message = 'This host has completed snapin tasking.';
-        self::$shortdesc = 'Complete';
+        self::$shortdesc = 'Snapin(s) Complete';
         parent::onEvent($event,$data);
     }
 }
