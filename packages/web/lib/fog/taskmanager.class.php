6866f74c0ccf29bb048adb4803a8a4950851c72f
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-03-30 00:00:15 +0000

    Make sure cancelling active task, snapins get cancelled as well
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5143 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/taskmanager.class.php
+++ packages/web/lib/fog/taskmanager.class.php
@@ -3,7 +3,7 @@ class TaskManager extends FOGManagerController {
     public function cancel($taskids) {
         $findWhere = array('taskID'=>(array)$taskids);
         $SnapinJobIDs = $this->getSubObjectIDs('SnapinTask',$findWhere,'jobID');
-        $SnapinTaskIDs = $this->getSubObjectIDs('SnapinTask',$findWhere,'taskID');
+        $SnapinTaskIDs = $this->getSubObjectIDs('SnapinTask',$findWhere);
         $MulticastSessionIDs = $this->getSubObjectIDs('MulticastSessionsAssociation',$findWhere,'msID');
         $this->array_change_key($findWhere,'taskID','id');
         if (count($SnapinTaskIDs)) self::getClass('SnapinTaskManager')->cancel($SnapinTaskIDs);
