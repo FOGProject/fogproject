c0b84e02e3dec969710faed4295f81ab8ef7561a
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-04-04 23:47:46 +0000

    Many minor adjustments
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5189 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/grouplisting.php
+++ packages/web/service/grouplisting.php
@@ -1,12 +1,12 @@
 <?php
 require_once('../commons/base.inc.php');
 try {
-    if (!$FOGCore::getClass('GroupManager')->count()) throw new Exception(_('There are no groups on this server.'));
-    foreach ((array)$FOGCore::getClass('GroupManager')->find() AS $i => &$Group) {
-        if (!$Group->isValid()) continue;
+    if (FOGCore::getClass('GroupManager')->count() < 1) throw new Exception(_('There are no groups on this server.'));
+    array_map(function(&$Group) {
+        if (!$Group->isValid()) return;
         printf('\tID# %d\t-\t%s\n',$Group->get('id'),$Group->get('name'));
         unset($Group);
-    }
+    },(array)FOGCore::getClass('GroupManager')->find());
 } catch (Exception $e) {
     echo $e->getMessage();
 }
