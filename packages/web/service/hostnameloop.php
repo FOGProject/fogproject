27ef7b9bae2d28b4f1f1fe50968396d52a469b25
Tom Elliott <tommygunsster@gmail.com>
2016-04-04 23:47:46 +0000

    Many minor adjustments
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5189 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/hostnameloop.php
+++ packages/web/service/hostnameloop.php
@@ -2,11 +2,8 @@
 require_once('../commons/base.inc.php');
 try {
     $hostname = trim(base64_decode(trim($_REQUEST['host'])));
-    foreach ($FOGCore::getClass('HostManager')->find(array('name' => $hostname)) AS &$Host) {
-        if (!$Host->isValid()) break;
-        throw new Exception(sprintf("\t%s\n%s: %s",_('A hostname with that name already exists.'),_('The MAC Address associated with this is'),$Host->get('mac')));
-        unset($Host);
-    }
+    $mac = array_filter((array)$FOGCore->getSubObjectIDs('MACAddressAssociation',array('hostID'=>$FOGCore->getSubObjectIDs('Host',array('name'=>$hostname))),'mac'));
+    if (count($mac)) throw new Exception(sprintf("\t%s\n%s: %s",_('A hostname with that name already exists.'),_('The MAC Address associated with this is'),array_shift($mac)));
     throw new Exception('#!ok');
 } catch (Exception $e) {
     echo $e->getMessage();
