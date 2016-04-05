7c5e53067814f598d9c6b73a7873db3d24d79ef9
Tom Elliott <tommygunsster@gmail.com>
2016-04-05 16:50:45 +0000

    Move the sec_time setting only when/where needed.  Ensure we pass the #!ok flag for the printer listing.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5193 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/printerlisting.php
+++ packages/web/service/printerlisting.php
@@ -2,6 +2,7 @@
 require_once('../commons/base.inc.php');
 try {
     if (FOGCore::getClass('PrinterManager')->count() < 1) throw new Exception("#!np\n");
+    echo "#!ok\n";
     $printers = (array)FOGCore::getClass('PrinterManager')->find('','AND','name','ASC','=',false,false,'name');
     array_walk($printers,function(&$name,$index) {
         echo "#printer=$index=$name\n";
