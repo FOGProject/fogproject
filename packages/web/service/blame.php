b10991b40baede327b898952f9fa2cd97fce11aa
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-04-08 00:30:57 +0000

    Move the blame file into its own class and call it that way.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5201 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/blame.php
+++ packages/web/service/blame.php
@@ -1,27 +1,3 @@
 <?php
 require_once('../commons/base.inc.php');
-try {
-    $Host = $FOGCore->getHostItem(false);
-    $Task = $Host->get('task');
-    if (!$Task->isValid()) throw new Exception(sprintf('%s: %s (%s)', _('No Active Task found for Host'), $Host->get('name'),$Host->get('mac')));
-    $imagingTasks = in_array($Task->get('typeID'),array(1,2,8,15,16,17));
-    $StorageGroup = $Task->getStorageGroup();
-    if ($imagingTasks && !$StorageGroup->isValid()) throw new Exception(_('Invalid Storage Group'));
-    $StorageNodes = FOGCore::getClass('StorageNodeManager')->find(array('id'=>$StorageGroup->get('enablednodes')));
-    if ($imagingTasks && !$StorageNodes) throw new Exception(_('Could not find a Storage Node. Is there one enabled within this Storage Group?'));
-    foreach ($StorageNodes AS $StorageNode) {
-        $blamed = $FOGCore->getAllBlamedNodes();
-        if ($Task->get('NFSMemberID') && !in_array($Task->get('NFSMemberID'),(array)$blamed)) {
-            $NodeFailure = FOGCore::getClass('NodeFailure')
-                ->set('storageNodeID',$Task->get('NFSMemberID'))
-                ->set('taskID',$Task->get('id'))
-                ->set('hostID',$Host->get('id'))
-                ->set('groupID',$Task->get('NFSGroupID'))
-                ->set('failureTime',$FOGCore->nice_date('+5 minutes')->format('Y-m-d H:i:s'));
-            if ($NodeFailure->save()) $Task->set('stateID',$FOGCore->getQueuedState());
-        } else $Task->set('stateID',$FOGCore->getQueuedState());
-    }
-    if ($Task->save()) echo '##';
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+FOGCore::getClass('Blame');
