14ce6ccf71a434735a0685e8e9c9b53a2859849d
Tom Elliott <tommygunsster@gmail.com>
2016-04-11 14:23:52 +0000

    Fix upload issue where if rename/put failed, it didn't upload the file properly and still completed as successful
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5211 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7128');
+        define('FOG_VERSION', '7130');
         define('FOG_SCHEMA', 216);
         define('FOG_BCACHE_VER',53);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
