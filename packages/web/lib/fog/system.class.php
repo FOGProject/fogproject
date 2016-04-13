0feeb720043d3618fefa3818e2ad8ef1cfa1ff74
Tom Elliott <tommygunsster@gmail.com>
2016-04-13 13:28:25 +0000

    Update iPXE files to latest pull.  Hopefully a slightly better approach to the rename and/or put functionality of the native fogftp class.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5229 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7166');
+        define('FOG_VERSION', '7168');
         define('FOG_SCHEMA', 216);
         define('FOG_BCACHE_VER',55);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
