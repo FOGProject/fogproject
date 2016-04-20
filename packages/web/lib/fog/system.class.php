52da15b2a2f6e41b3b180420dd22b8a42ac608cb
Tom Elliott <tommygunsster@gmail.com>
2016-04-20 11:19:56 +0000

    Add Donate link to footer and about page.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5258 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7224');
+        define('FOG_VERSION', '7226');
         define('FOG_SCHEMA', 216);
         define('FOG_BCACHE_VER',55);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
