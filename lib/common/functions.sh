090c5d8dd8627b2c8ad005cef49e67e45a1358bb
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-04-21 17:51:38 +0000

    Try to make sure the soft links are created for the web dir folder in the case we don't have it already.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5272 71f96598-fa45-0410-b640-bcd6f8691b32

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -1803,6 +1803,8 @@ class Config {
     chown -R ${apacheuser}:${apacheuser} $webdirdest
     errorStat $?
     rm -f "$webdirdest/mobile/css/font-awesome.css" $webdirdest/mobile/{fonts,less,scss} &>>$workingdir/error_logs/fog_error_${version}.log 2>&1
+    [[ -d /var/www/html/ && ! -e /var/www/html/fog/ ]] && ln -s "$webdirdest" /var/www/html/
+    [[ -d /var/www/ && ! -e /var/www/fog ]] && ln -s "$webdirdest" /var/www/
     ln -s "$webdirdest/management/css/font-awesome.css" "$webdirdest/mobile/css/font-awesome.css"
     ln -s "$webdirdest/management/fonts" "$webdirdest/mobile/"
     ln -s "$webdirdest/management/less" "$webdirdest/mobile/"
