5f5f0d3fff1b853d33bbe07fe092e1d7412dafc4
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-04-22 17:38:18 +0000

    Apparently all will be bad if connection is made with db name without the dbname already existing.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5281 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7270');
+        define('FOG_VERSION', '7272');
         define('FOG_SCHEMA', 217);
         define('FOG_BCACHE_VER',55);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
