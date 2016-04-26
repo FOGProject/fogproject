bcbfc2ee8a4bedf7b90c1bf442825beaa9fb81d1
Tom Elliott <tommygunsster@gmail.com>
2016-04-26 21:15:47 +0000

    Should, hopefully, prevent new tasks from unlinking the multicast log file.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5310 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7328');
+        define('FOG_VERSION', '7330');
         define('FOG_SCHEMA', 217);
         define('FOG_BCACHE_VER',55);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
