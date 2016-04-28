322db63dc382661f87da78ddeca7dd7279165586
Tom Elliott <tommygunsster@gmail.com>
2016-04-28 00:16:56 +0000

    should send all modules with their enabled status by global or host where global takes preference
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5335 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/getversion.php
+++ packages/web/service/getversion.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../commons/base.inc.php');
 if (isset($_REQUEST['client'])) echo FOG_CLIENT_VERSION;
-else if (isset($_REQUEST['clientver'])) exit;
+else if (isset($_REQUEST['clientver'])) echo '9.9.99';
 else echo FOG_VERSION;
 exit;
