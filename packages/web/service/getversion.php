cbe4a032be0bde9e4e7551d0f7a929e4fa6226aa
Tom Elliott <tommygunsster@gmail.com>
2016-05-02 15:05:17 +0000

    Make the hard coded string 0.9.12
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5362 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/getversion.php
+++ packages/web/service/getversion.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../commons/base.inc.php');
-if (isset($_REQUEST['client'])) echo '9.9.99';
+if (isset($_REQUEST['client'])) echo '0.9.12';
 else if (isset($_REQUEST['clientver'])) echo FOG_CLIENT_VERSION;
 else echo FOG_VERSION;
 exit;
