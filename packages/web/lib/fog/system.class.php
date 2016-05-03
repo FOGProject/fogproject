8277c7b3b8279622f0250ce675184876d6df8ee9
Tom Elliott <tommygunsster@gmail.com>
2016-05-03 20:21:58 +0000

    Fix usertracker so we don't get erroneous error messages that don't really mean anything but kind of seem like they do
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5381 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7470');
+        define('FOG_VERSION', '7472');
         define('FOG_SCHEMA', 217);
         define('FOG_BCACHE_VER',55);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
