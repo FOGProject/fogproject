c747a3b8dd374873ed3de89ed76c5839e7425edc
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-05-05 17:29:43 +0000

    Fix snapins so the client get's the array properly
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5390 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/multicastsessionsmanager.class.php
+++ packages/web/lib/fog/multicastsessionsmanager.class.php
@@ -2,7 +2,8 @@
 class MulticastSessionsManager extends FOGManagerController {
     public function cancel($multicastsessionids) {
         $findWhere = array('id'=>(array)$multicastsessionids);
-        $this->update($findWhere,'',array('stateID'=>$this->getCancelledState(),'completetime'=>$this->formatTime('','Y-m-d H:i:s'),'clients'=>0));
+        $cancelled = $this->getCancelledState();
+        $this->update($findWhere,'',array('stateID'=>$cancelled));
         $this->array_change_key($findWhere,'id','msID');
         self::getClass('MulticastSessionsAssociationManager')->destroy($findWhere);
     }
