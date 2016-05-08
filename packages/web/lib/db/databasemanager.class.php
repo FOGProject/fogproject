5bda25a49390fca7bfeb67f2f04fcb1f80388f69
Tom Elliott <tommygunsster@gmail.com>
2016-05-08 01:16:21 +0000

    Now that sanitization is proper, fix up and use original code.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5398 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/db/databasemanager.class.php
+++ packages/web/lib/db/databasemanager.class.php
@@ -15,7 +15,7 @@ class DatabaseManager extends FOGCore {
             self::$DB = FOGCore::getClass('Oracle');
             break;
         }
-        if (!isset($_POST['export']) && $this->getVersion() < FOG_SCHEMA && !preg_match('#schemaupdater#i',htmlentities($_SERVER['QUERY_STRING'],ENT_QUOTES,'utf-8'))) $this->redirect('?node=schemaupdater');
+        if (!isset($_POST['export']) && $this->getVersion() < FOG_SCHEMA && !preg_match('#schemaupdater#i',htmlspecialchars($_SERVER['QUERY_STRING'],ENT_QUOTES,'utf-8'))) $this->redirect('?node=schemaupdater');
         return $this;
     }
     public function getDB() {
