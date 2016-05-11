1546d7434c274b9e0142a9565b1f3c30a19485cb
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-05-11 16:58:33 +0000

    Should more appropriately fix issues.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5439 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/jobs.class.php
+++ packages/web/lib/client/jobs.class.php
@@ -1,18 +1,10 @@
 <?php
 class Jobs extends FOGClient implements FOGClientSend {
     public function send() {
-        try {
-            $this->send = '#!nj';
-            $Task = $this->Host->get('task');
-            if ($Task->isValid() && $Task->isInitNeededTasking()) $this->send = '#!ok';
-            if ($this->json && stripos(strtolower($_SERVER['SCRIPT_NAME']),'jobs.php')) throw new Exception($this->send);
-            if ($this->json) return array('job'=>(bool)preg_match('#ok#i',preg_replace('/^[#][!]/','',$this->send)));
-        } catch (Exception $e) {
-            if ($this->json) {
-                echo json_encode(array('error'=>preg_replace('/^[#][!]?/','',$e->getMessage())));
-                exit;
-            }
-            throw new Exception($e->getMessage());
-        }
+        $this->send = '#!nj';
+        $Task = $this->Host->get('task');
+        if ($Task->isValid() && $Task->isInitNeededTasking()) $this->send = '#!ok';
+        if ($this->json && stripos(strtolower($_SERVER['SCRIPT_NAME']),'jobs.php')) throw new Exception($this->send);
+        if ($this->json) return array('job'=>(bool)preg_match('#ok#i',preg_replace('/^[#][!]/','',$this->send)));
     }
 }
