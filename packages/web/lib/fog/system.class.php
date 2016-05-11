844e9c45fbbfc111a430668f6201a0466575577e
Tom Elliott <tommygunsster@gmail.com>
2016-05-11 15:25:20 +0000

    Revert iPXE binaries to what was there during svn version 5380.  Something was introduced later on that is causing many other issues.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5436 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7581');
+        define('FOG_VERSION', '7583');
         define('FOG_SCHEMA', 218);
         define('FOG_BCACHE_VER',56);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
