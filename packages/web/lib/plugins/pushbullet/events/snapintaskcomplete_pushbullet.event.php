ee36760be8163d00c081b1451d6ab337b09f1a5e
Tom Elliott <tommygunsster@gmail.com>
2016-05-13 13:25:56 +0000

    Attempt to fix issue with snapin completion events on pushbullet and slack plugins.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5451 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/plugins/pushbullet/events/snapintaskcomplete_pushbullet.event.php
+++ packages/web/lib/plugins/pushbullet/events/snapintaskcomplete_pushbullet.event.php
@@ -10,8 +10,8 @@ class SnapinTaskComplete_PushBullet extends PushbulletExtends {
     protected $author = 'Tom Elliott';
     public $active = true;
     public function onEvent($event, $data) {
-        self::$message = sprintf('The snapin has completed with status code: %s',$event['SnapinTask']->get('return'));
-        self::$shortdesc = sprintf('%s completed',$event['Snapin']->get('name'));
+        self::$message = sprintf('The snapin has completed installation on %s with status code: %s',$data['Host']->get('name'),$data['SnapinTask']->get('return'));
+        self::$shortdesc = sprintf('%s completed',$data['Snapin']->get('name'));
         parent::onEvent($event,$data);
     }
 }
