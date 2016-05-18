a9cea9f5aecb0e9a2c09aabef1be3823e719f840
Tom Elliott <tommygunsster@gmail.com>
2016-05-18 21:09:25 +0000

    Make status files use array_walk and fix schema issues
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5499 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/hw.php
+++ packages/web/status/hw.php
@@ -2,11 +2,12 @@
 ob_start();
 require_once('../commons/base.inc.php');
 header('Content-Type: text/event-stream');
-array_map(function(&$val) {
+header('Connection: close');
+$hwinfo = $FOGCore->getHWInfo();
+@array_walk($hwinfo,function(&$val,&$index) {
     echo "$val\n";
     unset($val);
-},(array)$FOGCore->getHWInfo());
-header('Connection: close');
+});
 flush();
 ob_flush();
 ob_end_flush();
