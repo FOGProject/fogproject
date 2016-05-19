033084d88706756e1574c47425242ca1f3a500f8
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-05-19 11:14:56 +0000

    Should fix various issues particularly pertaining to the schema page.  We should report message as failed if something failed along the way that is kind of required.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5502 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/pages/schemaupdaterpage.class.php
+++ packages/web/lib/pages/schemaupdaterpage.class.php
@@ -14,24 +14,30 @@ class SchemaUpdaterPage extends FOGPage {
      * The first page displayed especially when a user logs in.
      */
     public function index() {
-        $this->title = _('Database Schema Installer / Updater');
-        $vals = array(
-            _('Your FOG database schema is not up to date, either because you have updated FOG or this is a new FOG Installation. If this is an upgrade, there will be a database back stored on your FOG Server defaulting under the folder /home/fogDBbackups/. Should anything go wrong, this backup will enable you to return to the previous install if needed.'),
-            _('Are you sure you wish to install or update the FOG database?'),
-            $this->formAction,
-            _('Install/Upgrade Now'),
-            _('If you would like to backup your FOG database you can do so using MySQL Administrator or by running the following command in a terminal window (Applications -> System Tools -> Terminal), this will save sqldump in your home directory).'),
-            "\n",
-            _('Alternatively, you can use the button below to obtain a copy of your current fog database.'),
-            _('Export-Backup DB'),
-        );
-        vprintf('<p>%s</p><p>%s</p><br/><form method="post" action="%s"><p class="c"><input type="submit" name="confirm" value="%s"/></p></form><p>%s</p><div id="sidenotes"><pre><code>cd%smysqldump --allow-keywords -x -v fog > fogbackup.sql</code></pre></div><br/><p>%s</p><form method="post" action="../management/export.php"><input type="hidden" name="type" value="sql"/><p class="c"><input type="submit" name="export" value="%s"/></p></form>',$vals);
+        if (!self::$DB->link()) {
+            $this->title = _('Database Connection Invalid');
+            echo _('Your database connection appears to be invalid. FOG is unable to communicate with the database.  There are many reasons why this could be the case.  Please check your credentials in '.dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'fog/config.class.php. Also confirm that the database is indeed running. If credentials are correct, and the Database service is running, check to ensure your filesystem has enough space.');
+        } else {
+            $this->title = _('Database Schema Installer / Updater');
+            $vals = array(
+                _('Your FOG database schema is not up to date, either because you have updated FOG or this is a new FOG Installation. If this is an upgrade, there will be a database back stored on your FOG Server defaulting under the folder /home/fogDBbackups/. Should anything go wrong, this backup will enable you to return to the previous install if needed.'),
+                _('Are you sure you wish to install or update the FOG database?'),
+                $this->formAction,
+                _('Install/Upgrade Now'),
+                _('If you would like to backup your FOG database you can do so using MySQL Administrator or by running the following command in a terminal window (Applications -> System Tools -> Terminal), this will save sqldump in your home directory).'),
+                "\n",
+                _('Alternatively, you can use the button below to obtain a copy of your current fog database.'),
+                _('Export-Backup DB'),
+            );
+            vprintf('<p>%s</p><p>%s</p><br/><form method="post" action="%s"><p class="c"><input type="submit" name="confirm" value="%s"/></p></form><p>%s</p><div id="sidenotes"><pre><code>cd%smysqldump --allow-keywords -x -v fog > fogbackup.sql</code></pre></div><br/><p>%s</p><form method="post" action="../management/export.php"><input type="hidden" name="type" value="sql"/><p class="c"><input type="submit" name="export" value="%s"/></p></form>',$vals);
+        }
     }
     public function index_post() {
         if (!isset($_REQUEST['confirm'])) return;
         require_once(sprintf('%s%scommons%sschema.php',BASEPATH,DIRECTORY_SEPARATOR,DIRECTORY_SEPARATOR));
         $errors = array();
         try {
+            if (!self::$DB->link()) throw new Exception(_('No connection available'));
             if (count($this->schema) <= $this->mySchema) throw new Exception(_('Update not required!'));
             $items = array_slice($this->schema,$this->mySchema,null,true);
             foreach($items AS $version => &$updates) {
@@ -46,22 +52,16 @@ class SchemaUpdaterPage extends FOGPage {
             }
             unset($updates);
             self::$DB->current_db(self::$DB->returnThis());
-            if (self::$DB->db_name()) {
-                $newSchema = self::getClass('SchemaManager')->find();
-                $newSchema = @array_shift($newSchema);
-                if (!($newSchema instanceof Schema && $newSchema->isValid())) $newSchema = self::getClass('Schema');
-                $newSchema->set('version',$version);
-                if (!$newSchema->save() || count($this->schema) != $newSchema->get('version')) {
-                    printf('<p>%s</p>',_('Install / Update Failed!'));
-                    if (count($errors)) throw new Exception(sprintf('<h2>%s</h2>%s<bad>',_('The following errors occurred'),implode('<hr/>',$errors)));
-                }
+            $newSchema = self::getClass('Schema',1)->set('version',$version);
+            if (!self::$DB->db_name() || !$newSchema->save() || count($this->schema) != $newSchema->get('version')) {
+                $errmsg = sprintf('<p>%s</p>',_('Install / Update Failed!'));
+                if (count($errors)) $errmsg .= sprintf('<h2>%s</h2>%s',_('The following errors occurred'),implode('<hr/>',$errors));
+                throw new Exception($errmsg);
             }
-            if (count($errors)) throw new Exception(sprintf('<h2>%s</h2>%s',_('The following errors occured'),implode('<hr/>',$errors)));
-            throw new Exception(_('Install/Upgrade Successful!'));
+            printf('<p>%s</p><p>%s <a href="index.php">%s</a> %s</p>',_('Install / Update Successful!'),_('Click'),_('here'),_('to login'));
+            if (count($errors)) printf('<h2>%s</h2>%s',_('The following errors occured'),implode('<hr/>',$errors));
         } catch (Exception $e) {
-            if (strpos('<bad>',$e->getMessage())) die(str_replace('<bad>','',$e->getMessage()));
             printf('<p>%s</p>',$e->getMessage());
         }
-        printf('<p>%s <a href="./index.php">%s</a> %s</p>',_('Click'),_('here'),_('to login'));
     }
 }
