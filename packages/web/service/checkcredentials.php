dc7206cce01514828b35ec5632ae880444563963
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-05-26 14:17:37 +0000

    Make sure the vals are trimmed
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5584 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/checkcredentials.php
+++ packages/web/service/checkcredentials.php
@@ -1,8 +1,8 @@
 <?php
 require_once('../commons/base.inc.php');
 try {
-    $username = base64_decode(trim($_REQUEST['username']));
-    $password = base64_decode(trim($_REQUEST['password']));
+    $username = trim(base64_decode(trim($_REQUEST['username'])));
+    $password = trim(base64_decode(trim($_REQUEST['password'])));
     if (!FOGCore::getClass('User')->password_validate($username,$password)) throw new Exception('#!il');
     echo '#!ok';
 } catch (Exception $e) {
