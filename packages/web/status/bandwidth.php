ae25ec9ebb6d48ffd8cc0a6ce828264c38f2561b
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-05-27 14:59:29 +0000

    More Tweaks toward bandwidth checking.  Fix pending mac that has no value being inserted on host registration when host is not already registered
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5598 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/bandwidth.php
+++ packages/web/status/bandwidth.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: text/event-stream');
+header('Connection: close');
 require('../commons/base.inc.php');
 $dev = trim($_REQUEST['dev'] ? basename($_REQUEST['dev']) : 'eth0');
 $dirints = array_diff(scandir('/sys/class/net'),array('..','.'));
@@ -9,17 +11,9 @@ $dirints = array_diff(scandir('/sys/class/net'),array('..','.'));
 $interface = preg_grep("#$dev#",(array)$interfaces);
 $dev = @array_shift($interface);
 if (empty($dev)) $dev = FOGCore::getMasterInterface($FOGCore->resolveHostname($_SERVER['SERVER_ADDR']));
+$rx = trim(file_get_contents(sprintf('/sys/class/net/%s/statistics/rx_bytes',$dev)));
+$tx = trim(file_get_contents(sprintf('/sys/class/net/%s/statistics/tx_bytes',$dev)));
+$ret = array('dev'=>$dev,'rx'=>$rx,'tx'=>$tx);
 if (empty($dev)) $ret = array('dev'=>'Unknown','rx'=>0,'tx'=>0);
-else {
-    $rx = trim(file_get_contents(sprintf('/sys/class/net/%s/statistics/rx_bytes',$dev)));
-    $tx = trim(file_get_contents(sprintf('/sys/class/net/%s/statistics/tx_bytes',$dev)));
-    $ret = array('dev'=>$dev,'rx'=>$rx,'tx'=>$tx);
-}
-ob_start();
-header('Content-Type: text/event-stream');
-header('Connection: close');
 echo json_encode($ret);
-flush();
-ob_flush();
-ob_end_flush();
 exit;
