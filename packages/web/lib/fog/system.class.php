a4fa394b1d7b6d4b6e819d07e381eb20b3f0b801
Tom Elliott <tommygunsster@gmail.com>
2016-06-03 16:12:00 +0000

    THe hostinfo.php shouldn't just blatently define the variables, so check the length of the content in the variable.  If it's 0 let the call here set it, otherwise leave it be.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5633 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '7979');
+        define('FOG_VERSION', '7981');
         define('FOG_SCHEMA', 222);
         define('FOG_BCACHE_VER',63);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
