95e89077630879cb6db020a683627c3a351fa30f
Tom Elliott <tommygunsster@gmail.com>
2016-06-05 17:41:53 -0400

    Make sure the powermanagement feature operates as expected per Joe's coding

--- packages/web/lib/client/jobs.class.php
+++ packages/web/lib/client/jobs.class.php
@@ -2,11 +2,7 @@
 class Jobs extends FOGClient implements FOGClientSend {
     public function json() {
         if ($this->Host->get('task')->isInitNeededTasking()) return stripos(strtolower($_SERVER['SCRIPT_NAME']),'jobs.php') ? array('error'=>'ok') : array('job'=>true);
-        if (!self::getClass('PowerManagementManager')->count(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1))) return stripos(strtolower($_SERVER['SCRIPT_NAME']),'jobs.php') ? array('error'=>'nj') : array('job' => false);
-        $actions = self::getSubObjectIDs('PowerManagement',array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'action');
-        self::getClass('PowerManagementManager')->update(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'',array('onDemand'=>0));
-        if (in_array('shutdown',$actions)) return array('action'=>'shutdown');
-        if (in_array('reboot',$actions)) return array('action'=>'reboot');
+        return stripos(strtolower($_SERVER['SCRIPT_NAME']),'jobs.php') ? array('error'=>'nj') : array('job' => false);
     }
     public function send() {
         if ($this->Host->get('task')->isInitNeededTasking()) $this->send = '#!ok';
--- packages/web/lib/client/pm.class.php
+++ packages/web/lib/client/pm.class.php
@@ -1,18 +1,18 @@
 <?php
 class PM extends FOGClient {
     public function json() {
-        if (self::getClass('PowerManagementManager')->count(array('hostID'=>$this->Host->get('id'))) < 1) return array('error'=>'na');
+        $actions = self::getSubObjectIDs('PowerManagement',array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'action');
+        self::getClass('PowerManagementManager')->update(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'',array('onDemand'=>0));
         return array('tasks'=>array_filter(array_map(function(&$pm) {
             if (!$pm->isValid()) return;
             if ($pm->get('action') === 'wol') return;
             if ($pm->get('onDemand')) return;
-            //$onDemand = (bool)$pm->get('onDemand');
-            //if ($onDemand === true) $pm->set('onDemand','0')->save();
             return array(
                 'cron' => sprintf('%s %s %s %s %s',$pm->get('min'),$pm->get('hour'),$pm->get('dom'),$pm->get('month'),$pm->get('dow')),
-                //'onDemand' => $onDemand,
                 'action' => $pm->get('action'),
             );
-        },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'))))));
+        },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'))))),
+            'onDemand'=>(in_array('shutdown',$actions) ? 'shutdown' : (in_array('reboot',$actions) ? 'reboot' : '')),
+        );
     }
 }
