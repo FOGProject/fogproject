f0563f25ec27b6145745bbbae9150da3468cae6b
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-06-07 17:30:12 +0000

    Add fileintegrity plugin
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5639 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ packages/web/lib/plugins/fileintegrity/class/taskstateeditmanager.class.php
@@ -0,0 +1,9 @@
+<?php
+class TaskstateeditManager extends TaskStateManager {
+    public function install($name) {
+        return true;
+    }
+    public function uninstall() {
+        return true;
+    }
+}
---
+++ packages/web/lib/plugins/fileintegrity/hooks/addfileintegritytype.hook.php
@@ -0,0 +1,10 @@
+<?php
+class AddFileIntegrityType extends Hook {
+    public $name = 'AddFileIntegrityType';
+    public $description = 'Add Report Management Type';
+    public $author = 'Tom Elliott';
+    public $active = true;
+    public $node = 'fileintegrity';
+}
+$AddFileIntegrityType = new AddFileIntegrityType();
+$HookManager->register('REPORT_TYPES',array($AddFileIntegrityType,'reportTypes'));
