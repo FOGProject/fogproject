56d42c4f614b7133dbe9307dd742365c45394cbe
Tom Elliott <tommygunsster@gmail.com>
2016-06-08 09:40:18 +0000

    Should properly add spaces as/where needed
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5645 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '8030');
+        define('FOG_VERSION', '8032');
         define('FOG_SCHEMA', 223);
         define('FOG_BCACHE_VER',64);
         define('FOG_SVN_REVISION', '$Revision: 2868 $');
