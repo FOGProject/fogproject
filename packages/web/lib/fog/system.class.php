6e3506f4c2ca2a7acd2fbbf4d17f32891c9c7057
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-06-11 03:44:56 +0000

    Make sure we set the correct color for the text
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5663 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,10 +1,10 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '8066');
+        define('FOG_VERSION', '8068');
         define('FOG_SCHEMA', 224);
         define('FOG_BCACHE_VER',67);
-        define('FOG_SVN_REVISION',5662);
+        define('FOG_SVN_REVISION',5663);
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.10.6');
         define('PHP_VERSION_REQUIRED', '5.3.0');
