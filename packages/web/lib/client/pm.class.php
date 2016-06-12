89f37f51f7e11f9c736a685290f75ba799706137
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-06-12 00:06:38 +0000

    Destroy ondemand power management items once performed otherwise they may pop back into the system and there's no real simple way to delete them from the tables.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5666 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/pm.class.php
+++ packages/web/lib/client/pm.class.php
@@ -2,23 +2,25 @@
 class PM extends FOGClient {
     public function json() {
         $actions = self::getSubObjectIDs('PowerManagement',array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'action');
-        self::getClass('PowerManagementManager')->update(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'',array('onDemand'=>0));
-        return array('tasks'=>array_filter(array_map(function(&$pm) {
-            if (!$pm->isValid()) return;
-            if ($pm->get('action') === 'wol') return;
-            if ($pm->get('onDemand') > 0) return;
-            $min = trim($pm->get('min'));
-            $hour = trim($pm->get('hour'));
-            $dom = trim($pm->get('dom'));
-            $month = trim($pm->get('month'));
-            $dow = trim($pm->get('dow'));
-            if (!($min && $hour && $dom && $month && $dow)) return;
-            return array(
-                'cron' => sprintf('%s %s %s %s %s',$min,$hour,$dom,$month,$dow),
-                'action' => $pm->get('action'),
-            );
-        },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'),'onDemand'=>0)))),
+        self::getClass('PowerManagementManager')->destroy(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1));
+        return array(
             'onDemand'=>(in_array('shutdown',$actions) ? 'shutdown' : (in_array('reboot',$actions) ? 'restart' : '')),
-        );
+            'tasks'=>array_filter(
+                array_map(function(&$pm) {
+                    if (!$pm->isValid()) return;
+                    if ($pm->get('action') === 'wol') return;
+                    if ($pm->get('onDemand') > 0) return;
+                    $min = trim($pm->get('min'));
+                    $hour = trim($pm->get('hour'));
+                    $dom = trim($pm->get('dom'));
+                    $month = trim($pm->get('month'));
+                    $dow = trim($pm->get('dow'));
+                    if (!($min && $hour && $dom && $month && $dow)) return;
+                    return array(
+                        'cron' => sprintf('%s %s %s %s %s',$min,$hour,$dom,$month,$dow),
+                        'action' => $pm->get('action'),
+                    );
+                },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'),'onDemand'=>0)))
+        ));
     }
 }
