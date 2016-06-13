b5648ec023a6ae0e5d48ba86098cd62a11902c8b
Rowlett <Rowlett@users.noreply.github.com>
2016-06-13 07:32:29 +0100

    Update addlocationtasks.hook.php
    Overwrite correct tableheader in task page for location plugin

--- packages/web/lib/plugins/location/hooks/addlocationtasks.hook.php
+++ packages/web/lib/plugins/location/hooks/addlocationtasks.hook.php
@@ -8,13 +8,13 @@ class AddLocationTasks extends Hook {
     public function TasksActiveTableHeader($arguments) {
         if (!in_array($this->node,(array)$_SESSION['PluginsInstalled'])) return;
         if ($_REQUEST['node'] != 'task') return;
-        $arguments['headerData'][4] = _('Location');
+        $arguments['headerData'][3] = _('Location');
     }
     public function TasksActiveData($arguments) {
         if (!in_array($this->node,(array)$_SESSION['PluginsInstalled'])) return;
         if ($_REQUEST['node'] != 'task') return;
-        $arguments['templates'][4] = '${location}';
-        $arguments['attributes'][4] = array('class'=>'r');
+        $arguments['templates'][3] = '${location}';
+        $arguments['attributes'][3] = array('class'=>'r');
         foreach ((array)$arguments['data'] AS $i => &$data) {
             $locationID = self::getSubObjectIDs('LocationAssociation',array('hostID'=>$data['host_id']),'locationID');
             $locID = array_shift($locationID);
