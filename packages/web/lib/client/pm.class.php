c9722ae6085d7e6fd57c6e132ae6df8da8a0c105
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-06-15 01:12:12 +0000

    Don't erroneously delete a powermanagement task
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5678 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/pm.class.php
+++ packages/web/lib/client/pm.class.php
@@ -1,15 +1,17 @@
 <?php
 class PM extends FOGClient {
     public function json() {
-        $actions = self::getSubObjectIDs('PowerManagement',array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1),'action');
-        self::getClass('PowerManagementManager')->destroy(array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>1));
+        $actions = self::getSubObjectIDs('PowerManagement',array('id'=>$this->Host->get('powermanagementtasks'),'onDemand'=>'1'),'action');
         return array(
             'onDemand'=>(in_array('shutdown',$actions) ? 'shutdown' : (in_array('reboot',$actions) ? 'restart' : '')),
             'tasks'=>array_filter(
                 array_map(function(&$pm) {
                     if (!$pm->isValid()) return;
                     if ($pm->get('action') === 'wol') return;
-                    if ($pm->get('onDemand') > 0) return;
+                    if ($pm->get('onDemand') > 0) {
+                        $pm->destroy();
+                        return;
+                    }
                     $min = trim($pm->get('min'));
                     $hour = trim($pm->get('hour'));
                     $dom = trim($pm->get('dom'));
@@ -20,7 +22,7 @@ class PM extends FOGClient {
                         'cron' => sprintf('%s %s %s %s %s',$min,$hour,$dom,$month,$dow),
                         'action' => $pm->get('action'),
                     );
-                },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'),'onDemand'=>0)))
+                },(array)self::getClass('PowerManagementManager')->find(array('hostID'=>$this->Host->get('id'))))
         ));
     }
 }
