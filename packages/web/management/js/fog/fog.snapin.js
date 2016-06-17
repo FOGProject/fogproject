ac0de27b58a2124717f2f1710b434e4ca0fbf704
Tom Elliott <tommygunsster@gmail.com>
2016-06-17 11:50:31 +0000

    Fix snapin command let display so if a file is to be uploaded, it displays properly.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5703 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/management/js/fog/fog.snapin.js
+++ packages/web/management/js/fog/fog.snapin.js
@@ -7,14 +7,17 @@ $(function() {
         updateCmdStore();
     });
     updateCmdStore();
-    $('.cmdlet1,.cmdlet2,.cmdlet4').on('change, keyup',function(e) {
+    $('.cmdlet1,.cmdlet2,.cmdlet3,.cmdlet4').on('change, keyup',function(e) {
         updateCmdStore();
     });
-    $('.cmdlet3').on('change, keyup',function(e) {
+    $('.cmdlet3').change(function(e) {
         updateCmdStore();
-    });
+    })
 });
 function updateCmdStore() {
-    var snapCMD = [$('.cmdlet1').val(),$('.cmdlet2').val(),$('.cmdlet3').val(),$('.cmdlet4').val()];
+    test = $('[type="file"]')[0].files.length;
+    if (test < 1) test = $('select.cmdlet3').val();
+    else test = $('[type="file"]')[0].files[0].name;
+    var snapCMD = [$('.cmdlet1').val(),$('.cmdlet2').val(),test,$('.cmdlet4').val()];
     $('.snapincmd').val(snapCMD.join(' ')).css({width: '100%',border: 0,resize: 'none'});
 }
