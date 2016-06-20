d12296b3ff49f0fae8eacc948a0ab2aba31a5298
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-06-20 11:44:57 +0000

    Fix timeout so it's not so rediculously long.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5714 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,10 +1,10 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '8169');
+        define('FOG_VERSION', '8173');
         define('FOG_SCHEMA', 226);
         define('FOG_BCACHE_VER',75);
-        define('FOG_SVN_REVISION',5712);
+        define('FOG_SVN_REVISION',5714);
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.11.0');
         define('PHP_VERSION_REQUIRED', '5.3.0');
