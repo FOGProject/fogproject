625fc0bfd7e4871ad252b67bf89e371400d1965e
Tom Elliott <tommygunsster@gmail.com>
2016-07-07 16:51:25 +0000

    Make all node master elements loop through all potential storage node masters
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5853 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/service/multicastmanager.class.php
+++ packages/web/lib/service/multicastmanager.class.php
@@ -43,83 +43,87 @@ class MulticastManager extends FOGService {
     }
     private function serviceLoop() {
         while(true) {
-            try {
-                $StorageNode = $this->checkIfNodeMaster();
-                $myroot = $StorageNode->get('path');
-                $taskCount = self::getClass('MulticastSessionsManager')->count(array('stateID'=>array_merge($this->getQueuedStates(),(array)$this->getProgressState())));
-                if ($taskCount != $oldCount) $allTasks = self::getClass('MulticastTask')->getAllMulticastTasks($myroot,$StorageNode->get('id'));
-                $RMTasks = $this->getMCTasksNotInDB($KnownTasks,$allTasks);
-                if (!count($RMTasks) && (!$taskCount || $taskCount < 0)) throw new Exception(' * No tasks found!');
-                $jobcancelled = false;
-                self::outall(sprintf(" | %d task%s to be cleaned",count($RMTask),count($RMTask) != 1 ? 's' : ''));
-                if (count($RMTasks)) {
-                    self::outall(sprintf(" | Cleaning %s task(s) removed from FOG Database.",count($RMTasks)));
-                    foreach ((array)$RMTasks AS $i => &$RMTask) {
-                        self::outall(sprintf(" | Cleaning Task (%s) %s",$RMTask->getID(),$RMTask->getName()));
-                        $KnownTasks = $this->removeFromKnownList($KnownTasks,$RMTask->getID());
-                        $taskIDs = self::getSubObjectIDs('MulticastSessionsAssociation',array('msID'=>$RMTask->getID()),'taskID');
-                        if (self::getClass('TaskManager')->count(array('id'=>$taskIDs,'stateID'=>$this->getCancelledState()) > 0)) $jobcancelled = true;
-                        if ($jobcancelled || self::getClass('MulticastSessions',$RMTask->getID())->get('stateID') == $this->getCancelledState()) {
-                            $RMTask->killTask();
-                            self::outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$RMTask->getID(),$RMTask->getName()));
-                            self::getClass('MulticastSessionsAssociationManager')->destroy(array('msID'=>$RMTask->getID()));
-                        } else {
-                            self::outall(sprintf(" | Task (%s) %s has been cleaned as complete.",$RMTask->getID(),$RMTask->getName()));
-                            self::getClass('MulticastSessionsAssociationManager')->destroy(array('msID'=>$RMTask->getID()));
-                        }
-                        unset($RMTask);
-                    }
-                }
-                if ($taskCount > 0) self::outall(sprintf(' | %s task%s found',$taskCount,($taskCount > 1 || !$taskCount ? 's' : '')));
-                if (count($allTasks)) {
-                    foreach ((array)$allTasks AS $i => &$curTask) {
-                        if ($this->isMCTaskNew($KnownTasks, $curTask->getID())) {
-                            self::outall(sprintf(" | Task (%s) %s is new!",$curTask->getID(),$curTask->getName()));
-                            if(!file_exists($curTask->getImagePath())) throw new Exception(sprintf(" Task (%s) %s failed to execute, image file:%s not found!",$curTask->getID(),$curTask->getName(),$curTask->getImagePath()));
-                            if (!$curTask->getClientCount()) throw new Exception(sprintf(" Task (%s) %s failed to execute, no clients are included!",$curTask->getID(),$curTask->getName()));
-                            if (!is_numeric($curTask->getPortBase()) || !($curTask->getPortBase() % 2 == 0)) throw new Exception(sprintf(" Task (%s) %s failed to execute, port must be even and numeric.",$curTask->getID(),$curTask->getName()));
-                            if (!$curTask->startTask()) {
-                                self::outall(sprintf(" | Task (%s) %s failed to start!",$curTask->getID(),$curTask->getName()));
-                                self::outall(sprintf(" | * Don't panic, check all your settings!"));
-                                self::outall(sprintf(" |       even if the interface is incorrect the task won't start."));
-                                self::outall(sprintf(" |       If all else fails run the following command and see what it says:"));
-                                self::outall(sprintf(" |  %s",$curTask->getCMD()));
-                                $curTask->killTask();
-                                throw new Exception(" Task (%s) %s has been cleaned.");
-                            }
-                            self::outall(sprintf(" | Task (%s) %s has been cleaned.",$curTask->getID(),$curTask->getName()));
-                            self::outall(sprintf(" | Task (%s) %s image file found.",$curTask->getID(),$curTask->getImagePath()));
-                            self::outall(sprintf(" | Task (%s) %s client(s) found.",$curTask->getID(),$curTask->getClientCount()));
-                            self::outall(sprintf(" | Task (%s) %s sending on base port: %s",$curTask->getID(),$curTask->getName(),$curTask->getPortBase()));
-                            self::outall(sprintf(" | CMD: %s",$curTask->getCMD()));
-                            self::outall(sprintf(" | Task (%s) %s has started.",$curTask->getID(),$curTask->getName()));
-                            $KnownTasks[] = $curTask;
-                        } else {
-                            $runningTask = $this->getMCExistingTask($KnownTasks, $curTask->getID());
-                            $taskIDs = self::getSubObjectIDs('MulticastSessionsAssociation',array('msID'=>$runningTask->getID()),'taskID');
+            $StorageNodes = $this->checkIfNodeMaster();
+            foreach((array)$StorageNodes AS &$StorageNode) {
+                try {
+                    $myroot = $StorageNode->get('path');
+                    $taskCount = self::getClass('MulticastSessionsManager')->count(array('stateID'=>array_merge($this->getQueuedStates(),(array)$this->getProgressState())));
+                    if ($taskCount != $oldCount) $allTasks = self::getClass('MulticastTask')->getAllMulticastTasks($myroot,$StorageNode->get('id'));
+                    $RMTasks = $this->getMCTasksNotInDB($KnownTasks,$allTasks);
+                    if (!count($RMTasks) && (!$taskCount || $taskCount < 0)) throw new Exception(' * No tasks found!');
+                    $jobcancelled = false;
+                    self::outall(sprintf(" | %d task%s to be cleaned",count($RMTask),count($RMTask) != 1 ? 's' : ''));
+                    if (count($RMTasks)) {
+                        self::outall(sprintf(" | Cleaning %s task(s) removed from FOG Database.",count($RMTasks)));
+                        foreach ((array)$RMTasks AS $i => &$RMTask) {
+                            self::outall(sprintf(" | Cleaning Task (%s) %s",$RMTask->getID(),$RMTask->getName()));
+                            $KnownTasks = $this->removeFromKnownList($KnownTasks,$RMTask->getID());
+                            $taskIDs = self::getSubObjectIDs('MulticastSessionsAssociation',array('msID'=>$RMTask->getID()),'taskID');
                             if (self::getClass('TaskManager')->count(array('id'=>$taskIDs,'stateID'=>$this->getCancelledState()) > 0)) $jobcancelled = true;
-                            if ($runningTask->isRunning($runningTask->procRef)) {
-                                self::outall(sprintf(" | Task (%s) %s is already running PID %s",$runningTask->getID(),$runningTask->getName(),$runningTask->getPID($runningTask->procRef)));
-                                $runningTask->updateStats();
+                            if ($jobcancelled || self::getClass('MulticastSessions',$RMTask->getID())->get('stateID') == $this->getCancelledState()) {
+                                $RMTask->killTask();
+                                self::outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$RMTask->getID(),$RMTask->getName()));
+                                self::getClass('MulticastSessionsAssociationManager')->destroy(array('msID'=>$RMTask->getID()));
                             } else {
-                                self::outall(sprintf(" | Task (%s) %s is no longer running.",$runningTask->getID(),$runningTask->getName()));
-                                if ($jobcancelled || self::getClass('MulticastSessions',$runningTask->getID())->get('stateID') == $this->getCancelledState()) {
-                                    $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
-                                    if (!$runningTask->killTask()) throw new Exception(sprintf(" Failed to kill task (%s) %s PID:%s!",$runningTask->getID(),$runningTask->getName(),$runningTask->getPID($runningTask->procRef)));
-                                    self::outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$runningTask->getID(),$runningTask->getName()));
+                                self::outall(sprintf(" | Task (%s) %s has been cleaned as complete.",$RMTask->getID(),$RMTask->getName()));
+                                self::getClass('MulticastSessionsAssociationManager')->destroy(array('msID'=>$RMTask->getID()));
+                            }
+                            unset($RMTask);
+                        }
+                    }
+                    if ($taskCount > 0) self::outall(sprintf(' | %s task%s found',$taskCount,($taskCount > 1 || !$taskCount ? 's' : '')));
+                    if (count($allTasks)) {
+                        foreach ((array)$allTasks AS $i => &$curTask) {
+                            if ($this->isMCTaskNew($KnownTasks, $curTask->getID())) {
+                                self::outall(sprintf(" | Task (%s) %s is new!",$curTask->getID(),$curTask->getName()));
+                                if(!file_exists($curTask->getImagePath())) throw new Exception(sprintf(" Task (%s) %s failed to execute, image file:%s not found!",$curTask->getID(),$curTask->getName(),$curTask->getImagePath()));
+                                if (!$curTask->getClientCount()) throw new Exception(sprintf(" Task (%s) %s failed to execute, no clients are included!",$curTask->getID(),$curTask->getName()));
+                                if (!is_numeric($curTask->getPortBase()) || !($curTask->getPortBase() % 2 == 0)) throw new Exception(sprintf(" Task (%s) %s failed to execute, port must be even and numeric.",$curTask->getID(),$curTask->getName()));
+                                if (!$curTask->startTask()) {
+                                    self::outall(sprintf(" | Task (%s) %s failed to start!",$curTask->getID(),$curTask->getName()));
+                                    self::outall(sprintf(" | * Don't panic, check all your settings!"));
+                                    self::outall(sprintf(" |       even if the interface is incorrect the task won't start."));
+                                    self::outall(sprintf(" |       If all else fails run the following command and see what it says:"));
+                                    self::outall(sprintf(" |  %s",$curTask->getCMD()));
+                                    $curTask->killTask();
+                                    throw new Exception(" Task (%s) %s has been cleaned.");
+                                }
+                                self::outall(sprintf(" | Task (%s) %s has been cleaned.",$curTask->getID(),$curTask->getName()));
+                                self::outall(sprintf(" | Task (%s) %s image file found.",$curTask->getID(),$curTask->getImagePath()));
+                                self::outall(sprintf(" | Task (%s) %s client(s) found.",$curTask->getID(),$curTask->getClientCount()));
+                                self::outall(sprintf(" | Task (%s) %s sending on base port: %s",$curTask->getID(),$curTask->getName(),$curTask->getPortBase()));
+                                self::outall(sprintf(" | CMD: %s",$curTask->getCMD()));
+                                self::outall(sprintf(" | Task (%s) %s has started.",$curTask->getID(),$curTask->getName()));
+                                $KnownTasks[] = $curTask;
+                            } else {
+                                $runningTask = $this->getMCExistingTask($KnownTasks, $curTask->getID());
+                                $taskIDs = self::getSubObjectIDs('MulticastSessionsAssociation',array('msID'=>$runningTask->getID()),'taskID');
+                                if (self::getClass('TaskManager')->count(array('id'=>$taskIDs,'stateID'=>$this->getCancelledState()) > 0)) $jobcancelled = true;
+                                if ($runningTask->isRunning($runningTask->procRef)) {
+                                    self::outall(sprintf(" | Task (%s) %s is already running PID %s",$runningTask->getID(),$runningTask->getName(),$runningTask->getPID($runningTask->procRef)));
+                                    $runningTask->updateStats();
                                 } else {
-                                    self::getClass('MulticastSessions',$runningTask->getID())->set('clients',0)->set('completetime',self::nice_date()->format('Y-m-d H:i:s'))->set('name','')->set('stateID',$this->getCompleteState())->save();
-                                    $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
-                                    self::outall(sprintf(" | Task (%s) %s has been cleaned as complete.",$runningTask->getID(),$runningTask->getName()));
+                                    self::outall(sprintf(" | Task (%s) %s is no longer running.",$runningTask->getID(),$runningTask->getName()));
+                                    if ($jobcancelled || self::getClass('MulticastSessions',$runningTask->getID())->get('stateID') == $this->getCancelledState()) {
+                                        $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
+                                        if (!$runningTask->killTask()) throw new Exception(sprintf(" Failed to kill task (%s) %s PID:%s!",$runningTask->getID(),$runningTask->getName(),$runningTask->getPID($runningTask->procRef)));
+                                        self::outall(sprintf(" | Task (%s) %s has been cleaned as cancelled.",$runningTask->getID(),$runningTask->getName()));
+                                    } else {
+                                        self::getClass('MulticastSessions',$runningTask->getID())->set('clients',0)->set('completetime',self::nice_date()->format('Y-m-d H:i:s'))->set('name','')->set('stateID',$this->getCompleteState())->save();
+                                        $KnownTasks = $this->removeFromKnownList($KnownTasks,$runningTask->getID());
+                                        self::outall(sprintf(" | Task (%s) %s has been cleaned as complete.",$runningTask->getID(),$runningTask->getName()));
+                                    }
                                 }
                             }
+                            unset($curTask);
                         }
-                        unset($curTask);
                     }
+                } catch(Exception $e) {
+                    self::outall($e->getMessage());
                 }
-            } catch(Exception $e) {
-                self::outall($e->getMessage());
+                unset($StorageNode);
             }
+            unset($StorageNodes);
             self::out(' +---------------------------------------------------------',static::$dev);
             $tmpTime = self::getSetting(self::$sleeptime);
             if (static::$zzz != $tmpTime) {
--- packages/web/lib/service/snapinreplicator.class.php
+++ packages/web/lib/service/snapinreplicator.class.php
@@ -10,44 +10,46 @@ class SnapinReplicator extends FOGService {
         static::$zzz = ($zzz ? $zzz : 600);
     }
     private function commonOutput() {
-        try {
-            $StorageNode = $this->checkIfNodeMaster();
-            self::out(' * I am the group manager',static::$dev);
-            self::wlog(' * I am the group manager','/opt/fog/log/groupmanager.log');
-            $myStorageGroupID = $StorageNode->get('storageGroupID');
-            $myStorageNodeID = $StorageNode->get('id');
-            self::outall(" * Starting Snapin Replication.");
-            self::outall(sprintf(" * We are group ID: #%s",$myStorageGroupID));
-            self::outall(sprintf(" | We are group name: %s",self::getClass('StorageGroup',$myStorageGroupID)->get('name')));
-            self::outall(sprintf(" * We have node ID: #%s",$myStorageNodeID));
-            self::outall(sprintf(" | We are node name: %s",self::getClass('StorageNode',$myStorageNodeID)->get('name')));
-            $SnapinIDs = self::getSubObjectIDs('Snapin',array('isEnabled'=>1,'toReplicate'=>1));
-            $SnapinAssocs = self::getSubObjectIDs('SnapinGroupAssociation',array('snapinID'=>$SnapinIDs),'snapinID',true);
-            if (count($SnapinAssocs)) self::getClass('SnapinGroupAssociationManager')->destroy(array('snapinID'=>$SnapinAssocs));
-            unset($SnapinAssocs);
-            $SnapinAssocCount = self::getClass('SnapinGroupAssociationManager')->count(array('storageGroupID'=>$myStorageGroupID,'snapinID'=>$SnapinIDs));
-            $SnapinCount = self::getClass('SnapinManager')->count();
-            if ($SnapinAssocCount <= 0 || $SnapinCount <= 0) throw new Exception(_('There is nothing to replicate'));
-            unset($SnapinAssocCount,$SnapinCount);
-            $Snapins = self::getClass('SnapinManager')->find(array('id'=>self::getSubObjectIDs('SnapinGroupAssociation',array('storageGroupID'=>$myStorageGroupID,'snapinID'=>$SnapinIDs),'snapinID')));
-            unset($SnapinIDs);
-            foreach ((array)$Snapins AS $i => &$Snapin) {
-                if (!$Snapin->isValid()) continue;
-                if (!$Snapin->getPrimaryGroup($myStorageGroupID)) {
-                    self::outall(_(" | Not syncing Snapin: {$Snapin->get(name)}"));
-                    self::outall(_(' | This is not the primary group'));
-                    continue;
+        $StorageNodes = $this->checkIfNodeMaster();
+        foreach ((array)$StorageNodes AS &$StorageNode) {
+            try {
+                self::out(' * I am the group manager',static::$dev);
+                self::wlog(' * I am the group manager','/opt/fog/log/groupmanager.log');
+                $myStorageGroupID = $StorageNode->get('storageGroupID');
+                $myStorageNodeID = $StorageNode->get('id');
+                self::outall(" * Starting Snapin Replication.");
+                self::outall(sprintf(" * We are group ID: #%s",$myStorageGroupID));
+                self::outall(sprintf(" | We are group name: %s",self::getClass('StorageGroup',$myStorageGroupID)->get('name')));
+                self::outall(sprintf(" * We have node ID: #%s",$myStorageNodeID));
+                self::outall(sprintf(" | We are node name: %s",self::getClass('StorageNode',$myStorageNodeID)->get('name')));
+                $SnapinIDs = self::getSubObjectIDs('Snapin',array('isEnabled'=>1,'toReplicate'=>1));
+                $SnapinAssocs = self::getSubObjectIDs('SnapinGroupAssociation',array('snapinID'=>$SnapinIDs),'snapinID',true);
+                if (count($SnapinAssocs)) self::getClass('SnapinGroupAssociationManager')->destroy(array('snapinID'=>$SnapinAssocs));
+                unset($SnapinAssocs);
+                $SnapinAssocCount = self::getClass('SnapinGroupAssociationManager')->count(array('storageGroupID'=>$myStorageGroupID,'snapinID'=>$SnapinIDs));
+                $SnapinCount = self::getClass('SnapinManager')->count();
+                if ($SnapinAssocCount <= 0 || $SnapinCount <= 0) throw new Exception(_('There is nothing to replicate'));
+                unset($SnapinAssocCount,$SnapinCount);
+                $Snapins = self::getClass('SnapinManager')->find(array('id'=>self::getSubObjectIDs('SnapinGroupAssociation',array('storageGroupID'=>$myStorageGroupID,'snapinID'=>$SnapinIDs),'snapinID')));
+                unset($SnapinIDs);
+                foreach ((array)$Snapins AS $i => &$Snapin) {
+                    if (!$Snapin->isValid()) continue;
+                    if (!$Snapin->getPrimaryGroup($myStorageGroupID)) {
+                        self::outall(_(" | Not syncing Snapin: {$Snapin->get(name)}"));
+                        self::outall(_(' | This is not the primary group'));
+                        continue;
+                    }
+                    $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Snapin,true);
+                    unset($Snapin);
                 }
-                $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Snapin,true);
-                unset($Snapin);
+                foreach ($Snapins AS $i => &$Snapin) {
+                    $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Snapin,false);
+                    unset($Snapin);
+                }
+                unset($Snapins);
+            } catch (Exception $e) {
+                self::outall(' * '.$e->getMessage());
             }
-            foreach ($Snapins AS $i => &$Snapin) {
-                $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Snapin,false);
-                unset($Snapin);
-            }
-            unset($Snapins);
-        } catch (Exception $e) {
-            self::outall(' * '.$e->getMessage());
         }
     }
     public function serviceRun() {
