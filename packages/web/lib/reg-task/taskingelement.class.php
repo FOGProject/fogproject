806c2c72cf8adeef4dcd58efd06ee259a6ce9092
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-07-07 17:21:33 +0000

    Should approriately work on the host image log
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5854 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/reg-task/taskingelement.class.php
+++ packages/web/lib/reg-task/taskingelement.class.php
@@ -55,13 +55,16 @@ abstract class TaskingElement extends FOGBase {
             ->save();
     }
     protected function ImageLog($checkin = false) {
-        if ($checkin === true) return self::getClass('ImagingLog',@max(self::getSubObjectIDs('ImagingLog',array('hostID'=>$this->Host->get('id'),'type'=>$_REQUEST['type'],'finish'=>'0000-00-00 00:00:00'))))
+        if ($checkin === true) {
+            self::getClass('ImagingLogManager')->destroy(array('finish'=>'0000-00-00 00:00:00'));
+            return self::getClass('ImagingLog')
             ->set('hostID',$this->Host->get('id'))
             ->set('start',$this->formatTime('','Y-m-d H:i:s'))
             ->set('image',$this->Image->get('name'))
             ->set('type',$_REQUEST['type'])
             ->set('createdBy',$this->Task->get('createdBy'))
             ->save();
+        }
         return self::getClass('ImagingLog',@max(self::getSubObjectIDs('ImagingLog',array('hostID'=>$this->Host->get('id')))))
             ->set('finish',$this->formatTime('','Y-m-d H:i:s'))
             ->save();
