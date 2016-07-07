df44db4c4e2fe6159e2acc0d6e6a975c44470368
Tom Elliott <tommygunsster@gmail.com>
2016-07-07 21:43:47 +0000

    Refix version so it works properly
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5858 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/service/getversion.php
+++ packages/web/service/getversion.php
@@ -3,5 +3,5 @@ require('../commons/base.inc.php');
 $clientUpdate = (bool)$FOGCore->getSetting('FOG_CLIENT_AUTOUPDATE');
 if (isset($_REQUEST['client'])) echo $clientUpdate ? '9.9.99' : '0.0.0';
 else if (isset($_REQUEST['clientver'])) echo $clientUpdate ? FOG_CLIENT_VERSION : '0.0.0';
-else echo isset($_REQUEST['json']) ? json_encode(array('version'=>FOG_VERSION)) : FOG_VERSION;
+else echo FOG_VERSION;
 exit;
