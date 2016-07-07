29c0b8497345cb532fcccaa18cb63da7a1195e1e
Tom Elliott <tommygunsster@gmail.com>
2016-07-07 16:38:24 +0000

    Hopefully enable replication to occur between groups even if the same node is found (essentially) in multiple groups
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5852 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/service/imagereplicator.class.php
+++ packages/web/lib/service/imagereplicator.class.php
@@ -11,40 +11,45 @@ class ImageReplicator extends FOGService {
     }
     private function commonOutput() {
         try {
-            $StorageNode = $this->checkIfNodeMaster();
-            self::out(' * I am the group manager',static::$dev);
-            self::wlog(' * I am the group manager','/opt/fog/log/groupmanager.log');
-            $myStorageGroupID = $StorageNode->get('storageGroupID');
-            $myStorageNodeID = $StorageNode->get('id');
-            self::outall(" * Starting Image Replication.");
-            self::outall(sprintf(" * We are group ID: #%s",$myStorageGroupID));
-            self::outall(sprintf(" | We are group name: %s",self::getClass('StorageGroup',$myStorageGroupID)->get('name')));
-            self::outall(sprintf(" * We have node ID: #%s",$myStorageNodeID));
-            self::outall(sprintf(" | We are node name: %s",self::getClass('StorageNode',$myStorageNodeID)->get('name')));
-            $ImageIDs = self::getSubObjectIDs('Image',array('isEnabled'=>1,'toReplicate'=>1));
-            $ImageAssocs = self::getSubObjectIDs('ImageAssociation',array('imageID'=>$ImageIDs),'imageID',true);
-            if (count($ImageAssocs)) self::getClass('ImageAssociationManager')->destroy(array('imageID'=>$ImageAssocs));
-            unset($ImageAssocs);
-            $ImageAssocCount = self::getClass('ImageAssociationManager')->count(array('storageGroupID'=>$myStorageGroupID,'imageID'=>$ImageIDs));
-            $ImageCount = self::getClass('ImageManager')->count();
-            if ($ImageAssocCount <= 0 || $ImageCount <= 0) throw new Exception(_('There is nothing to replicate'));
-            unset($ImageAssocCount,$ImageCount);
-            $Images = self::getClass('ImageManager')->find(array('id'=>self::getSubObjectIDs('ImageAssociation',array('storageGroupID'=>$myStorageGroupID,'imageID'=>$ImageIDs),'imageID')));
-            unset($ImageIDs);
-            foreach ((array)$Images AS $Image) {
-                if (!$Image->isValid()) continue;
-                if (!$Image->getPrimaryGroup($myStorageGroupID)) {
-                    self::outall(_(" | Not syncing Image: {$Image->get(name)}"));
-                    self::outall(_(' | This is not the primary group'));
-                    continue;
+            $StorageNodes = $this->checkIfNodeMaster();
+            foreach ((array)$StorageNodes AS $StorageNode) {
+                $myStorageGroupID = $StorageNode->get('storageGroupID');
+                self::out(' * I am the group manager',static::$dev);
+                self::wlog(' * I am the group manager','/opt/fog/log/groupmanager.log');
+                if (count($myStorageGroupID) === 1) $myStorageGroupID = array_shift($myStorageGroupID);
+                $myStorageNodeID = $StorageNode->get('id');
+                self::outall(" * Starting Image Replication.");
+                self::outall(sprintf(" * We are group ID: #%s",$myStorageGroupID));
+                self::outall(sprintf(" | We are group name: %s",self::getClass('StorageGroup',$myStorageGroupID)->get('name')));
+                self::outall(sprintf(" * We have node ID: #%s",$myStorageNodeID));
+                self::outall(sprintf(" | We are node name: %s",self::getClass('StorageNode',$myStorageNodeID)->get('name')));
+                $ImageIDs = self::getSubObjectIDs('Image',array('isEnabled'=>1,'toReplicate'=>1));
+                $ImageAssocs = self::getSubObjectIDs('ImageAssociation',array('imageID'=>$ImageIDs),'imageID',true);
+                if (count($ImageAssocs)) self::getClass('ImageAssociationManager')->destroy(array('imageID'=>$ImageAssocs));
+                unset($ImageAssocs);
+                $ImageAssocCount = self::getClass('ImageAssociationManager')->count(array('storageGroupID'=>$myStorageGroupID,'imageID'=>$ImageIDs));
+                $ImageCount = self::getClass('ImageManager')->count();
+                if ($ImageAssocCount <= 0 || $ImageCount <= 0) throw new Exception(_('There is nothing to replicate'));
+                unset($ImageAssocCount,$ImageCount);
+                $Images = self::getClass('ImageManager')->find(array('id'=>self::getSubObjectIDs('ImageAssociation',array('storageGroupID'=>$myStorageGroupID,'imageID'=>$ImageIDs),'imageID')));
+                unset($ImageIDs);
+                foreach ((array)$Images AS $Image) {
+                    if (!$Image->isValid()) continue;
+                    if (!$Image->getPrimaryGroup($myStorageGroupID)) {
+                        self::outall(_(" | Not syncing Image: {$Image->get(name)}"));
+                        self::outall(_(' | This is not the primary group'));
+                        continue;
+                    }
+                    $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Image,true);
                 }
-                $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Image,true);
+                foreach ($Images AS $i => &$Image) {
+                    $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Image,false);
+                    unset($Image);
+                }
+                unset($Images);
+                unset($StorageNode);
             }
-            foreach ($Images AS $i => &$Image) {
-                $this->replicate_items($myStorageGroupID,$myStorageNodeID,$Image,false);
-                unset($Image);
-            }
-            unset($Images);
+            unset($StorageNodes);
         } catch (Exception $e) {
             self::outall(' * '.$e->getMessage());
         }
