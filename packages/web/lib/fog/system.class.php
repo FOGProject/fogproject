8be82b918f5203896af8f39441e13b577e05bbf7
Tom Elliott <tommygunsster@gmail.com>
2016-07-13 19:29:01 -0400

    Fix conflicts

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -9,6 +9,10 @@ class System {
         define('FOG_CLIENT_VERSION', '0.11.4');
         define('PHP_VERSION_REQUIRED', '5.3.0');
         define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
+        define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
+        define('FOG_CLIENT_VERSION', '0.11.3');
+        define('PHP_VERSION_REQUIRED', '5.3.0');
+        define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
         define('SPACE_DEFAULT_STORAGE', '/images');
         if (PHP_COMPATIBLE === false) {
             die(sprintf(_('Your systems PHP version is not sufficient. You have version %s, version %s is required.'), PHP_VERSION, PHP_VERSION_REQUIRED));
