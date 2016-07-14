187554230ce968cf78e70555e8a6785dfc2dd29a
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-07-14 11:25:23 +0000

    Move away from dedicated MySQL in preference to call a URL to perform actions. This way authentication cannot be as simply bypassed.  Use this same auth to establish a means of autoaccept security methods
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5896 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/maintenance/create_update_node.php
+++ packages/web/maintenance/create_update_node.php
@@ -1,18 +1,24 @@
 <?php
 require '../commons/base.inc.php';
+if (!isset($_POST['fogverified'])) return;
 if (isset($_POST['newNode'])) {
     if (FOGCore::getClass('StorageNodeManager')->count(array('ip'=>$_POST['ip'])) > 0) return;
     FOGCore::getClass('StorageNode')
         ->set('name',trim($_POST['ip']))
+        ->set('path',trim($_POST['path']))
+        ->set('ftppath',trim($_POST['ftppath']))
+        ->set('snapinpath',trim($_POST['snapinpath']))
+        ->set('sslpath',trim($_POST['sslpath']))
         ->set('ip',trim($_POST['ip']))
+        ->set('maxClients',trim($_POST['maxClients']))
         ->set('user',trim($_POST['user']))
         ->set('pass',trim($_POST['pass']))
         ->set('interface',trim($_POST['interface']))
-        ->set('description','Auto generated fog nfs group member')
+        ->set('bandwidth',trim($_POST['bandwidth']))
+        ->set('webroot',trim($_POST['webroot']))
         ->set('isEnabled','1')
         ->save();
-}
-if (isset($_POST['nodePass'])) {
+} else if (isset($_POST['nodePass'])) {
     $Nodes = FOGCore::getClass('StorageNodeManager')->find(array('ip'=>$_POST['ip']));
     array_walk($Nodes,function(&$Node,&$index) {
         if (!$Node->isValid()) return;
