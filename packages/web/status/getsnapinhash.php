15dc7c923bc1a9789746b7f7729c223cbe6ea8a7
Tom Elliott <tommygunsster@gmail.com>
2016-07-16 00:47:26 +0000

    Try to get snapin hash and filesize in an architecture agnostic way.  Use system tools for hashes
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5913 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/status/getsnapinhash.php
+++ packages/web/status/getsnapinhash.php
@@ -6,9 +6,41 @@ if (!isset($_REQUEST['filepath'])) {
     echo '|0';
     exit;
 }
+function getfilesize($file) {
+    $fh = fopen($file,'rb');
+    $size = 0;
+    $char = '';
+    fseek($fh, 0, SEEK_SET);
+    $count = 0;
+    while (true) {
+        fseek($fh, 1048576, SEEK_CUR);
+        if (($char = fgetc($fh)) !== false) {
+            $count++;
+        } else {
+            fseek($fh, -1048576, SEEK_CUR);
+            break;
+        }
+    }
+    $size = bcmul('1048577',$count);
+    $fine = 0;
+    while (false !== ($char = fgetc($fh))) {
+        $fine++;
+    }
+    $size = bcadd($size,$fine);
+    fclose($fh);
+    return $size;
+}
 $req = $_REQUEST['filepath'];
 $dir = dirname($req);
 $name = basename($req);
 $file = sprintf('%s/%s',$dir,$name);
 $fileexist = file_exists($file);
-printf('%s|%s',$fileexist ? hash_file('sha512',$file) : '',$fileexist ? filesize($file) : 0);
+if ($fileexist) {
+    $filesize = getfilesize($file);
+    $filehash = exec("sha512sum $file");
+} else {
+    $filesize = 0;
+    $filehash = '';
+}
+printf('%s|%s',$filehash,$filesize);
+exit;
