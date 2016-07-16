d8a0f4ace0023e00d5dae6df297dc8f076d6052b
Tom Elliott <tommygunsster@gmail.com>
2016-07-16 22:03:24 +0000

    Update iPXE files to current pull.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5918 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,10 +1,10 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '8625');
+        define('FOG_VERSION', '8627');
         define('FOG_SCHEMA', 232);
         define('FOG_BCACHE_VER',90);
-        define('FOG_SVN_REVISION',5917);
+        define('FOG_SVN_REVISION',5918);
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.11.3');
         define('PHP_VERSION_REQUIRED', '5.3.0');
