b5326333846f64f3ce6bc7a6180f50cfb1878d50
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-07-17 21:59:30 +0000

    Update fog client to 0.11.4
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5929 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,12 +1,12 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '8647');
+        define('FOG_VERSION', '8649');
         define('FOG_SCHEMA', 232);
         define('FOG_BCACHE_VER',91);
-        define('FOG_SVN_REVISION',5928);
+        define('FOG_SVN_REVISION',5929);
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
-        define('FOG_CLIENT_VERSION', '0.11.3');
+        define('FOG_CLIENT_VERSION', '0.11.4');
         define('PHP_VERSION_REQUIRED', '5.3.0');
         define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
         define('SPACE_DEFAULT_STORAGE', '/images');
