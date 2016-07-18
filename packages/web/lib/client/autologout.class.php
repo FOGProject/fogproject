3c5fb06129cdda289e059189bb299f07097b71bd
Tom Elliott <tommygunsster@gmail.com>
2016-07-18 23:11:44 +0000

    Fix so only new client get's the calculation otherwise legacy will be way out of whack
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5934 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/client/autologout.class.php
+++ packages/web/lib/client/autologout.class.php
@@ -5,13 +5,13 @@ class Autologout extends FOGClient implements FOGClientSend {
     public function json() {
         $time = $this->Host->getAlo();
         if ($time < 5) return array('error'=>'time');
-        return array('time'=>$time);
+        return array('time'=>$time * 60);
     }
     public function send() {
         $time = $this->Host->getAlo();
         if ($this->newService) {
             if ($time < 5) throw new Exception('#!time');
-            $this->send = $time;
+            $this->send = $time * 60;
         } else $this->send = base64_encode($time);
     }
 }
