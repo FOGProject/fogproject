cf51d518391e7b293051cfd098cc44c39b3234b2
Tom Elliott <tommygunsster@gmail.com>
2016-07-23 12:28:06 -0400

    Make sure version is set to 1.3.0-RC-2

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', 'working-RC-2 13');
+        define('FOG_VERSION', '1.3.0-RC-2');
         define('FOG_SCHEMA', 232);
         define('FOG_BCACHE_VER',92);
         define('FOG_SVN_REVISION',5937);
