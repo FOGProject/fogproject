29216d4d865649e6d24b21151dc68deab5fd124d
masterzune <masterzune@71f96598-fa45-0410-b640-bcd6f8691b32>
2016-07-26 22:06:48 +0000

    Fix issue with inability to cancel multicast tasks when the main tasks are cancelled (and are multicast)
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@5941 71f96598-fa45-0410-b640-bcd6f8691b32

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,10 +1,10 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '1.3.0-RC-3');
+        define('FOG_VERSION', '1.3.0-RC-4');
         define('FOG_SCHEMA', 232);
         define('FOG_BCACHE_VER',93);
-        define('FOG_SVN_REVISION',5940);
+        define('FOG_SVN_REVISION',5941);
         define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
         define('FOG_CLIENT_VERSION', '0.11.4');
         define('PHP_VERSION_REQUIRED', '5.3.0');
