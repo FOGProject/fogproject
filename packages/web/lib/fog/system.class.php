15e35109e0228d48560b5407a2ce7665cdf06759
Tom Elliott <tommygunsster@gmail.com>
2016-08-03 16:41:19 -0400

    Make 1.3.0 RC-6

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,7 +1,7 @@
 <?php
 class System {
     public function __construct() {
-        define('FOG_VERSION', '23');
+        define('FOG_VERSION', '1.3.0-RC-6');
         define('FOG_SCHEMA', 232);
         define('FOG_BCACHE_VER',95);
         define('FOG_SVN_REVISION',5944);
