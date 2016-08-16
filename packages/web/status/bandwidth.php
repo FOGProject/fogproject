540e6f26be36f267ed4350e4bf2ae4a83b837df9
Tom Elliott <tommygunsster@gmail.com>
2016-08-16 09:40:00 -0400

    Minimal work to get files into PSR-2 standard.
    Update kernel drivers to allow for keyboards.

--- packages/web/status/bandwidth.php
+++ packages/web/status/bandwidth.php
@@ -6,18 +6,32 @@ set_time_limit(0);
 header('Content-Type: text/event-stream');
 header('Connection: close');
 $dev = trim($_REQUEST['dev'] ? basename($_REQUEST['dev']) : 'eth0');
-$dirints = array_diff(scandir('/sys/class/net'),array('..','.'));
-array_walk($dirints,function(&$iface,&$index) use (&$interfaces) {
-    if (trim(file_get_contents(sprintf('/sys/class/net/%s/operstate',$iface))) !== 'up') return;
-    $interfaces[] = $iface;
-});
-$interface = preg_grep("#$dev#",(array)$interfaces);
+$dirints = array_diff(scandir('/sys/class/net'), array('..','.'));
+array_walk(
+    $dirints,
+    function (&$iface, &$index) use (&$interfaces) {
+        $operfile = "/sys/class/net/$iface/operstate";
+        $contents = file_get_contents($operfile);
+        $content = trim($contents);
+        if ($content !== 'up') {
+            return;
+        }
+        $interfaces[] = $iface;
+    }
+);
+$interface = preg_grep("#$dev#", (array)$interfaces);
 $dev = array_shift($interface);
-if (empty($dev)) $dev = FOGCore::getMasterInterface($FOGCore->resolveHostname($_SERVER['SERVER_ADDR']));
+if (empty($dev)) {
+    $srvaddr = $_SERVER['SERVER_ADDR'];
+    $resname = $FOGCore->resolveHostname($srvaddr);
+    $dev = FOGCore::getMasterInterface($resname);
+}
 $rx_data = trim(file_get_contents("/sys/class/net/$dev/statistics/rx_bytes"));
 $tx_data = trim(file_get_contents("/sys/class/net/$dev/statistics/tx_bytes"));
 $rx = is_numeric($rx_data) && $rx_data > 0 ? $rx_data * 8 / 1024 : 0;
 $tx = is_numeric($tx_data) && $tx_data > 0 ? $tx_data * 8 / 1024 : 0;
 $ret = array('dev'=>$dev,'rx'=>$rx,'tx'=>$tx);
-if (empty($dev)) $ret = array('dev'=>'Unknown','rx'=>0,'tx'=>0);
+if (empty($dev)) {
+    $ret = array('dev'=>'Unknown','rx'=>0,'tx'=>0);
+}
 die(json_encode($ret));
--- packages/web/status/getfiles.php
+++ packages/web/status/getfiles.php
@@ -1,7 +1,6 @@
 <?php
-session_write_close();
-ignore_user_abort(true);
-set_time_limit(0);
-header('Content-Type: text/event-stream');
-header('Connection: close');
-die(json_encode(glob(sprintf('%s%s*',preg_replace('#[\\/]#',DIRECTORY_SEPARATOR,urldecode($_REQUEST['path'])),DIRECTORY_SEPARATOR))));
+$decodedPath = urldecode($_REQUEST['path']);
+$replaced_dir_sep = preg_replace('#[\\/]#', DIRECTORY_SEPARATOR, $decodedPath);
+$glob_str = sprintf('%s%s*', $replaced_dir_sep);
+$files = glob($glob_str);
+die(json_encode($files));
