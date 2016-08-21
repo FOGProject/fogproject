284c45d7b7d25ea35a701401128c3b3128e9b51b
Tom Elliott <tommygunsster@gmail.com>
2016-08-21 18:59:40 -0400

    Cleanup system class and make it commented.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -1,20 +1,63 @@
 <?php
+/**
+ * System, the basic system layout
+ *
+ * PHP Version 5
+ *
+ * This just presents the system variables
+ *
+ * @category System
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * System, the basic system layout
+ *
+ * @category System
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
 class System
 {
+    const PHP_REQUIRED = '5.3.0';
+    /**
+     * Checks the php version against what we require
+     *
+     * @return void
+     */
+    private static function _versionCompare()
+    {
+        $msg = '';
+        if (false === version_compare(PHP_VERSION, PHP_REQUIRED, '>=')) {
+            $msg = sprintf(
+                '%s. %s %s, %s 5.3.0 %s.',
+                _('Your system PHP Version is not sufficient'),
+                _('You have version'),
+                PHP_VERSION,
+                _('version'),
+                _('is required')
+            );
+        }
+        if ($msg) {
+            die($msg);
+        }
+    }
+    /**
+     * Constructs the system variables
+     *
+     * @return void
+     */
     public function __construct()
     {
-        define('FOG_VERSION', '34');
+        self::_versionCompare();
+        define('FOG_VERSION', '35');
         define('FOG_SCHEMA', 233);
         define('FOG_BCACHE_VER', 99);
-        define('FOG_SVN_REVISION',5951);
-        define('FOG_SVN_LAST_UPDATE', '$LastChangedDate: 2015-01-01 14:16:56 -0500 (Thu, 01 Jan 2015) $');
+        define('FOG_SVN_REVISION', 5951);
         define('FOG_CLIENT_VERSION', '0.11.5');
-        define('PHP_VERSION_REQUIRED', '5.3.0');
-        define('PHP_COMPATIBLE', version_compare(PHP_VERSION, PHP_VERSION_REQUIRED, '>='));
-        define('SPACE_DEFAULT_STORAGE', '/images');
-        if (PHP_COMPATIBLE === false) {
-            die(sprintf(_('Your systems PHP version is not sufficient. You have version %s, version %s is required.'), PHP_VERSION, PHP_VERSION_REQUIRED));
-            exit;
-        }
     }
 }
