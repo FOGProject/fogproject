d8cf66365353ad95712714788cc1ef641cd46fc2
Tom Elliott <tommygunsster@gmail.com>
2016-08-27 20:02:42 -0400

    Follow PSR for event and hook classes.

--- packages/web/lib/fog/event.class.php
+++ packages/web/lib/fog/event.class.php
@@ -1,14 +1,77 @@
 <?php
+/**
+ * Allows Events and defines how they operate.
+ * Because of the similarities of use for events and hooks
+ * the event class here is the hook base model as well.
+ *
+ * PHP version 5
+ *
+ * @category Event
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Allows Events and defines how they operate.
+ * Because of the similarities of use for events and hooks
+ * the event class here is the hook base model as well.
+ *
+ * @category Event
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
 abstract class Event extends FOGBase
 {
+    /**
+     * The name.
+     *
+     * @var string
+     */
     protected $name;
+    /**
+     * A description.
+     *
+     * @var string
+     */
     protected $description;
+    /**
+     * The author creating.
+     *
+     * @var string
+     */
     protected $author;
+    /**
+     * The active flag of this.
+     *
+     * @var bool
+     */
     public $active = true;
+    /**
+     * The log level.
+     *
+     * @var int
+     */
     public $logLevel = 0;
+    /**
+     * Whether to store log to file
+     *
+     * @var bool
+     */
     public $logToFile = false;
+    /**
+     * Whether to show log in browser
+     *
+     * @var bool
+     */
     public $logToBrowser = true;
-    public $delformat;
+    /**
+     * Initializes the base elements of the event or hook
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
@@ -16,28 +79,100 @@ abstract class Event extends FOGBase
             self::$FOGUser = self::getClass('User', $_SESSION['FOG_USER']);
         }
     }
+    /**
+     * How to log this file.
+     *
+     * @param string $txt   The text to log.
+     * @param int    $level The basic log level.
+     *
+     * @return void
+     */
     public function log($txt, $level = 1)
     {
         if (self::$ajax) {
             return;
         }
-        $txt = trim(preg_replace(array("#\r#", "#\n#", '#\s+#', '# ,#'), array('', ' ', ' ', ','), $txt));
+        $findArr = array(
+            "#\r#",
+            "#\n#",
+            '#\s+#',
+            '# ,#',
+        );
+        $repArr = array(
+            '',
+            ' ',
+            ' ',
+            ','
+        );
+        $txt = preg_replace($findArr, $repArr, $txt);
+        $txt = trim($txt);
         if (empty($txt)) {
             return;
         }
-        $txt = sprintf('[%s] %s', self::niceDate()->format('Y-m-d H:i:s'), $txt);
-        if ($this->logToBrowser && $this->logLevel >= $level && !self::$post) {
-            printf('%s<div class="debug-hook">%s</div>%s', "\n", $txt, "\n");
+        $txt = sprintf(
+            '[%s] %s',
+            self::niceDate()->format('Y-m-d H:i:s'),
+            $txt
+        );
+        $msg = '%s<div class="debug-hook">%s</div>%s';
+        if (!self::$post && $this->logToBrowser) {
+            if ($this->logLevel >= $level) {
+                printf(
+                    $msg,
+                    "\n",
+                    $txt,
+                    "\n"
+                );
+            }
+        }
+        if ($this instanceof Hook) {
+            $typePath = 'hooks';
+        } elseif ($this instanceof Event) {
+            $typePath = 'events';
         }
         if ($this->logToFile) {
-            file_put_contents(sprintf('%s/lib/%s/%s.log', BASEPATH, $this instanceof Hook ? 'hooks' : 'events', get_class($this)), sprintf("[%s] %s\r\n", self::niceDate()->format('d-m-Y H:i:s'), $log), FILE_APPEND | LOCK_EX);
+            $log = sprintf(
+                '%s/lib/%s/%s.log',
+                BASEPATH,
+                $typePath,
+                get_class($this)
+            );
+            $logtxt = sprintf(
+                "[%s] %s\r\n",
+                self::niceDate()->format('d-m-Y H:i:s'),
+                $txt
+            );
+            file_put_contents(
+                $log,
+                $logtxt,
+                FILE_APPEND | LOCK_EX
+            );
         }
     }
+    /**
+     * Simply adds the run method, though should be more defined.
+     *
+     * @param mixed $arguments the item to work from
+     *
+     * @return mixed
+     */
     public function run($arguments)
     {
     }
+    /**
+     * This is a default function for events only.
+     *
+     * @param string $event the event to work off.
+     * @param mixed  $data  the data, though unused.
+     *
+     * @return mixed
+     */
     public function onEvent($event, $data)
     {
-        printf('%s %s', $event, _('Registered'));
+        printf(
+            '%s %s',
+            $event,
+            _('Registered')
+        );
     }
 }
--- packages/web/lib/fog/hook.class.php
+++ packages/web/lib/fog/hook.class.php
@@ -1,6 +1,43 @@
 <?php
+/**
+ * Hooks allow customization between different aspects.
+ *
+ * While not everything is hookable, there is quite a lot
+ * that is able to be customized.
+ *
+ * Most of the accessible elements are handled from the event class.
+ *
+ * PHP version 5
+ *
+ * @category Hook
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Hooks allow customization between different aspects.
+ *
+ * While not everything is hookable, there is quite a lot
+ * that is able to be customized.
+ *
+ * @category Hook
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
 abstract class Hook extends Event
 {
+    /**
+     * Function enables reportTypes
+     * to allow plugins, and all hooks really, to tie into
+     * report structures.
+     *
+     * @param mixed $arguments the item to tie into.
+     *
+     * @return void
+     */
     public function reportTypes($arguments)
     {
         $arguments['types'][$this->node] = 4;
