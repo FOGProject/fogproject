7f6e104488c8cbfd3aafe73f1116bd5ade530fc4
Tom Elliott <tommygunsster@gmail.com>
2016-08-29 18:46:40 -0400

    Fix versioning issue in DB during updates.  All were applied but it was losing the main version number.

--- packages/web/lib/pages/schemaupdaterpage.class.php
+++ packages/web/lib/pages/schemaupdaterpage.class.php
@@ -1,34 +1,129 @@
 <?php
+/**
+ * Handles the display of schema and schema updating in general.
+ *
+ * PHP version 5
+ *
+ * @category SchemaUpdaterPage
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Handles the display of schema and schema updating in general.
+ *
+ * @category SchemaUpdaterPage
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
 class SchemaUpdaterPage extends FOGPage
 {
+    /**
+     * The relavent calling node url
+     *
+     * @var string
+     */
     public $node = 'schema';
+    /**
+     * The page initializer
+     *
+     * @param string $name The name to work from.
+     *
+     * @return void
+     */
     public function __construct($name = '')
     {
-        parent::__construct($this->name);
-        if (self::getClass('Schema', 1)->get('version') >= FOG_SCHEMA) {
+        parent::__construct($name);
+        $schema = new Schema(1);
+        if ($schema->get('version') >= FOG_SCHEMA) {
             self::redirect('index.php');
         }
         $this->name = 'Database Schema Installer / Updater';
         $this->menu = array();
         $this->subMenu = array();
     }
-    /** index()
-     * The first page displayed especially when a user logs in.
+    /**
+     * The first page displayed if on GUI
+     *
+     * @return void
      */
     public function index()
     {
         $this->title = _('Database Schema Installer / Updater');
         $vals = array(
-                _('Your FOG database schema is not up to date, either because you have updated FOG or this is a new FOG Installation. If this is an upgrade, there will be a database back stored on your FOG Server defaulting under the folder /home/fogDBbackups/. Should anything go wrong, this backup will enable you to return to the previous install if needed.'),
-                _('Are you sure you wish to install or update the FOG database?'),
-                $this->formAction,
-                _('Install/Upgrade Now'),
-                _('If you would like to backup your FOG database you can do so using MySQL Administrator or by running the following command in a terminal window (Applications -> System Tools -> Terminal), this will save sqldump in your home directory).'),
-                "\n",
-            );
-        vprintf('<div id="dbRunning" class="hidden"><p>%s</p><p>%s</p><br/><form method="post" action="%s"><p class="c"><input type="hidden" name="fogverified"/><input type="submit" name="confirm" value="%s"/></p></form><p>%s</p><div id="sidenotes"><pre><code>cd%smysqldump --allow-keywords -x -v fog > fogbackup.sql</code></pre></div><br/></div>', $vals);
-        echo '<div id="dbNotRunning" class="hidden">'._('Your database connection appears to be invalid. FOG is unable to communicate with the database.  There are many reasons why this could be the case.  Please check your credentials in '.dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'fog/config.class.php. Also confirm that the database is indeed running. If credentials are correct, and the Database service is running, check to ensure your filesystem has enough space.').'</div>';
+            sprintf(
+                '%s, %s %s. %s, %s %s %s. %s, %s %s.',
+                _('Your FOG database schema is not up to date'),
+                _('either because you have updated'),
+                _('or this is a new FOG installation'),
+                _('If this is an upgrade'),
+                _('there will be a database backup stored on your'),
+                _('FOG server defaulting under the folder'),
+                '/home/fogDBbackups',
+                _('Should anything go wrong'),
+                _('this backup will enable you to return to the'),
+                _('previous install if needed')
+            ),
+            sprintf(
+                '%s %s?',
+                _('Are you sure you wish to'),
+                _('install or update the FOG database')
+            ),
+            $this->formAction,
+            _('Install/Upgrade Now'),
+            sprintf(
+                '%s %s %s %s %s (%s->%s->%s), %s %s.',
+                _('If you would like to backup your'),
+                _('FOG database you can do so using'),
+                _('MySQL Administrator or by running'),
+                _('the following command in a terminal'),
+                _('window'),
+                _('Applications'),
+                _('System Tools'),
+                _('Terminal'),
+                _('this will save the backup in your home'),
+                _('directory')
+            ),
+            "\n",
+        );
+        vprintf(
+            '<div id="dbRunning" class="hidden">'
+            . '<p>%s</p><p>%s</p><br/>'
+            . '<form method="post" action="%s">'
+            . '<p class="c"><input type="hidden" '
+            . 'name="fogverified"/><input type="submit" '
+            . 'name="confirm" value="%s"/></p></form>'
+            . '<p>%s</p><div id="sidenotes">'
+            . '<pre><code>cd%smysqldump --allow-keywords '
+            . '-x -v fog > fogbackup.sql</code></pre></div>'
+            . '<br/></div>',
+            $vals
+        );
+        echo '<div id="dbNotRunning" class="hidden">';
+        printf(
+            '%s. %s. %s. %s %s%s%s. %s. %s, %s, %s.',
+            _('Your database connection appears to be invalid'),
+            _('FOG is unable to communicate with the database'),
+            _('There are many reasons why this could be the case'),
+            _('Please check your credentials in'),
+            dirname(dirname(__FILE__)),
+            DIRECTORY_SEPARATOR,
+            'fog/config.class.php',
+            _('Also confirm that the database is indeed running'),
+            _('If credentials are correct'),
+            _('and if the Database service is running'),
+            _('check to ensure your filesystem has enough space')
+        );
+        echo '</div>';
     }
+    /**
+     * When a form is submitted, this function handles it.
+     *
+     * @return void
+     */
     public function index_post()
     {
         if (!isset($_POST['fogverified'])) {
@@ -37,7 +132,12 @@ class SchemaUpdaterPage extends FOGPage
         if (!isset($_POST['confirm'])) {
             return;
         }
-        require sprintf('%s%scommons%sschema.php', BASEPATH, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR);
+        include sprintf(
+            '%s%scommons%sschema.php',
+            BASEPATH,
+            DIRECTORY_SEPARATOR,
+            DIRECTORY_SEPARATOR
+        );
         $errors = array();
         try {
             if (!self::$DB->link()) {
@@ -46,9 +146,15 @@ class SchemaUpdaterPage extends FOGPage
             if (count($this->schema) <= self::$mySchema) {
                 throw new Exception(_('Update not required!'));
             }
-            $items = array_slice($this->schema, self::$mySchema, null, true);
+            $items = array_slice(
+                $this->schema,
+                self::$mySchema,
+                null,
+                true
+            );
+            $ver = 0;
             foreach ($items as $version => &$updates) {
-                ++$version;
+                $version++;
                 foreach ($updates as $i => &$update) {
                     if (is_callable($update)) {
                         $result = $update();
@@ -59,11 +165,12 @@ class SchemaUpdaterPage extends FOGPage
                         $errors[] = sprintf('<p><b>Update ID:</b> %s</p><p><b>Database Error:</b> <pre>%s</pre></p><p><b>Database SQL:</b> <pre>%s</pre></p>', "$version - $i", self::$DB->sqlerror(), $update);
                     }
                 }
+                $ver = $version;
                 unset($update);
             }
             unset($updates);
             self::$DB->currentDb(self::$DB->returnThis());
-            $newSchema = self::getClass('Schema', 1)->set('version', $version);
+            $newSchema = self::getClass('Schema', 1)->set('version', $ver);
             if (!self::$DB->dbName() || !$newSchema->save() || count($this->schema) != $newSchema->get('version')) {
                 $errmsg = sprintf('<p>%s</p>', _('Install / Update Failed!'));
                 if (count($errors)) {
@@ -77,7 +184,7 @@ class SchemaUpdaterPage extends FOGPage
             }
         } catch (Exception $e) {
             printf('<p>%s</p>', $e->getMessage());
-            exit 1;
+            die(1);
         }
     }
 }
