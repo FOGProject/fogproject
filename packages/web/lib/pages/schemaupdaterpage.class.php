3001b6543394b19bc9d7d70e68752924f9a92874
Tom Elliott <tommygunsster@gmail.com>
2016-09-01 22:26:51 -0400

    hopeful more fixing

--- packages/web/lib/pages/schemaupdaterpage.class.php
+++ packages/web/lib/pages/schemaupdaterpage.class.php
@@ -12,17 +12,17 @@ class SchemaUpdaterPage extends FOGPage {
      * The first page displayed especially when a user logs in.
      */
     public function index() {
-            $this->title = _('Database Schema Installer / Updater');
-            $vals = array(
-                _('Your FOG database schema is not up to date, either because you have updated FOG or this is a new FOG Installation. If this is an upgrade, there will be a database back stored on your FOG Server defaulting under the folder /home/fogDBbackups/. Should anything go wrong, this backup will enable you to return to the previous install if needed.'),
-                _('Are you sure you wish to install or update the FOG database?'),
-                $this->formAction,
-                _('Install/Upgrade Now'),
-                _('If you would like to backup your FOG database you can do so using MySQL Administrator or by running the following command in a terminal window (Applications -> System Tools -> Terminal), this will save sqldump in your home directory).'),
-                "\n",
-            );
-            vprintf('<div id="dbRunning" class="hidden"><p>%s</p><p>%s</p><br/><form method="post" action="%s"><p class="c"><input type="hidden" name="fogverified"/><input type="submit" name="confirm" value="%s"/></p></form><p>%s</p><div id="sidenotes"><pre><code>cd%smysqldump --allow-keywords -x -v fog > fogbackup.sql</code></pre></div><br/></div>',$vals);
-            echo '<div id="dbNotRunning" class="hidden">'._('Your database connection appears to be invalid. FOG is unable to communicate with the database.  There are many reasons why this could be the case.  Please check your credentials in '.dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'fog/config.class.php. Also confirm that the database is indeed running. If credentials are correct, and the Database service is running, check to ensure your filesystem has enough space.').'</div>';
+        $this->title = _('Database Schema Installer / Updater');
+        $vals = array(
+            _('Your FOG database schema is not up to date, either because you have updated FOG or this is a new FOG Installation. If this is an upgrade, there will be a database back stored on your FOG Server defaulting under the folder /home/fogDBbackups/. Should anything go wrong, this backup will enable you to return to the previous install if needed.'),
+            _('Are you sure you wish to install or update the FOG database?'),
+            $this->formAction,
+            _('Install/Upgrade Now'),
+            _('If you would like to backup your FOG database you can do so using MySQL Administrator or by running the following command in a terminal window (Applications -> System Tools -> Terminal), this will save sqldump in your home directory).'),
+            "\n",
+        );
+        vprintf('<div id="dbRunning" class="hidden"><p>%s</p><p>%s</p><br/><form method="post" action="%s"><p class="c"><input type="hidden" name="fogverified"/><input type="submit" name="confirm" value="%s"/></p></form><p>%s</p><div id="sidenotes"><pre><code>cd%smysqldump --allow-keywords -x -v fog > fogbackup.sql</code></pre></div><br/></div>',$vals);
+        echo '<div id="dbNotRunning" class="hidden">'._('Your database connection appears to be invalid. FOG is unable to communicate with the database.  There are many reasons why this could be the case.  Please check your credentials in '.dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'fog/config.class.php. Also confirm that the database is indeed running. If credentials are correct, and the Database service is running, check to ensure your filesystem has enough space.').'</div>';
     }
     public function index_post() {
         if (!isset($_POST['fogverified'])) return;
@@ -42,14 +42,14 @@ class SchemaUpdaterPage extends FOGPage {
                     } else if (false === self::$DB->query($update)) $errors[] = sprintf('<p><b>Update ID:</b> %s</p><p><b>Database Error:</b> <pre>%s</pre></p><p><b>Database SQL:</b> <pre>%s</pre></p>', "$version - $i",self::$DB->sqlerror(),$update);
                 }
                 unset($update);
-            }
-            unset($updates);
-            self::$DB->currentDb(self::$DB->returnThis());
-            $newSchema = self::getClass('Schema',1)->set('version',$version);
-            if (!self::$DB->dbName() || !$newSchema->save() || count($this->schema) != $newSchema->get('version')) {
-                $errmsg = sprintf('<p>%s</p>',_('Install / Update Failed!'));
-                if (count($errors)) $errmsg .= sprintf('<h2>%s</h2>%s',_('The following errors occurred'),implode('<hr/>',$errors));
-                throw new Exception($errmsg);
+                self::$DB->currentDb(self::$DB->returnThis());
+                $newSchema = self::getClass('Schema',1)->set('version',$version);
+                if (!self::$DB->dbName() || !$newSchema->save() || count($this->schema) != $newSchema->get('version')) {
+                    $errmsg = sprintf('<p>%s</p>',_('Install / Update Failed!'));
+                    if (count($errors)) $errmsg .= sprintf('<h2>%s</h2>%s',_('The following errors occurred'),implode('<hr/>',$errors));
+                    throw new Exception($errmsg);
+                }
+                unset($updates);
             }
             printf('<p>%s</p><p>%s <a href="index.php">%s</a> %s</p>',_('Install / Update Successful!'),_('Click'),_('here'),_('to login'));
             if (count($errors)) printf('<h2>%s</h2>%s',_('The following errors occured'),implode('<hr/>',$errors));
