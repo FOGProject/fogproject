3248a3b35847e99fa02079ddc5082d2a434d0dec
Tom Elliott <tommygunsster@gmail.com>
2016-09-01 21:55:28 -0400

    more test

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -59,7 +59,7 @@ updateDB() {
     case $dbupdate in
         [Yy]|[Yy][Ee][Ss])
             dots "Updating Database"
-            cmd='wget -qO - --post-data="confirm&fogverified" --no-proxy "http://${ipaddress}/${webroot}management/index.php?node=schema" >>$workingdir/error_logs/fog_error_${version}.log 2>&1'
+            cmd="wget -qO - --post-data=\"confirm&fogverified\" --no-proxy http://${ipaddress}/${webroot}management/index.php?node=schema >>$workingdir/error_logs/fog_error_${version}.log 2>&1"
             echo $cmd
             return
             errorStat $?
