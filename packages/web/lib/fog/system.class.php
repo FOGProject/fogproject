f63b2434d9e10bf7b1e9974b09d7ab4650533b04
Tom Elliott <tommygunsster@gmail.com>
2016-09-08 22:34:55 -0400

    Push RC10

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -55,7 +55,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '37');
+        define('FOG_VERSION', '1.3.0-RC-10');
         define('FOG_SCHEMA', 234);
         define('FOG_BCACHE_VER', 100);
         define('FOG_SVN_REVISION', 5953);
