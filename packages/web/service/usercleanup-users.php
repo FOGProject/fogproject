b84f56b7dbb3fe1c7bbfe96863a2779213bbc01d
Tom Elliott <tommygunsster@gmail.com>
2016-10-02 16:50:23 -0400

    More PSR finish up the service directory

--- packages/web/service/usercleanup-users.php
+++ packages/web/service/usercleanup-users.php
@@ -1,3 +1,30 @@
 <?php
-require('../commons/base.inc.php');
-FOGCore::getClass('UserCleaner', true, false, false, false, isset($_REQUEST['newService']));
+/**
+ * Cleans up users ony good for Windows XP
+ *
+ * PHP version 5
+ *
+ * @category Usercleanup_Users
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Cleans up users ony good for Windows XP
+ *
+ * @category Usercleanup_Users
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+require '../commons/base.inc.php';
+FOGCore::getClass(
+    'UserCleaner',
+    true,
+    false,
+    false,
+    false,
+    isset($_REQUEST['newService'])
+);
--- packages/web/service/usertracking.report.php
+++ packages/web/service/usertracking.report.php
@@ -1,3 +1,27 @@
 <?php
-require('../commons/base.inc.php');
-FOGCore::getClass('UserTrack', true, !isset($_REQUEST['newService']));
+/**
+ * Tracks users logging in and out
+ *
+ * PHP version 5
+ *
+ * @category UserTrack
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Tracks users logging in and out
+ *
+ * @category UserTrack
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+require '../commons/base.inc.php';
+FOGCore::getClass(
+    'UserTrack',
+    true,
+    !isset($_REQUEST['newService'])
+);
