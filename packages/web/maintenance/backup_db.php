0d9b0502b937962f01153e9345bdf045336c8358
Tom Elliott <tommygunsster@gmail.com>
2016-10-04 18:19:40 -0400

    Remove the parenthesis from all the require bits.  PSR the index.php pages for mobile and main

--- packages/web/maintenance/backup_db.php
+++ packages/web/maintenance/backup_db.php
@@ -1,3 +1,23 @@
 <?php
-require('../commons/base.inc.php');
+/**
+ * Backs up the db for us
+ *
+ * PHP version 5
+ *
+ * @category Backup_DB
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Backs up the db for us
+ *
+ * @category Backup_DB
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+require '../commons/base.inc.php';
 FOGCore::getClass('ReportMaker')->outputReport();
--- packages/web/management/index.php
+++ packages/web/management/index.php
@@ -1,13 +1,37 @@
 <?php
-require('../commons/base.inc.php');
+/**
+ * The main index presenter
+ *
+ * PHP version 5
+ *
+ * @category Index_Page
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * The main index presenter
+ *
+ * @category Index_Page
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+require '../commons/base.inc.php';
 $FOGPageManager = FOGCore::getClass('FOGPageManager');
-if (isset($_SESSION['delitems']) && !in_array($sub, array('deletemulti', 'deleteconf'))) {
+if (isset($_SESSION['delitems'])
+    && !in_array($sub, array('deletemulti', 'deleteconf'))
+) {
     unset($_SESSION['delitems']);
 }
 FOGCore::getClass('ProcessLogin')->processMainLogin();
-require('../commons/text.php');
+require '../commons/text.php';
 $Page = FOGCore::getClass('Page');
-if (!in_array($_REQUEST['node'], array('schema', 'client')) && ($node == 'logout' || !$currentUser->isValid())) {
+if (!in_array($node, array('schema', 'client'))
+    && ($node == 'logout' || !$currentUser->isValid())
+) {
     $currentUser->logout();
     $Page->setTitle($foglang['Login']);
     $Page->setSecTitle($foglang['ManagementLogin']);
