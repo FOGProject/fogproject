1f50ca5a0a49eb2ef90890ef1fb5521d7c81b3b4
Tom Elliott <tommygunsster@gmail.com>
2016-10-24 20:32:44 -0400

    Ensure trigger get's created

--- packages/web/lib/plugins/persistentgroups/class/persistentgroupsmanager.class.php
+++ packages/web/lib/plugins/persistentgroups/class/persistentgroupsmanager.class.php
@@ -33,57 +33,48 @@ class PersistentGroupsManager extends FOGManagerController
     public function install($name)
     {
         $this->uninstall();
-        $sql = 'DELIMITER $$'
-            . 'CREATE TRIGGER `new_groupmember_added` '
-            . 'AFTER INSERT ON `groupMembers` '
-            . 'FOR EACH ROW'
-            . 'BEGIN'
-            . 'SET @myHostID = `NEW`.`gmHostID`;'
-            . 'SET @myGroupID = `NEW`.`gmGroupID`;'
-            . 'SET @myTemplateID = '
-            . '(SELECT `hostID` FROM `groups` '
-            . 'INNER JOIN `hosts` ON '
-            . '(`groupName` = `hostName`) WHERE `groupID`=@myGroupID);'
-            . 'IF (@myTemplateID IS NOT NULL) AND (@myHostID <> @myTemplateID) THEN'
-            . 'UPDATE `hosts` `d`, (SELECT `hostImage`, '
-            . '`hostBuilding`, `hostUseAD`, `hostADDomain`, `hostADOU`, '
-            . '`hostADUser`, `hostADPass`, `hostADPassLegacy`, '
-            . '`hostProductKey`, `hostPrinterLevel`, `hostKernelArgs`, '
-            . '`hostExitBios`, `hostExitEfi`, `hostEnforce` '
-            . 'FROM `hosts` WHERE `hostID`=@myTemplateID) `s`'
-            . 'SET `d`.`hostImage`=`s`.`hostImage`, `d`.`hostBuilding`='
-            . '`s`.`hostBuilding`, '
-            . '`d`.`hostUseAD`=`s`.`hostUseAD`, `d`.`hostADDomain`='
-            . '`s`.`hostADDomain`,'
-            . '`d`.`hostADOU`=`s`.`hostADOU`, `d`.`hostADUser`=`s`.`hostADUser`, '
-            . '`d`.`hostADPass`=`s`.`hostADPass`, `d`.`hostADPassLegacy`='
-            . '`s`.`hostADPassLegacy`,'
-            . '`d`.`hostProductKey`=`s`.`hostProductKey`, '
-            . '`d`.`hostPrinterLevel`=`s`.`hostPrinterLevel`, `d`.`hostKernelArgs`='
-            . '`s`.`hostKernelArgs`,'
-            . '`d`.`hostExitBios`=`s`.`hostExitBios`, '
-            . '`d`.`hostExitEfi`=`s`.`hostExitEfi`, '
-            . '`d`.`hostEnforce`=`s`.`hostEnforce`'
-            . 'WHERE `d`.`hostID`=@myHostID;'
-            . 'INSERT INTO `locationAssoc` (`laHostID`,`laLocationID`)'
-            . 'SELECT @myHostID as `laHostID`,`laLocationID`'
-            . 'FROM `locationAssoc` WHERE `laHostID`=@myTemplateID;'
-            . 'INSERT INTO `printerAssoc` '
-            . '(`paHostID`,`paPrinterID`,`paIsDefault`,`paAnon1`,'
-            . '`paAnon2`,`paAnon3`,`paAnon4`)'
-            . 'SELECT @myHostID as `paHostID`,`paPrinterID`,'
-            . '`paIsDefault`,`paAnon1`,`paAnon2`,`paAnon3`,`paAnon4`'
-            . 'FROM `printerAssoc` WHERE `paHostID`=@myTemplateID;'
-            . 'INSERT INTO `snapinAssoc` (`saHostID`,`saSnapinID`)'
-            . 'SELECT @myHostID as `saHostID`,`saSnapinID` '
-            . 'FROM `snapinAssoc` WHERE `saHostID`=@myTemplateID;'
-            . 'INSERT INTO `moduleStatusByHost` (`msHostID`,`msModuleID`,`msState`)'
-            . 'SELECT @myHostID as `msHostID`,`msModuleID`,`msState`'
-            . 'FROM `moduleStatusByHost` WHERE `msHostID`=@myTemplateID;'
-            . 'END IF;'
-            . 'END;'
-            . '$$'
-            . 'DELIMITER ;';
+        $sql = "CREATE TRIGGER `persistentGroups` 
+            AFTER INSERT ON `groupMembers` 
+            FOR EACH ROW
+                BEGIN
+
+                SET @myHostID = `NEW`.`gmHostID`;
+        SET @myGroupID = `NEW`.`gmGroupID`;
+
+        SET @myTemplateID = (SELECT `hostID` FROM `groups` INNER JOIN `hosts` ON (`groupName` = `hostName`) WHERE `groupID`=@myGroupID);
+
+        IF (@myTemplateID IS NOT NULL) AND (@myHostID <> @myTemplateID) THEN
+            UPDATE `hosts` `d`, (SELECT `hostImage`, `hostBuilding`, `hostUseAD`, `hostADDomain`, `hostADOU`, 
+            `hostADUser`, `hostADPass`, `hostADPassLegacy`, `hostProductKey`, `hostPrinterLevel`, `hostKernelArgs`, 
+            `hostExitBios`, `hostExitEfi`, `hostEnforce` FROM `hosts` WHERE `hostID`=@myTemplateID) `s`
+            SET `d`.`hostImage`=`s`.`hostImage`, `d`.`hostBuilding`=`s`.`hostBuilding`, `d`.`hostUseAD`=`s`.`hostUseAD`, `d`.`hostADDomain`=`s`.`hostADDomain`,
+            `d`.`hostADOU`=`s`.`hostADOU`, `d`.`hostADUser`=`s`.`hostADUser`, `d`.`hostADPass`=`s`.`hostADPass`, `d`.`hostADPassLegacy`=`s`.`hostADPassLegacy`,
+            `d`.`hostProductKey`=`s`.`hostProductKey`, `d`.`hostPrinterLevel`=`s`.`hostPrinterLevel`, `d`.`hostKernelArgs`=`s`.`hostKernelArgs`,
+            `d`.`hostExitBios`=`s`.`hostExitBios`, `d`.`hostExitEfi`=`s`.`hostExitEfi`, `d`.`hostEnforce`=`s`.`hostEnforce`
+            WHERE `d`.`hostID`=@myHostID;
+
+        SET @myDBTest = (SELECT count(`table_name`) FROM information_schema.tables WHERE `table_schema` = 'fog' AND `table_name` = 'locationAssoc' LIMIT 1);
+        if (@myDBTest > 0) THEN
+            INSERT INTO `locationAssoc` (`laHostID`,`laLocationID`)
+            SELECT @myHostID as `laHostID`,`laLocationID`
+            FROM `locationAssoc` WHERE `laHostID`=@myTemplateID;
+        END IF;
+
+        INSERT INTO `printerAssoc` (`paHostID`,`paPrinterID`,`paIsDefault`,`paAnon1`,`paAnon2`,`paAnon3`,`paAnon4`)
+            SELECT @myHostID as `paHostID`,`paPrinterID`,`paIsDefault`,`paAnon1`,`paAnon2`,`paAnon3`,`paAnon4`
+            FROM `printerAssoc` WHERE `paHostID`=@myTemplateID;
+
+        INSERT INTO `snapinAssoc` (`saHostID`,`saSnapinID`)
+            SELECT @myHostID as `saHostID`,`saSnapinID` 
+            FROM `snapinAssoc` WHERE `saHostID`=@myTemplateID;
+
+        INSERT INTO `moduleStatusByHost` (`msHostID`,`msModuleID`,`msState`)
+            SELECT @myHostID as `msHostID`,`msModuleID`,`msState`
+            FROM `moduleStatusByHost` WHERE `msHostID`=@myTemplateID;
+
+        END IF;
+
+        END;";
         return self::$DB->query($sql);
     }
     /**
