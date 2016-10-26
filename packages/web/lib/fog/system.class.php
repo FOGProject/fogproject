cf0cf46a609a2694c0286e298f9a49c9006c9ff4
Tom Elliott <tommygunsster@gmail.com>
2016-10-26 10:38:45 -0400

    Update ipxe files to latest pull

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,10 +53,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.3.0-RC-16');
+        define('FOG_VERSION', '17');
         define('FOG_SCHEMA', 235);
         define('FOG_BCACHE_VER', 101);
-        define('FOG_SVN_REVISION', 5981);
+        define('FOG_SVN_REVISION', 5982);
         define('FOG_CLIENT_VERSION', '0.11.5');
     }
 }
