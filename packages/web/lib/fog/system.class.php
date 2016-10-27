69ac499ed2db3541b0290af4137b5012bb833e0e
Tom Elliott <mastacontrola@users.noreply.github.com>
2016-10-27 07:02:51 -0400

    Update system.class.php

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -56,7 +56,7 @@ class System
         define('FOG_VERSION', '1.3.0-RC-16');
         define('FOG_SCHEMA', 235);
         define('FOG_BCACHE_VER', 101);
-        define('FOG_SVN_REVISION', 5983);
+        define('FOG_SVN_REVISION', 5984);
         define('FOG_CLIENT_VERSION', '0.11.5');
     }
 }
