dbf462aceb28511f13bd14fda6419a9de7172352
Tom Elliott <mastacontrola@users.noreply.github.com>
2016-10-31 07:42:45 -0400

    Document internally
    Add more comments for informative data to the MACAddress class.
    
    Should help others get an idea of the logic flow for this particular class item.

--- packages/web/lib/fog/macaddress.class.php
+++ packages/web/lib/fog/macaddress.class.php
@@ -5,10 +5,9 @@
  * PHP version 5
  *
  * @category MACAddress
- *
+ * @package  FOGProject
  * @author   Tom Elliott <tommygunsster@gmail.com>
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
- *
  * @link     https://fogproject.org
  */
 /**
@@ -17,10 +16,9 @@
  * PHP version 5
  *
  * @category MACAddress
- *
+ * @package  FOGProject
  * @author   Tom Elliott <tommygunsster@gmail.com>
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
- *
  * @link     https://fogproject.org
  */
 class MACAddress extends FOGBase
@@ -68,13 +66,32 @@ class MACAddress extends FOGBase
      */
     public function __construct($mac)
     {
+        /**
+         * Defines our grep/search patterns for
+         * validating a MAC Address.
+         */
         self::$_pattern = '/^(?:[[:xdigit:]]{2}([-:]))';
         self::$_pattern .= '(?:[[:xdigit:]]{2}\1){4}[[:xdigit:]]{2}$';
         self::$_pattern .= '|^(?:[[:xdigit:]]{12})$|^(?:[[:xdigit:]]';
         self::$_pattern .= '{4}([.])){2}[[:xdigit:]]{4}$/';
+        /**
+         * Pull in our base initialized items.
+         */
         parent::__construct();
+        /**
+         * Temp storage of our passed mac address.
+         */
         $this->tmpMAC = $mac;
+        /**
+         * Makes our required changes to the mac.
+         *
+         * @throws Exception
+         */
         $this->setMAC();
+        /**
+         * If the mac is already registered, sets our
+         * MAC Variable to the instance of the db item.
+         */
         $this->_MAC = self::getClass('MACAddressAssociation')
             ->set('mac', $this->__toString())
             ->load('mac');
@@ -83,12 +100,19 @@ class MACAddress extends FOGBase
      * Sets the mac.
      *
      * @throws Exception
-     *
      * @return object
      */
     protected function setMAC()
     {
         try {
+            /**
+             * If the mac is already an instance of MACAddress,
+             * MACAddressAssociation, or plain string, normalize
+             * our protected MAC as lowercase without ., -, or :.
+             *
+             * If the mac is an arraw, take the first mac address and
+             * perform the same as above.
+             */
             if ($this->tmpMAC instanceof self) {
                 $this->MAC = self::normalizeMAC($this->tmpMAC);
             } elseif ($this->tmpMAC instanceof MACAddressAssociation) {
@@ -98,25 +122,46 @@ class MACAddress extends FOGBase
             } else {
                 $this->MAC = self::normalizeMAC($this->tmpMAC);
             }
+            /**
+             * If the mac address is not valid throw Invalid MAC message.
+             */
             if (!$this->isValid()) {
                 throw new Exception("#!im\n");
             }
+            /**
+             * Split the normalized mac into an array using 2 characters
+             * as the split. For example:
+             * 012345abcdef would become
+             * ['01','23,'45','ab','cd','ef']
+             */
             $splitter = str_split($this->MAC, 2);
+            /**
+             * This creates a portion of the message string for WOL requests.
+             */
             foreach ((array) $splitter as &$split) {
                 $hwAddr .= chr(hexdec($split));
                 unset($split);
             }
+            /**
+             * This creates and combines the message string.
+             * Message string is:
+             * 6 times inline 0xff
+             * 16 times hwAddr
+             */
             $this->_msg = sprintf(
                 '%s%s',
                 str_repeat(chr(255), 6),
                 str_repeat($hwAddr, 16)
             );
         } catch (Exception $e) {
-            if (self::$debug) {
-                self::$FOGCore->debug($e->getMessage().' MAC: %s', $this->MAC);
-            }
+            self::$FOGCore->debug(
+                sprintf(
+                    '%s MAC: %s',
+                    $e->getMessage(),
+                    $this->MAC
+                )
+            );
         }
-
         return $this;
     }
     /**
@@ -128,11 +173,28 @@ class MACAddress extends FOGBase
      */
     protected static function normalizeMAC($mac)
     {
-        $mac = preg_grep(self::$_pattern, (array) $mac);
+        /**
+         * Pull out the valid mac addresses in an array format.
+         */
+        $mac = preg_grep(
+            self::$_pattern,
+            (array) $mac
+        );
+        /**
+         * If there aren't any found valid mac's set the string to be empty
+         * and return immediately.
+         */
         if (count($mac) !== 1) {
             return '';
         }
+        /**
+         * As this class only works on an individual mac, we pull the first instance
+         * out and store it as a string for processing.
+         */
         $mac = array_shift($mac);
+        /**
+         * Strips the ., -, or ':' out of the passed mac address.
+         */
         $mac = str_replace(
             array(
                 '.',
@@ -142,8 +204,13 @@ class MACAddress extends FOGBase
             '',
             $mac
         );
+        /**
+         * Sets the string to lower case.
+         */
         $mac = strtolower($mac);
-
+        /**
+         * Return the normalized string.
+         */
         return $mac;
     }
     /**
@@ -153,14 +220,30 @@ class MACAddress extends FOGBase
      */
     public function getMACPrefix()
     {
+        /**
+         * Gets the prefix of the mac address.
+         */
         $strMod = substr(
             $this->MAC,
             0,
             6
         );
+        /**
+         * Splits into the segments for easier joining.
+         * For example, 012345 will become:
+         * ['01','23','45']
+         */
         $strMod = str_split($strMod, 2);
+        /**
+         * OUI text is in format xx-xx-xx,
+         * This puts the string together properly so
+         * string will become:
+         * 01-23-45
+         */
         $strMod = implode('-', $strMod);
-
+        /**
+         * Returns our string.
+         */
         return $strMod;
     }
     /**
@@ -170,9 +253,20 @@ class MACAddress extends FOGBase
      */
     public function __toString()
     {
+        /**
+         * Splits the mac to be an array for joining together easier.
+         * For example, mac 012345abcdef will become:
+         * ['01','23','45','ab','cd','ef'].
+         */
         $strMod = str_split($this->MAC, 2);
+        /**
+         * Joins the array with colons so our mac will be like:
+         * 01:23:45:ab:cd:ef
+         */
         $strMod = implode(':', $strMod);
-
+        /**
+         * Returns our string
+         */
         return $strMod;
     }
     /**
@@ -182,7 +276,14 @@ class MACAddress extends FOGBase
      */
     public function isValid()
     {
-        return (bool) preg_match(self::$_pattern, $this->MAC);
+        /**
+         * Tests the pattern to see if we are safe.
+         * Returns as bool.
+         */
+        return (bool) preg_match(
+            self::$_pattern,
+            $this->MAC
+        );
     }
     /**
      * Tests if mac is a pending mac.
@@ -235,27 +336,47 @@ class MACAddress extends FOGBase
      * @param string $ip   the ip to send to
      * @param int    $port the port to sent from
      *
-     * @return bool|void
+     * @return bool
      */
     public function wake($ip, $port = 9)
     {
+        /**
+         * Assume return will be true for now.
+         */
+        $ret = true;
+        /**
+         * Create our socket resource
+         */
         $sock = @socket_create(
             AF_INET,
             SOCK_DGRAM,
             SOL_UDP
         );
+        /**
+         * If failed, immediately return.
+         */
         if ($sock == false) {
             return false;
         }
+        /**
+         * Set our coket options
+         */
         $set_opt = @socket_set_option(
             $sock,
             SOL_SOCKET,
             SO_BROADCAST,
             true
         );
+        /**
+         * If invalid close socket and return immediately.
+         */
         if ($set_opt < 0) {
+            @socket_close($sock);
             return false;
         }
+        /**
+         * Send our wake up packet.
+         */
         $sendto = @socket_sendto(
             $sock,
             $this->_msg,
@@ -264,8 +385,19 @@ class MACAddress extends FOGBase
             $ip,
             $port
         );
-        if ($sendto) {
-            @socket_close($sock);
+        /**
+         * If failed set return to false;
+         */
+        if (!$sendto) {
+            $ret = false;
         }
+        /**
+         * Close the socket.
+         */
+        @socket_close($sock);
+        /**
+         * Return value
+         */
+        return $ret;
     }
 }
