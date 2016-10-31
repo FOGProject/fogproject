d9e374087207542dccf8f1f8af463ee34275f911
Tom Elliott <mastacontrola@users.noreply.github.com>
2016-10-31 07:56:37 -0400

    Update base.inc.php

--- packages/web/commons/base.inc.php
+++ packages/web/commons/base.inc.php
@@ -10,11 +10,29 @@
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
+/**
+ * Base that commonizes the requirements of FOG.
+ *
+ * @category Base
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Setup our more secure friendly header information.
+ */
 header('X-Frame-Options: sameorigin');
 header('X-XSS-Protection: 1; mode=block');
 header('X-Content-Type-Options: nosniff');
 header('Strict-Transport-Security: max-age=31536000');
 header('Access-Control-Allow-Origin: *');
+/**
+ * Our required files, text for language and init to initialize system.
+ */
 require 'text.php';
 require 'init.php';
+/**
+ * All output should be sanitized for faster browser experience.
+ */
 ob_start(array('Initiator', 'sanitizeOutput'));
