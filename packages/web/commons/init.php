cd1b38f1a33435debfb303a88b9fcbe846fdacc3
Tom Elliott <mastacontrola@users.noreply.github.com>
2016-10-31 08:17:17 -0400

    Update init.php

--- packages/web/commons/init.php
+++ packages/web/commons/init.php
@@ -40,36 +40,80 @@ class Initiator
      */
     public function __construct()
     {
-        $self = !preg_match('#service#i', $_SERVER['PHP_SELF']);
+        /**
+         * Find out if the link has service in the call.
+         */
+        $self = !preg_match(
+            '#service#i',
+            $_SERVER['PHP_SELF']
+        );
+        /**
+         * Set useragent to false.
+         */
         $useragent = false;
+        /**
+         * If user agent is passed, define the useragent
+         */
         if (isset($_SERVER['HTTP_USER_AGENT'])) {
             $useragent = $_SERVER['HTTP_USER_AGENT'];
         }
+        /**
+         * If we are not a service file
+         * and we have a user agent string
+         * and the Session hasn't been started,
+         * Start the session and make sure no cache is stored.
+         */
         if ($self && $useragent && !isset($_SESSION)) {
             session_start();
             session_cache_limiter('nocache');
         }
+        /**
+         * Define our base path (/var/www/, /var/www/html/, etc...)
+         */
         define('BASEPATH', self::_determineBasePath());
+        /**
+         * Regex pattern to search for files of type.
+         */
         $regext = '#^.*\.(report|event|class|hook)\.php$#';
+        /**
+         * Use our basepath and find all files that are not dots.
+         */
         $RecursiveDirectoryIterator = new RecursiveDirectoryIterator(
             BASEPATH,
             FileSystemIterator::SKIP_DOTS
         );
+        /**
+         * Create iterator item based on the directory iterator returns.
+         */
         $RecursiveIteratorIterator = new RecursiveIteratorIterator(
             $RecursiveDirectoryIterator
         );
+        /**
+         * Filter our iterator using the regex applied earlier.
+         */
         $RegexIterator = new RegexIterator(
             $RecursiveIteratorIterator,
             $regext,
             RegexIterator::GET_MATCH
         );
+        /**
+         * Set our iterator items into an array format.
+         */
         $paths = iterator_to_array($RegexIterator, true);
-        $allpaths = array_map(
-            function ($element) {
-                return dirname($element[0]);
-            },
-            $paths
-        );
+        /**
+         * Define all paths as an array.
+         */
+        $allpaths = array();
+        /**
+         * Loop our paths from earlier storing the dirname of the element.
+         */
+        foreach ((array)$paths as &$element) {
+            $allpaths[] = dirname($element[0]);
+            unset($element);
+        }
+        /**
+         * Set our include paths as all paths.
+         */
         set_include_path(
             sprintf(
                 '%s%s%s',
@@ -81,7 +125,13 @@ class Initiator
                 get_include_path()
             )
         );
+        /**
+         * Define with extenstions we need to auto load.
+         */
         spl_autoload_extensions('.class.php,.event.php,.hook.php,.report.php');
+        /**
+         * Pass our autoloaded items through our custom loader method.
+         */
         spl_autoload_register(array($this, '_fogLoader'));
     }
     /**
@@ -91,13 +141,22 @@ class Initiator
      */
     private static function _determineBasePath()
     {
+        /**
+         * Gets our script name and path.
+         */
         $script_name = $_SERVER['SCRIPT_NAME'];
+        /**
+         * Stores our matching if fog is in the name variable
+         */
         $match = preg_match('#/fog/#', $script_name);
         if ($match) {
             $match = 'fog/';
         } else {
             $match = '';
         }
+        /**
+         * Defines our webroot path.
+         */
         define(
             'WEB_ROOT',
             sprintf(
@@ -105,6 +164,10 @@ class Initiator
                 $match
             )
         );
+        /**
+         * Check for /srv/http/fog, /var/www/html/fog, or /var/www/fog.
+         * Otherwise use the document root as defined by the server.
+         */
         if (file_exists('/srv/http/fog')) {
             $path = '/srv/http/fog';
         } elseif (file_exists('/var/www/html/fog')) {
@@ -124,7 +187,7 @@ class Initiator
         return $path;
     }
     /**
-     * __destruct() Cleanup after no longer needed
+     * Cleanup after no longer needed
      *
      * @return void
      */
@@ -139,10 +202,26 @@ class Initiator
      */
     public static function startInit()
     {
+        /**
+         * Setup our error reporting information.
+         */
         error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+        /**
+         * This enables the autoloader to work.
+         */
         new self();
+        /**
+         * Check if the version of php is valid.
+         */
         self::_verCheck();
+        /**
+         * Check if the extensions fog needs are available.
+         */
         self::_extCheck();
+        /**
+         * Sets up variables for sub/node callers among other
+         * request/post/get variables for neater access.
+         */
         $globalVars = array(
             'newService',
             'json',
@@ -158,6 +237,9 @@ class Initiator
             'tab',
             'type',
         );
+        /**
+         * Sets our variables to always be trimmed.
+         */
         foreach ($globalVars as &$x) {
             global $$x;
             if (isset($_REQUEST[$x])) {
@@ -165,7 +247,13 @@ class Initiator
             }
             unset($x);
         }
+        /**
+         * Initialize the system itself.
+         */
         new System();
+        /**
+         * Initialize the configuration.
+         */
         new Config();
     }
     /**
@@ -177,6 +265,14 @@ class Initiator
      */
     public static function sanitizeItems(&$value = '')
     {
+        /**
+         * Lambda to sanitize our user input data.
+         *
+         * @param mixed $key the key of the array.
+         * @param mixed $val the value of the array.
+         *
+         * @return void
+         */
         $sanitize_items = function (&$val, &$key) use (&$value) {
             if (is_string($val)) {
                 $value[$key] = htmlentities($val, ENT_QUOTES, 'utf-8');
@@ -185,6 +281,11 @@ class Initiator
                 self::sanitizeItems($value[$key]);
             }
         };
+        /**
+         * If the value isn't specified, it will sanitize
+         * all REQUEST, COOKIE, POST, and GET data.
+         * Otherwise it will clean the passed value.
+         */
         if (!count($value)) {
             array_walk($_REQUEST, $sanitize_items);
             array_walk($_COOKIE, $sanitize_items);
@@ -204,6 +305,9 @@ class Initiator
     private static function _verCheck()
     {
         try {
+            /**
+             * If the version is less than 5.5.0 fail.
+             */
             if (!version_compare(phpversion(), '5.5.0', '>=')) {
                 throw new Exception(
                     sprintf(
@@ -225,9 +329,27 @@ class Initiator
      */
     private static function _extCheck()
     {
-        $requiredExtensions = array('gettext','mysqli');
+        /**
+         * List of required extensions.
+         */
+        $requiredExtensions = array(
+            'gettext',
+            'mysqli'
+        );
+        /**
+         * Get the loaded extensions.
+         */
         $loadedExtensions = get_loaded_extensions();
-        $has = array_intersect($requiredExtensions, $loadedExtensions);
+        /**
+         * Cross reference our required with what's loaded.
+         */
+        $has = array_intersect(
+            $requiredExtensions,
+            $loadedExtensions
+        );
+        /**
+         * If the count doesn't match our required we know we're missing something.
+         */
         if (count($has) < count($requiredExtensions)) {
             throw new Exception(_('Missing one or more extensions.'));
         }
@@ -242,14 +364,29 @@ class Initiator
      */
     private function _fogLoader($className)
     {
+        /**
+         * Sanity check, if the classname is not a string fail.
+         */
         if (!is_string($className)) {
             throw new Exception(_('Classname must be a string'));
         }
+        /**
+         * If the class exists, we know it's already been loaded.
+         * Return as we don't need to do anything.
+         */
         if (class_exists($className, false)) {
             return;
         }
+        /**
+         * Ensure the event and hook managers are available.
+         * Really only needed for the respective class but
+         * doesn't hurt to have in either case.
+         */
         global $EventManager;
         global $HookManager;
+        /**
+         * Load the class.
+         */
         spl_autoload($className);
     }
     /**
@@ -261,17 +398,29 @@ class Initiator
      */
     public static function sanitizeOutput($buffer)
     {
+        /**
+         * Line commented for clarity
+         */
         $search = array(
             '/\>[^\S ]+/s', //strip whitespaces after tags, except space
             '/[^\S ]+\</s', //strip whitespaces before tags, except space
             '/(\s)+/s',  // shorten multiple whitespace sequences
         );
+        /**
+         * Replaces what's found with same element here.
+         */
         $replace = array(
             '>',
             '<',
             '\\1',
         );
+        /**
+         * Perform our replace.
+         */
         $buffer = preg_replace($search, $replace, $buffer);
+        /**
+         * Returns the cleaned data.
+         */
         return $buffer;
     }
 }
