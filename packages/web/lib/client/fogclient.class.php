5d668341f948ecb6aedcc851ce2b4724f75cd5f9
Tom Elliott <tommygunsster@gmail.com>
2016-11-02 19:29:36 -0400

    Remove erroneous decoder that was thought to originally be a typo in requesting the string

--- packages/web/lib/client/fogclient.class.php
+++ packages/web/lib/client/fogclient.class.php
@@ -112,7 +112,6 @@ abstract class FOGClient extends FOGBase
                 return print $e->getMessage();
             }
             $message = $e->getMessage();
-            $message = json_decode($message);
             if (json_last_error() !== JSON_ERROR_NONE) {
                 $msg = preg_replace('/^[#][!]?/', '', $message);
                 $message = json_encode(
