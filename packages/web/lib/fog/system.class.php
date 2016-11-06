40ecdf18fd4684800e65a51cf23c3f27fbd44a3e
Tom Elliott <tommygunsster@gmail.com>
2016-11-06 11:44:02 -0500

    Push up RC 20

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '47');
+        define('FOG_VERSION', '1.3.0-RC-20');
         define('FOG_SCHEMA', 236);
         define('FOG_BCACHE_VER', 102);
         define('FOG_SVN_REVISION', 6004);
