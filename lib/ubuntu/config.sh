7ee655478f3c49b87b426fafcdc6c05316a2abf5
Tom Elliott <tommygunsster@gmail.com>
2016-11-17 14:08:07 -0500

    Fix left over fi

--- lib/ubuntu/config.sh
+++ lib/ubuntu/config.sh
@@ -50,6 +50,7 @@ if [[ $linuxReleaseName == +(*[Bb][Uu][Nn][Tt][Uu]*) ]]; then
                 ;;
         esac
     fi
+fi
 [[ -z $phpfpm ]] && phpfpm="php${php_ver}-fpm" || phpfpm="php5-fpm"
 [[ $php_ver != 5 ]] && phpcmd="php" || phpcmd="php5"
 [[ -z $packageQuery ]] && packageQuery="dpkg -l \$x | grep '^ii'"
