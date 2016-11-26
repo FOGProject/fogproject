a63b974abf09ff8b6cad907636aa791adaf35ff3
Tom Elliott <tommygunsster@gmail.com>
2016-11-26 11:34:08 -0500

    Do the removal/layout as needed

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '27');
+        define('FOG_VERSION', '28');
         define('FOG_SCHEMA', 239);
         define('FOG_BCACHE_VER', 105);
         define('FOG_SVN_REVISION', 6022);
