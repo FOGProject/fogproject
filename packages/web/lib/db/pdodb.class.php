40b4770363ba8567877cab8040cce53a6cb32487
Tom Elliott <tommygunsster@gmail.com>
2016-11-26 11:45:58 -0500

    Only set to false if necessary

--- packages/web/lib/db/pdodb.class.php
+++ packages/web/lib/db/pdodb.class.php
@@ -158,7 +158,6 @@ class PDODB extends DatabaseManager
             }
             self::query("SET SESSION sql_mode=''");
         } catch (PDOException $e) {
-            self::$_link = false;
             if ($dbexists) {
                 $this->_connect(false);
             } else {
@@ -169,6 +168,7 @@ class PDODB extends DatabaseManager
                     _('Error'),
                     $e->getMessage()
                 );
+                self::$_link = false;
             }
         }
         return $this;
