a7bd2db35297d8ecf47c757e79793e206bf1a118
Tom Elliott <tommygunsster@gmail.com>
2016-12-11 16:38:59 -0500

    Push up RC-33

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '39');
+        define('FOG_VERSION', '1.3.0-RC-33');
         define('FOG_SCHEMA', 244);
         define('FOG_BCACHE_VER', 106);
         define('FOG_SVN_REVISION', 6038);
