791595e7ee5bbe2096487bdfe519c4810dadc04c
Tom Elliott <tommygunsster@gmail.com>
2016-12-16 22:56:39 -0500

    More fixes, particularly tweak the ist error throw to always run in json encoded form.

--- packages/web/lib/fog/fogpage.class.php
+++ packages/web/lib/fog/fogpage.class.php
@@ -2647,6 +2647,12 @@ abstract class FOGPage extends FOGBase
                 );
                 exit;
             }
+            if ($e->getMessage() == '#!ist') {
+                echo json_encode(
+                    array('error' => 'ist')
+                );
+                exit;
+            }
             echo  $e->getMessage();
         }
         exit;
@@ -2809,7 +2815,14 @@ abstract class FOGPage extends FOGBase
                 }
                 unset($key);
             }
-            $this->sendData(json_encode($array, JSON_UNESCAPED_UNICODE), true, $array);
+            $this->sendData(
+                json_encode(
+                    $array,
+                    JSON_UNESCAPED_UNICODE
+                ),
+                true,
+                $array
+            );
             //echo json_encode($array, JSON_UNESCAPED_UNICODE);
         } catch (Exception $e) {
             echo $e->getMessage();
