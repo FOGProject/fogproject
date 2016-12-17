55833919c03af37e73737b7d4f334b9b33464d16
Tom Elliott <tommygunsster@gmail.com>
2016-12-16 21:58:13 -0500

    Fix return to json to ensure we don't escape the unicode elements

--- packages/web/lib/fog/fogpage.class.php
+++ packages/web/lib/fog/fogpage.class.php
@@ -2810,7 +2810,7 @@ abstract class FOGPage extends FOGBase
                 unset($key);
             }
             $this->sendData(json_encode($array), true, $array);
-            //echo json_encode($array);
+            echo json_encode($array, JSON_UNESCAPED_UNICODE);
         } catch (Exception $e) {
             echo $e->getMessage();
         }
