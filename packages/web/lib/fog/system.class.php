57eb9fac51da2e3c45056e0114a04c6483180083
Tom Elliott <tommygunsster@gmail.com>
2017-01-07 13:33:37 -0500

    Push up RC-5.
    This adds an update to the FOG Client and improves replication logging.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,10 +53,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '7');
+        define('FOG_VERSION', '1.3.1-RC-5');
         define('FOG_SCHEMA', 245);
         define('FOG_BCACHE_VER', 111);
         define('FOG_SVN_REVISION', 6052);
-        define('FOG_CLIENT_VERSION', '0.11.7');
+        define('FOG_CLIENT_VERSION', '0.11.8');
     }
 }
