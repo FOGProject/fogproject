55be040b531378661f2d438aed55e7c6a8e72c00
Tom Elliott <tommygunsster@gmail.com>
2017-01-14 20:02:03 -0500

    Push up 1.3.1

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,10 +53,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '14');
+        define('FOG_VERSION', '1.3.1');
         define('FOG_SCHEMA', 245);
         define('FOG_BCACHE_VER', 112);
-        define('FOG_SVN_REVISION', 6052);
+        define('FOG_SVN_REVISION', 6053);
         define('FOG_CLIENT_VERSION', '0.11.8');
     }
 }
