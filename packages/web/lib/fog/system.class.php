b9b03e99c2c200a63f5283682760c2d269aa8a73
Tom Elliott <tommygunsster@gmail.com>
2017-01-19 16:04:30 -0500

    Push up 1.3.4
    This is in working 1.3.4 branch and should be ready for official release.
    Updated kernels to 4.9.4.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,10 +53,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '71');
+        define('FOG_VERSION', '1.3.4');
         define('FOG_SCHEMA', 246);
         define('FOG_BCACHE_VER', 112);
-        define('FOG_SVN_REVISION', 6063);
+        define('FOG_SVN_REVISION', 6064);
         define('FOG_CLIENT_VERSION', '0.11.8');
     }
 }
