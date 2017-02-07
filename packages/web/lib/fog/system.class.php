cf1ac76ecf85034173f9a287f3bb83508b92f39f
Tom Elliott <tommygunsster@gmail.com>
2017-02-07 15:42:36 -0500

    Push up 1.3.5-RC-2

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '110');
+        define('FOG_VERSION', '1.3.5-RC-2');
         define('FOG_SCHEMA', 249);
         define('FOG_BCACHE_VER', 112);
         define('FOG_SVN_REVISION', 6066);
