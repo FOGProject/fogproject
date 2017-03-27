45aead0b2f88c29492ef4e761d4fc6f21fcd115c
fernandoGietz <fernando.gietz@ehu.eus>
2017-03-27 17:34:05 +0200

    Create a new multi-index in the table rules

--- packages/web/lib/plugins/accesscontrol/class/accesscontrolrulemanager.class.php
+++ packages/web/lib/plugins/accesscontrol/class/accesscontrolrulemanager.class.php
@@ -83,10 +83,7 @@ class AccessControlRuleManager extends FOGManagerController
                 'CURRENT_TIMESTAMP',
                 false
             ),
-            array(
-                'ruleID',
-                'ruleValue'
-            ),
+            array(),
             'MyISAM',
             'utf8',
             'ruleID',
@@ -175,6 +172,9 @@ class AccessControlRuleManager extends FOGManagerController
             . '(39, "SUB_MENULINK-config", "SUB_MENULINK", '
             . '"config", "menu", "fog", NOW(), "about")';
         if (self::$DB->query($sql)) {
+        	$sql = "CREATE UNIQUE INDEX `indexmul` "
+        			. "`rules` (`ruleValue`, `ruleNode`)";
+        	self::$DB->query($sql);
             return self::getClass('AccessControlRuleAssociationManager')->install();
         } else {
             return true;
