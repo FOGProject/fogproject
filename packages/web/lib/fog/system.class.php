7a3a53fdd5158183da4334f16e5d213dc2772661
Tom Elliott <tommygunsster@gmail.com>
2017-04-10 18:33:50 -0400

    Move inventory to individual report

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '83');
+        define('FOG_VERSION', '84');
         define('FOG_SCHEMA', 257);
         define('FOG_BCACHE_VER', 115);
         define('FOG_SVN_REVISION', 6068);
--- packages/web/lib/pages/reportmanagementpage.class.php
+++ packages/web/lib/pages/reportmanagementpage.class.php
@@ -85,7 +85,6 @@ class ReportManagementPage extends FOGPage
         $this->menu = array(
             'home' => self::$foglang['Home'],
             'imaginglog' => self::$foglang['ImageLog'],
-            'inventory' => self::$foglang['Inventory'],
             'pendmac' => self::$foglang['PendingMACs'],
             'snapinlog' => self::$foglang['SnapinLog'],
             'usertrack' => self::$foglang['LoginHistory'],
@@ -445,93 +444,6 @@ class ReportManagementPage extends FOGPage
         unset($ImagingLogIDs, $id);
         $this->ReportMaker->appendHTML($this->__toString());
         $this->ReportMaker->outputReport(0);
-        $_SESSION['foglastreport'] = serialize($this->ReportMaker);
-    }
-    /**
-     * Presents and reports all Inventory data for hosts.
-     *
-     * @return void
-     */
-    public function inventory()
-    {
-        $this->title = _('Full Inventory Export');
-        printf(
-            $this->reportString,
-            'InventoryReport',
-            _('Export CSV'),
-            _('Export CSV'),
-            self::$csvfile,
-            'InventoryReport',
-            _('Export PDF'),
-            _('Export PDF'),
-            self::$pdffile
-        );
-        array_walk(
-            self::$inventoryCsvHead,
-            function (&$classGet, &$csvHeader) {
-                $this->ReportMaker->addCSVCell($csvHeader);
-                unset($classGet, $csvHeader);
-            }
-        );
-        $this->ReportMaker->endCSVLine();
-        $this->headerData = array(
-            _('Host name'),
-            _('Memory'),
-            _('System Product'),
-            _('System Serial'),
-        );
-        $this->templates = array(
-            '${host_name}<br/><small>${host_mac}</small>',
-            '${memory}',
-            '${sysprod}',
-            '${sysser}',
-        );
-        $this->attributes = array(
-            array(),
-            array(),
-            array(),
-            array(),
-        );
-        foreach ((array)self::getClass('HostManager')
-            ->find() as &$Host
-        ) {
-            $Image = $Host->getImage();
-            $Inventory = $Host->get('inventory');
-            $this->data[] = array(
-                'host_name' => $Host->get('name'),
-                'host_mac' => $Host->get('mac'),
-                'memory' => $Inventory->getMem(),
-                'sysprod' => $Inventory->get('sysproduct'),
-                'sysser' => $Inventory->get('sysserial'),
-            );
-            foreach (self::$inventoryCsvHead as $head => &$classGet) {
-                switch ($head) {
-                case _('Host ID'):
-                    $this->ReportMaker->addCSVCell($Host->get('id'));
-                    break;
-                case _('Host name'):
-                    $this->ReportMaker->addCSVCell($Host->get('name'));
-                    break;
-                case _('Host MAC'):
-                    $this->ReportMaker->addCSVCell($Host->get('mac'));
-                    break;
-                case _('Host Desc'):
-                    $this->ReportMaker->addCSVCell($Host->get('description'));
-                    break;
-                case _('Memory'):
-                    $this->ReportMaker->addCSVCell($Inventory->getMem());
-                    break;
-                default:
-                    $this->ReportMaker->addCSVCell($Inventory->get($classGet));
-                    break;
-                }
-                unset($classGet, $head);
-            }
-            $this->ReportMaker->endCSVLine();
-            unset($Inventory, $Host);
-        }
-        $this->ReportMaker->appendHTML($this->__toString());
-        $this->ReportMaker->outputReport(false);
         $_SESSION['foglastreport'] = serialize($this->ReportMaker);
     }
     /**
