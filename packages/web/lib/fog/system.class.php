4b069544e48da20d03dd46a89724180318266472
Tom Elliott <tommygunsster@gmail.com>
2017-04-17 07:57:54 -0400

    Update ipxe to latest (17-APR-2017)

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '107');
+        define('FOG_VERSION', '108');
         define('FOG_SCHEMA', 257);
         define('FOG_BCACHE_VER', 115);
         define('FOG_SVN_REVISION', 6068);
