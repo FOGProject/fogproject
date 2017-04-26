53c30d316ed6ab7841dc6a1a406efc15a7db210b
Tom Elliott <tommygunsster@gmail.com>
2017-04-26 15:24:34 -0400

    Test with gpg signed

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '28');
+        define('FOG_VERSION', '29');
         define('FOG_SCHEMA', 259);
         define('FOG_BCACHE_VER', 115);
         define('FOG_SVN_REVISION', 6068);
