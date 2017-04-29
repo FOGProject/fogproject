acaef366ffb1e8e240c8bfba373d75c016037689
Tom Elliott <tommygunsster@gmail.com>
2017-04-29 13:22:24 -0400

    more proper directory checking.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '97');
+        define('FOG_VERSION', '1.4.0-RC-9.1');
         define('FOG_SCHEMA', 259);
         define('FOG_BCACHE_VER', 115);
         define('FOG_SVN_REVISION', 6068);
