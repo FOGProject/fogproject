f9bf1d0b30b4f14f418cc31346b0d9635dd8ef44
Tom Elliott <tommygunsster@gmail.com>
2017-05-05 08:54:07 -0400

    Fix image path to allow . and - in the name

--- packages/web/management/js/fog/fog.image.js
+++ packages/web/management/js/fog/fog.image.js
@@ -4,7 +4,7 @@ $(function() {
     $('#iFile').on('change keyup',function(e) {
         var start = this.selectionStart,
             end = this.selectionEnd;
-        this.value = this.value.replace(/[^\w+\/]/g,'');
+        this.value = this.value.replace(/[^\w+\/\.-]/g,'');
         this.setSelectionRange(start,end);
         iFileVal = this.value;
         e.preventDefault();
@@ -13,12 +13,12 @@ $(function() {
         var start = this.selectionStart,
             end = this.selectionEnd;
         if (typeof iFileVal === 'undefined') return;
-        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+\/\.-]/g,''));
         this.setSelectionRange(start,end);
         e.preventDefault();
     }).blur(function(e) {
         if (typeof iFileVal === 'undefined') return;
-        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+]/g,''));
+        if (iFileVal.length == 0) $('#iFile').val(this.value.replace(/[^\w+\/.-]/g,''));
         iFileVal = $('#iFile').val();
         e.preventDefault();
     });
