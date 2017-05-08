10a8d390fba98557ba9665d5530fc440a0efe0bc
Tom Elliott <tommygunsster@gmail.com>
2017-05-08 08:36:12 -0400

    Fix example plugin uninstall

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '14');
+        define('FOG_VERSION', '15');
         define('FOG_SCHEMA', 259);
         define('FOG_BCACHE_VER', 116);
         define('FOG_SVN_REVISION', 6068);
