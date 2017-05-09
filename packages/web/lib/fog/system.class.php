e609b51f025e8f03eaf34783a946c9002a865410
Tom Elliott <tommygunsster@gmail.com>
2017-05-09 19:09:21 -0400

    Fix group search dn to not be required. If not set it will parse the userDN passed and return only the DC components. If no DC components, sorry it would not work anyway.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.4.0-RC-12');
+        define('FOG_VERSION', '19');
         define('FOG_SCHEMA', 259);
         define('FOG_BCACHE_VER', 116);
         define('FOG_SVN_REVISION', 6068);
