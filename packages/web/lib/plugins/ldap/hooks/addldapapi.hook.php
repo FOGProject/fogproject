f98e2baee898197c7aa73e891e390ff0b88e4049
Tom Elliott <tommygunsster@gmail.com>
2017-05-11 17:41:00 -0400

    Add api elements to the other plugins.

---
+++ packages/web/lib/plugins/ldap/hooks/addldapapi.hook.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Injects ldap stuff into the api system.
+ *
+ * PHP version 5
+ *
+ * @category AddLDAPAPI
+ * @package  FOGProject
+ * @author   Fernando Gietz <fernando.gietz@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Injects LDAP stuff into the api system.
+ *
+ * @category AddLDAPAPI
+ * @package  FOGProject
+ * @author   Fernando Gietz <fernando.gietz@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class AddLDAPAPI extends Hook
+{
+    /**
+     * Add LDAP API
+     *
+     * @var string
+     */
+    public $name = 'AddLDAPAPI';
+    /**
+     * The description.
+     *
+     * @var string
+     */
+    public $description = 'Add LDAP stuff into the api system.';
+    /**
+     * For posterity.
+     *
+     * @var bool
+     */
+    public $active = true;
+    /**
+     * The node to work with.
+     *
+     * @var string
+     */
+    public $node = 'ldap';
+    /**
+     * Initialize object.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        self::$HookManager
+            ->register(
+                'API_VALID_CLASSES',
+                array(
+                    $this,
+                    'injectAPIElements'
+                )
+            )
+            ->register(
+                'API_GETTER',
+                array(
+                    $this,
+                    'adjustGetter'
+                )
+            )
+            ->register(
+                'API_INDIVDATA_MAPPING',
+                array(
+                    $this,
+                    'adjustIndivInfoUpdate'
+                )
+            )
+            ->register(
+                'API_MASSDATA_MAPPING',
+                array(
+                    $this,
+                    'adjustMassInfo'
+                )
+            );
+    }
+    /**
+     * This function injects site elements for
+     * api access.
+     *
+     * @param mixed $arguments The arguments to modify.
+     *
+     * @return void
+     */
+    public function injectAPIElements($arguments)
+    {
+        if (!in_array($this->node, (array)self::$pluginsinstalled)) {
+            return;
+        }
+        $arguments['validClasses'] = self::fastmerge(
+            $arguments['validClasses'],
+            array(
+                'ldap'
+            )
+        );
+    }
+    /**
+     * This function changes the api data map as needed.
+     *
+     * @param mixed $arguments The arguments to modify.
+     *
+     * @return void
+     */
+    public function adjustIndivInfoUpdate($arguments)
+    {
+        if (!in_array($this->node, (array)self::$pluginsinstalled)) {
+            return;
+        }
+    }
+    /**
+     * This function changes the api data map as needed.
+     *
+     * @param mixed $arguments The arguments to modify.
+     *
+     * @return void
+     */
+    public function adjustMassInfo($arguments)
+    {
+        if (!in_array($this->node, (array)self::$pluginsinstalled)) {
+            return;
+        }
+    }
+    /**
+     * This function changes the getter to enact on this particular item.
+     *
+     * @param mixed $arguments The arguments to modify.
+     *
+     * @return void
+     */
+    public function adjustGetter($arguments)
+    {
+        if (!in_array($this->node, (array)self::$pluginsinstalled)) {
+            return;
+        }
+    }
+}
