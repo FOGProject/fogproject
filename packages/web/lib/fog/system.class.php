126fad92eac88da71279674cfb4517deaf001c73
Tom Elliott <tommygunsster@gmail.com>
2017-05-21 22:08:03 -0400

    Allow header to be expanded beyond 540px;

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '44668');
+        define('FOG_VERSION', '151');
         define('FOG_SCHEMA', 260);
         define('FOG_BCACHE_VER', 118);
         define('FOG_SVN_REVISION', 6070);
