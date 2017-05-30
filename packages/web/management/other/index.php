892739784d747d83f79347708dfb2d45e2bb5d3f
Tom Elliott <thomas.g.elliott@uscis.dhs.gov>
2017-05-30 09:45:26 -0400

    Many fixes/tweaks to main template page

--- packages/web/management/other/index.php
+++ packages/web/management/other/index.php
@@ -19,16 +19,15 @@
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
-echo '<!DOCTYPE html>';
-printf(
-    '<html lang="%s">',
-    ProcessLogin::getLocale()
-);
+echo '<!DOCTYPE>';
+echo '<html lang="'
+    . ProcessLogin::getLocale()
+    . '">';
 echo '<head>';
 echo '<meta charset="utf-8"/>';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge"/>';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1"/>';
-echo '<title>'.$this->title.'</title>';
+echo '<title>' . $this->title . '</title>'
 self::$HookManager
     ->processEvent(
         'CSS',
@@ -37,31 +36,34 @@ self::$HookManager
         )
     );
 foreach ((array)$this->stylesheets as &$stylesheet) {
-    printf(
-        '<link href="%s?ver=%d" rel="stylesheet" type="text/css"/>',
-        $stylesheet,
-        FOG_BCACHE_VER
-    );
+    echo '<link href="'
+        . $stylesheet
+        . '?ver='
+        . FOG_BCACHE_VER
+        . '" rel="stylesheet" type="text/css"/>';
     unset($stylesheet);
 }
 unset($this->stylesheets);
 echo '<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>';
 echo '</head>';
 echo '<body>';
-printf(
-    '<input type="hidden" class="fog-delete" id="FOGDeleteAuth" value="%s"/>',
-    (int)self::$fogdeleteactive
-);
-printf(
-    '<input type="hidden" class="fog-export" id="FOGExportAuth" value="%s"/>',
-    (int)self::$fogexportactive
-);
-printf(
-    '<input type="hidden" class="fog-variable" id="screenview" value="%s"/>',
-    self::$defaultscreen
-);
+echo '<div class="wrapper'
+    . (
+        !self::$FOGUser->isValid() ?
+        ' signin' :
+        ''
+    )
+    . '">';
 if (self::$FOGUser->isValid()) {
-    echo '<div class="wrapper">';
+    echo '<input type="hidden" class="fog-delete" id="FOGDeleteAuth" value="'
+        . (int)self::$fogdeleteactive
+        . '"/>';
+    echo '<input type="hidden" class="fog-export" id="FOGExportAuth" value="'
+        . (int)self::$fogexportactive
+        . '"/>';
+    echo '<input type="hidden" class="fog-variable" id="screenview" value="'
+        . self::$defaultscreen
+        . '"/>';
     echo '<div class="sidebar" data-color="blue">';
     echo '<div class="sidebar-wrapper">';
     echo $this->menu;
@@ -71,20 +73,20 @@ if (self::$FOGUser->isValid()) {
     echo '<nav class="navbar navbar-default navbar-fixed">';
     echo '<div class="container-fluid">';
     echo '<div class="navbar-header navbar-fixed">';
-    echo '<button type="button" class="navbar-toggle" data-toggle='
-        . '"collapse" data-target="#navigation-example-2">';
-    echo '<span class="sr-only">Toggle navigation</span>';
+    echo '<button type="button" class="navbar-toggle" data-toggle="collapse">';
+    echo '<span class="sr-only">'
+        . _('Toggle Navigation')
+        . '</span>';
     echo '<span class="icon-bar"></span>';
     echo '<span class="icon-bar"></span>';
     echo '<span class="icon-bar"></span>';
     echo '</button>';
     echo '<a class="navbar-brand" href="?node=home">';
-    printf(
-        '<img src="%s" alt="%s" title="%s" class="logoimg"/>',
-        '../favicon.ico',
-        self::$foglang['Slogan'],
-        self::$foglang['Home']
-    );
+    echo '<img src="../favicon.ico" alt="'
+        . self::$foglang['Slogan']
+        . '" title="'
+        . self::$foglang['Home']
+        . '" class="logoimg"/>';
     echo '</a>';
     echo '</div>';
     echo '<div class="collapse navbar-collapse">';
@@ -92,24 +94,21 @@ if (self::$FOGUser->isValid()) {
         echo self::$FOGPageManager->getSideMenu();
     }
     echo '<ul class="nav navbar-nav navbar-right">';
-    echo '<li>';
-    echo '<a href="?node=logout">';
-    echo self::$foglang['Logout'];
-    echo '</a>';
-    echo '</li>';
+    echo '<li><a href="?node=logout">'
+        . self::$foglang['Logout']
+        . '</a></li>';
     echo '<li class="separator hidden-1g hidden-md"></li>';
     echo '</ul>';
     echo '</div>';
     echo '</div>';
     echo '</nav>';
-    printf(
-        '<div class="content%s">',
-        (
+    echo '<div class="content'
+        . (
             $this->isHomepage ?
             ' dashboard' :
             ''
         )
-    );
+        . '">';
     self::$HookManager
         ->processEvent(
             'CONTENT_DISPLAY',
@@ -122,79 +121,62 @@ if (self::$FOGUser->isValid()) {
     echo '<div class="container-fluid">';
     echo '<div class="card">';
     echo '<div class="row text-center">';
-    printf(
-        '<h4 class="title">%s</h4>',
-        $this->sectionTitle
-    );
+    echo '<h4 class="title">'
+        . $this->sectionTitle
+        . '</h4>';
     echo '</div>';
-    echo '<div class="row text-center">';
-    if (self::$FOGUser->isValid() && $this->pageTitle) {
-        printf(
-            '<h5 class="title">%s</h5>',
-            $this->pageTitle
-        );
+    if (self::$FOGUser->isValid && $this->pageTitle) {
+        echo '<div class="row text-center">';
+        echo '<h5 class="title">'
+            . $this->pageTitle
+            . '</h5>';
+        echo '</div>';
     }
-    echo '</div>';
     echo $this->body;
     echo '</div>';
     echo '</div>';
     echo '</div>';
     echo '<footer class="footer">';
     echo '<div class="container-fluid">';
-    //echo '<nav class="pull-left navbar navbar-default">';
     echo '<nav class="pull-left">';
     echo '<ul>';
-    echo '<li>';
-    printf(
-        '<a href="http://fogproject.org/wiki/index.php?title=Credits">%s</a>',
-        _('Credits')
-    );
-    echo '</li>';
-    echo '<li>';
-    printf(
-        '<a href="?node=client">%s</a>',
-        _('FOG Client')
-    );
-    echo '</li>';
-    echo '<li>';
-    printf(
-        '<a href="https://www.paypal.com/cgi-bin/webscr?'
-        . 'item_name=Donation+to+FOG+-+A+Free+Cloning+Solution&cmd=_donations'
-        . '&business=fogproject.org@gmail.com">%s</a>',
-        _('Donate to FOG')
-    );
-    echo '<li>';
+    echo '<li><a href="https://wiki.fogproject.org/wiki/index.php?title=Credits">'
+        . _('Credits')
+        . '</a></li>';
+    echo '<li><a href="?node=client">'
+        . _('FOG Client')
+        . '</a></li>';
+    echo '<li><a href="https://www.paypal.com/cgi-bin/webscr?item_name=Donation'
+        . '+to+FOG+-+A+Free+Cloning+Solution&cmd=_donations&business=fogproject.org'
+        . '@gmail.com">'
+        . _('Donate to FOG')
+        . '</a></li>';
     echo '</ul>';
     echo '</nav>';
     echo '</div>';
     echo '</footer>';
 } else {
-    echo '<div class="wrapper signin">';
     echo $this->body;
     echo '<footer class="footer">';
     echo '<div class="container-fluid">';
     echo '<nav class="navbar navbar-default navbar-fixed-bottom">';
     echo '<div class="container text-center">';
     echo '<div class="col-xs-4">';
-    printf(
-        '<a href="http://fogproject.org/wiki/index.php?title=Credits">%s</a>',
-        _('Credits')
-    );
+    echo '<a href="https://wiki.fogproject.org/wiki/index.php?title=Credits">'
+        . _('Credits')
+        . '</a>';
     echo '</div>';
     echo '<div class="col-xs-4">';
-    printf(
-        '<a href="?node=client">%s</a>',
-        _('FOG Client')
-    );
+    echo '<a href="?node=client">'
+        . _('FOG Client')
+        . '</a>';
     echo '</div>';
     echo '<div class="col-xs-4">';
-    printf(
-        '<a href="https://www.paypal.com/cgi-bin/webscr?'
-        . 'item_name=Donation+to+FOG+-+A+Free+Cloning+Solution&cmd=_donations'
-        . '&business=fogproject.org@gmail.com">%s</a>',
-        _('Donate to FOG')
-    );
-    echo '</div>';
+    echo '<a href="https://www.paypal.com/cgi-bin/webscr?item_name=Donation'
+        . '+to+FOG+-+A+Free+Cloning+Solution&cmd=_donations&business=fogproject.org'
+        . '@gmail.com">'
+        . _('Donate to FOG')
+        . '</a>';
     echo '</div>';
     echo '</div>';
     echo '</nav>';
@@ -203,11 +185,11 @@ if (self::$FOGUser->isValid()) {
 }
 echo '</div>';
 foreach ((array)$this->javascripts as &$javascript) {
-    printf(
-        '<script src="%s?ver=%d" type="text/javascript"></script>',
-        $javascript,
-        FOG_BCACHE_VER
-    );
+    echo '<script src="'
+        . $javascript
+        . '?ver='
+        . FOG_BCACHE_VER
+        . '" type="text/javascript"></script>';
     unset($javascript);
 }
 unset($this->javascripts);
