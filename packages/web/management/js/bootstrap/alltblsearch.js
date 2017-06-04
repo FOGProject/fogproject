2da5fc4497fe12b11ae84f6e768ce0422add7fe6
Tom Elliott <tommygunsster@gmail.com>
2017-06-04 12:36:10 -0400

    Fix up table stuff a bit

--- packages/web/management/js/bootstrap/alltblsearch.js
+++ packages/web/management/js/bootstrap/alltblsearch.js
@@ -1,19 +1,22 @@
 $(function() {
     var activeSystemClass = $('.list-group-item.active');
-    $('#system-search').keyup( function() {
+    $('.system-search').keyup( function() {
         var that = this;
-        var tableBody = $('.table-list-search:first tbody');
-        var tableRowsClass = $('.table-list-search:first tbody tr');
+        var div = $(that).parents('.row');
+        var tableBody = $('.table-list-search tbody', div);
+        var tableRowsClass = $('.table-list-search tbody tr', div);
         $('.search-sf').remove();
         tableRowsClass.each( function(i, val) {
             var rowText = $(val).text().toLowerCase();
             var inputText = $(that).val().toLowerCase();
             if (inputText != '') {
                 $('.search-query-sf').remove();
-                tableBody.prepend('<tr class="search-query-sf"><td colspan="6">'
-                        + '<strong>Searching for: "'
-                        + $(that).val()
-                        + '"</strong></td></tr>');
+                tableBody.prepend(
+                    '<tr class="search-query-sf"><td colspan="6">'
+                    + '<strong>Searching for: "'
+                    + $(that).val()
+                    + '"</strong></td></tr>'
+                );
             } else {
                 $('.search-query-sf').remove();
             }
@@ -26,9 +29,9 @@ $(function() {
         });
         if(tableRowsClass.children(':visible').length == 0) {
             tableBody.append(
-                    '<tr class="search-sf"><td class="text-muted" colspan="6">'
-                    + 'No entries found.</td></tr>'
-                    );
+                '<tr class="search-sf"><td class="text-muted" colspan="6">'
+                + 'No entries found.</td></tr>'
+            );
         }
     });
 });
