94932953267b100596f23c22d635f000cfd7c77f
Tom Elliott <tommygunsster@gmail.com>
2017-06-18 21:36:57 -0400

    Minor fixes missed center on user api and make host js work/look the same when adding a new mac

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1122');
+        define('FOG_VERSION', '3456');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 120);
         define('FOG_SVN_REVISION', 6076);
