e8397ffeab73cc922ec3ecfba28e91a78d30652a
Tom Elliott <tommygunsster@gmail.com>
2017-06-25 12:56:00 -0400

    Update import elements a little bit to be more nicely displayed

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '578918');
+        define('FOG_VERSION', '12785');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 120);
         define('FOG_SVN_REVISION', 6076);
