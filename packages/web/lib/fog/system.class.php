a89aa52a24ed6368aab13232a889db59cac5f3fe
Tom Elliott <tommygunsster@gmail.com>
2017-07-03 10:15:58 -0400

    Make group snapins bootstrap friendly

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '3281');
+        define('FOG_VERSION', '8945074');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 120);
         define('FOG_SVN_REVISION', 6079);
