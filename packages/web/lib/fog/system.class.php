a632dfc400f7864bc68f1e95adc7c1ab8db68e8a
Tom Elliott <tommygunsster@gmail.com>
2017-07-07 13:54:06 -0400

    More work on access control bootstrapping

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '41207');
+        define('FOG_VERSION', '2626');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 120);
         define('FOG_SVN_REVISION', 6079);
