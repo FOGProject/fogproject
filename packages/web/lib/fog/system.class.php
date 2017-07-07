c287f3d58b707408dcf3cfdc44a984cd845b628e
Tom Elliott <tommygunsster@gmail.com>
2017-07-07 14:58:29 -0400

    Tweak login page a bit to make it look slightly neater

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '4888');
+        define('FOG_VERSION', '81787');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 120);
         define('FOG_SVN_REVISION', 6079);
--- packages/web/lib/pages/processlogin.class.php
+++ packages/web/lib/pages/processlogin.class.php
@@ -130,8 +130,9 @@ class ProcessLogin extends FOGPage
      */
     private function _specLang()
     {
-        if (isset($_REQUEST['ulang'])) {
-            self::$locale = $_REQUEST['ulang'];
+        $ulang = filter_input(INPUT_POST, 'ulang');
+        if (isset($ulang)) {
+            self::$locale = $ulang;
         } else {
             self::$locale = $this->_transLang();
         }
@@ -219,7 +220,7 @@ class ProcessLogin extends FOGPage
             unset($value);
         }
         if (count($http_query) < 1) {
-            unset($_REQUEST['login']);
+            unset($redirect['login']);
             self::redirect('index.php');
         }
         $query = trim(http_build_query($http_query));
@@ -238,15 +239,17 @@ class ProcessLogin extends FOGPage
     {
         global $currentUser;
         $this->setLang();
-        $this->_username = trim($_REQUEST['uname']);
-        $this->_password = trim($_REQUEST['upass']);
+        $uname = filter_input(INPUT_POST, 'uname');
+        $upass = filter_input(INPUT_POST, 'upass');
+        $this->_username = $uname;
+        $this->_password = $upass;
         $type = self::$FOGUser->get('type');
         self::$HookManager
             ->processEvent(
                 'USER_TYPE_HOOK',
                 array('type' => &$type)
             );
-        if (!isset($_REQUEST['login'])) {
+        if (!isset($_POST['login'])) {
             return;
         }
         if (!$this->_username) {
@@ -278,70 +281,85 @@ class ProcessLogin extends FOGPage
     public function mainLoginForm()
     {
         $this->setLang();
-        if (in_array($_REQUEST['node'], array('login', 'logout'))) {
+        global $node;
+        if (in_array($node, array('login', 'logout'))) {
             if (session_status() != PHP_SESSION_NONE) {
                 self::setMessage($_SESSION['FOG_MESSAGES']);
             }
-            unset($_REQUEST['login']);
+            unset($_GET['login']);
             self::redirect('index.php');
         }
         $this->_getLanguages();
         $logininfo = self::getSetting('FOG_LOGIN_INFO_DISPLAY');
         $extra = '';
         if ($logininfo) {
-            $extra = sprintf(
-                '<div id="login-form-info">'
-                . '<p>%s: <b><i class="icon fa fa-circle-o-notch fa-spin fa-fw">'
-                . '</i></b></p><p>%s: <b><i class="icon fa fa-circle-o-notch fa-'
-                . 'spin fa-fw"></i></b></p><p>%s: <b><i class="icon fa fa-circle-'
-                . 'o-notch fa-spin fa-fw"></i></b></p><p>%s: <b><i class="icon '
-                . 'fa fa-circle-o-notch fa-spin fa-fw"></i></b></p></div>',
-                self::$foglang['FOGSites'],
-                self::$foglang['LatestVer'],
-                self::$foglang['LatestDevVer'],
-                self::$foglang['LatestSvnVer']
-            );
+            $extra = '<div id="login-form-info">'
+                . '<p>'
+                . self::$foglang['FOGSites']
+                . ': <b>'
+                . '<i class="icon fa fa-circle-o-notch fa-spin fa-fw"></i>'
+                . '</b>'
+                . '</p>'
+                . '<p>'
+                . self::$foglang['LatestVer']
+                . ': <b>'
+                . '<i class="icon fa fa-circle-o-notch fa-spin fa-fw"></i>'
+                . '</b>'
+                . '</p>'
+                . '<p>'
+                . self::$foglang['LatestDevVer']
+                . ': <b>'
+                . '<i class="icon fa fa-circle-o-notch fa-spin fa-fw"></i>'
+                . '</b>'
+                . '</p>'
+                . '<p>'
+                . self::$foglang['LatestSvnVer']
+                . ': <b>'
+                . '<i class="icon fa fa-circle-o-notch fa-spin fa-fw"></i>'
+                . '</b>'
+                . '</p>'
+                . '</div>';
         }
         // Login form
-        echo '<div class="form-group form-signin">';
-        echo '<form class="form-horizontal" action="';
+        echo '<div class="form-signin">';
+        echo '<form class="form-horizontal" method="post" action="';
         echo $this->formAction;
-        echo '" method="post">';
-        echo '<h2 class="form-signin-heading">';
-        printf(
-            '<img src="%s" alt="%s" class="logoimg"/>',
-            '../favicon.ico',
-            self::$foglang['Slogan']
-        );
-        echo '&nbsp;&nbsp;';
+        echo '">';
+        echo '<h3 class="form-signin-heading text-center">';
+        echo '<span class="col-xs-1">';
+        echo '<img src="../favicon.ico" class="logoimg" alt="'
+            . self::$foglang['Slogan']
+            . '"/>';
+        echo '</span>';
         echo _('FOG Project');
-        echo '</h2>';
+        echo '</h3>';
+        echo '<hr/>';
         // Username
         echo '<div class="form-group">';
-        echo '<label class="control-label col-xs-2" for="uname">';
+        echo '<label class="control-label col-md-2" for="uname">';
         echo self::$foglang['Username'];
         echo '</label>';
-        echo '<div class="col-xs-10">';
+        echo '<div class="col-md-10">';
         echo '<input type="text" class="form-control" name="uname" '
             . 'required="" autofocus="" id="uname"/>';
         echo '</div>';
         echo '</div>';
         // Password
         echo '<div class="form-group">';
-        echo '<label class="control-label col-xs-2" for="upass">';
+        echo '<label class="control-label col-md-2" for="upass">';
         echo self::$foglang['Password'];
         echo '</label>';
-        echo '<div class="col-xs-10">';
+        echo '<div class="col-md-10">';
         echo '<input type="password" class="form-control" name="upass" '
             . 'required="" id="upass"/>';
         echo '</div>';
         echo '</div>';
         // Language
         echo '<div class="form-group">';
-        echo '<label class="control-label col-xs-2" for="ulang">';
+        echo '<label class="control-label col-md-2" for="ulang">';
         echo self::$foglang['LanguagePhrase'];
         echo '</label>';
-        echo '<div class="col-xs-10">';
+        echo '<div class="col-md-10">';
         echo '<select class="form-control" name="ulang" id="ulang">';
         echo $this->_langMenu;
         echo '</select>';
@@ -349,7 +367,7 @@ class ProcessLogin extends FOGPage
         echo '</div>';
         // Submit button
         echo '<div class="form-group">';
-        echo '<div class="col-xs-offset-2 col-xs-10">';
+        echo '<div class="col-md-offset-2 col-md-10">';
         echo '<button class="btn btn-default btn-block" '
             . 'type="submit" name="login">';
         echo self::$foglang['Login'];
@@ -357,6 +375,7 @@ class ProcessLogin extends FOGPage
         echo '</div>';
         echo '</div>';
         echo '</form>';
+        echo '<hr/>';
         // Login information
         echo '<div class="row">';
         echo '<div class="form-group">';
