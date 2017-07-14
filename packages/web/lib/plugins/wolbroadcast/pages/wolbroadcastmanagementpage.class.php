b28a1cc8957b157fd07f75da35b823d9696a3754
Tom Elliott <tommygunsster@gmail.com>
2017-07-14 10:09:59 -0400

    Fix wolbroadcast plugin to be bootstrap and ajax friendly

--- packages/web/lib/plugins/wolbroadcast/pages/wolbroadcastmanagementpage.class.php
+++ packages/web/lib/plugins/wolbroadcast/pages/wolbroadcastmanagementpage.class.php
@@ -43,7 +43,7 @@ class WOLBroadcastManagementPage extends FOGPage
         global $id;
         if ($id) {
             $this->subMenu = array(
-                $this->linkformat => self::$foglang['General'],
+                "$this->linkformat#wol-general" => self::$foglang['General'],
                 $this->delformat => self::$foglang['Delete'],
             );
             $this->notes = array(
@@ -54,16 +54,18 @@ class WOLBroadcastManagementPage extends FOGPage
         $this->headerData = array(
             '<input type="checkbox" name="toggle-checkbox" class='
             . '"toggle-checkboxAction" checked/>',
-            'Broadcast Name',
-            'Broadcast IP',
+            _('Broadcast Name'),
+            _('Broadcast IP')
         );
         $this->templates = array(
-            '<input type="checkbox" name="wolbroadcast[]" value='
-            . '"${id}" class="toggle-action" checked/>',
+            '<label for="toggler">'
+            . '<input type="checkbox" name="wolbroadcast[]" value='
+            . '"${id}" class="toggle-action" checked/>'
+            . '</label>',
             '<a href="?node=wolbroadcast&sub=edit&id=${id}" title="'
             . _('Edit')
             . '">${name}</a>',
-            '${wol_ip}',
+            '${wol_ip}'
         );
         $this->attributes = array(
             array(
@@ -71,7 +73,7 @@ class WOLBroadcastManagementPage extends FOGPage
                 'width' => '16'
             ),
             array(),
-            array(),
+            array()
         );
         /**
          * Lambda function to return data either by list or search.
@@ -96,6 +98,12 @@ class WOLBroadcastManagementPage extends FOGPage
      */
     public function add()
     {
+        $name = trim(
+            filter_input(INPUT_POST, 'name')
+        );
+        $broadcast = trim(
+            filter_input(INPUT_POST, 'broadcast')
+        );
         $this->title = _('New Broadcast Address');
         unset($this->headerData);
         $this->attributes = array(
@@ -107,23 +115,29 @@ class WOLBroadcastManagementPage extends FOGPage
             '${input}',
         );
         $fields = array(
-            _('Broadcast Name') => '<input class="smaller" type='
-            . '"text" name="name"/>',
-            _('Broadcast IP') => '<input class="smaller" type='
-            . '"text" name="broadcast"/>',
-            '' => sprintf(
-                '<input class="smaller" type="submit" value="%s" name="add"/>',
-                _('Add')
-            ),
+            '<label for="name">'
+            . _('Broadcast Name')
+            . '</label>' => '<div class="input-group">'
+            . '<input class="form-control wolinput-name" type='
+            . '"text" name="name" id="name" required value="'
+            . $name
+            . '"/>'
+            . '</div>',
+            '<label for="broadcast">'
+            . _('Broadcast IP')
+            . '</label>' => '<div class="input-group">'
+            . '<input class="form-control wolinput-ip" type='
+            . '"text" name="broadcast" id="broadcast" required value="'
+            . $broadcast
+            . '"/>',
+            '<label for="add">'
+            . _('Create WOL Broadcast?')
+            . '</label>' => '<button class="btn btn-info btn-block" name="'
+            . 'add" id="add" type="submit">'
+            . _('Create')
+            . '</button>'
         );
-        foreach ((array)$fields as $field => $input) {
-            $this->data[] = array(
-                'field' => $field,
-                'input' => $input,
-            );
-            unset($input);
-        }
-        unset($fields);
+        array_walk($fields, $this->fieldsToData);
         self::$HookManager
             ->processEvent(
                 'BROADCAST_ADD',
@@ -134,12 +148,23 @@ class WOLBroadcastManagementPage extends FOGPage
                     'attributes' => &$this->attributes
                 )
             );
-        printf(
-            '<form method="post" action="%s">',
-            $this->formAction
-        );
-        $this->render();
+        unset($fields);
+        echo '<div class="col-xs-9">';
+        echo '<div class="panel panel-info">';
+        echo '<div class="panel-heading text-center">';
+        echo '<h4 class="title">';
+        echo $this->title;
+        echo '</h4>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<form class="form-horizontal" method="post" action="'
+            . $this->formAction
+            . '">';
+        $this->render(12);
         echo '</form>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
     }
     /**
      * Actually create the items.
@@ -148,62 +173,84 @@ class WOLBroadcastManagementPage extends FOGPage
      */
     public function addPost()
     {
+        self::$HookManager->processEvent('BROADCAST_ADD_POST');
+        $name = trim(
+            filter_input(INPUT_POST, 'name')
+        );
+        $broadcast = trim(
+            filter_input(INPUT_POST, 'broadcast')
+        );
         try {
-            $name = $_REQUEST['name'];
-            $ip = $_REQUEST['broadcast'];
-            if (self::getClass('WolbroadcastManager')->exists($name)) {
-                throw new Exception(
-                    _('Broacast name already Exists, please try again.')
-                );
-            }
             if (!$name) {
                 throw new Exception(
-                    _('Please enter a name for this address.')
+                    _('A name is required!')
                 );
             }
-            if (empty($ip)) {
+            if (self::getClass('WolbroadcastManager')->exists($name)) {
                 throw new Exception(
-                    _('Please enter the broadcast address.')
+                    _('A broadcast already exists with this name!')
                 );
             }
-            if (strlen($ip) > 15
-                || !filter_var($ip, FILTER_VALIDATE_IP)
-            ) {
+            if (!$broadcast) {
+                throw new Exception(
+                    _('A broadcast address is required')
+                );
+            }
+            if (!filter_var($broadcast, FILTER_VALIDATE_IP)) {
                 throw new Exception(
                     _('Please enter a valid ip')
                 );
             }
             $WOLBroadcast = self::getClass('Wolbroadcast')
                 ->set('name', $name)
-                ->set('broadcast', $ip);
+                ->set('broadcast', $broadcast);
             if (!$WOLBroadcast->save()) {
-                throw new Exception(_('Failed to create'));
+                throw new Exception(_('Add broadcast failed!'));
             }
-            self::setMessage(_('Broadcast Added, editing!'));
-            self::redirect(
-                sprintf(
-                    '?node=wolbroadcast&sub=edit&id=%s',
-                    $WOLBroadcast->get('id')
+            $hook = 'BROADCAST_ADD_SUCCESS';
+            $msg = json_encode(
+                array(
+                    'msg' => _('Broadcast added!'),
+                    'title' => _('Broadcast Create Success')
                 )
             );
         } catch (Exception $e) {
-            self::setMessage($e->getMessage());
-            self::redirect($this->formAction);
+            $hook = 'BROADCAST_ADD_FAIL';
+            $msg = json_encode(
+                array(
+                    'msg' => $e->getMessage(),
+                    'title' => _('Broadcast Create Fail')
+                )
+            );
         }
+        self::$HookManager
+            ->processEvent(
+                $hook,
+                array('WOLBroadcast' => &$WOLBroadcast)
+            );
+        unset($WOLBroadcast);
+        echo $msg;
+        exit;
     }
     /**
-     * Edit the current item.
+     * WOL General tab.
      *
      * @return void
      */
-    public function edit()
+    public function wolGeneral()
     {
-        $this->title = sprintf(
-            '%s: %s',
-            _('Edit'),
-            $this->obj->get('name')
+        unset(
+            $this->form,
+            $this->data,
+            $this->headerData,
+            $this->attributes,
+            $this->templates
         );
-        unset($this->headerData);
+        $name = filter_input(INPUT_POST, 'name') ?:
+            $this->obj->get('name');
+        $broadcast = filter_input(INPUT_POST, 'broadcast') ?:
+            $this->obj->get('broadcast');
+        $this->title = _('WOL Broadcast General');
         $this->attributes = array(
             array('class' => 'col-xs-4'),
             array('class' => 'col-xs-8 form-group'),
@@ -213,43 +260,102 @@ class WOLBroadcastManagementPage extends FOGPage
             '${input}',
         );
         $fields = array(
-            _('Broadcast Name') => sprintf(
-                '<input class="smaller" type="text" name="name" value="%s"/>',
-                $this->obj->get('name')
-            ),
-            _('Broadcast Address') => sprintf(
-                '<input class="smaller" type="text" name="broadcast" value="%s"/>',
-                $this->obj->get('broadcast')
-            ),
-            '&nbsp;' => sprintf(
-                '<input class="smaller" type="submit" value="%s" name="update"/>',
-                _('Update')
-            ),
+            '<label for="name">'
+            . _('Broadcast Name')
+            . '</label>' => '<div class="input-group">'
+            . '<input class="form-control wolinput-name" type='
+            . '"text" name="name" id="name" required value="'
+            . $name
+            . '"/>'
+            . '</div>',
+            '<label for="broadcast">'
+            . _('Broadcast IP')
+            . '</label>' => '<div class="input-group">'
+            . '<input class="form-control wolinput-ip" type='
+            . '"text" name="broadcast" id="broadcast" required value="'
+            . $broadcast
+            . '"/>',
+            '<label for="updategen">'
+            . _('Make Changes?')
+            . '</label>' => '<button class="btn btn-info btn-block" name="'
+            . 'updategen" id="updategen" type="submit">'
+            . _('Create')
+            . '</button>'
         );
-        foreach ((array)$fields as $field => $input) {
-            $this->data[] = array(
-                'field' => $field,
-                'input' => $input,
-            );
-            unset($input);
-        }
-        unset($fields);
+        array_walk($fields, $this->fieldsToData);
         self::$HookManager
             ->processEvent(
                 'BROADCAST_EDIT',
                 array(
-                    'headerData' => &$this->headerData,
                     'data' => &$this->data,
-                    'templates' => &$this->templates,
-                    'attributes' => &$this->attributes
+                    'headerData' => &$this->headerData,
+                    'attributes' => &$this->attributes,
+                    'templates' => &$this->templates
                 )
             );
-        printf(
-            '<form method="post" action="%s">',
-            $this->formAction
-        );
-        $this->render();
+        unset($fields);
+        echo '<!-- General -->';
+        echo '<div class="tab-pane fade in active" id="wol-general">';
+        echo '<div class="panel panel-info">';
+        echo '<div class="panel-heading text-center">';
+        echo '<h4 class="title">';
+        echo $this->title;
+        echo '</h4>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<form class="form-horizontal" method="post" action="'
+            . $this->formAction
+            . '&tab=wol-general">';
+        $this->render(12);
         echo '</form>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        unset(
+            $this->form,
+            $this->data,
+            $this->headerData,
+            $this->attributes,
+            $this->templates
+        );
+    }
+    /**
+     * Edit the current item.
+     *
+     * @return void
+     */
+    public function edit()
+    {
+        echo '<div class="col-xs-9 tab-content">';
+        $this->wolGeneral();
+        echo '</div>';
+    }
+    /**
+     * WOL General Post()
+     *
+     * @return void
+     */
+    public function wolGeneralPost()
+    {
+        $name = trim(
+            filter_input(INPUT_POST, 'name')
+        );
+        $broadcast = trim(
+            filter_input(INPUT_POST, 'broadcast')
+        );
+        if ($this->obj->get('name') != $name
+            && self::getClass('WOLBroadcastManager')->exists(
+                $name,
+                $this->obj->get('id')
+            )
+        ) {
+            throw new Exception(
+                _('A broadcast already exists with this name')
+            );
+        }
+        $this->obj
+            ->set('name', $name)
+            ->set('broadcast', $broadcast);
     }
     /**
      * Submit the edits.
@@ -263,39 +369,38 @@ class WOLBroadcastManagementPage extends FOGPage
                 'BROADCAST_EDIT_POST',
                 array('Broadcast'=> &$this->obj)
             );
-        try {
-            $name = $_REQUEST['name'];
-            $ip = $_REQUEST['broadcast'];
-            if (!$name) {
-                throw new Exception(
-                    _('You need to have a name for the broadcast address.')
-                );
+        global $tab;
+        try{
+            switch ($tab) {
+            case 'wol-general':
+                $this->wolGeneralPost();
+                break;
             }
-            if (!$ip
-                || !filter_var($ip, FILTER_VALIDATE_IP)
-            ) {
-                throw new Exception(
-                    _('Please enter a valid IP address')
-                );
-            }
-            if ($_REQUEST['name'] != $this->obj->get('name')
-                && $this->obj->getManager()->exists($_REQUEST['name'])
-            ) {
-                throw new Exception(
-                    _('A broadcast with that name already exists.')
-                );
-            }
-            $this->obj
-                ->set('broadcast', $ip)
-                ->set('name', $name);
             if (!$this->obj->save()) {
-                throw new Exception(_('Failed to update'));
+                throw new Exception(_('Broadcast update failed!'));
             }
-            self::setMessage(_('Broadcast Updated'));
-            self::redirect($this->formAction);
+            $hook = 'BROADCAST_UPDATE_SUCCESS';
+            $msg = json_encode(
+                array(
+                    'msg' => _('Broadcast updated!'),
+                    'title' => _('Broadcast Update Success')
+                )
+            );
         } catch (Exception $e) {
-            self::setMessage($e->getMessage());
-            self::redirect($this->formAction);
+            $hook = 'BROADCAST_UPDATE_FAIL';
+            $msg = json_encode(
+                array(
+                    'error' => $e->getMessage(),
+                    'title' => _('Broadcast Update Fail')
+                )
+            );
         }
+        self::$HookManager
+            ->processEvent(
+                $hook,
+                array('WOLBroadcast' => &$this->obj)
+            );
+        echo $msg;
+        exit;
     }
 }
