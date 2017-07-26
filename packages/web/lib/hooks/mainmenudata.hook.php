614c2e31113c150d6af6e1806ff5a051035e9f97
Tom Elliott <tommygunsster@gmail.com>
2017-07-26 11:19:53 -0400

    Update messages.po with new mainmenudata example

--- packages/web/lib/hooks/mainmenudata.hook.php
+++ packages/web/lib/hooks/mainmenudata.hook.php
@@ -2,9 +2,20 @@
 /**
  * Main menu hook changer.
  *
+ * PHP version 5
+ *
  * @category MainMenuData
  * @package  FOGProject
- * @author   Sebastian Roth
+ * @author   Sebastian Roth <nah@nah.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Main menu hook changer.
+ *
+ * @category MainMenuData
+ * @package  FOGProject
+ * @author   Sebastian Roth <nah@nah.com>
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
@@ -72,14 +83,14 @@ class MainMenuData extends Hook
      */
     public function addToMainMenu($arguments)
     {
-	$link = strtolower($this->menuitem);
+        $link = strtolower($this->menuitem);
         self::arrayInsertAfter(
             $this->insertAfter,
             $arguments['main'],
             $link,
-	    array(
+            array(
                 _($this->menuitem),
-               'fa ' . $this->icon . ' fa-2x'
+                'fa ' . $this->icon . ' fa-2x'
             )
         );
     }
