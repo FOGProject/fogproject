b697d852d86ed64312ffde6d8a9a254f114aad7f
Tom Elliott <tommygunsster@gmail.com>
2017-08-09 13:09:20 -0400

    Fix issue with searching not working if table has no information

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,10 +53,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.0-RC-7');
+        define('FOG_VERSION', '10');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 127);
-        define('FOG_SVN_REVISION', 6080);
+        define('FOG_SVN_REVISION', 6079);
         define('FOG_CLIENT_VERSION', '0.11.12');
     }
 }
