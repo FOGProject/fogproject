b4bc94973289f04b69e0086d78f4c8b8222cc811
Tom Elliott <tommygunsster@gmail.com>
2017-09-01 18:20:58 -0400

    Fix issues #177 and #178

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,9 +53,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '18');
+        define('FOG_VERSION', '19');
         define('FOG_SCHEMA', 262);
-        define('FOG_BCACHE_VER', 127);
+        define('FOG_BCACHE_VER', 128);
         define('FOG_SVN_REVISION', 6079);
         define('FOG_CLIENT_VERSION', '0.11.12');
     }
