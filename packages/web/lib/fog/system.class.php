c11828efec9916803ea5892279a9428fa5512681
Tom Elliott <tommygunsster@gmail.com>
2017-09-01 18:00:45 -0400

    Fix issue #176

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '16');
+        define('FOG_VERSION', '17');
         define('FOG_SCHEMA', 262);
         define('FOG_BCACHE_VER', 127);
         define('FOG_SVN_REVISION', 6079);
