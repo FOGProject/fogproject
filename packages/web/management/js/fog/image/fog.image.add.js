0b810a2a3274fec0203346ff879f55513e5e33c1
Tom Elliott <tommygunsster@gmail.com>
2018-02-15 18:22:12 -0500

    Make the add functionality operational for image ajax

--- packages/web/management/js/fog/image/fog.image.add.js
+++ packages/web/management/js/fog/image/fog.image.add.js
@@ -1,3 +1,14 @@
 (function($) {
+    var createForm = $('#image-create-form'),
+        createFormBtn = $('#send');
+    createForm.on('submit', function(e) {
+        e.preventDefault();
+    });
+    createFormBtn.on('click', function() {
+        createFormBtn.prop('disabled', true);
+        Common.processForm(createForm, function(err) {
+            createFormBtn.prop('disabled', false);
+        });
+    });
     $('.slider').slider();
 })(jQuery);
