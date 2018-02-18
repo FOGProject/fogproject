027d39fd853c6e726b3645e713ab56b42d0c12d7
Tom Elliott <tommygunsster@gmail.com>
2018-02-18 18:34:04 -0500

    Fix up the image javascript FIles

--- packages/web/management/js/fog/image/fog.image.list.js
+++ packages/web/management/js/fog/image/fog.image.list.js
@@ -1,12 +1,10 @@
 (function($) {
-    var deleteSelected = $('#deleteSelected');
-    var deleteModal = $('#deleteModal');
-    var passwordField = $('#deletePassword');
-    var confirmDelete = $('#confirmDeleteModal');
-    var cancelDelete = $('#closeDeleteModal');
-
-    var numImageString = confirmDelete.val();
-
+    var deleteSelected = $('#deleteSelected'),
+        deleteModal = $('#deleteModal'),
+        passwordField = $('#deletePassword'),
+        confirmDelete = $('#confirmDeleteModal'),
+        cancelDelete = $('#closeDeleteModal'),
+        numImageString = confirmDelete.val();
     function disableButtons(disable) {
         deleteSelected.prop('disabled', disable);
     }
@@ -14,7 +12,6 @@
         var disabled = selected.count() == 0;
         disableButtons(disabled);
     }
-
     disableButtons(true);
     var table = Common.registerTable($('#dataTable'), onSelect, {
         order: [
@@ -68,15 +65,13 @@
         serverSide: true,
         ajax: {
             url: '../management/index.php?node='+Common.node+'&sub=list',
-            type: 'POST'
+            type: 'post'
         }
     });
-
     if (Common.search && Common.search.length > 0) {
         table.search(Common.search).draw();
     }
-
-    deleteSelected.click(function() {
+    deleteSelected.on('click', function() {
         disableButtons(true);
         confirmDelete.val(numImageString.format(''));
         Common.massDelete(null, function(err) {
