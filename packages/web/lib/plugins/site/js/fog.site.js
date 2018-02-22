312c0b5192f68c07303c36ac5730472a83c361c4
Tom Elliott <tommygunsster@gmail.com>
2018-02-22 15:30:31 -0500

    Get site plugin somewhat bootstrap and ajax friendly

--- packages/web/lib/plugins/site/js/fog.site.js
+++ packages/web/lib/plugins/site/js/fog.site.js
@@ -1,27 +1,13 @@
 $(function() {
     checkboxToggleSearchListPages();
-    $('.action-boxes.del').submit(function() {
-        var checked = $('input.toggle-action:checked');
-        var accesscontrolIDArray = new Array();
-        for (var i = 0,len = checked.size();i < len;i++) {
-            accesscontrolIDArray[accesscontrolIDArray.length] = checked.eq(i).attr('value');
+    validatorOpts = {
+        submitHandler: submithandlerfunc,
+        rules: {
+            name: {
+                required: true
+            }
         }
-        $('input[name="accesscontrolIDArray"]').val(accesscontrolIDArray.join(','));
-    });
-    $('.action-boxes.host').submit(function() {
-        var checked = $('input.toggle-action:checked');
-        var accesscontrolruleIDArray = new Array();
-        for (var i = 0,len = checked.size();i < len;i++) {
-            accesscontrolruleIDArray[accesscontrolruleIDArray.length] = checked.eq(i).attr('value');
-        }
-        $('input[name="accesscontrolruleIDArray"]').val(accesscontrolruleIDArray.join(','));
-    });
-    $('#hostMeShow:checkbox').change(function(e) {
-        if ($(this).is(':checked')) $('#hostNotInMe').show();
-        else $('#hostNotInMe').hide();
-        e.preventDefault();
-    });
-    $('#hostMeShow:checkbox').trigger('change');
-    checkboxAssociations('.toggle-checkboxuser:checkbox','.toggle-user:checkbox');
-    checkboxAssociations('.toggle-checkboxhost:checkbox','.toggle-host:checkbox');
+    };
+    if (sub == 'membership') return;
+    setupTimeoutElement('#add, #updategen', '.sitename-input', 1000);
 });
