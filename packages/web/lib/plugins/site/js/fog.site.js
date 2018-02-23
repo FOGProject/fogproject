00be0b966c60a2141c08f9539f24cb0e1c74f9c5
Tom Elliott <tommygunsster@gmail.com>
2018-02-23 08:55:06 -0500

    Finish the site plugin conversion to ajax/bootstrap.

--- packages/web/lib/plugins/site/js/fog.site.js
+++ packages/web/lib/plugins/site/js/fog.site.js
@@ -10,4 +10,28 @@ $(function() {
     };
     if (sub == 'membership') return;
     setupTimeoutElement('#add, #updategen', '.sitename-input', 1000);
+    $('.action-boxes.del').submit(function() {
+        var checked = $('input.toggle-action:checked');
+        var accesscontrolIDArray = new Array();
+        for (var i = 0,len = checked.size();i < len;i++) {
+            accesscontrolIDArray[accesscontrolIDArray.length] = checked.eq(i).attr('value');
+        }
+        $('input[name="accesscontrolIDArray"]').val(accesscontrolIDArray.join(','));
+    });
+    $('.action-boxes.host').submit(function() {
+        var checked = $('input.toggle-action:checked');
+        var accesscontrolruleIDArray = new Array();
+        for (var i = 0,len = checked.size();i < len;i++) {
+            accesscontrolruleIDArray[accesscontrolruleIDArray.length] = checked.eq(i).attr('value');
+        }
+        $('input[name="accesscontrolruleIDArray"]').val(accesscontrolruleIDArray.join(','));
+    });
+    $('#hostMeShow:checkbox').change(function(e) {
+        if ($(this).is(':checked')) $('#hostNotInMe').show();
+        else $('#hostNotInMe').hide();
+        e.preventDefault();
+    });
+    $('#hostMeShow:checkbox').trigger('change');
+    checkboxAssociations('.toggle-checkboxhost:checkbox','.toggle-host:checkbox');
+    checkboxAssociations('.toggle-checkboxhostrm:checkbox','.toggle-hostrm:checkbox');
 });
