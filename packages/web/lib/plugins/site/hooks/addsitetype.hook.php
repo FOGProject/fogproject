3501600cd4d3f14f17ff84b35ec94c6c116de84e
Tom Elliott <tommygunsster@gmail.com>
2018-02-24 16:36:49 -0500

    add the site report

---
+++ packages/web/lib/plugins/site/hooks/addsitetype.hook.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Adds the site report type.
+ *
+ * PHP version 5
+ *
+ * @category AddSiteType
+ * @package  FOGProject
+ * @author   Fernando Gietz <fernando.gietz@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Adds the site report type.
+ *
+ * @category AddSiteType
+ * @package  FOGProject
+ * @author   Fernando Gietz <fernando.gietz@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class AddSiteType extends Hook
+{
+    /**
+     * The name of this hook.
+     *
+     * @var string
+     */
+    public $name = 'AddSiteType';
+    /**
+     * The description of this hook.
+     *
+     * @var string
+     */
+    public $description = 'Add Report Management Type';
+    /**
+     * The active flag.
+     *
+     * @var bool
+     */
+    public $active = true;
+    /**
+     * The node this hook enacts with.
+     *
+     * @var string
+     */
+    public $node = 'site';
+    /**
+     * Initialize object.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        self::$HookManager
+            ->register(
+                'REPORT_TYPES',
+                array(
+                    $this,
+                    'reportTypes'
+                )
+            );
+    }
+}
