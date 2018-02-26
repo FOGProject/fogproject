4389a1ea00005a06df25fdaeaea85898e94d85cd
Tom Elliott <tommygunsster@gmail.com>
2018-02-25 20:16:23 -0500

    Site javascript/ajax.
    Get Site basic add/edit elements ajax friendly.

--- packages/web/lib/plugins/site/js/fog.site.edit.js
+++ packages/web/lib/plugins/site/js/fog.site.edit.js
@@ -1,8 +1,54 @@
 $(function() {
-    $('#userMeShow:checkbox').change(function(e) {
-        if ($(this).is(':checked')) $('#userNotInMe').show();
-        else $('#userNotInMe').hide();
+    // ---------------------------------------------------------------
+    // GENERAL TAB
+    var originalName = $('#site').val(),
+        updateName = function(newName) {
+            var e = $('#pageTitle'),
+                text = e.text();
+            text = text.replace(': ' + originalName, ': ' + newName);
+            document.title = text;
+            e.text(text);
+        };
+
+    var generalForm = $('#site-general-form'),
+        generalFormBtn = $('#general-send'),
+        generalDeleteBtn = $('#general-delete');
+
+    generalForm.on('submit',function(e) {
         e.preventDefault();
     });
-    $('#userMeShow:checkbox').trigger('change');
+    generalFormBtn.on('click',function() {
+        generalFormBtn.prop('disabled', true);
+        generalDeleteBtn.prop('disabled', true);
+        Common.processForm(generalForm, function(err) {
+            generalFormBtn.prop('disabled', false);
+            generalDeleteBtn.prop('disabled', false);
+            if (err) {
+                return;
+            }
+            updateName($('#site').val());
+            originalName = $('#site').val();
+        });
+    });
+    generalDeleteBtn.on('cilck', function() {
+        generalFormBtn.prop('disabled', true);
+        generalDeleteBtn.prop('disabled', true);
+        Common.massDelete(null, function(err) {
+            if (err) {
+                generalFormBtn.prop('disabled', false);
+                generalDeleteBtn.prop('disabled', false);
+                return;
+            }
+            window.location = '../management/index.php?node='
+            + Common.node
+            + '&sub=list';
+        });
+    });
+    // ---------------------------------------------------------------
+    // HOST ASSOCIATION TAB
+    // TODO: Make Functional
+
+    // ---------------------------------------------------------------
+    // USER ASSOCIATION TAB
+    // TODO: Make Functional
 });
