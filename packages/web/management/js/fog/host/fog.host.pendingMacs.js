9b94b81a952c61443b7b953fdd65e9b3c3b81a02
Tom Elliott <tommygunsster@gmail.com>
2018-03-06 01:40:27 -0500

    Fixup underscore issue with JS.
    1. fog.common.js now uses _.defaults which was causing issues. First
    issue appears to have been with the database.min.css file that was being
    used. It was around 2.5 MB so that leads me to think many files were
    concatenated within it. This, itself, isn't a big problem, but the order
    apparently matters. Rebuilt the datatables.min.js file using datatables
    download builder. This corrected this problem. However, it did not have
    the underscore js library built in for some reason.
    
    2. Downloaded the underscore js library and applied it, which fixed the
    issues with _.defaults entirely (it appears).
    
    3. Put pending macs on the host sub menu element since mac addresses are
    always associated to some sort of host device. Javascript has been
    created for handling it as well (seeing as it is very similar to pending
    hosts, but completely untested so far.)

---
+++ packages/web/management/js/fog/host/fog.host.pendingMacs.js
@@ -0,0 +1,115 @@
+(function($) {
+    // Approve
+    var approveSelected = $('#approve'),
+        approveModal = $('#approveModal'),
+        confirmApprove = $('#confirmApproveModal'),
+        cancelApprove = $('#cancelApprovalModal'),
+        // Delete
+        deleteSelected = $('#delete'),
+        deleteModal = $('#deleteModal'),
+        passwordField = $('#deletePassword'),
+        confirmDelete = $('#confirmDeleteModal'),
+        cancelDelete = $('#closeDeleteModal'),
+        // Form to work with.
+        pendingForm = $('#mac-pending-form'),
+        method = pendingForm.attr('method'),
+        action = pendingForm.attr('action');
+
+    function disableButtons (disable) {
+        approveSelected.prop('disabled', disable);
+        deleteSelected.prop('disabled', disable);
+    }
+    function onSelect (selected) {
+        var disabled = selected.count() == 0;
+        disableButtons(disabled);
+    }
+
+    disableButtons(true);
+    var table = Common.registerTable($('#dataTable'), onSelect, {
+        order: [
+            [1, 'asc']
+        ],
+        columns: [
+            {data: 'mac'},
+            {data: 'hostname'}
+        ],
+        rowId: 'id',
+        columnDefs: [
+            {
+                responsivePriority: -1,
+                render: function (data, type, row) {
+                    return '<a href="../management/index.php?node=host&sub=edit&id='
+                        + row.hostid
+                        + '">'
+                        + data
+                        + '</a>';
+                },
+                targets: 0
+            }
+            {
+                responsivePriority: 0,
+                targets: 1
+            },
+        ],
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: '../management/index.php?node='
+            + Common.node
+            + '&sub=getPendingMacList',
+            type: 'post'
+        }
+    });
+
+    if (Common.search && Common.search.length > 0) {
+        table.search(Common.search).draw();
+    }
+
+    disableButtons(true);
+    confirmDelete.on('click', function(e) {
+        e.preventDefault();
+        cancelDelete.prop('disabled', true);
+        confirmDelete.prop('disabled', true);
+        Common.massDelete(passwordField.val(), function(err) {
+            if (err) {
+                headerText = deleteModal.find('.modal-header').text();
+                deleteModal.modal('show');
+                deleteModal.find('.modal-header').text(err.responseJSON.error);
+                setTimeout(function() {
+                    deleteModal.find('.modal-header').text(headerText);
+                    passwordField.val('');
+                    confirmDelete.prop('disabled', false);
+                }, 2000);
+            } else {
+                deleteModal.modal('hide');
+                disableButtons(false);
+                table.draw(false);
+                table.rows({selected: true}).deselect();
+            }
+        }, table);
+    });
+
+    deleteSelected.on('click',function(e) {
+        e.preventDefault();
+        disableButtons(true);
+        confirmDelete.trigger('click');
+    });
+
+    approveSelected.on('click', function() {
+        disableButtons(true);
+        var rows = table.rows({selected: true}),
+            toApprove = Common.getSelectedIds(table),
+            opts = {
+                approvepending: 1,
+                pending: toApprove
+            };
+        Common.apiCall(method,action,opts,function(err) {
+            disableButtons(false);
+            if (err) {
+                return;
+            }
+            table.draw(false);
+            table.rows({selected: true}).deselect();
+        });
+    });
+})(jQuery);
