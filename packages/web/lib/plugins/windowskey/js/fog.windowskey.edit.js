1829cf3f11102ecfe44824482ba9de5df23fb9a5
Tom Elliott <tommygunsster@gmail.com>
2018-03-10 17:55:42 -0500

    Windows Key
    Get windows key mostly functional.
    
    Add the pnotify.mobile.js.map file to prevent error spamming.

---
+++ packages/web/lib/plugins/windowskey/js/fog.windowskey.edit.js
@@ -0,0 +1,57 @@
+$(function() {
+    // Any special functions that can be commonized for this element.
+    var onCheckboxSelect = function(event) {
+    };
+    // ---------------------------------------------------------------
+    // GENERAL TAB
+    var originalName = $('#windowskey').val(),
+        updateName = function(newName) {
+            var e = $('#pageTitle'),
+                text = e.text();
+            text = text.replace(': ' + originalName, ': ' + newName);
+            document.title = text;
+            e.text(text);
+        };
+
+    var generalForm = $('#windowskey-general-form'),
+        generalFormBtn = $('#general-send'),
+        generalDeleteBtn = $('#general-delete');
+
+    $('#key').inputmask({mask: Common.masks.productKey});
+    generalForm.on('submit',function(e) {
+        e.preventDefault();
+    });
+    generalFormBtn.on('click',function() {
+        generalFormBtn.prop('disabled', true);
+        generalDeleteBtn.prop('disabled', true);
+        Common.processForm(generalForm, function(err) {
+            generalFormBtn.prop('disabled', false);
+            generalDeleteBtn.prop('disabled', false);
+            if (err) {
+                return;
+            }
+            updateName($('#windowskey').val());
+            originalName = $('#windowskey').val();
+        });
+    });
+    generalDeleteBtn.on('cilck', function() {
+        generalFormBtn.prop('disabled', true);
+        generalDeleteBtn.prop('disabled', true);
+        Common.massDelete(null, function(err) {
+            if (err) {
+                generalFormBtn.prop('disabled', false);
+                generalDeleteBtn.prop('disabled', false);
+                return;
+            }
+            window.location = '../management/index.php?node='
+            + Common.node
+            + '&sub=list';
+        });
+    });
+
+    // ---------------------------------------------------------------
+    // IMAGES TAB
+
+    if (Common.search && Common.search.length > 0) {
+    }
+});
