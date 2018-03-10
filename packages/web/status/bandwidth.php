8883cb4ccf8919abf68f1b79d0df98cf7be5f85a
Tom Elliott <tommygunsster@gmail.com>
2018-03-10 14:20:15 -0500

    backport dashboard updates to 1.5.1 from 1.6.0 for better dashboard control overall.

--- packages/web/status/bandwidth.php
+++ packages/web/status/bandwidth.php
@@ -22,11 +22,7 @@
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
-require '../commons/base.inc.php';
-session_write_close();
-ignore_user_abort(true);
-set_time_limit(0);
-header('Content-Type: text/event-stream');
+header('Content-Type: application/json');
 /**
  * Lambda for returning the bytes from the file requested.
  *
@@ -39,43 +35,35 @@ header('Content-Type: text/event-stream');
  * @throws Exception
  * @return int
  */
-$getBytes = function ($dev, $file) {
+$getBytes = function ($dev) {
     if (!is_string($dev)) {
         throw new Exception(_('Device must be a string'));
     }
-    if (!is_string($file)) {
-        throw new Exception(_('File must be a string'));
-    }
-    if (!in_array($file, array('tx_bytes', 'rx_bytes'))) {
-        throw new Exception(_('Only tx and rx bytes files can be read'));
-    }
-    $path = "/sys/class/net/$dev/statistics/$file";
-    if (!(file_exists($path) && is_readable($path))) {
-        return 0;
-    } else {
-        $data = file_get_contents($path);
-        return trim($data);
-    }
+    $txpath = "/sys/class/net/$dev/statistics/tx_bytes";
+    $rxpath = "/sys/class/net/$dev/statistics/rx_bytes";
+    $tx = file_get_contents($txpath);
+    $rx = file_get_contents($rxpath);
+    return [$rx,$tx];
 };
 // Make sure a device is set
-if (!isset($_REQUEST['dev'])) {
-    $_REQUEST['dev'] = 'eth0';
+$dev = filter_input(INPUT_GET, 'dev');
+if (!$dev) {
+    $dev = 'eth0';
 }
 // Only use the last bit in case somebody is doing stuff bad
-$baseint = basename($_REQUEST['dev']);
-$dev = trim($baseint);
+$dev = trim(basename($dev));
 // Directory to check for interfaces and get all system interfaces
 $scan = scandir('/sys/class/net');
 // Filter out dots
 $dir_interfaces = array_diff(
     $scan,
-    array(
+    [
         '..',
         '.'
-    )
+    ]
 );
 // Initiate our interfaces variable
-$interfaces = array();
+$interfaces = [];
 // Loop the captured data and set up interfaces
 foreach ($dir_interfaces as &$iface) {
     $operstateFile = "/sys/class/net/$iface/operstate";
@@ -91,6 +79,10 @@ foreach ($dir_interfaces as &$iface) {
 $interface = preg_grep("#^$dev$#", $interfaces);
 // If our interface isn't found, try getting it directly off the system
 if (count($interface) < 1) {
+    include '../commons/base.inc.php';
+    session_write_close();
+    ignore_user_abort(true);
+    set_time_limit(0);
     // Find our server address
     $srvAddr = $_SERVER['SERVER_ADDR'];
     // If accessed by hostname resolve to ip
@@ -102,23 +94,26 @@ if (count($interface) < 1) {
 $dev = trim($dev);
 // If the device is not set or found return Unknown
 if (!$dev) {
-    $ret = array(
+    $ret = [
         'dev' => 'Unknown',
         'rx' => 0,
         'tx' => 0,
-    );
+    ];
     echo json_encode($ret);
     exit;
 }
 // Set our rx and tx data values
-$rx = $getBytes($dev, 'rx_bytes');
-$tx = $getBytes($dev, 'tx_bytes');
+list($rxlast,$txlast) = $getBytes($dev);
+usleep(100000);
+list($rxcur,$txcur) = $getBytes($dev);
+$rx = round(ceil((int)(($rxcur - $rxlast)) / 1024 * 8 / 100), 2);
+$tx = round(ceil((int)(($txcur - $txlast)) / 1024 * 8 / 100), 2);
 // Setup our return array
-$ret = array(
+$ret = [
     'dev' => $dev,
     'rx' => $rx,
     'tx' => $tx,
-);
+];
 // Return
 echo json_encode($ret);
 exit;
