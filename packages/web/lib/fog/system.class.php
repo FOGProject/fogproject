2528b497aca6186746573fedb0d05ed4217d1a4e
Tom Elliott <tommygunsster@gmail.com>
2018-03-12 19:45:04 -0400

    Minor tweaks to group page

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.0.240');
+        define('FOG_VERSION', '1.5.0.243');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 264);
         define('FOG_BCACHE_VER', 132);
