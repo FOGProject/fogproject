63aac659dd9c7ff82d614419444af95beb0e55c6
Tom Elliott <tommygunsster@gmail.com>
2018-03-12 17:16:49 -0400

    Get the initial template updating the attribute fields where/as appropriate

--- packages/web/lib/plugins/ldap/js/fog.ldap.add.js
+++ packages/web/lib/plugins/ldap/js/fog.ldap.add.js
@@ -1,6 +1,9 @@
 (function($) {
     var createForm = $('#ldap-create-form'),
-        createFormBtn = $('#send');
+        createFormBtn = $('#send'),
+        templateSel = $('#template'),
+        userNameAttr = $('#userNameAttr'),
+        grpMemberAttr = $('#grpMemberAttr');
     createForm.on('submit', function(e) {
         e.preventDefault();
     });
@@ -9,5 +12,29 @@
         Common.processForm(createForm, function(err) {
             createFormBtn.prop('disabled', false);
         });
+    });
+    templateSel.on('change blur focus focusout', function(e) {
+        e.preventDefault();
+        selected = this.value;
+        switch (selected) {
+            case '0':
+                usrAttr = 'samAccountName';
+                grpAttr = 'member';
+                break;
+            case '1':
+                usrAttr = 'cn';
+                grpAttr = 'member';
+                break;
+            case '2':
+                usrAttr = 'uid';
+                grpAttr = 'uniqueMember';
+                break;
+            default:
+                usrAttr = '';
+                grpAttr = '';
+                break;
+        }
+        userNameAttr.val(usrAttr);
+        grpMemberAttr.val(grpAttr);
     });
 })(jQuery);
