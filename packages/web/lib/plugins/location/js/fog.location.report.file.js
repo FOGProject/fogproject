ef35d66d22362cefdeead7fe3a5b54355353df83
Tom Elliott <tommygunsster@gmail.com>
2018-03-14 13:01:27 -0400

    Update report js files for location and core

---
+++ packages/web/lib/plugins/location/js/fog.location.report.file.js
@@ -0,0 +1,12 @@
+(function($) {
+
+    reportString = window.atob(Common.f);
+
+    // This will call our respective calls
+    // to report the requested data.
+    switch (reportString) {
+        // Equipment Loan
+        case 'location report':
+            break;
+    }
+})(jQuery);
---
+++ packages/web/management/js/fog/report/fog.report.file.js
@@ -0,0 +1,39 @@
+(function($) {
+
+    reportString = window.atob(Common.f);
+
+    // This will call our respective calls
+    // to report the requested data.
+    switch (reportString) {
+        // Equipment Loan
+        case 'equipment loan':
+            break;
+        // History Report
+        case 'history report':
+            break;
+        // Host List
+        case 'host list':
+            break;
+        // Hosts and users
+        case 'hosts and users':
+            break;
+        // Imaging Log
+        case 'imaging log':
+            break;
+        // Inventory Report
+        case 'inventory report':
+            break;
+        // Pending MAC
+        case 'pending mac list':
+            break;
+        // Product Keys
+        case 'product keys':
+            break;
+        // Snapin Log
+        case 'snapin log':
+            break;
+        // User Tracking
+        case 'user tracking':
+            break;
+    }
+})(jQuery);
