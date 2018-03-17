2b3632ef3a45851ec89b0ec385fafcdbc0d731c1
Tom Elliott <tommygunsster@gmail.com>
2018-03-17 15:20:30 -0400

    Add pushbullet api hook

---
+++ packages/web/lib/plugins/pushbullet/hooks/addpushbulletapi.hook.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Injects pushbullet stuff into the api system.
+ *
+ * PHP version 5
+ *
+ * @category AddPushbulletAPI
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Injects pushbullet stuff into the api system.
+ *
+ * @category AddPushbulletAPI
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class AddPushbulletAPI extends Hook
+{
+    /**
+     * The name of the hook.
+     *
+     * @var string
+     */
+    public $name = 'AddPushbulletAPI';
+    /**
+     * The description.
+     *
+     * @var string
+     */
+    public $description = 'Add Pushbullet stuff into the api system.';
+    /**
+     * For posterity.
+     *
+     * @var bool
+     */
+    public $active = true;
+    /**
+     * The node the hook works with.
+     *
+     * @var string
+     */
+    public $node = 'pushbullet';
+    /**
+     * Initialize object.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        if (!in_array($this->node, (array)self::$pluginsinstalled)) {
+            return;
+        }
+        self::$HookManager->register(
+            'API_VALID_CLASSES',
+            [$this, 'injectAPIElements']
+        );
+    }
+    /**
+     * This function injects pushbullet elements for
+     * api access.
+     *
+     * @param mixed $arguments The arguments to modify.
+     *
+     * @return void
+     */
+    public function injectAPIElements($arguments)
+    {
+        $arguments['validClasses'] = self::fastmerge(
+            $arguments['validClasses'],
+            ['pushbullet']
+        );
+    }
+}
