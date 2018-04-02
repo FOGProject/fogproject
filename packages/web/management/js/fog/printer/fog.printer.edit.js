1577bc986d68c12b2beda101074f9198509bb289
Tom Elliott <tommygunsster@gmail.com>
2018-04-01 22:28:46 -0400

    Get the javascript files all updated for the new delete modal

--- packages/web/management/js/fog/printer/fog.printer.edit.js
+++ packages/web/management/js/fog/printer/fog.printer.edit.js
@@ -25,7 +25,10 @@
 
     var generalForm = $('#printer-general-form'),
         generalFormBtn = $('#general-send'),
-        generalDeleteBtn = $('#general-delete');
+        generalDeleteBtn = $('#general-delete'),
+        generalDeleteModal = $('#deleteModal'),
+        generalDeleteModalConfirm $('#confirmDeleteModal'),
+        generalDeleteModalCancel = $('#closeDeleteModal');
 
     generalForm.on('submit',function(e) {
         e.preventDefault();
@@ -43,16 +46,24 @@
             originalName = $('#printer'+type).val();
         }, ':input:visible');
     });
-    generalDeleteBtn.on('click',function() {
-        generalFormBtn.prop('disabled', true);
-        generalDeleteBtn.prop('disabled', true);
-        Common.massDelete(null, function(err) {
+    generalDeleteBtn.on('click', function() {
+        generalDeleteModal.modal('show');
+    });
+    generalDeleteModalConfirm.on('click', function() {
+        var method = 'post',
+            action = '../management/index.php?node='
+                + Common.node
+                + '&sub=delete&id='
+                + Common.id;
+        Common.apiCall(method, action, null, function(err) {
             if (err) {
-                generalDeleteBtn.prop('disabled', false);
-                generalFormBtn.prop('disabled', false);
                 return;
             }
-            window.location = '../management/index.php?node='+Common.node+'&sub=list';
+            setTimeout(function() {
+                window.location = '../management/index.php?node='
+                    + Common.node
+                    + '&sub=list';
+            }, 2000);
         });
     });
     // ---------------------------------------------------------------
