4e0ad0f0768d9f9d9f0106f32dfc8501a4ea3226
Tom Elliott <tommygunsster@gmail.com>
2018-04-07 15:05:11 -0400

    Make hosts and users report group by date (Mon day Year) format.

--- packages/web/management/js/fog/report/fog.report.file.js
+++ packages/web/management/js/fog/report/fog.report.file.js
@@ -8,7 +8,11 @@
             {
                 extend: 'pdfHtml5',
                 download: 'open',
-                alignment: 'left',
+                exportOptions: {
+                    columns: ':visible',
+                    search: 'applied',
+                    order: 'applied'
+                },
                 customize: function (doc) {
                     doc.content[1].table.widths =
                         Array(doc.content[1].table.body[0].length + 1).join('*').split('');
@@ -21,72 +25,6 @@
     // This will call our respective calls
     // to report the requested data.
     switch (reportString) {
-        // Equipment Loan
-        case 'equipment loan':
-            var userSelBtn = $('#selectuser'),
-                userSelector = $('#user'),
-                downloadPdfBtn = $('#downloadpdf'),
-                printPdfBtn = $('#printpdf'),
-                equipForm = $('#equipmentloan-form'),
-                docDefinition = {};
-
-            function disableEquipBtns(disable) {
-                userSelBtn.prop('disabled', disable);
-                downloadPdfBtn.prop('disabled', disable);
-                printPdfBtn.prop('disabled', disable);
-                if (disable === true) {
-                    downloadPdfBtn.addClass('hidden');
-                    printPdfBtn.addClass('hidden');
-                }
-            }
-
-            equipForm.on('submit', function(e) {
-                e.preventDefault();
-            });
-            userSelector.on('change', function() {
-                disableEquipBtns(true);
-                userSelBtn.trigger('click');
-            });
-            userSelBtn.on('click', function(e) {
-                e.preventDefault();
-                downloadPdfBtn.addClass('hidden');
-                printPdfBtn.addClass('hidden');
-                disableEquipBtns(true);
-                Common.processForm(equipForm, function(err, data) {
-                    disableEquipBtns(false);
-                    if (err) {
-                        downloadPdfBtn.addClass('hidden');
-                        printPdfBtn.addClass('hidden');
-                        return;
-                    }
-                    downloadPdfBtn.removeClass('hidden');
-                    //printPdfBtn.removeClass('hidden');
-                    docDefinition = data._data;
-
-                    docDefinition.header = function(currentPage, pageCount) {
-                        return {
-                            text: docDefinition.head,
-                            alignment: 'center'
-                        };
-                    };
-
-                    docDefinition.footer = function(currentPage, pageCount) {
-                        return {
-                            text: currentPage.toString() + ' of ' + pageCount,
-                            alignment: 'center'
-                        };
-                    };
-                });
-            });
-            downloadPdfBtn.on('click', function(e) {
-                e.preventDefault();
-                pdfMake.createPdf(docDefinition).download(docDefinition.filename);
-            });
-            printPdfBtn.on('click', function(e) {
-                e.preventDefault();
-                pdfMake.createPdf(docDefinition).print();
-            });
-            break;
         // History Report
         case 'history report':
             var historyTable = $('#history-table'),
@@ -94,6 +32,11 @@
                     order: [
                         [1, 'desc']
                     ],
+                    rowGroup: {
+                        dataSrc: function(row) {
+                            return moment(row.createdTime, moment.ISO_8601).format('MMM DD YYYY');
+                        }
+                    },
                     buttons: reportButtons,
                     columns: [
                         {data: 'createdBy'},
