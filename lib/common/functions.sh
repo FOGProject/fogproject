f758dabdbe4673df83e5c36223a54bb826aae412
Tom Elliott <tommygunsster@gmail.com>
2018-04-10 14:38:14 -0400

    Hopeful attempt to get php-fpm working on RH based installs

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -1481,10 +1481,25 @@ EOF
                 fi
                 errorStat $?
                 ln -s $webdirdest $webdirdest/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1
+                case in $osid
+                    1)
+                        phpfpmconf='/etc/php-fpm.d/www.conf';
+                        ;;
+                    2)
+                        if [[ $php_ver -eq 5 ]]; then
+                            phpfpmconf="/etc/php$php_ver/fpm/pool.d/www.conf"
+                        else
+                            phpfpmconf="/etc/php/$php_ver/fpm/pool.d/www.conf"
+                        fi
+                        ;;
+                    3)
+                        phpfpmconf=''
+                        ;;
+                esac
+                sed -i 's/listen = .*/listen = 127.0.0.1:9000/g' $phpfpmconf >>$workingdif/error_logs/fog_error_${version}.log 2>&1
                 if [[ $osid -eq 2 ]]; then
                     a2enmod $phpcmd >>$workingdir/error_logs/fog_error_${version}.log 2>&1
                     a2enmod proxy_fcgi setenvif >>$workingdir/error_logs/fog_error_${version}.log 2>&1
-                    sed -i 's/listen = .*/listen = 127.0.0.1:9000/g' /etc/php/$php_ver/fpm/pool.d/www.conf
                     a2enmod rewrite >>$workingdir/error_logs/fog_error_${version}.log 2>&1
                     a2enmod ssl >>$workingdir/error_logs/fog_error_${version}.log 2>&1
                     a2ensite "001-fog" >>$workingdir/error_logs/fog_error_${version}.log 2>&1
