f84c7a869b5c05bc43372b1794e7e1ceaa77508f
Tom Elliott <tommygunsster@gmail.com>
2018-04-15 00:17:24 -0400

    Refine the names method a bit nicer

--- packages/web/lib/router/route.class.php
+++ packages/web/lib/router/route.class.php
@@ -1797,11 +1797,12 @@ class Route extends FOGBase
     /**
      * Returns only the ids and names of the class.
      *
-     * @param string $class The class to get list of.
+     * @param string $class      The class to get list of.
+     * @param string $whereItems If we want to filter items.
      *
      * @return mixed
      */
-    public function names($class)
+    public function names($class, $whereItems = [])
     {
         header('Content-type: application/json');
         $data = [];
@@ -1815,13 +1816,50 @@ class Route extends FOGBase
             [],
             'id'
         );
-        foreach ($names as $i => &$v) {
-            $data[] = [
-                'id' => $ids[$i],
-                'name' => $v
-            ];
-            unset($v);
+        $classname = strtolower($class);
+        $classVars = self::getClass(
+            $class,
+            '',
+            true
+        );
+
+        $sql = 'SELECT `'
+            . $classVars['databaseFields']['id']
+            . '`,`'
+            . $classVars['databaseFields']['name']
+            . '` FROM `'
+            . $classVars['databaseTable']
+            . '`';
+
+        if (count($whereItems) > 0) {
+            $where = '';
+            foreach ($whereItems as $key => &$field) {
+                if (!$where) {
+                    $where = " WHERE `$key`";
+                } else {
+                    $where .= " AND `$key`";
+                }
+                if (is_array($field)) {
+                    $where .= " IN ('"
+                        . implode("','", $field)
+                        . "')";
+                } else {
+                    $where .= " = '"
+                        . $field
+                        . "'";
+                }
+            }
+            $sql .= $where;
         }
+        $vals = self::$DB->query($sql)->fetch('', 'fetch_all')->get();
+        foreach ($vals as &$val) {
+            $data[] = [
+                'id' => $val[$classVars['databaseFields']['id']],
+                'name' => $val[$classVars['databaseFields']['name']]
+            ];
+            unset($val);
+        }
+
         self::$data = $data;
     }
     /**
