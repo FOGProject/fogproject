b96c935baf18f97be2b76abf7fc4a29094501b18
Tom Elliott <tommygunsster@gmail.com>
2018-05-17 11:35:26 -0400

    More appropriate remove testing

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.2.529');
+        define('FOG_VERSION', '1.5.2.530');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 267);
         define('FOG_BCACHE_VER', 136);
