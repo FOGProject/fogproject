b71c81c4189212a045105d3a68f02d5a55f73786
Tom Elliott <tommygunsster@gmail.com>
2018-05-24 04:58:21 -0400

    Fixup removal of items to be much more proper

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.2.495');
+        define('FOG_VERSION', '1.5.2.505');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 267);
         define('FOG_BCACHE_VER', 135);
