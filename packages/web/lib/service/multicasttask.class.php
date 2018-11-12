7572c5a5fb1b11b4a1a995d595998d1e1148659f
Sebastian Roth <sebaroth@gmx.de>
2018-11-12 23:42:55 +0100

    Fix multicast imaging of single partitions

--- packages/web/lib/service/multicasttask.class.php
+++ packages/web/lib/service/multicasttask.class.php
@@ -390,6 +390,18 @@ class MulticastTask extends FOGService
         ->get('bitrate');
     }
     /**
+     * Returns the partition id to be cloned, 0 for all
+     *
+     * @return int
+     */
+    public function getPartitions()
+    {
+        return (int)self::getClass(
+            'Image',
+            $this->_MultiSess->get('image')
+        )->getPartitionType();
+    }
+    /**
      * Returns the session class
      *
      * @return object
@@ -608,7 +620,15 @@ class MulticastTask extends FOGService
             break;
         }
         natcasesort($filelist);
-        $filelist = array_values((array)$filelist);
+        $partid = self::getPartitions();
+        if ($partid < 1) {
+            $filelist = array_values((array)$filelist);
+        }
+        else {
+            $filelist = array_values(
+                preg_grep("/^d[0-9]p$partid\.img$/", (array)$filelist)
+            );
+        }
         ob_start();
         foreach ($filelist as $i => &$file) {
             printf(
