f07b2a2eece4cec5abc18e474b16619f2aa04d36
Sebastian Roth <sebaroth@gmx.de>
2018-12-16 16:08:05 +0100

    Remove at-sign from the service name as it seems to be present in Fedora and Arch

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -978,8 +978,8 @@ configureMySql() {
         done
     fi
     dots "Setting up and starting MySQL"
-    dbservice=$(systemctl list-units | grep -e mysql -e mariadb | awk '{print $1}')
-    [[ -z $dbservice ]] && dbservice=$(systemctl list-unit-files | grep -e mysql -e mariadb | grep -v bad | awk '{print $1}')
+    dbservice=$(systemctl list-units | grep -e mysql -e mariadb | awk '{print $1}' | tr -d '@')
+    [[ -z $dbservice ]] && dbservice=$(systemctl list-unit-files | grep -e mysql -e mariadb | grep -v bad | awk '{print $1}' | tr -d '@')
     for mysqlconf in $(grep -rl '.*skip-networking' /etc | grep -v init.d); do
         sed -i '/.*skip-networking/ s/^#*/#/' -i $mysqlconf >>$workingdir/error_logs/fog_error_${version}.log 2>&1
     done
