eaea0c2c532438fad048dbfcef2737e8e7f5b1c4
Tom Elliott <tommygunsster@gmail.com>
2018-12-23 11:38:50 -0600

    Fix missing fi for if statement in ubuntu config.sh

--- lib/ubuntu/config.sh
+++ lib/ubuntu/config.sh
@@ -49,6 +49,7 @@ elif [[ $linuxReleaseName == +(*[Uu][Bb][Uu][Nn][Tt][Uu]*|*[Mm][Ii][Nn][Tt]*) ]]
                 dots "Stopping web services"
                 if [[ $systemctl == yes ]]; then
                     systemctl is-active --quiet apache2 && systemctl stop apache2 >/dev/null 2>&1 || service apache2 stop >/dev/null 2>&1
+                fi
                 [[ ! $? -eq 0 ]] && echo "Failed" || echo "Done"
                 dots "Removing the apache and php packages"
                 DEBIAN_FRONTEND=noninteractive apt-get purge -yq 'apache2*' 'php5*' 'php7*' 'libapache*' >/dev/null 2>&1
