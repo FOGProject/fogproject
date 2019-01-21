383ffaf30feaa33ea01043e15627c950687df504
Tom Elliott <tommygunsster@gmail.com>
2019-01-21 07:35:07 -0600

    Change paths to work with dynamic directory.
    1. Change paths to work with fog directory that's named differently than
    when simply pulled from github (e.g. was going to always look for
    fogproject which might not exist if unzipped.)
    2. Remove php-mcrypt from .fogsettings file if its set already inside.

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -531,6 +531,7 @@ installPackages() {
             packages="$packages php-bcmath bc"
             packages="${packages// mod_fastcgi/}"
             packages="${packages// mod_evasive/}"
+            packages="${packages// php-mcrypt/}"
             case $linuxReleaseName in
                 *[Ff][Ee][Dd][Oo][Rr][Aa]*)
                     packages="$packages php-json"
@@ -563,10 +564,14 @@ installPackages() {
         2)
             packages="${packages// libapache2-mod-fastcgi/}"
             packages="${packages// libapache2-mod-evasive/}"
+            packages="${packages// php${php_ver}-mcrypt/}"
             packages="${packages} php${php_ver}-bcmath bc"
             case $linuxReleaseName in
 				*[Uu][Bb][Uu][Nn][Tt][Uu]*|*[Mm][Ii][Nn][Tt]*) addUbuntuRepo ;;
             esac
+            ;;
+        3)
+            packages="${packages// php-mcrypt/}"
             ;;
     esac
     errorStat $?
