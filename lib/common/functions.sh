00ae45a5e61cbeb6261778ac7b7fd90fc6b348de
Tom Elliott <tommygunsster@gmail.com>
2019-02-04 10:52:19 -0600

    Clean up old sql entries in schema.

--- lib/common/functions.sh
+++ lib/common/functions.sh
@@ -1986,6 +1986,7 @@ class Config
         define('FOG_JPGRAPH_VERSION', '2.3');
         define('FOG_REPORT_DIR', './reports/');
         define('FOG_CAPTUREIGNOREPAGEHIBER', true);
+        define('FOG_THEME', 'default/fog.css');
     }
 }" > "${webdirdest}/lib/fog/config.class.php"
     errorStat $?
