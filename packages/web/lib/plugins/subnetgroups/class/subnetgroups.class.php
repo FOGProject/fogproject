d7a54f4ad96ce9df108cca155cf52fda1ec80e6a
sctt <stefano@termos.sinervis.pri>
2019-02-05 17:12:35 +0100

    Add SubnetGroups Plugin

---
+++ packages/web/lib/plugins/subnetgroups/class/subnetgroups.class.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Subnetgroups Class handler.
+ *
+ * PHP version 5
+ *
+ * @category Subnetgroups
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Subnetgroups Class handler.
+ *
+ * @category Subnetgroups
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class Subnetgroups extends FOGController
+{
+    /**
+     * The subnetgroups table
+     *
+     * @var string
+     */
+    protected $databaseTable = 'subnetgroups';
+    /**
+     * The subnetgroups fields and common names
+     *
+     * @var array
+     */
+    protected $databaseFields = array(
+        'id' => 'sgID',
+        'name' => 'sgName',
+        'groupID' => 'sgGroupID',
+        'subnets' => 'sgSubnets',
+    );
+    /**
+     * The required fields.
+     *
+     * @var array
+     */
+    protected $databaseFieldsRequired = array(
+        'groupID',
+        'subnets',
+    );
+    /**
+     * Additional fields
+     *
+     * @var array
+     */
+    protected $additionalFields = array(
+        'group',
+    );
+    /**
+     * Database -> Class field relationships
+     *
+     * @var array
+     */
+    protected $databaseFieldClassRelationships = array(
+        'Group' => array(
+            'id',
+            'groupID',
+            'group'
+        ),
+    );
+    /**
+     * Load the group object
+     *
+     * @return object
+     */
+    protected function loadGroup()
+    {
+        $group = self::getSubObjectIDs(
+            'Group',
+            array('id' => $this->get('groupID')),
+            'data'
+        );
+        if (isset($group[0])) {
+            $this->set('group', $group[0]);
+        }
+    }
+}
+
---
+++ packages/web/lib/plugins/subnetgroups/class/subnetgroupsmanager.class.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Manager class for subnetgroups
+ *
+ * PHP Version 5
+ *
+ * @category SubnetgroupsManager
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * Manager class for subnetgroups
+ *
+ * @category SubnetgroupsManager
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class SubnetgroupsManager extends FOGManagerController
+{
+    /**
+     * The base table name.
+     *
+     * @var string
+     */
+    public $tablename = 'subnetgroups';
+    /**
+     * Perform the database and plugin installation
+     *
+     * @return bool
+     */
+    public function install()
+    {
+        $this->uninstall();
+        $sql = Schema::createTable(
+            $this->tablename,
+            true,
+            array(
+                'sgID',
+                'sgName',
+                'sgGroupID',
+                'sgSubnets'
+            ),
+            array(
+                'INTEGER',
+                'VARCHAR(255)',
+                'INTEGER',
+                'TEXT',
+            ),
+            array(
+                false,
+                false,
+                false,
+                false,
+            ),
+            array(
+                false,
+                false,
+                false,
+                false,
+            ),
+            array(
+                'sgID'
+            ),
+            'MyISAM',
+            'utf8',
+            'sgID',
+            'sgID'
+        );
+        return self::$DB->query($sql);
+    }
+}
