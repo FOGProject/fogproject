a5fbff8c3f2b037355594e4f6c51bc08040cf37e
Tom Elliott <tommygunsster@gmail.com>
2019-02-25 11:43:05 -0600

    More work on PR #289 by creating page and modal. Still needs to get posting and edit elements completed.

---
+++ packages/web/lib/plugins/subnetgroup/js/fog.subnetgroup.export.js
@@ -0,0 +1,35 @@
+(function($) {
+    var exportTable = $('#subnet-export-table').registerTable(null, {
+        buttons: [
+            'copy',
+            {
+                extend: 'csv',
+                header: false
+            },
+            'excel',
+            'print',
+            'colvis'
+        ],
+        order: [
+            [0, 'asc']
+        ],
+        columns: [
+            {data: 'name'}, // 0
+            {data: 'groupID'}, // 1
+        ],
+        rowId: 'id',
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: '../management/index.php?node='
+            + Common.node
+            + '&sub=getExportList',
+            type: 'post'
+        }
+    });
+
+    // Enable searching
+    if (Common.search && Common.search.length > 0) {
+        exportTable.search(Common.search).draw();
+    }
+})(jQuery);
