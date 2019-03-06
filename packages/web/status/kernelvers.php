f5c2d00fecf139e208e6bf6b99d849cc285240bb
Tom Elliott <tommygunsster@gmail.com>
2019-03-06 09:23:41 -0600

    Fixup a few security issues in regards to getfiles.php and kernelvers.php

--- packages/web/status/kernelvers.php
+++ packages/web/status/kernelvers.php
@@ -24,8 +24,18 @@ session_write_close();
 ignore_user_abort(true);
 set_time_limit(0);
 header('Content-Type: text/event-stream');
-if (isset($_REQUEST['url'])) {
-    $url = $_REQUEST['url'];
+$url = filter_input(INPUT_GET, 'url');
+if (!$currentUser->isValid()) {
+    echo _('Unauthorized');
+    exit;
+}
+if (empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+    || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest'
+) {
+    echo _('Unauthorized');
+    exit;
+}
+if ($url) {
     $res = $FOGURLRequests
         ->process($url);
     foreach ((array)$res as &$response) {
