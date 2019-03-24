2351acebc0f8280d7215bedbf6ac0700f0e72f75
Sebastian Roth <sebaroth@gmx.de>
2019-03-24 21:54:41 +0100

    Add link target blank for external links to open in a new page/tab

--- packages/web/lib/fog/fogsubmenu.class.php
+++ packages/web/lib/fog/fogsubmenu.class.php
@@ -450,6 +450,7 @@ class FOGSubMenu extends FOGBase
                 echo '<ul class="nav nav-pills nav-stacked">';
                 foreach ((array) $data as $label => &$link) {
                     $hash = '';
+                    $target = '';
                     if (!$this->isExternalLink($link)) {
                         $hash = $this->getTarget($link);
                         if ($hash) {
@@ -460,12 +461,16 @@ class FOGSubMenu extends FOGBase
                             );
                         }
                     }
+                    else {
+                        $target = ' target="_blank"';
+                    }
                     if ($label == 'class') {
                         continue;
                     }
                     $string = sprintf(
-                        '<li><a class="%s" href="${link}${hash}">%s</a></li>',
+                        '<li><a class="%s" href="${link}${hash}"%s>%s</a></li>',
                         $hash ?: $node.'-'.$sub,
+                        $target,
                         $label
                     );
                     if ($this->isExternalLink($link)) {
