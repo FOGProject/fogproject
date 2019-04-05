6c123daf01689bb88f24aecf4baa1babf61ca250
Fernando Gietz <fernando.gietz@gmail.com>
2019-04-05 17:14:31 +0200

    Clean trash and change the version of LDAP plugin in config file

--- packages/web/lib/plugins/ldap/config/plugin.config.php
+++ packages/web/lib/plugins/ldap/config/plugin.config.php
@@ -25,7 +25,7 @@ $fog_plugin['description'] = 'LDAP plugin to use a LDAP validation with FOG'
     . '. Ensure you have the php ldap module installed and loaded on your '
     . 'server.  This can be done typically by using your distros package '
     . 'manager software.  (e.g. apt-get install php5-ldap, '
-    . 'yum install php-ldap). Version: 1.5.5';
+    . 'yum install php-ldap). Version: 1.5.5_1';
 $fog_plugin['menuicon'] = 'fa fa-key fa-fw';
 $fog_plugin['menuicon_hover'] = null;
 $fog_plugin['entrypoint'] = 'html/run.php';
