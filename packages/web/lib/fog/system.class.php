122ac7aa2384bfa41329eb7230782e4ba2354276
Tom Elliott <tommygunsster@gmail.com>
2019-04-13 11:58:15 -0500

    Fix up group to set storage node ID.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.5.692');
+        define('FOG_VERSION', '1.5.5.694');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 273);
         define('FOG_BCACHE_VER', 140);
