db55b1d0c38b0f06d99bf6ed3efdde0532b52050
Sebastian Roth <sebaroth@gmx.de>
2019-04-15 21:36:20 +0200

    Rename the fog FTP account to hopefully prevent people from using it

--- lib/common/config.sh
+++ lib/common/config.sh
@@ -13,7 +13,7 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-[[ -z $username ]] && username="fog"
+[[ -z $username || "x$username" = "xfog" ]] && username="fogproject"
 [[ -z $webdirsrc ]] && webdirsrc="../packages/web"
 [[ -z $tftpdirsrc ]] && tftpdirsrc="../packages/tftp"
 [[ -z $buildipxesrc ]] && buildipxesrc="../utils/FOGiPXE"
