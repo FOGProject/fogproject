aa59945f06448c25a97682a0ee40379ace108cd9
Sebastian Roth <sebaroth@gmx.de>
2019-07-20 13:43:03 +0200

    Fix code styling

--- packages/web/lib/reg-task/taskqueue.class.php
+++ packages/web/lib/reg-task/taskqueue.class.php
@@ -78,11 +78,8 @@ class TaskQueue extends TaskingElement
                     }
 
                     $this->StorageNode = self::nodeFail(
-                self::getClass(
-                            'StorageNode',
-                            $this->Task->get('storagenodeID')
-                        ),
-                self::$Host->get('id')
+                        self::getClass('StorageNode', $this->Task->get('storagenodeID')),
+                        self::$Host->get('id')
                     );
                     if ($MulticastSession->get('stateID') == 1) {
                         $msg = sprintf(
