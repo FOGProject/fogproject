FOGProject/fogproject

commit 3b4ff68ecd4944920366b18993f23f2da8f13075

Prevent from very long page load times with lots of user history data

packages/web/lib/pages/hostmanagementpage.class.php
@@ -3764,7 +3764,13 @@ class HostManagementPage extends FOGPage
         $MainDate_1 = self::niceDate($date)
             ->modify('+1 day')
             ->getTimestamp();
-        Route::listem('UserTracking');
+        Route::listem(
+            'UserTracking',
+            array(
+                'hostID' => $this->obj->get('id'),
+                'action' => array('', 0, 1)
+            )
+        );
         $UserTracks = json_decode(
             Route::getData()
         );

commit fdf3378fde3c312b133031ec4d4ea8e62f792551

Fix PHP warning caused by commit 3b4ff68e

packages/web/lib/pages/hostmanagementpage.class.php
@@ -3766,6 +3766,8 @@ class HostManagementPage extends FOGPage
             ->getTimestamp();
         Route::listem(
             'UserTracking',
+            'name',
+            'ASC',
             array(
                 'hostID' => $this->obj->get('id'),
                 'action' => array('', 0, 1)