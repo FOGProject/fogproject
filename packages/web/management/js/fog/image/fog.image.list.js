1acaf5d2938dbe30a854247956b4adcbca26c5a4
Tom Elliott <tommygunsster@gmail.com>
2019-11-14 10:08:47 -0600

    Fix image so if path is not set, it will get set to image name less certain special characters

--- packages/web/management/js/fog/image/fog.image.list.js
+++ packages/web/management/js/fog/image/fog.image.list.js
@@ -64,7 +64,17 @@
     if (Common.search && Common.search.length > 0) {
         table.search(Common.search).draw();
     }
-
+    $('.imagepath-input').on('keyup change blur focus focusout', function(e) {
+        var start = this.selectionStart,
+            end = this.selectionEnd;
+        this.value = this.value.replace(/[^\w+\/\.\-]/g,'');
+        this.setSelectionRange(start,end);
+        e.preventDefault();
+    });
+    $('.imagename-input').on('keyup change blur focus focusout', function(e) {
+        if ($('.imagepath-input').val().length > 0) return;
+        $('.imagepath-input').val(this.value).trigger('change');
+    });
     $('.slider').slider();
     var image = $('#image'),
         path = $('#path');
