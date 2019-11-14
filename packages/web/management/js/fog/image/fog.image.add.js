b84026bb03f0d5244f9ade6a086dd17d8b1dd74f
Tom Elliott <tommygunsster@gmail.com>
2019-11-14 10:08:47 -0600

    Fix image so if path is not set, it will get set to image name less certain special characters

--- packages/web/management/js/fog/image/fog.image.add.js
+++ packages/web/management/js/fog/image/fog.image.add.js
@@ -10,5 +10,16 @@
             createFormBtn.prop('disabled', false);
         });
     });
+    $('.imagepath-input').on('keyup change blur focus focusout', function(e) {
+        var start = this.selectionStart,
+            end = this.selectionEnd;
+        this.value = this.value.replace(/[^\w+\/\.\-]/g,'');
+        this.setSelectionRange(start,end);
+        e.preventDefault();
+    });
+    $('.imagename-input').on('keyup change blur focus focusout', function(e) {
+        if ($('.imagepath-input').val().length > 0) return;
+        $('.imagepath-input').val(this.value).trigger('change');
+    });
     $('.slider').slider();
 })(jQuery);
