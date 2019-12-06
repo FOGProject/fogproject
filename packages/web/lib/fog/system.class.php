ab055501287b4ffbfd0010d73ce82c9b10c07b5c
Tom Elliott <tommygunsster@gmail.com>
2019-12-06 16:53:17 -0600

    Fix active snapin tasks to display correct number of snapin tasks

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.773');
+        define('FOG_VERSION', '1.5.7.774');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 275);
         define('FOG_BCACHE_VER', 140);
