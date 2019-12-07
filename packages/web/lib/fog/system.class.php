29c8a831111ebfa9c11b08efdba0e165646d08d2
Tom Elliott <tommygunsster@gmail.com>
2019-12-07 12:17:38 -0600

    Add schema checking for divergence from dev-branch
    1. Fix and close #346 by pushing divergence to new schema update.
    2. Move all divergence points to ensure safety net.

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,9 +59,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.774');
+        define('FOG_VERSION', '1.5.7.775');
         define('FOG_CHANNEL', 'Alpha');
-        define('FOG_SCHEMA', 275);
+        define('FOG_SCHEMA', 276);
         define('FOG_BCACHE_VER', 140);
         define('FOG_CLIENT_VERSION', '0.11.16');
     }
