801b15c69b3c4c90ce09dee46ba1434e62d5bce3
Tom Elliott <tommygunsster@gmail.com>
2019-12-17 13:34:08 -0600

    Re-add servicemodule as it appears to still be needed per issue #352. Of note this is not whats causing the snapin to fail to be uploaded.

---
+++ packages/web/lib/client/servicemodule.class.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The service module checks
+ *
+ * PHP version 5
+ *
+ * @category ServiceModule
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+/**
+ * The service module checks
+ *
+ * @category ServiceModule
+ * @package  FOGProject
+ * @author   Tom Elliott <tommygunsster@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link     https://fogproject.org
+ */
+class ServiceModule extends FOGClient implements FOGClientSend
+{
+    /**
+     * Creates the send string and stores to send variable
+     *
+     * @return void
+     */
+    public function send()
+    {
+        $mods = self::getGlobalModuleStatus(
+            false,
+            true
+        );
+        $mod = strtolower(
+            Initiator::sanitizeItems(
+                $_REQUEST['moduleid']
+            )
+        );
+        switch ($mod) {
+        case 'dircleaner':
+            $mod = 'dircleanup';
+            break;
+        case 'snapin':
+            $mod = 'snapinclient';
+            break;
+        }
+        if (!in_array($mod, $mods)) {
+            throw new Exception('#!um');
+        }
+        $remArr = array(
+            'dircleanup',
+            'usercleanup',
+            'clientupdater'
+        );
+        $globalModules = (
+            !self::$newService ?
+            self::getGlobalModuleStatus(
+                false,
+                true
+            ) :
+            array_diff(
+                self::getGlobalModuleStatus(
+                    false,
+                    true
+                ),
+                $remArr
+            )
+        );
+        $globalInfo = self::getGlobalModuleStatus();
+        $globalDisabled = array();
+        foreach ((array)$globalInfo as $key => &$en) {
+            if (self::$newService && in_array($key, $remArr)) {
+                continue;
+            }
+            if (!$en) {
+                $globalDisabled[] = $key;
+            }
+            unset($en);
+        }
+        $hostModules = self::getSubObjectIDs(
+            'Module',
+            array('id' => self::$Host->get('modules')),
+            'shortName'
+        );
+        $hostEnabled = (
+            self::$newService ?
+            array_diff(
+                (array)$hostModules,
+                $remArr
+            ) :
+            $hostModules
+        );
+        $hostDisabled = array_diff(
+            (array)$globalModules,
+            $hostEnabled
+        );
+        if (in_array(
+            $mod,
+            self::fastmerge(
+                (array)$globalDisabled,
+                (array)$hostDisabled
+            )
+        )
+        ) {
+            throw new Exception(
+                sprintf(
+                    "#!n%s\n",
+                    in_array($mod, $globalDisabled) ?
+                    'g' :
+                    'h'
+                )
+            );
+        }
+        $this->send = "#!ok\n";
+    }
+}
