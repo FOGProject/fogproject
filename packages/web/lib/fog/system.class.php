98dae6640b85960d172b2abaa487b70f7f20f342
Tom Elliott <tommygunsster@gmail.com>
2020-01-12 17:03:04 -0600

    Test signed commit

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.886');
+        define('FOG_VERSION', '1.5.7.887');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 277);
         define('FOG_BCACHE_VER', 141);
