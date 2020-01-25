f39c85ab8f6012b9489c0087d8bb0d6155f550a7
Sebastian Roth <sebaroth@gmx.de>
2020-01-25 10:18:17 +0100

    Re-try querying size and hash information via HTTPS because services default to HTTP and we can't know if storage node is setup to force HTTPS

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.98');
+        define('FOG_VERSION', '1.5.7.99');
         define('FOG_SCHEMA', 267);
         define('FOG_BCACHE_VER', 133);
         define('FOG_SVN_REVISION', 6080);
