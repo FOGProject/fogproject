bcdc5e5a63d0d6f999fe6bdd0aa12720145c9a79
Tom Elliott <tommygunsster@gmail.com>
2020-01-30 11:11:35 -0600

    Fix group export table

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,10 +59,10 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.927');
+        define('FOG_VERSION', '1.5.7.928');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 277);
-        define('FOG_BCACHE_VER', 141);
+        define('FOG_BCACHE_VER', 142);
         define('FOG_CLIENT_VERSION', '0.11.18');
     }
 }
