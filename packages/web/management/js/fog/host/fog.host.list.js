ae98719896d006a94e56574884fa499b09bcca11
Tom Elliott <tommygunsster@gmail.com>
2020-01-30 09:32:03 -0600

    Remove the getNames method from fogpage as it was rather pointless. Alter query to only show searched items as typed. Still todo to create and make functional the group creation and adding hosts to the groups after

--- packages/web/management/js/fog/host/fog.host.list.js
+++ packages/web/management/js/fog/host/fog.host.list.js
@@ -1,76 +1,4 @@
 (function($) {
-    // DEFINE: CUSTOM DATA ADAPTER; FOR FETCHING THE GROUPS
-
-    $.fn.select2.amd.define('select2/data/fetchAdapter', [
-        'select2/data/array',
-        'select2/utils'
-    ], function(ArrayAdapter, Utils){
-        function FetchDataAdapter ($element, options) {
-            FetchDataAdapter.__super__.constructor.call(this, $element, options);
-        }
-
-        Utils.Extend(FetchDataAdapter, ArrayAdapter);
-
-        FetchDataAdapter.prototype.postQuery = function(params, callback, dataStore) {
-            var response = [];
-
-            if(dataStore != undefined){
-                var data = dataStore.results;
-
-                data.forEach(function(element, index){
-                    if(params.term == undefined || element.text.toLowerCase().indexOf(params.term.toLowerCase()) !== -1){
-                        response.push(element);
-                    }
-                });
-
-                callback({
-                    results: response
-                });
-            }
-
-            return;
-        }
-
-        FetchDataAdapter.prototype.query = function (params, callback) {
-            if(this.options.options.url == undefined){
-                throw new DOMException("Invalid URL supplied!");
-            }
-
-            if(this.dataStore == undefined){
-                var self = this;
-
-                $.ajax({
-                    url: this.options.options.url,
-                    dataType: 'json',
-                    quietMillis: 200,
-                    data: function(params){
-                        return params;
-                    },
-                    success: function(data){
-                        var mappedData = $.map(data, function (obj) {
-                            obj.text = obj.name;
-                            return obj;
-                        });
-
-                        self.dataStore = {
-                            results: mappedData
-                        }
-
-                        self.postQuery(params, callback, self.dataStore);
-                        return;
-                    }
-                });
-            }
-
-            this.postQuery(params, callback, this.dataStore);
-        };
-
-        return FetchDataAdapter;
-    });
-
-    // END: CUSTOM DATA ADAPTER
-
-
     var addToGroup = $('#addSelectedToGroup'),
         deleteSelected = $('#deleteSelected'),
         groupModal = $('#addToGroupModal'),
@@ -95,20 +23,27 @@
     function loadGroupSelect(){
         groupModalSelect.select2({
             tags: true,
-            multiple: true,
             tokenSeparators: [',', ' '],
             ajax: {
-                url: '../group/names',
+                url: function(params) {
+                    return '../group/names/name='
+                        + encodeURIComponent(
+                            '%'
+                            + params.term
+                            + '%'
+                        );
+                },
                 dataType: 'json',
                 processResults: function(data, params) {
-                    params.page = params.page || 1;
                     return {
                         results: $.map(data, function(item) {
                             return {
-                                text: item.name,
-                                id: item.id
+                                id: item.name,
+                                name: item.name,
+                                text: item.name
                             };
-                        })
+                        }),
+                        totals: data.length
                     };
                 }
             },
