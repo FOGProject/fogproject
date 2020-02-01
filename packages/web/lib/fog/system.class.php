41d93255a1bd1d652c0d2ff99f69db951ac4f022
Sebastian Roth <sebaroth@gmx.de>
2020-02-01 10:47:52 +0100

    Add username substitution to email field for deployment notification

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,8 +53,8 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.101');
-        define('FOG_SCHEMA', 267);
+        define('FOG_VERSION', '1.5.7.102');
+        define('FOG_SCHEMA', 268);
         define('FOG_BCACHE_VER', 134);
         define('FOG_CLIENT_VERSION', '0.11.18');
     }
