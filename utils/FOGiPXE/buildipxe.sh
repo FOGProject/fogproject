e424b0417fc56dba2d7ba34665817e0f7b0f857c
Sebastian Roth <sebaroth@gmx.de>
2020-02-03 23:28:11 +0100

    Rearange compile parameters as iPXE fails on SSL with CERT/TRUST not being last

--- utils/FOGiPXE/buildipxe.sh
+++ utils/FOGiPXE/buildipxe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ -r $1 ]]; then
-  BUILDOPTS="TRUST=$1 CERT=$1"
+  BUILDOPTS="CERT=$1 TRUST=$1"
 elif [[ -r /opt/fog/snapins/ssl/CA/.fogCA.pem ]]; then
-  BUILDOPTS="TRUST=/opt/fog/snapins/ssl/CA/.fogCA.pem CERT=/opt/fog/snapins/ssl/CA/.fogCA.pem"
+  BUILDOPTS="CERT=/opt/fog/snapins/ssl/CA/.fogCA.pem TRUST=/opt/fog/snapins/ssl/CA/.fogCA.pem"
 fi
 IPXEGIT="https://github.com/ipxe/ipxe"
 
@@ -32,14 +32,14 @@ cp ${FOGDIR}/src/ipxe/src/config/settings.h config/
 cp ${FOGDIR}/src/ipxe/src/config/console.h config/
 
 # Build the files
-make EMBED=ipxescript ${BUILDOPTS} bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb
+make EMBED=ipxescript bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb ${BUILDOPTS}
 
 # Copy files to repo location as required
 cp bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb ${FOGDIR}/packages/tftp/
 cp bin/ipxe.lkrn ${FOGDIR}/packages/tftp/ipxe.krn
 
 # Build with 10 second delay
-make EMBED=ipxescript10sec ${BUILDOPTS} bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb
+make EMBED=ipxescript10sec bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb ${BUILDOPTS}
 
 # Copy files to repo location as required
 cp bin/ipxe.iso bin/{undionly,ipxe,intel,realtek}.{,k,kk}pxe bin/ipxe.lkrn bin/ipxe.usb ${FOGDIR}/packages/tftp/10secdelay/
@@ -68,8 +68,8 @@ cp ${FOGDIR}/src/ipxe/src-efi/config/settings.h config/
 cp ${FOGDIR}/src/ipxe/src-efi/config/console.h config/
 
 # Build the files
-make EMBED=ipxescript ${BUILDOPTS} bin-{i386,x86_64}-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi
-[[ "x$armsupport" == "x1" ]] && make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 EMBED=ipxescript ${BUILDOPTS} bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi
+make EMBED=ipxescript bin-{i386,x86_64}-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${BUILDOPTS}
+[[ "x$armsupport" == "x1" ]] && make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 EMBED=ipxescript bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${BUILDOPTS}
 
 # Copy the files to upload
 [[ "x$armsupport" == "x1" ]] && cp bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${FOGDIR}/packages/tftp/arm64-efi/
@@ -77,8 +77,8 @@ cp bin-i386-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${FOGDIR}/pac
 cp bin-x86_64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${FOGDIR}/packages/tftp/
 
 # Build with 10 second delay
-make EMBED=ipxescript10sec ${BUILDOPTS} bin-{i386,x86_64}-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi
-[[ "x$armsupport" == "x1" ]] && make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 EMBED=ipxescript10sec ${BUILDOPTS} bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi
+make EMBED=ipxescript10sec bin-{i386,x86_64}-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${BUILDOPTS}
+[[ "x$armsupport" == "x1" ]] && make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 EMBED=ipxescript10sec bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${BUILDOPTS}
 
 # Copy the files to upload
 [[ "x$armsupport" == "x1" ]] && cp bin-arm64-efi/{snp{,only},ipxe,intel,realtek,ncm--ecm--axge}.efi ${FOGDIR}/packages/tftp/10secdelay/arm64-efi/
