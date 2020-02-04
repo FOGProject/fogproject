b827a94354c076b6bcf30ba87e055a9d50f3aa10
Tom Elliott <tommygunsster@gmail.com>
2020-02-04 10:10:41 -0600

    Update tftp files

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.7.945');
+        define('FOG_VERSION', '1.5.7.946');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 278);
         define('FOG_BCACHE_VER', 142);
