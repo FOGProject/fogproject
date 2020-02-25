f55c5cafe4c7d00543c041b4784f35872166b6c7
Sebastian Roth <sebaroth@gmx.de>
2020-02-25 18:29:18 +0100

    Fix exec command line in systemd FOGSnapinReplicator file

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.8.960');
+        define('FOG_VERSION', '1.5.8.961');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 278);
         define('FOG_BCACHE_VER', 142);
