d1db3f1f800709974f2bfaf5aacf3d0e0b6bd147
Sebastian Roth <sebaroth@gmx.de>
2020-03-01 23:17:01 +0100

    Improve input check on aesdecrypt in case of unencrypted passwords with pipe character

--- packages/web/lib/fog/fogbase.class.php
+++ packages/web/lib/fog/fogbase.class.php
@@ -1538,22 +1538,31 @@ abstract class FOGBase
         $key = false,
         $enctype = 'aes-128-cbc'
     ) {
-        $iv_size = openssl_cipher_iv_length($enctype);
+        $iv_size = openssl_cipher_iv_length($enctype) * 2;
         if (false === strpos($encdata, '|')) {
             return $encdata;
         }
         $data = explode('|', $encdata);
-        if ($iv = pack('H*', $data[0])) {
-            return '';
+        if (strlen($data[0]) != $iv_size || strlen($data[1]) != $iv_size) {
+            return $encdata;
         }
-        if ($encoded != pack('H*', $data[1])) {
-            return '';
-        }
-        if (!$key && $data[2]) {
-            if ($key != pack('H*', $data[2])) {
-                return '';
+        // add error handler to catch warnings we might get from pack() with non-hex strings
+        set_error_handler(
+            function ($severity, $message, $file, $line) {
+                throw new ErrorException($message, $severity, $severity, $file, $line);
+            }
+        );
+        try {
+            $iv = pack('H*', $data[0]);
+            $encoded = pack('H*', $data[1]);
+            if (!$key && isset($data[2]) && strlen($data[2]) == $iv_size) {
+                $key = pack('H*', $data[2]);
             }
         }
+        catch (Exception $e) {
+            return $encdata;
+        }
+        restore_error_handler();
         if (empty($key)) {
             return '';
         }
