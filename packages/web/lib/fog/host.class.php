FOGProject/fogproject

commit de4fc0ad6e3209c303b2b2d8534ce96f98acf92e

Change task name for host tasks as well and generalize more

packages/web/lib/fog/host.class.php
@@ -1296,6 +1296,7 @@ class Host extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
+        $taskName .= ' - ' . $this->get('name');
         try {
             if (!$this->isValid()) {
                 throw new Exception(self::$foglang['HostNotValid']);

packages/web/lib/fog/group.class.php
@@ -446,6 +446,7 @@ class Group extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
+        $taskName .= ' - ' . $this->get('name');
         $hostCount = $this->getHostCount();
         if ($hostCount < 1) {
             throw new Exception(_('No hosts to task'));
@@ -525,7 +526,7 @@ class Group extends FOGController
                     $port = $defaultPort;
                 }
                 $MulticastSession = self::getClass('MulticastSession')
-                    ->set('name', $taskName . ' - ' . $this->get('name'))
+                    ->set('name', $taskName)
                     ->set('port', $port)
                     ->set('logpath', $Image->get('path'))
                     ->set('image', $Image->get('id'))

commit dc1956033517ec77034ffdd78aa81cb02b45d003

Fix multicast session join which broke with commit de4fc0a

packages/web/lib/fog/host.class.php
@@ -904,10 +904,12 @@ class Host extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
-        $taskName .= ' - '
-            . $this->get('name')
-            . ' '
-            . self::niceDate()->format('Y-m-d H:i:s');
+        if (!$sessionjoin) {
+            $taskName .= ' - '
+                . $this->get('name')
+                . ' '
+                . self::niceDate()->format('Y-m-d H:i:s');
+        }
         $serverFault = false;
         try {
             if (!$this->isValid()) {

packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.981');
+        define('FOG_VERSION', '1.6.0-alpha.982');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 278);
         define('FOG_BCACHE_VER', 142);