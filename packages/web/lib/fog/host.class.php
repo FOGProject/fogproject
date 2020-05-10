FOGProject/fogproject

commit 77e6e716c9e79b87351443da6f8d49914bc02086

Change task name for host tasks as well and generalize more

packages/web/lib/fog/host.class.php
@@ -1296,6 +1296,7 @@ class Host extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
+        $taskName .= ' - ' . $this->get('name');
         try {
             if (!$this->isValid()) {
                 throw new Exception(self::$foglang['HostNotValid']);

packages/web/lib/fog/group.class.php
@@ -446,6 +446,7 @@ class Group extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
+        $taskName .= ' - ' . $this->get('name');
         $hostCount = $this->getHostCount();
         if ($hostCount < 1) {
             throw new Exception(_('No hosts to task'));
@@ -525,7 +526,7 @@ class Group extends FOGController
                     $port = $defaultPort;
                 }
                 $MulticastSession = self::getClass('MulticastSession')
-                    ->set('name', $taskName . ' - ' . $this->get('name'))
+                    ->set('name', $taskName)
                     ->set('port', $port)
                     ->set('logpath', $Image->get('path'))
                     ->set('image', $Image->get('id'))

commit 640262bf226ca370514c485cb38088ac3038c762

Fix multicast session join which broke with commit 77e6e716

packages/web/lib/fog/host.class.php
@@ -1293,7 +1293,9 @@ class Host extends FOGController
         $sessionjoin = false,
         $wol = false
     ) {
-        $taskName .= ' - ' . $this->get('name');
+        if (!$sessionjoin) {
+            $taskName .= ' - ' . $this->get('name');
+        }
         try {
             if (!$this->isValid()) {
                 throw new Exception(self::$foglang['HostNotValid']);

packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9-RC1.5');
+        define('FOG_VERSION', '1.5.9-RC1.6');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 134);
         define('FOG_CLIENT_VERSION', '0.12.0');