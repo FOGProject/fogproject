c292d8030393783ff6a09a767181fd218dd6effc
Sebastian Roth <sebaroth@gmx.de>
2020-05-11 12:39:11 +0200

    Add checks when adding external repos on CentOS installs to fix #389

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9-RC1.7');
+        define('FOG_VERSION', '1.5.9-RC1.8');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 134);
         define('FOG_CLIENT_VERSION', '0.12.0');
