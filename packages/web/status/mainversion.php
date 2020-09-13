00f64754cbf9ad55c35f9069381fabfc73c5efdd
Sebastian Roth <sebaroth@gmx.de>
2020-09-13 15:17:43 +0200

    Directly query release information from github instead of fogproject.org

--- packages/web/status/mainversion.php
+++ packages/web/status/mainversion.php
@@ -23,16 +23,52 @@ require '../commons/base.inc.php';
 session_write_close();
 ignore_user_abort(true);
 set_time_limit(0);
-$url = 'https://fogproject.org/version/index.php';
-$data = array(
-    'version' => FOG_VERSION,
+
+$curversion = FOG_VERSION;
+$urls = array(
+    'https://api.github.com/repos/fogproject/fogproject/tags',
+    'https://raw.githubusercontent.com/FOGProject/fogproject/dev-branch/packages/web/lib/fog/system.class.php',
+    'https://raw.githubusercontent.com/FOGProject/fogproject/working-1.6/packages/web/lib/fog/system.class.php'
 );
-$res = $FOGURLRequests->process(
-    $url,
-    'POST',
-    $data
-);
-$res = array_shift($res);
-$res = json_encode($res);
-echo $res;
+$resp = $FOGURLRequests->process($urls);
+
+$tags = json_decode(array_shift($resp));
+foreach ($tags AS $tag) {
+    if (preg_match('/^[0-9]\.[0-9]\.[0-9]$/', $tag->name)) {
+        $stableversion = $tag->name;
+        break;
+    }
+}
+$systemclass = array_shift($resp);
+if (preg_match("/FOG_VERSION', '([0-9.RCalphbet-]*)'/", $systemclass, $fogver)) {
+     $devversion = $fogver[1];
+}
+$systemclass = array_shift($resp);
+if (preg_match("/FOG_VERSION', '([0-9.RCalphbet-]*)'/", $systemclass, $fogver)) {
+     $alphaversion = $fogver[1];
+}
+
+$stablecheck = version_compare($curversion, $stableversion, '=');
+$devcheck = version_compare($curversion, $devversion, '=');
+$alphacheck = version_compare($curversion, $alphaversion, '=');
+
+if (!$stablecheck && !$devcheck && !$alphacheck) {
+    $result = '<font face="arial" color="red" size="4"><b>You are not running the most current version of FOG!</b></font>'
+    . "<p>You are currently running version: $curversion</p>"
+    . "<p>Latest stable version is " . $stableversion . "</p>"
+    . "<p>Latest dev-branch version is $devversion</p>"
+    . "<p>Latest alpha-branch version is $alphaversion</p>";
+} else {
+    $result = "<b>Your version of FOG is up to date.</b><br/>";
+    if ($stablecheck) {
+        $result .= "You're running the latest stable version: " . $stableversion;
+    } else if ($devcheck) {
+        $result .= "You're running the latest dev-branch version: " . $devversion;
+    } else {
+        $result .= "You're running the latest alpha-branch version: " . $alphaversion;
+    }
+}
+
+
+echo json_encode($result);
 exit;
