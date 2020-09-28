77a7f0c72319a7a7c2b08c96f4f02b3c7f3a769a
Tom Elliott <tommygunsster@gmail.com>
2020-09-28 08:49:08 -0500

    Update report_analytics script to use a single command to get information

--- utils/analytics/report_analytics.sh
+++ utils/analytics/report_analytics.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Get the OS Name.
-os_name=$(lsb_release -i | cut -d':' -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-echo "os_name=${os_name}"
-
-# Get the OS Version.
-os_vers=$(lsb_release -r | cut -d':' -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+# Get the OS Information.
+read -r os_name os_version <<< $(lsb_release -ir | cut -d':' -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | tr '\n' ' ')
 
 # Get the FOG Version.
 fog_version=$(cat /var/www/fog/lib/fog/system.class.php | grep FOG_VERSION | cut -d',' -f2 | cut -d"'" -f2)
 
 # Format payload.
 payload='{"fog_version":"'${fog_version}'","os_name":"'${os_name}'","os_version":"'${os_version}'"}'
-echo "payload=${payload}"
+
+#echo "os_name=${os_name}"
+#echo "os_version=${os_version}"
+#echo "fog_version=${fog_version}"
+#echo "payload=${payload}"
 
 # Send to reporting endpoint.
-curl -s -X POST -H "Content-Type: application/json" -d "${payload}" https://fog-analytics-entries.theworkmans.us:/api/record
+curl -s -X POST -H "Content-Type: application/json" -d "${payload}" https://fog-analytics-entries.theworkmans.us:/api/records
