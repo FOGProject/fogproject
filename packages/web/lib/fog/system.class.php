f8d2aa17a14562f78f5fbb2f47562dc0d77911df
Sebastian Roth <sebaroth@gmx.de>
2021-05-15 22:59:55 +0200

    Fix installer password character check

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.90');
+        define('FOG_VERSION', '1.5.9.91');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 136);
         define('FOG_CLIENT_VERSION', '0.12.0');
