b03ab2f8d506a11177318a42b048e33560ed5535
Tom Elliott <tommygunsster@gmail.com>
2021-08-07 12:53:25 -0500

    Update schema to ensure dmidecodeKeys gets generated

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,9 +59,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.1126');
+        define('FOG_VERSION', '1.6.0-alpha.1127');
         define('FOG_CHANNEL', 'Alpha');
-        define('FOG_SCHEMA', 282);
+        define('FOG_SCHEMA', 283);
         define('FOG_BCACHE_VER', 143);
         define('FOG_CLIENT_VERSION', '0.12.0');
     }
