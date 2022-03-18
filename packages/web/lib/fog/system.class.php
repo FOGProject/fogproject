cbd127d0d44d7b6f3e2c772f2b4c628867ec7026
Sebastian Roth <sebaroth@gmx.de>
2022-03-18 06:59:53 +0100

    Update dev-branch version

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.133');
+        define('FOG_VERSION', '1.5.9.136');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 136);
         define('FOG_CLIENT_VERSION', '0.12.0');
