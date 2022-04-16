ea17b00c2179a8bcd051e12c877aff1e5cdef823
Sebastian Roth <sebaroth@gmx.de>
2022-04-16 17:56:07 +0200

    Allow hostname regex for printers as well as IP addresses

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,9 +53,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.139');
+        define('FOG_VERSION', '1.5.9.140');
         define('FOG_SCHEMA', 270);
-        define('FOG_BCACHE_VER', 136);
+        define('FOG_BCACHE_VER', 137);
         define('FOG_CLIENT_VERSION', '0.12.0');
     }
 }
