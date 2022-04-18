00b005811dfac7fa07219ef6b88735791d37f1d1
Sebastian Roth <sebaroth@gmx.de>
2022-04-18 21:33:28 +0200

    Add minor changes for Alma and Rocky Linux support

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.142');
+        define('FOG_VERSION', '1.5.9.144');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 137);
         define('FOG_CLIENT_VERSION', '0.12.0');
