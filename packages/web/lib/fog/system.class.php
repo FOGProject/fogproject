178fc4c6677756ec2c4d3a1786642b3db4fab455
Sebastian Roth <sebaroth@gmx.de>
2022-09-25 01:21:56 +0200

    Change the installer to work without version numbers for PHP packages on Ubuntu and Debian and get rid of some of the old logic

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.170');
+        define('FOG_VERSION', '1.5.9.171');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 139);
         define('FOG_CLIENT_VERSION', '0.12.0');
