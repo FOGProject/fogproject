4cd6ef41c20869419f62a6631aa148cd12543cfc
Sebastian Roth <sebaroth@gmx.de>
2022-09-24 19:09:19 +0200

    Update select2 JS and CSS library to latest version 4.0.13 due to security issue in version < 4.0.6

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,9 +53,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.167');
+        define('FOG_VERSION', '1.5.9.169');
         define('FOG_SCHEMA', 270);
-        define('FOG_BCACHE_VER', 138);
+        define('FOG_BCACHE_VER', 139);
         define('FOG_CLIENT_VERSION', '0.12.0');
     }
 }
