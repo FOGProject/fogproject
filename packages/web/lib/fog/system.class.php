02730e966257643264d4f34b18a077b960f115d4
Luke Barone <lukebarone@gmail.com>
2022-12-23 11:14:29 -0800

    Closes #502

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.231');
+        define('FOG_VERSION', '1.5.9.235');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 139);
         define('FOG_CLIENT_VERSION', '0.12.2');
