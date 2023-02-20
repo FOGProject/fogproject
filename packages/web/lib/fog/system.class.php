6d486ed085005f36a1ee2c12c8f363a6f8d9207b
Sebastian Roth <sebaroth@gmx.de>
2023-02-20 23:12:37 +0100

    Add FOG version information to the dashboard storage node dropdown

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,9 +53,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.239');
+        define('FOG_VERSION', '1.5.9.240');
         define('FOG_SCHEMA', 270);
-        define('FOG_BCACHE_VER', 139);
+        define('FOG_BCACHE_VER', 140);
         define('FOG_CLIENT_VERSION', '0.12.2');
     }
 }
