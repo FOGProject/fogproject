7376da754bf1898e026183da329b631444d6ce2f
Sebastian Roth <sebaroth@gmx.de>
2023-03-05 22:01:51 +0100

    Update fog-client version 0.13.0

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,9 +53,9 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.257');
+        define('FOG_VERSION', '1.5.9.258');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 140);
-        define('FOG_CLIENT_VERSION', '0.12.2');
+        define('FOG_CLIENT_VERSION', '0.13.0');
     }
 }
