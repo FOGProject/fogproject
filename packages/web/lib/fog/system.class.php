FOGProject/fogproject

commit b33e4d9f672aa531fc724555bfbf049a8af02d4b

Fix location based snapin download by adding location URL (#552)
based on protocol information now stored in the database
for each location storage node individually.
Fixes 371

packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.9.255');
+        define('FOG_VERSION', '1.5.9.256');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 140);
         define('FOG_CLIENT_VERSION', '0.12.2');

packages/web/lib/plugins/location/pages/locationmanagementpage.class.php
@@ -73,6 +73,7 @@ class LocationManagementPage extends FOGPage
             _('Location Name'),
             _('Storage Group'),
             _('Storage Node'),
+            _('Storage Node Protocol'),
             _('Kernels/Inits from location'),
         );
         $this->templates = array(
@@ -85,6 +86,7 @@ class LocationManagementPage extends FOGPage
             . '${name}">${name}</a>',
             '${storageGroup}',
             '${storageNode}',
+            '${storageNodeProtocol}',
             '${tftp}',
         );
         $this->attributes = array(
@@ -92,6 +94,7 @@ class LocationManagementPage extends FOGPage
                 'class' => 'parser-false filter-false',
                 'width' => 16
             ),
+            array(),
             array(),
             array(),
             array(),
@@ -111,6 +114,7 @@ class LocationManagementPage extends FOGPage
                 'name' => $Location->name,
                 'storageGroup' => $Location->storagegroup->name,
                 'storageNode' => $sn,
+                'storageNodeProtocol' => $Location->protocol,
                 'tftp' => $Location->tftp ? _('Yes') : _('No'),
             );
             unset($Location);
@@ -147,6 +151,10 @@ class LocationManagementPage extends FOGPage
             INPUT_POST,
             'storagenode'
         );
+        $protocol = filter_input(
+            INPUT_POST,
+            'storagenodeprotocol'
+        );
         $name = filter_input(
             INPUT_POST,
             'name'
@@ -156,6 +164,9 @@ class LocationManagementPage extends FOGPage
         );
         $snbuild = self::getClass('StorageNodeManager')->buildSelectBox(
             $storagenode
+        );
+        $snprotocol = self::getClass('LocationManager')->buildProtocolSelectBox(
+            $protocol,
         );
         $tftp = isset($_POST['tftp']) ? ' checked' : '';
         $fields = array(
@@ -174,6 +185,9 @@ class LocationManagementPage extends FOGPage
             '<label for="storagenode">'
             . _('Storage Node')
             . '</label>' => $snbuild,
+            '<label for="storagenodeprotocol">'
+            . _('Storage Node Protocol')
+            . '</label>' => $snprotocol,
             '<label for="isen">'
             . _('Use inits and kernels from this node')
             . '</label>' => '<input type="checkbox" name="tftp" '
@@ -225,6 +239,7 @@ class LocationManagementPage extends FOGPage
         $name = filter_input(INPUT_POST, 'name');
         $storagegroup = filter_input(INPUT_POST, 'storagegroup');
         $storagenode = filter_input(INPUT_POST, 'storagenode');
+        $protocol = filter_input(INPUT_POST, 'storagenodeprotocol');
         $tftp = isset($_POST['tftp']);
         try {
             if (self::getClass('LocationManager')->exists($name)) {
@@ -251,6 +266,7 @@ class LocationManagementPage extends FOGPage
                 ->set('name', $name)
                 ->set('storagegroupID', $sgID)
                 ->set('storagenodeID', $storagenode)
+                ->set('protocol', $protocol)
                 ->set('tftp', (int)$tftp);
             if (!$Location->save()) {
                 throw new Exception(
@@ -317,11 +333,18 @@ class LocationManagementPage extends FOGPage
             INPUT_POST,
             'storagenode'
         ) ?: $this->obj->get('storagenodeID');
+        $protocol = filter_input(
+            INPUT_POST,
+            'storagenodeprotocol'
+        ) ?: $this->obj->get('protocol');
         $sgbuild = self::getClass('StorageGroupManager')->buildSelectBox(
             $storagegroup
         );
         $snbuild = self::getClass('StorageNodeManager')->buildSelectBox(
             $storagenode
+        );
+        $snprotocol = self::getClass('LocationManager')->buildProtocolSelectBox(
+            $protocol
         );
         $tftp = isset($_POST['tftp']) ? ' checked' : '';
         if (!$tftp) {
@@ -343,6 +366,9 @@ class LocationManagementPage extends FOGPage
             '<label for="storagenode">'
             . _('Storage Node')
             . '</label>' => $snbuild,
+            '<label for="storagenodeprotocol">'
+            . _('Storage Node Protocol')
+            . '</label>' => $snprotocol,
             '<label for="isen">'
             . _('Use inits and kernels from this node')
             . '</label>' => '<input type="checkbox" name="tftp" '
@@ -412,6 +438,7 @@ class LocationManagementPage extends FOGPage
         $name = filter_input(INPUT_POST, 'name');
         $storagegroup = filter_input(INPUT_POST, 'storagegroup');
         $storagenode = filter_input(INPUT_POST, 'storagenode');
+        $protocol = filter_input(INPUT_POST, 'storagenodeprotocol');
         $tftp = isset($_POST['tftp']);
         try {
             if ($name != $this->obj->get('name')
@@ -436,6 +463,7 @@ class LocationManagementPage extends FOGPage
                     ->set('name', $name)
                     ->set('storagegroupID', $sgID)
                     ->set('storagenodeID', $storagenode)
+                    ->set('protocol', $protocol)
                     ->set('tftp', (int)$tftp);
                 if (!$this->obj->save()) {
                     throw new Exception(

packages/web/lib/client/snapinclient.class.php
@@ -162,11 +162,6 @@ class SnapinClient extends FOGClient implements FOGClientSend
                             continue;
                         }
                     }
-                    $location = sprintf(
-                        'http://%s/%s',
-                        $StorageNode->get('ip'),
-                        $StorageNode->get('webroot')
-                    );
                     $path = sprintf(
                         '/%s',
                         trim($StorageNode->get('snapinpath'), '/')
@@ -189,7 +184,7 @@ class SnapinClient extends FOGClient implements FOGClientSend
                         $action = 'reboot';
                     }
                     $info['snapins'][] = array(
-                        'pack' =>( bool)$Snapin->get('packtype'),
+                        'pack' =>(bool)$Snapin->get('packtype'),
                         'hide' => (bool)$Snapin->get('hide'),
                         'timeout' => $Snapin->get('timeout'),
                         'jobtaskid' => $SnapinTask->get('id'),
@@ -202,7 +197,7 @@ class SnapinClient extends FOGClient implements FOGClientSend
                         'runwithargs' => $Snapin->get('runWithArgs'),
                         'hash' => strtoupper($hash),
                         'size' => $Snapin->get('size'),
-                        'url' => rtrim($location, '/'),
+                        'url' => rtrim(isset($StorageNode->location_url) ? $StorageNode->location_url : '', '/'),
                     );
                     unset($Snapin, $SnapinTask);
                 }

packages/web/lib/plugins/location/class/location.class.php
@@ -43,6 +43,7 @@ class Location extends FOGController
         'storagegroupID' => 'lStorageGroupID',
         'storagenodeID' => 'lStorageNodeID',
         'tftp' => 'lTftpEnabled',
+        'protocol' => 'lStorageNodeProto',
     );
     /**
      * The required fields

packages/web/lib/plugins/location/class/locationmanager.class.php
@@ -46,7 +46,8 @@ class LocationManager extends FOGManagerController
                 'lStorageNodeID',
                 'lCreatedBy',
                 'lCreatedTime',
-                'lTftpEnabled'
+                'lTftpEnabled',
+                'lStorageNodeProto'
             ),
             array(
                 'INTEGER',
@@ -56,9 +57,11 @@ class LocationManager extends FOGManagerController
                 'INTEGER',
                 'VARCHAR(40)',
                 'TIMESTAMP',
-                "ENUM('0', '1')"
+                "ENUM('0', '1')",
+                "ENUM('https', 'http')"
             ),
             array(
+                false,
                 false,
                 false,
                 false,
@@ -76,6 +79,7 @@ class LocationManager extends FOGManagerController
                 false,
                 false,
                 'CURRENT_TIMESTAMP',
+                false,
                 false
             ),
             array(
@@ -150,5 +154,40 @@ class LocationManager extends FOGManagerController
         }
         self::getClass('LocationAssociationManager')->destroy($findWhere);
         return true;
+    }
+
+    /**
+     * Builds the protocol selection box
+     *
+     * @return string
+     */
+    public static function buildProtocolSelectBox($preselection)
+    {
+        $protocols = array(
+            'https' => 'HTTPS',
+            'http' => 'HTTP'
+        );
+        ob_start();
+        foreach ((array)$protocols as $short => &$long) {
+            printf(
+                '<option value="%s"%s>%s</option>',
+                $short,
+                (
+                    $preselection === $short ?
+                    ' selected' :
+                    ''
+                ),
+                $long
+            );
+            unset($short, $long);
+        }
+        $optionProtocol = '<select class="form-control" name="storagenodeprotocol" '
+            . 'id="storagenodeprotocol">'
+            . '<option value="">- '
+            . self::$foglang['PleaseSelect']
+            . ' -</option>'
+            . ob_get_clean()
+            . '</select>';
+        return $optionProtocol;
     }
 }

packages/web/lib/plugins/location/hooks/changeitems.hook.php
@@ -152,7 +152,7 @@ class ChangeItems extends Hook
             Route::getData()
         );
         $Task = $arguments['Host']->get('task');
-        $TaskType = $arguments['TaskType'];
+        $TaskType = isset($arguments['TaskType']) ? $arguments['TaskType'] : null;
         $method = false;
         foreach ($LocationAssocs->locationassociations as &$LocationAssoc) {
             $Location = self::getClass('Location', $LocationAssoc->locationID);
@@ -176,6 +176,12 @@ class ChangeItems extends Hook
                     && self::getSetting('FOG_SNAPIN_LOCATION_SEND_ENABLED') > 0)
                 ) {
                     $arguments['StorageNode'] = $Location->getStorageNode();
+                    $arguments['StorageNode']->{"location_url"} = sprintf(
+                        '%s://%s/%s',
+                        $Location->get('protocol'),
+                        $arguments['StorageNode']->get('ip'),
+                        $arguments['StorageNode']->get('webroot')
+                    );
                 }
                 if (!$method) {
                     continue;

commit 20ba114eb0dcf2ee9e7975b53d91696bebf1ec4d

Fix syntax error in location update introduced in b33e4d9 to fix #561

packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.10');
+        define('FOG_VERSION', '1.5.10.0');
         define('FOG_SCHEMA', 270);
         define('FOG_BCACHE_VER', 140);
         define('FOG_CLIENT_VERSION', '0.13.0');

packages/web/lib/plugins/location/pages/locationmanagementpage.class.php
@@ -166,7 +166,7 @@ class LocationManagementPage extends FOGPage
             $storagenode
         );
         $snprotocol = self::getClass('LocationManager')->buildProtocolSelectBox(
-            $protocol,
+            $protocol
         );
         $tftp = isset($_POST['tftp']) ? ' checked' : '';
         $fields = array(