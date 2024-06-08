6b3d4104be7eb2eed8e902b4a0fad348ee1ef80e
Tom Elliott <tommygunsster@gmail.com>
2024-06-08 07:23:01 -0500

    Fixup the autonaming

--- packages/web/management/js/fog/about/fog.about.kernel.js
+++ packages/web/management/js/fog/about/fog.about.kernel.js
@@ -52,7 +52,7 @@
             url: downloadparts['file'],
             arch: downloadparts['arch']
           };
-          val = parts.arch == 32 ? 'bzImage32' : 'bzImage';
+          val = parts.arch == 32 ? 'bzImage32' : parts.arch == 'arm64' ? 'arm_Image' : 'bzImage';
           $('#kernel-name').prop('placeholder', val).prop('value', val);
         });
         confirmDownloadBtn.on('click', function(e) {
