6f1d1bb1e133da8f86fcd33399c3ff20e00ca36a
Tom Elliott <tommygunsster@gmail.com>
2024-06-08 07:23:01 -0500

    Fixup the autonaming

--- packages/web/management/js/fog/about/fog.about.initrd.js
+++ packages/web/management/js/fog/about/fog.about.initrd.js
@@ -21,7 +21,7 @@
               style: 'single'
             },
             order: [
-              [3, 'desc']
+              [0, 'desc']
             ],
             columns: [
               {data: 'version'},
@@ -52,7 +52,7 @@
             url: downloadparts['file'],
             arch: downloadparts['arch']
           };
-          val = parts.arch == 32 ? 'bzImage32' : 'bzImage';
+          val = parts.arch == 32 ? 'init_32.xz' : parts.arch == 'arm64' ? 'arm_init.cpio.gz' : 'init.xz';
           $('#initrd-name').prop('placeholder', val).prop('value', val);
         });
         confirmDownloadBtn.on('click', function(e) {
