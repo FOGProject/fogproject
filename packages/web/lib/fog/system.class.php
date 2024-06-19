66d43b6a1515dcbae26c97b35b4e5ae7b263cb66
Tom Elliott <tommygunsster@gmail.com>
2024-06-19 06:07:57 -0500

    Should enable user defined CA Key, CA PEM, CSR, and public certificate

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.1248');
+        define('FOG_VERSION', '1.6.0-alpha.1249');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 285);
         define('FOG_BCACHE_VER', 145);
