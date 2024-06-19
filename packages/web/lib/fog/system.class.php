4f2428ba10688a7cd5e08d53953afe2e4a2fc83d
Tom Elliott <tommygunsster@gmail.com>
2024-06-19 15:49:31 -0500

    Go with some more explicite calling for ftp

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.1254');
+        define('FOG_VERSION', '1.6.0-alpha.1255');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 285);
         define('FOG_BCACHE_VER', 145);
