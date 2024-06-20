5f64de0d9e0d168be83b6d50cc39c67b6ed72dcc
Tom Elliott <tommygunsster@gmail.com>
2024-06-20 10:51:49 -0500

    Should properly login ftp

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.1259');
+        define('FOG_VERSION', '1.6.0-alpha.1260');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 286);
         define('FOG_BCACHE_VER', 145);
