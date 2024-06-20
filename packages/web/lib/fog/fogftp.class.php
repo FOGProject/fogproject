28bed9dc4853fe9a9c663c243ecbb46ae5f52d4a
Tom Elliott <tommygunsster@gmail.com>
2024-06-20 09:27:38 -0500

    Should address ftp_rename issue

--- packages/web/lib/fog/fogftp.class.php
+++ packages/web/lib/fog/fogftp.class.php
@@ -329,7 +329,7 @@ class FOGFTP
         $oldname,
         $newname
     ) {
-        if (!($this->__call('rename', [$oldname, $newname]))) {
+        if (!@ftp_rename($this->_link, $oldname, $newname)) {
             if (!$this->put($newname, $oldname, $this->mode)) {
                 $this->ftperror($this->data);
             }
