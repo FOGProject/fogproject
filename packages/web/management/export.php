d3c0ff1c2100d123558f98538ff3bfede3be8f70
Tom Elliott <tommygunsster@gmail.com>
2024-06-28 15:38:10 -0500

    Should address advisory

--- packages/web/management/export.php
+++ packages/web/management/export.php
@@ -20,6 +20,14 @@
  * @link     https://fogproject.org
  */
 require '../commons/base.inc.php';
+$unauthorized = !(isset($currentUser) && $currentUser->isValid()) || empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+    || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest';
+
+if ($unauthorized) {
+    echo _('Unauthorized');
+    exit;
+}
+
 $report = unserialize($_SESSION['foglastreport']);
 if (!($report instanceof ReportMaker)) {
     $report = FOGCore::getClass('ReportMaker');
