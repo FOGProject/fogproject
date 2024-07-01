c4add54bf3dd0c920c4b198f1cc7a33391995183
Tom Elliott <tommygunsster@gmail.com>
2024-07-01 05:08:08 -0500

    Fixup verbiage and runtime output

--- packages/web/lib/service/filedeleter.class.php
+++ packages/web/lib/service/filedeleter.class.php
@@ -75,6 +75,73 @@ class FileDeleter extends FOGService
         );
     }
     /**
+     * Return more human friendly time.
+     *
+     * @param int    $diff the difference passed
+     * @param string $unit the unit of time (minute, hour, etc...)
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    private static function humanifyRun($diff, $unit)
+    {
+        if (!is_numeric($diff)) {
+            throw new Exception(_('Diff parameter must be numeric'));
+        }
+        if (!is_string($unit)) {
+            throw new Exception(_('Unit of time must be a string'));
+        }
+        $before = $after = '';
+        if ($diff < 0) {
+            $before = sprintf('%s ', _('in'));
+        }
+        if ($diff < 0) {
+            $after = sprintf(' %s', _('from now'));
+        }
+        $diff = floor(abs($diff));
+        if ($diff != 1) {
+            $unit .= 's';
+        }
+
+        return sprintf(
+            '%s%d %s%s',
+            $before,
+            $diff,
+            $unit,
+            $after
+        );
+    }
+    private static function formatRunTime($time)
+    {
+        if (!$time instanceof DateTime) {
+            $time = self::niceDate($time);
+        }
+        $now = self::niceDate('now');
+        $diff = $now->format('U') - $time->format('U');
+        $absolute = abs($diff);
+        if (is_nan($diff)) {
+            return _('Not a number');
+        }
+        if (!self::validDate($time)) {
+            return _('No Data');
+        }
+        $date = $time->format('Y/m/d');
+        if ($now->format('Y/m/d') == $date) {
+            if (0 <= $diff && $absolute < 60) {
+                return _('moments from now');
+            } elseif ($diff < 0 && $absolute < 60) {
+                return _('seconds ago');
+            } elseif ($absolute <= 3600) {
+                return self::humanifyRun($diff / 60, 'minute');
+            } elseif ($absolute <= 86400) {
+                return self::humanifyRun($diff / 60 / 60, 'hour');
+            } else {
+                return self::humanifyRun($diff / 60 / 60 / 24, 'day');
+            }
+        }
+    }
+    /**
      * Makes the output for this service
      *
      * @return void
@@ -93,31 +160,29 @@ class FileDeleter extends FOGService
 
             self::outall(
                 sprintf(
-                    " * %s item%s to delete found.",
+                    " * %s %s to delete found.",
                     $taskCount,
-                    (
-                        $taskCount === 1 ?
-                        '' :
-                        's'
-                    )
+                    $taskCount === 1 ? _('item') : _('items')
                 )
             );
             unset($taskCount);
             $currTime = time();
             foreach ($filedeletes->data as $filedelete) {
-                $createdTime = strtotime($filedelete->createdTime);
-                $timediff = $currTime - $createdTime;
+                $createdtime = strtotime($filedelete->createdTime);
+                $timediff = $currTime - $createdtime;
                 $toRunTime = self::$zzz - $timediff;
+                $createdTime = self::niceDate($filedelete->createdTime);
+                $timeDiff = clone $createdTime;
+                $timeDiff->modify('+'.$toRunTime+self::$zzz.' seconds');
                 if ($toRunTime > 0) {
                     self::outall(
                         sprintf(
-                            "%s %d %s.\n\t - %s: %s\n\t - %s: %s\n\t - %s: %s",
-                            _('Found a task that we will wait on completing for'),
-                            $toRunTime,
-                            $toRunTime != 1 ? _('seconds') : _('second'),
+                            "%s %s.\n\t - %s: %s\n\t - %s: %s\n\t - %s: %s",
+                            _('Found an item that we will wait on completing'),
+                            self::formatRunTime($timeDiff),
                             _('File Path Type'),
                             $filedelete->pathtype,
-                            _('File Path'),
+                            _('File Path Basename'),
                             $filedelete->path,
                             _('Created Time'),
                             $filedelete->createdTime
@@ -128,10 +193,10 @@ class FileDeleter extends FOGService
                 self::outall(
                     sprintf(
                         "%s.\n\t - %s: %s\n\t - %s: %s\n\t - %s: %s",
-                        _('Found a task that we will get deleted'),
+                        _('Found an item that we will get deleted'),
                         _('File Path Type'),
                         $filedelete->pathtype,
-                        _('File Path'),
+                        _('File Path Basename'),
                         $filedelete->path,
                         _('Created Time'),
                         $filedelete->createdTime
@@ -173,7 +238,7 @@ class FileDeleter extends FOGService
                     if (!self::$FOGSSH->connect()) {
                         self::outall(
                             sprintf(
-                                '%s: %s. %s.',
+                                "%s: %s.\n\t - %s.",
                                 _('Failed to SSH to Storage Node Named'),
                                 $StorageNode->name,
                                 _('Skipping, will need manual removal')
@@ -183,7 +248,7 @@ class FileDeleter extends FOGService
                     } else if (!self::$FOGSSH->exists($deleteFile)) {
                         self::outall(
                             sprintf(
-                                '%s: %s. %s: %s. %s.',
+                                "%s: %s.\n\t - %s: %s.\n\t - %s.",
                                 _('Nothing to do on Storage Node Named'),
                                 $StorageNode->name,
                                 _('File Path did not exist, but should have been'),
@@ -194,7 +259,7 @@ class FileDeleter extends FOGService
                     } else if (!self::$FOGSSH->delete($deleteFile)) {
                         self::outall(
                             sprintf(
-                                '%s: %s. %s: %s. %s.',
+                                "%s: %s.\n\t -  %s: %s.\n\t - %s.",
                                 _('Failed to remove File Path from Storage Node Named'),
                                 $StorageNode->name,
                                 _('File Path to delete was'),
@@ -205,7 +270,7 @@ class FileDeleter extends FOGService
                     } else {
                         self::outall(
                             sprintf(
-                                '%s: %s. %s: %s.',
+                                "%s: %s.\n\t - %s: %s.",
                                 _('File Path successfully removed from Storage Node Named'),
                                 $StorageNode->name,
                                 _('File Path removed was'),
