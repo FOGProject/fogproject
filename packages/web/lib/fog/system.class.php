1601f23cc7ae7079868aa9724602df797c6a81f3
JJ Fullmer <ldscellist@Gmail.com>
2024-07-11 10:55:14 -0600

    increment version

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-alpha.1357');
+        define('FOG_VERSION', '1.6.0-alpha.1358');
         define('FOG_CHANNEL', 'Alpha');
         define('FOG_SCHEMA', 290);
         define('FOG_BCACHE_VER', 147);
