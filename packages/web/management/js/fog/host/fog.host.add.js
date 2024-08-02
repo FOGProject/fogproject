2957698f2ca8103d9cd94f2a6cda5cd80926554f
Tom Elliott <tommygunsster@gmail.com>
2024-08-02 09:10:10 -0500

    Should enable AD on create new host direct ui option

--- packages/web/management/js/fog/host/fog.host.add.js
+++ packages/web/management/js/fog/host/fog.host.add.js
@@ -10,6 +10,38 @@
             createFormBtn.prop('disabled', false);
         });
     });
+    var ADJoinDomain = $('#adEnabled');
+
+    ADJoinDomain.on('ifClicked', function(e) {
+        e.preventDefault();
+        $(this).prop('checked', !this.checked);
+        if (!this.checked) {
+            return;
+        }
+        var indomain = $('#adDomain'),
+            inou = $('#adOU'),
+            inuser = $('#adUsername'),
+            inpass = $('#adPassword');
+        if (indomain.val() && inou.val() && inuser.val() && inpass.val()) {
+            return;
+        }
+        Pace.ignore(function() {
+            $.get('../management/index.php?sub=adInfo', function(data) {
+                if (!indomain.val()) {
+                    indomain.val(data.domainname);
+                }
+                if (!inou.val()) {
+                    inou.val(data.ou)
+                }
+                if (!inuser.val()) {
+                    inuser.val(data.domainuser);
+                }
+                if (!inpass.val()) {
+                    inpass.val(data.domainpass);
+                }
+            }, 'json');
+        });
+    });
     $('#mac').inputmask({mask: Common.masks.mac});
     $('#key').inputmask({mask: Common.masks.productKey});
 })(jQuery);
