249da8d4f1ef04f9d99733b1946ffe90efc7564f
JJ Fullmer <jfullmer@arrowheaddental.com>
2024-08-05 15:13:18 -0600

    add docs to readme

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.10.68');
+        define('FOG_VERSION', '1.5.10.69');
         define('FOG_SCHEMA', 271);
         define('FOG_BCACHE_VER', 141);
         define('FOG_CLIENT_VERSION', '0.13.0');
