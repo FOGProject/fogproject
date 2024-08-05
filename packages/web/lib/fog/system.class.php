ae97c14689573f95e86e2db5ad4e7945d25a0fdc
Tom Elliott <tommygunsster@gmail.com>
2024-08-05 14:37:08 -0500

    Re-add branchend

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '.0-beta.1406');
+        define('FOG_VERSION', '1.6.0-beta.1407');
         define('FOG_CHANNEL', 'Beta');
         define('FOG_SCHEMA', 291);
         define('FOG_BCACHE_VER', 151);
