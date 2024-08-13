6d631460e03b9bc3347790c3c4f42855f32d00bd
Tom Elliott <tommygunsster@gmail.com>
2024-08-13 10:24:05 -0500

    Should ensure the uploaded file is of proper filetype

--- packages/web/lib/pages/fogconfigurationpage.page.php
+++ packages/web/lib/pages/fogconfigurationpage.page.php
@@ -1224,16 +1224,10 @@ class FOGConfigurationPage extends FOGPage
                             'jpeg',
                             'png',
                         ];
-                        $extensionCheck = strtolower(pathinfo($src, PATHINFO_EXTENSION));
-                        if (!in_array($extensionCheck, $validExtensions)) {
-                            throw new Exception(
-                                _('Upload file extension must be, jpg, jpeg, or png')
-                            );
-                        }
                         $extensionCheck = strtolower(pathinfo($set, PATHINFO_EXTENSION));
                         if (!in_array($extensionCheck, $validExtensions)) {
                             throw new Exception(
-                                _('Created file extension must be, jpg, jpeg, or png')
+                                _('Upload file extension must be, jpg, jpeg, or png')
                             );
                         }
                         if ($width != 650) {
