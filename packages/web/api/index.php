5e4c59921480c31eb3adb7f87b1697db2a452722
Tom Elliott <tommygunsster@gmail.com>
2024-08-15 10:28:03 -0500

    Just refactor ever so slightly

--- packages/web/api/index.php
+++ packages/web/api/index.php
@@ -2,7 +2,7 @@
 /**
  * Index/handler for api subsystem.
  *
- * PHP Version 5
+ * PHP Version 7.4
  *
  * @category APIHandler
  * @package  FOGProject
@@ -10,14 +10,8 @@
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
-/**
- * Index/handler for api subsystem.
- *
- * @category APIHandler
- * @package  FOGProject
- * @author   Tom Elliott <tommygunsster@gmail.com>
- * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
- * @link     https://fogproject.org
- */
+
+declare(strict_types=1);
+
 require '../commons/base.inc.php';
-new Route;
+new Route();
