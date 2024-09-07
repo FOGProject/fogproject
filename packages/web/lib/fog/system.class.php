447efb01ea31a673d7272e24014a49ad27821b3d
geotsot <geotsot@gmail.com>
2024-09-07 09:44:37 +0300

    FOG VERSION to 1606

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,7 +53,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.10.1607');
+        define('FOG_VERSION', '1.5.10.1606');
         define('FOG_SCHEMA', 272);
         define('FOG_BCACHE_VER', 141);
         define('FOG_CLIENT_VERSION', '0.13.0');
