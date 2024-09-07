6db22596a268197e1876479fc980eeeba8e37ceb
geotsot <geotsot@gmail.com>
2024-09-07 09:29:32 +0300

    change FOG VERSION and FOG_SCHEMA to resolve merge conflicts

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -53,8 +53,8 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.5.10.1605');
-        define('FOG_SCHEMA', 271);
+        define('FOG_VERSION', '1.5.10.1607');
+        define('FOG_SCHEMA', 272);
         define('FOG_BCACHE_VER', 141);
         define('FOG_CLIENT_VERSION', '0.13.0');
     }
