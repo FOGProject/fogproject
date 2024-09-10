d6323a3c2c5c9579379965ea7ebe2813679cd667
JJ Fullmer <jfullmer@arrowheaddental.com>
2024-09-10 09:14:50 -0600

    Fix issue with lsb-release not installing in rhel 9 distros during install

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-beta.2124');
+        define('FOG_VERSION', '1.6.0-beta.2126');
         define('FOG_CHANNEL', 'Beta');
         define('FOG_SCHEMA', 292);
         define('FOG_BCACHE_VER', 152);
