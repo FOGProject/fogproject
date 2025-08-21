6db016bf25aa3a1a2ad95a469feda73bf8827d52
Tom Elliott <tommygunsster@gmail.com>
2025-08-21 06:49:31 -0500

    Should fix queue more properly though probably has our ever persistent race condition.

--- packages/web/lib/reg-task/taskqueue.class.php
+++ packages/web/lib/reg-task/taskqueue.class.php
@@ -38,11 +38,7 @@ class TaskQueue extends TaskingElement
             )->set(
                 'checkInTime',
                 self::formatTime('now', 'Y-m-d H:i:s')
-            )->save();
-            // $this->Task
-            //     ->set('stateID', self::getCheckedInState())
-            //     ->set('checkInTime', self::formatTime('now', 'Y-m-d H:i:s'))
-            //     ->save();
+            );
             if (!$this->Task->save()) {
                 throw new Exception(_('Failed to update task'));
             }
