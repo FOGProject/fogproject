45497c37daac1ed2f4fcd05a896fd644c552196d
JJ Fullmer <jfullmer@arrowheaddental.com>
2025-08-31 15:21:46 -0600

    Bring back cleaner ternary operator without accessing possible invalid array index Fixes #707

--- packages/web/lib/fog/fogcron.class.php
+++ packages/web/lib/fog/fogcron.class.php
@@ -159,11 +159,7 @@ class FOGCron extends FOGBase
         $v = explode(',', $field);
         foreach ($v as &$vv) {
             $vvv = explode('/', $vv);
-            if (count($vvv ?: []) > 2) {
-                $step = !$vvv[1] ? 1 : $vvv[1];
-            } else {
-                $step = 1;
-            }
+            $step = count($vvv ?: []) <= 1 ? 1 : $vvv[1];
             $vvvv = explode('-', $vvv[0]);
             $_min = (
                 count($vvvv ?: []) == 2 ?
