44bbc27f9802f1c4885254adf6ac78d3c9f71b98
Tom Elliott <tommygunsster@gmail.com>
2025-09-18 10:50:14 -0500

    Should remove the CSRF check auth stuff as they will likely always be the same

--- packages/web/lib/fog/fogbase.class.php
+++ packages/web/lib/fog/fogbase.class.php
@@ -2879,8 +2879,7 @@ abstract class FOGBase
     {
         $authorized = (self::$FOGUser && self::$FOGUser->isValid())
             || ((self::$newService || filter_input(INPUT_GET, 'clientver')) && basename(self::$scriptname) == 'getversion.php')
-            || (self::$newService && self::$Host->isValid() && self::$Host->get('pub_key'))
-            || (CSRF::token() == $_SESSION['_csrf_token']);
+            || (self::$newService && self::$Host->isValid() && self::$Host->get('pub_key'));
         if ($return_bool) {
             return $authorized;
         }
