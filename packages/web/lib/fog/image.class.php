6f1446a325403cdf8a3166af006aa9314b90a94b
Tom Elliott <tommygunsster@gmail.com>
2026-01-21 08:08:42 -0600

    Try assocID setting again?

--- packages/web/lib/fog/image.class.php
+++ packages/web/lib/fog/image.class.php
@@ -493,7 +493,7 @@ class Image extends FOGController
                 'imageID' => $this->get('id')
             )
         );
-        $assocID = count($assocID) ? min((array) $assocID) : null;
+        $assocID = count((array)$assocID ?: []) ? min($assocID) : 0;
 
         return self::getClass('ImageAssociation', $assocID)->isPrimary();
     }
