c2d9d70c2065429b529394587734f2191cbc27ff
Tom Elliott <tommygunsster@gmail.com>
2026-03-06 06:23:16 -0600

    Should attempt to fix a few security issues

--- packages/web/status/freespace.php
+++ packages/web/status/freespace.php
@@ -19,20 +19,87 @@
  * @license  http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link     https://fogproject.org
  */
-session_write_close();
+require '../commons/base.inc.php';
 ignore_user_abort(true);
 set_time_limit(0);
-header('Content-Type: text/event-stream');
-$decodePath = base64_decode($_REQUEST['path']);
-if (!(file_exists($decodePath)
-    && is_readable($decodePath)
-    && is_dir($decodePath))
-) {
-    return;
+header('Content-Type: application/json');
+FOGCore::checkAuthAndCSRF();
+$decodePath = filter_input(INPUT_GET, 'path');
+$path = base64_decode((string)$decodePath, true);
+if (!$path) {
+    $error = _('Invalid path');
+    $title = _('Invalid Path');
+    http_response_code(400);
+    echo json_encode(
+        [
+            'free' => 0,
+            'used' => 0,
+            'error' => $error,
+            'title' => $title
+        ]
+    );
+    exit;
 }
-$folder = escapeshellarg(
-    base64_decode($_REQUEST['path'])
+$path = realpath($path);
+if (!(is_string($path) && is_dir($path) && is_readable($path))) {
+    $error = _('File or path does not exist');
+    $title = _('File Not Found');
+    http_response_code(401);
+    echo json_encode(
+        [
+            'free' => 0,
+            'used' => 0,
+            'error' => $error,
+            'title' => $title
+        ]
+    );
+    exit;
+}
+
+Route::ids('storagenode', [], 'path');
+$imagePaths = json_decode(Route::getData(), true) ?: [];
+Route::ids('storagenode', [], 'snapinpath');
+$snapinPaths = json_decode(Route::getData(), true) ?: [];
+
+$validPaths = array_merge(
+    (array)$imagePaths,
+    (array)$snapinPaths
 );
+$realValidPaths = [];
+foreach ((array)$validPaths as $validPath) {
+    foreach ((array)glob($validPath) as $expandedPath) {
+        $realPath = realpath($expandedPath);
+        if (!is_string($realPath) || !is_dir($realPath)) {
+            continue;
+        }
+        $realValidPaths[] = rtrim($realPath, DS) . DS;
+    }
+}
+
+$allowed = false;
+$comparePath = rtrim($path, DS) . DS;
+foreach (array_unique($realValidPaths) as $realValidPath) {
+    if (strpos($comparePath, $realValidPath) === 0) {
+        $allowed = true;
+        break;
+    }
+}
+
+if (!$allowed) {
+    $error = _('Path not allowed');
+    $title = _('Access Denied');
+    http_response_code(403);
+    echo json_encode(
+        [
+            'free' => 0,
+            'used' => 0,
+            'error' => $error,
+            'title' => $title
+        ]
+    );
+    exit;
+}
+$folder = escapeshellarg($path);
 $output = `df -PB1 $folder | tail -1`;
 $test = preg_match(
     '/\d+\s+(\d+)\s+(\d+)\s+\d+\%.*$/',
@@ -40,13 +107,25 @@ $test = preg_match(
     $match
 );
 if (!$test) {
-    return;
+    http_response_code(204);
+    $error = _('No data found');
+    $title = _('No Data Available');
+    echo json_encode(
+        [
+            'free' => 0,
+            'used' => 0,
+            'error' => $error,
+            'title' => $title
+        ]
+    );
+    exit;
 }
-$hdfree = $match[2];
-$hdused = $match[1];
-$data = array(
-    'free' => $hdfree,
-    'used' => $hdused,
+http_response_code(200);
+echo json_encode(
+    [
+        'free' => (int)$match[2],
+        'used' => (int)$match[1]
+    ]
 );
-echo json_encode($data);
 exit;
+
--- packages/web/status/hw.php
+++ packages/web/status/hw.php
@@ -20,12 +20,11 @@
  * @link     https://fogproject.org
  */
 require '../commons/base.inc.php';
-session_write_close();
+FOGCore::checkAuthAndCSRF();
 ignore_user_abort(true);
 set_time_limit(0);
 header('Content-Type: text/event-stream');
 $hwinfo = FOGCore::getHWInfo();
-foreach ((array)$hwinfo as $index => &$val) {
+foreach ((array)$hwinfo as $index => $val) {
     echo "$val\n";
-    unset($val);
 }
