ffc9b06f53e35c747627720e4462fa7352d707e9
JJ Fullmer <jfullmer@arrowheaddental.com>
2026-08-03 13:58:30 -0600

    sync version

--- packages/web/lib/fog/system.class.php
+++ packages/web/lib/fog/system.class.php
@@ -59,7 +59,7 @@ class System
     public function __construct()
     {
         self::_versionCompare();
-        define('FOG_VERSION', '1.6.0-beta.3143');
+        define('FOG_VERSION', '1.6.0-beta.3144');
         define('FOG_CHANNEL', 'Beta');
         define('FOG_SCHEMA', 322);
         define('FOG_BCACHE_VER', 269);
